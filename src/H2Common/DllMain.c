518445c20b30e336db951226ca9cf97e566ff8f6
m-x-d <j.maxed@gmail.com>
2024-06-05 15:40:41 +0300

    Add solution and H2Common project.

---
+++ src/H2Common/DllMain.c
@@ -0,0 +1,13 @@
+//
+// DllMain.c
+//
+// Copyright 1998 Raven Software
+// Heretic II
+//
+
+#include <windows.h>
+
+BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
+{
+	return TRUE;
+}
