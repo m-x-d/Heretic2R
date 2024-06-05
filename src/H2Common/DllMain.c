5d332165a508f1be66de0c5c56fa46d41a5e8d41
m-x-d <j.maxed@gmail.com>
2024-06-05 15:47:47 +0300

    Added ResourceManager from Heretic II Toolkit v1.06.

--- src/H2Common/DllMain.c
+++ src/H2Common/DllMain.c
@@ -2,12 +2,19 @@
 // DllMain.c
 //
 // Copyright 1998 Raven Software
-// Heretic II
 //
 
 #include <windows.h>
+#include "..\qcommon\ResourceManager.h"
+
+ResourceManager_t res_mgr;
 
 BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
 {
+	if (fdwReason == 1)
+		ResMngr_Con(&res_mgr, 8, 256);
+	else
+		ResMngr_Des(&res_mgr);
+
 	return TRUE;
 }
