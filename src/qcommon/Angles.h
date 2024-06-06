075da7c1a34855d3fe3ab54d8e241ed199e910f4
m-x-d <j.maxed@gmail.com>
2024-06-06 17:45:30 +0300

    Cleanup Angles.h.

--- src/qcommon/Angles.h
+++ src/qcommon/Angles.h
@@ -1,54 +1,56 @@
-// Angles in radians
+//
+// Angles.h
+//
+// Copyright 1998 Raven Software
+//
 
-#define ANGLE_0			0.0F
-#define ANGLE_1			0.017453292F
-#define ANGLE_5			0.087266462F
-#define ANGLE_10		0.174532925F
-#define ANGLE_15		0.261799387F
-#define ANGLE_20		0.392699081F
-#define ANGLE_30		0.523598775F
-#define ANGLE_45		0.785398163F
-#define ANGLE_60		1.047197551F
-#define ANGLE_72		1.256637061F
-#define ANGLE_90		1.570796327F
-#define ANGLE_120		2.094395102F
-#define ANGLE_135		2.35619449F
-#define ANGLE_144		2.513274123F
-#define ANGLE_180		3.141592653F
-#define	ANGLE_225		3.926990817F
-#define ANGLE_270		4.71238898F
-#define	ANGLE_315		5.497787144F
-#define ANGLE_360		6.283185307F
+#pragma once
+
+// Angles in radians
+#define ANGLE_0			0.0f
+#define ANGLE_1			0.017453292f
+#define ANGLE_5			0.087266462f
+#define ANGLE_10		0.174532925f
+#define ANGLE_15		0.261799387f
+#define ANGLE_20		0.392699081f
+#define ANGLE_30		0.523598775f
+#define ANGLE_45		0.785398163f
+#define ANGLE_60		1.047197551f
+#define ANGLE_72		1.256637061f
+#define ANGLE_90		1.570796327f
+#define ANGLE_120		2.094395102f
+#define ANGLE_135		2.35619449f
+#define ANGLE_144		2.513274123f
+#define ANGLE_180		3.141592653f
+#define	ANGLE_225		3.926990817f
+#define ANGLE_270		4.71238898f
+#define	ANGLE_315		5.497787144f
+#define ANGLE_360		6.283185307f
 
 // Angles in degrees
-
-#define DEGREE_0		0.0F
-#define DEGREE_180		180.0F
-#define DEGREE_45		(DEGREE_180 / 4.0F)
-#define DEGREE_90		(DEGREE_180 / 2.0F)
+#define DEGREE_0		0.0f
+#define DEGREE_180		180.0f
+#define DEGREE_45		(DEGREE_180 / 4.0f)
+#define DEGREE_90		(DEGREE_180 / 2.0f)
 #define DEGREE_135		(DEGREE_90 + DEGREE_45)
 #define DEGREE_270		(DEGREE_180 + DEGREE_90)
-#define DEGREE_360		(DEGREE_180 * 2.0F)
+#define DEGREE_360		(DEGREE_180 * 2.0f)
 
 #define	DEGREE_225		(DEGREE_180 + DEGREE_45)
 #define	DEGREE_315		(DEGREE_270 + DEGREE_45)
 
-#define DEGREE_30		(DEGREE_180 / 6.0F)
-#define DEGREE_60		(DEGREE_180 / 3.0F)
-#define DEGREE_120		(DEGREE_360 / 3.0F)
+#define DEGREE_30		(DEGREE_180 / 6.0f)
+#define DEGREE_60		(DEGREE_180 / 3.0f)
+#define DEGREE_120		(DEGREE_360 / 3.0f)
 
-#define DEGREE_1		(DEGREE_180 / 180.0F)
-#define DEGREE_5		(DEGREE_180 / 36.0F)
-#define DEGREE_10		(DEGREE_180 / 18.0F)
-#define DEGREE_15		(DEGREE_180 / 12.0F)
-#define DEGREE_20		(DEGREE_180 / 8.0F)
+#define DEGREE_1		(DEGREE_180 / 180.0f)
+#define DEGREE_5		(DEGREE_180 / 36.0f)
+#define DEGREE_10		(DEGREE_180 / 18.0f)
+#define DEGREE_15		(DEGREE_180 / 12.0f)
+#define DEGREE_20		(DEGREE_180 / 8.0f)
 
 // Conversion routines
-
 #define ANGLE_TO_RAD	ANGLE_1
-#define RAD_TO_ANGLE	(180.0F / ANGLE_180)
+#define RAD_TO_ANGLE	(180.0f / ANGLE_180)
 
-#define	SHORT_TO_ANGLE	(360.0/65536)
-
-
-#pragma warning(disable : 4305)		// 'initializing' : truncation from 'const double ' to 'float '
+#define	SHORT_TO_ANGLE	(360.0f / 65536.0f)
