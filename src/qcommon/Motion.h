49761f773072abfcdd3d8e0e8390980f2f0e2887
m-x-d <j.maxed@gmail.com>
2024-06-06 18:14:35 +0300

    Added Motion.c.

--- src/qcommon/Motion.h
+++ src/qcommon/Motion.h
@@ -1,9 +1,15 @@
+//
+// Motion.h
+//
+// Copyright 1998 Raven Software
+//
+
+#pragma once
+
 #include "H2Common.h"
 #include "q_typedef.h"
 
-H2COMMON_API float GetTimeToReachDistance(float, float, float);
-H2COMMON_API float GetDistanceOverTime(float, float, float);
-H2COMMON_API void GetPositionOverTime(vec3_t, vec3_t, vec3_t, float, vec3_t);
-H2COMMON_API void GetVelocityOverTime(vec3_t velocity, vec3_t accel, float time, vec3_t output);
-
-// end
+H2COMMON_API float GetTimeToReachDistance(float vel, float accel, float dist);
+H2COMMON_API float GetDistanceOverTime(float vel, float accel, float time);
+H2COMMON_API void GetPositionOverTime(const vec3_t origin, const vec3_t vel, const vec3_t accel, float time, vec3_t out);
+H2COMMON_API void GetVelocityOverTime(const vec3_t vel, const vec3_t accel, float time, vec3_t out);
