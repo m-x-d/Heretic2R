33895939a430451a78cfa00be9e50bb71a058060
m-x-d <j.maxed@gmail.com>
2024-06-11 17:11:15 +0300

    Add SurfaceProps.c (GROSS HACKS edition), remove currently unused headers.

---
+++ src/H2Common/SurfaceProps.c
@@ -0,0 +1,93 @@
+//
+// SurfaceProps.c
+//
+// Copyright 1998 Raven Software
+//
+
+#include "SurfaceProps.h"
+#include "q_shared.h" //TODO: remove (referenced in p_types.h)
+
+//TODO: Why are you like this?..
+char* SurfaceMaterialNames[MAX_SURFACE_MAT_NAMES] =
+{
+	"gravel", "metal", "stone",	"wood",
+	"gravel", "metal", "stone", "wood",
+	"gravel", "metal", "stone", "wood",
+	"gravel", "metal", "stone", "wood",
+	"gravel", "metal", "stone", "wood",
+	"gravel", "metal", "stone", "wood",
+	"gravel", "metal", "stone", "wood",
+	"gravel", "metal", "stone", "wood",
+	"gravel", "metal", "stone", "wood",
+	"gravel", "metal", "stone", "wood",
+	"gravel", "metal", "stone", "wood",
+	"gravel", "metal", "stone", "wood",
+	"gravel", "metal", "stone", "wood",
+	"gravel", "metal", "stone", "wood",
+	"gravel", "metal", "stone", "wood",
+	"gravel", "metal", "stone", "wood",
+	"gravel", "metal", "stone",	"wood",
+	"gravel", "metal", "stone", "wood",
+	"gravel", "metal", "stone", "wood",
+	"gravel", "metal", "stone", "wood",
+	"gravel", "metal", "stone", "wood",
+	"gravel", "metal", "stone", "wood",
+	"gravel", "metal", "stone", "wood",
+	"gravel", "metal", "stone", "wood",
+	"gravel", "metal", "stone", "wood",
+	"gravel", "metal", "stone", "wood",
+	"gravel", "metal", "stone", "wood",
+	"gravel", "metal", "stone", "wood",
+	"gravel", "metal", "stone", "wood",
+	"gravel", "metal", "stone", "wood",
+	"gravel", "metal", "stone", "wood",
+	"gravel", "metal", "stone", "wood",
+	"gravel", "metal", "stone",	"wood",
+	"gravel", "metal", "stone", "wood",
+	"gravel", "metal", "stone", "wood",
+	"gravel", "metal", "stone", "wood",
+	"gravel", "metal", "stone", "wood",
+	"gravel", "metal", "stone", "wood",
+	"gravel", "metal", "stone", "wood",
+	"gravel", "metal", "stone", "wood",
+	"gravel", "metal", "stone", "wood",
+	"gravel", "metal", "stone", "wood",
+	"gravel", "metal", "stone", "wood",
+	"gravel", "metal", "stone", "wood",
+	"gravel", "metal", "stone", "wood",
+	"gravel", "metal", "stone", "wood",
+	"gravel", "metal", "stone", "wood",
+	"gravel", "metal", "stone", "wood",
+	"gravel", "metal", "stone",	"wood",
+	"gravel", "metal", "stone", "wood",
+	"gravel", "metal", "stone", "wood",
+	"gravel", "metal", "stone", "wood",
+	"gravel", "metal", "stone", "wood",
+	"gravel", "metal", "stone", "wood",
+	"gravel", "metal", "stone", "wood",
+	"gravel", "metal", "stone", "wood",
+	"gravel", "metal", "stone", "wood",
+	"gravel", "metal", "stone", "wood",
+	"gravel", "metal", "stone", "wood",
+	"gravel", "metal", "stone", "wood",
+	"gravel", "metal", "stone", "wood",
+	"gravel", "metal", "stone", "wood",
+	"gravel", "metal", "stone", "wood",
+	"gravel", "metal", "stone", "wood",
+};
+
+//TODO: mxd. Seems to be used from Player.dll only. Move there?
+//TODO: mxd. GROSS HACKS (because I don't want to include Player just sources yet...)
+//H2COMMON_API char* GetClientGroundSurfaceMaterialName(const playerinfo_t* playerinfo)
+H2COMMON_API char* GetClientGroundSurfaceMaterialName(const void* playerinfo)
+{
+	//const csurface_t* surf = playerinfo->GroundSurface;
+	csurface_t* surf = *(csurface_t**)((int)playerinfo + 256);
+	if (surf != NULL)
+	{
+		const int mat_index = surf->flags >> 24;
+		return SurfaceMaterialNames[mat_index];
+	}
+
+	return NULL;
+}
--- src/qcommon/SurfaceProps.h
+++ src/qcommon/SurfaceProps.h
@@ -1,20 +1,19 @@
 //
-// surfaceprops.h
+// SurfaceProps.h
 //
-// Heretic II
 // Copyright 1998 Raven Software
 //
 
-#ifndef _SURFACEPROPS_H_
-#define _SURFACEPROPS_H_
+#pragma once
 
-//#include "p_types.h"
-#include "player.h"
+#include "H2Common.h" //mxd. For H2COMMON_API
+////#include "p_types.h"
+//#include "Player.h"
 
 #define MAX_SURFACE_MAT_NAMES 256
 
-extern char *SurfaceMaterialNames[MAX_SURFACE_MAT_NAMES];
+//extern char* SurfaceMaterialNames[MAX_SURFACE_MAT_NAMES]; //TODO: mxd. Disabled. Not exported in original H2Common.dll. Doesn't seem to be used anywhere.
 
-extern H2COMMON_API char *GetClientGroundSurfaceMaterialName(playerinfo_t *playerinfo);
-
-#endif // _SURFACEPROPS_H_
+//TODO: fix definition
+//extern H2COMMON_API char* GetClientGroundSurfaceMaterialName(const playerinfo_t* playerinfo);
+extern H2COMMON_API char* GetClientGroundSurfaceMaterialName(const void* playerinfo);
