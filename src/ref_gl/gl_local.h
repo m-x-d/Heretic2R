c9d910ba68e33f95bfe3c022fc0010d6c87edbc6
m-x-d <j.maxed@gmail.com>
2024-06-17 14:33:26 +0300

    Add ref_gl skeleton project.

---
+++ src/ref_gl/gl_local.h
@@ -0,0 +1,7 @@
+//
+// gl_local.h
+//
+// Copyright 1998 Raven Software
+//
+
+#pragma once
---
+++ src/ref_gl/gl_main.c
@@ -0,0 +1,14 @@
+//
+// gl_main.c
+//
+// Copyright 1998 Raven Software
+//
+
+#include "gl_local.h"
+
+void* GetRefAPI(void* rimp)
+{
+	int	re = 0;
+
+	return re;
+}
