bae0b641044b962b38805cfd29f9cf1642a43fce
m-x-d <j.maxed@gmail.com>
2024-06-25 18:53:02 +0300

    Add q_shwin.c skeleton.

---
+++ src/win32/q_shwin.c
@@ -0,0 +1,30 @@
+//
+// q_shwin.c
+//
+// Copyright 1998 Raven Software
+//
+
+#include "qcommon.h"
+
+void* Hunk_Begin(int maxsize)
+{
+	NOT_IMPLEMENTED
+	return NULL;
+}
+
+void* Hunk_Alloc(int size)
+{
+	NOT_IMPLEMENTED
+	return NULL;
+}
+
+int Hunk_End(void)
+{
+	NOT_IMPLEMENTED
+	return 0;
+}
+
+void Hunk_Free(void* base)
+{
+	NOT_IMPLEMENTED
+}
