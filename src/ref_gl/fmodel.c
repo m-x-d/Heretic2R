748d9faada1168896b488594d9397e93c7160303
m-x-d <j.maxed@gmail.com>
2024-06-25 17:42:21 +0300

    Add fmodel.c skeleton.

---
+++ src/ref_gl/fmodel.c
@@ -0,0 +1,30 @@
+//
+// fmodel.c
+//
+// Copyright 1998 Raven Software
+//
+
+#include "gl_local.h"
+#include "fmodel.h"
+
+fmdl_t* fmodel;
+
+void Mod_LoadFlexModel(model_t* mod, void* buffer, int length)
+{
+	NOT_IMPLEMENTED
+}
+
+void Mod_RegisterFlexModel(model_t* mod)
+{
+	NOT_IMPLEMENTED
+}
+
+void R_DrawFlexModel(entity_t* e)
+{
+	NOT_IMPLEMENTED
+}
+
+void GL_LerpVert(vec3_t newPoint, vec3_t oldPoint, vec3_t interpolatedPoint, float move[3], float frontv[3], float backv[3])
+{
+	NOT_IMPLEMENTED
+}
