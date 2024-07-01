7526001bf91536f2a996020c789366f1a5d82f4c
m-x-d <j.maxed@gmail.com>
2024-07-01 18:05:10 +0300

    Cosmetic changes in m_SkeletalCluster.h. Cosmetic changes in m_Skeleton.h. Add m_Skeleton.h to VS project.

--- src/ref_gl/m_SkeletalCluster.h
+++ src/ref_gl/m_SkeletalCluster.h
@@ -1,10 +1,17 @@
-typedef int qboolean;
+//
+// m_SkeletalCluster.h
+//
+// Copyright 1998 Raven Software
+//
+
+#pragma once
+
+#include "q_Typedef.h"
 
 typedef struct M_SkeletalCluster_s
 {
-	int children;		// must be the first field
+	int children; // Must be the first field
 	int numVerticies;
-	int *verticies;
+	int* verticies;
 	qboolean inUse;
 } M_SkeletalCluster_t;
-
