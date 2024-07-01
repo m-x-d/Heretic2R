85eb9c6ce0273eafa6a866e193f9fc767e2ca55e
m-x-d <j.maxed@gmail.com>
2024-07-01 14:28:15 +0300

    Implement Mod_RegisterFlexModel().

--- src/ref_gl/fmodel.c
+++ src/ref_gl/fmodel.c
@@ -254,7 +254,12 @@ void Mod_LoadFlexModel(model_t* mod, void* buffer, int length)
 
 void Mod_RegisterFlexModel(model_t* mod)
 {
-	NOT_IMPLEMENTED
+	fmodel = (fmdl_t*)mod->extradata;
+
+	// Precache skins... //TODO: also done in fmLoadSkin(). One of these isn't needed?
+	char* skin_name = fmodel->skin_names;
+	for (int i = 0; i < fmodel->header.num_skins; i++, skin_name += MAX_QPATH)
+		mod->skins[i] = GL_FindImage(skin_name, it_skin);
 }
 
 void R_DrawFlexModel(entity_t* e)
