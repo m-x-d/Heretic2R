b2169db559305cd81a366172eba9970b2bfdd344
m-x-d <j.maxed@gmail.com>
2024-07-12 04:34:47 +0300

    Implement ChopImage().

--- src/ref_gl/gl_draw_cinematic.c
+++ src/ref_gl/gl_draw_cinematic.c
@@ -19,10 +19,46 @@ typedef struct CinematicFrame_s
 static CinematicFrame_t cinematic_frames[MAX_CINEMATIC_FRAMES];
 static int num_cinematic_frames;
 
-static int ChopImage(int w, int h)
+static int GetCoords(int size, int* image_sizes)
 {
 	NOT_IMPLEMENTED
 	return 0;
+}
+
+static int ChopImage(const int width, const int height)
+{
+	int img_widths[64];
+	int img_heights[64];
+	
+	const int tiles_w = GetCoords(width, img_widths) - 1;
+	const int tiles_h = GetCoords(height, img_heights) - 1;
+
+	if (tiles_h < 1)
+		return 0;
+
+	CinematicFrame_t* frame = cinematic_frames;
+
+	for (int h = 0; h < tiles_h; h++)
+	{
+		for (int w = 0; w < tiles_w; w++, frame++)
+		{
+			image_t* image = GL_GetFreeImage();
+
+			image->registration_sequence = registration_sequence;
+			image->width = img_widths[w + 1] - img_widths[w];
+			image->height = img_heights[h + 1] - img_heights[h];
+			image->type = it_pic;
+			image->palette = NULL;
+			image->texnum = TEXNUM_IMAGES + (image - gltextures);
+			image->has_alpha = 0;
+
+			frame->width = img_widths[w];
+			frame->height = img_heights[h];
+			frame->image = image;
+		}
+	}
+
+	return tiles_w * tiles_h;
 }
 
 void Draw_InitCinematic(const int w, const int h, char* overlay, char* backdrop)
