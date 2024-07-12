5c54e7164303fa7cff6731f900700e36b571f8ac
m-x-d <j.maxed@gmail.com>
2024-07-12 04:53:18 +0300

    Implement GetCoords(). Rename some cinematic-related vars to better match with the logic.

--- src/ref_gl/gl_draw_cinematic.c
+++ src/ref_gl/gl_draw_cinematic.c
@@ -6,79 +6,106 @@
 
 #include "gl_local.h"
 
-#define MAX_CINEMATIC_FRAMES 50
+#define MAX_CINEMATIC_TILES 50
 
 //mxd. Reconstructed data type. Original name unknown...
-typedef struct CinematicFrame_s
+typedef struct
 {
-	int width;
-	int height;
+	int offset_x;
+	int offset_y;
 	image_t* image;
-} CinematicFrame_t;
+} CinematicTile_t;
 
-static CinematicFrame_t cinematic_frames[MAX_CINEMATIC_FRAMES];
-static int num_cinematic_frames;
+static CinematicTile_t cinematic_tiles[MAX_CINEMATIC_TILES];
+static int num_cinematic_tiles;
 
-static int GetCoords(int size, int* image_sizes)
+static int GetCoords(int size, int* offsets)
 {
-	NOT_IMPLEMENTED
-	return 0;
+	int num_tiles = 1;
+	int target_size = 256;
+	int total_size = 0;
+
+	if (size == 0)
+		return num_tiles;
+
+	int* cur_offset = offsets;
+	*cur_offset = 0;
+	cur_offset++;
+
+	do
+	{
+		if (size < target_size)
+		{
+			target_size >>= 1;
+		}
+		else
+		{
+			total_size += target_size;
+			size -= target_size;
+			*cur_offset = total_size;
+			
+			cur_offset++;
+			num_tiles++;
+		}
+	} while (size != 0);
+
+	return num_tiles;
 }
 
 static int ChopImage(const int width, const int height)
 {
-	int img_widths[64];
-	int img_heights[64];
+	int offsets_x[64];
+	int offsets_y[64];
 	
-	const int tiles_w = GetCoords(width, img_widths) - 1;
-	const int tiles_h = GetCoords(height, img_heights) - 1;
+	const int tiles_x = GetCoords(width, offsets_x) - 1;
+	const int tiles_y = GetCoords(height, offsets_y) - 1;
 
-	if (tiles_h < 1)
+	if (tiles_y < 1)
 		return 0;
 
-	CinematicFrame_t* frame = cinematic_frames;
+	CinematicTile_t* tile = cinematic_tiles;
 
-	for (int h = 0; h < tiles_h; h++)
+	for (int y = 0; y < tiles_y; y++)
 	{
-		for (int w = 0; w < tiles_w; w++, frame++)
+		for (int x = 0; x < tiles_x; x++, tile++)
 		{
 			image_t* image = GL_GetFreeImage();
 
 			image->registration_sequence = registration_sequence;
-			image->width = img_widths[w + 1] - img_widths[w];
-			image->height = img_heights[h + 1] - img_heights[h];
+			image->width = offsets_x[x + 1] - offsets_x[x];
+			image->height = offsets_y[y + 1] - offsets_y[y];
 			image->type = it_pic;
 			image->palette = NULL;
 			image->texnum = TEXNUM_IMAGES + (image - gltextures);
 			image->has_alpha = 0;
 
-			frame->width = img_widths[w];
-			frame->height = img_heights[h];
-			frame->image = image;
+			tile->offset_x = offsets_x[x];
+			tile->offset_y = offsets_y[y];
+			tile->image = image;
 		}
 	}
 
-	return tiles_w * tiles_h;
+	return tiles_x * tiles_y;
 }
 
 void Draw_InitCinematic(const int w, const int h, char* overlay, char* backdrop)
 {
-	num_cinematic_frames = ChopImage(w, h);
+	num_cinematic_tiles = ChopImage(w, h);
 }
 
 // New in H2
 void Draw_CloseCinematic(void)
 {
 	int i;
-	CinematicFrame_t* info;
+	CinematicTile_t* tile;
 
-	for (i = 0, info = cinematic_frames; i < num_cinematic_frames; i++, info++)
+	for (i = 0, tile = cinematic_tiles; i < num_cinematic_tiles; i++, tile++)
 	{
-		info->image->palette = NULL; //TODO: GL_FreeImageNoHash frees image->palette. Explicitly set to NULL here to avoid that?
-		GL_FreeImageNoHash(info->image);
+		tile->image->palette = NULL; //TODO: GL_FreeImageNoHash frees image->palette. Explicitly set to NULL here to avoid that?
+		GL_FreeImageNoHash(tile->image);
 	}
 
-	num_cinematic_frames = 0;
+	num_cinematic_tiles = 0;
 }
 
 void Draw_Cinematic(int cols, int rows, byte* data, paletteRGB_t* palette, float alpha)
