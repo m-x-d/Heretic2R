61ee00eb8a3c0172998f0a216ed853d94e0eb76f
m-x-d <j.maxed@gmail.com>
2024-08-13 16:28:55 +0300

    Add quake2.dll skeleton project.

---
+++ src/win32/Quake2Main.h
@@ -0,0 +1,12 @@
+//
+// Quake2Main.h -- Exposes the only function needed by Heretic2R.exe.
+//
+// Copyright 1998 Raven Software
+//
+
+#pragma once
+
+#include <windows.h>
+#include "Heretic2.h"
+
+int GAME_DECLSPEC Quake2Main(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow);
---
+++ src/win32/sys_win.c
@@ -0,0 +1,15 @@
+//
+// sys_win.c
+//
+// Copyright 1998 Raven Software
+//
+
+#include "Quake2Main.h"
+#include "q_shared.h"
+
+int GAME_DECLSPEC Quake2Main(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	NOT_IMPLEMENTED
+
+	return 1;
+}
