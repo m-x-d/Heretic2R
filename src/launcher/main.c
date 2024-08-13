8723b7958923198a82fe1dfa8b296e256f309bc6
m-x-d <j.maxed@gmail.com>
2024-08-13 16:30:04 +0300

    Add Heretic2R project.

---
+++ src/launcher/main.c
@@ -0,0 +1,12 @@
+//
+// main.c -- Heretic2R launcher.
+//
+// Copyright 1998 Raven Software
+//
+
+#include "Quake2Main.h"
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	return Quake2Main(hInstance, hPrevInstance, lpCmdLine, nCmdShow);
+}
