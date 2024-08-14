83939fb0810f07ea7a55bd6917503b2d77698f12
m-x-d <j.maxed@gmail.com>
2024-08-14 14:33:39 +0300

    Implement Cvar_VariableString().

--- src/qcommon/cvar.c
+++ src/qcommon/cvar.c
@@ -6,10 +6,17 @@
 
 #include "qcommon.h"
 
-char* Cvar_VariableString(const char* var_name)
+static cvar_t* Cvar_FindVar(const char* var_name)
 {
 	NOT_IMPLEMENTED
 	return NULL;
+}
+
+// Q2 counterpart
+char* Cvar_VariableString(const char* var_name)
+{
+	const cvar_t* var = Cvar_FindVar(var_name);
+	return (var != NULL ? var->string : "");
 }
 
 cvar_t* Cvar_Get(char* var_name, char* var_value, int flags)
