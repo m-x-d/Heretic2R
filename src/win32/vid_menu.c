d63ad13624caaf0f9f70e374fa4a4df672864180
m-x-d <j.maxed@gmail.com>
2024-08-23 17:14:37 +0300

    Implement VID_Menu_GetDriverNames().

--- src/win32/vid_menu.c
+++ src/win32/vid_menu.c
@@ -12,9 +12,15 @@ static int s_current_menu_index;
 
 static menulist_s s_ref_list[2];
 
-static cvar_t* gl_driver; // New in H2
+// New in H2:
+#define NUM_DRIVERNAMES			16
+#define MAX_DRIVERNAME_LENGTH	20
+
+static cvar_t* gl_driver;
+
 static int gl_drivers_count;
-static char gl_drivernames[14][20];
+static char gl_drivernames[NUM_DRIVERNAMES - 2][MAX_DRIVERNAME_LENGTH];
+static char* gl_drivername_labels[NUM_DRIVERNAMES];
 
 static void VID_MenuSetDetail(int detail)
 {
@@ -23,7 +29,34 @@ static void VID_MenuSetDetail(int detail)
 
 static void VID_Menu_GetDriverNames(void)
 {
-	NOT_IMPLEMENTED
+	char buffer[MAX_QPATH];
+
+	gl_drivername_labels[0] = "Software";
+	gl_drivername_labels[1] = "OpenGL32";
+	gl_drivers_count = 2;
+
+	for (int i = 0; i < NUM_DRIVERNAMES - gl_drivers_count; i++)
+	{
+		Com_sprintf(buffer, sizeof(buffer), "gl_driver%d", i);
+		const cvar_t* cvar = Cvar_Get(buffer, "", CVAR_ARCHIVE);
+		const uint len = strlen(cvar->string);
+
+		if (len == 0)
+			break;
+
+		if (len > MAX_DRIVERNAME_LENGTH - 1)
+		{
+			Com_Printf("*** ERROR : Invalid driver name %s\n", cvar->string);
+			strcpy_s(cvar->string, len, "Invalid Driver");
+		}
+
+		strncpy_s(gl_drivernames[i], sizeof(gl_drivernames[i]), cvar->string, MAX_DRIVERNAME_LENGTH - 1);
+
+		gl_drivername_labels[gl_drivers_count] = gl_drivernames[i];
+		gl_drivers_count++;
+	}
+
+	gl_drivername_labels[gl_drivers_count] = NULL;
 }
 
 void VID_PreMenuInit(void)
@@ -56,7 +89,7 @@ void VID_PreMenuInit(void)
 
 	if (Q_stricmp(vid_ref->string, "gl") == 0)
 	{
-		//TODO: this initializes 'Software' menu(?), so above check should be inverted?
+		//TODO: this initializes 'Software' menu, so above check should be inverted?
 		s_current_menu_index = 0;
 		s_ref_list[0].curvalue = 0;
 		s_ref_list[1].curvalue = 0;
