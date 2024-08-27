1df0d87de27cc1677f0c5969f7e3444d29ff8e36
m-x-d <j.maxed@gmail.com>
2024-08-27 14:30:17 +0300

    Implement SCR_DirtyScreen() and SCR_AddDirtyPoint().

--- src/client/cl_screen.c
+++ src/client/cl_screen.c
@@ -8,6 +8,16 @@
 
 cvar_t* scr_viewsize;
 
+typedef struct
+{
+	int x1;
+	int y1;
+	int x2;
+	int y2;
+} dirty_t;
+
+static dirty_t scr_dirty;
+
 void SCR_Init(void)
 {
 	NOT_IMPLEMENTED
@@ -18,7 +28,18 @@ void SCR_EndLoadingPlaque(void)
 	NOT_IMPLEMENTED
 }
 
+// Q2 counterpart
+void SCR_AddDirtyPoint(const int x, const int y)
+{
+	scr_dirty.x1 = min(x, scr_dirty.x1);
+	scr_dirty.x2 = max(x, scr_dirty.x2);
+	scr_dirty.y1 = min(y, scr_dirty.y1);
+	scr_dirty.y2 = max(y, scr_dirty.y2);
+}
+
+// Q2 counterpart
 void SCR_DirtyScreen(void)
 {
-	NOT_IMPLEMENTED
+	SCR_AddDirtyPoint(0, 0);
+	SCR_AddDirtyPoint(viddef.width - 1, viddef.height - 1);
 }
