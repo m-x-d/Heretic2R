0455d607ef29eaa18b66c27d068d5cd66208283b
m-x-d <j.maxed@gmail.com>
2024-08-27 19:33:48 +0300

    Implement V_Init().

--- src/client/cl_view.c
+++ src/client/cl_view.c
@@ -6,7 +6,12 @@
 
 #include "client.h"
 
+cvar_t* crosshair;
+static cvar_t* cl_stats;
+
+//mxd. Very stripped down compared to Q2 version.
 void V_Init(void)
 {
-	NOT_IMPLEMENTED
+	cl_stats = Cvar_Get("cl_stats", "0", 0);
+	crosshair = Cvar_Get("crosshair", "0", CVAR_ARCHIVE);
 }
