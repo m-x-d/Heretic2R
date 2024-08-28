fd38e27f87a91b8a318f34e44d0f023cdfa6efca
m-x-d <j.maxed@gmail.com>
2024-08-28 18:55:57 +0300

    Implement InitMessages(), LoadMessages() and SplitMessages().

--- src/client/cl_main.c
+++ src/client/cl_main.c
@@ -127,6 +127,21 @@ entity_state_t cl_parse_entities[MAX_PARSE_ENTITIES];
 
 static qboolean ignored_players[MAX_CLIENTS];
 
+// Game / level messages
+typedef struct GameMessage_s
+{
+	char* message;
+	char* sound_name;
+} GameMessage_t;
+
+#define MAX_MESSAGES	1000
+
+static char* game_messages_text;
+static GameMessage_t game_messages[MAX_MESSAGES];
+
+static char* level_messages_text;
+static GameMessage_t level_messages[MAX_MESSAGES];
+
 // Q2 counterpart
 // Adds the current command line as a clc_stringcmd to the client message.
 // Things like godmode, noclip, etc, are commands directed to the server,
@@ -452,9 +467,79 @@ static void CL_WriteConfiguration(void)
 	Cvar_WriteVariables(path);
 }
 
+static int LoadMessages(char* filename, char** message_out)
+{
+	char* buf;
+
+	const int size = FS_LoadFile(filename, (void**)&buf);
+	if (size < 1)
+		Sys_Error("Unable to load %s", filename);
+
+	*message_out = Z_Malloc(size + 1);
+	memcpy(*message_out, buf, size);
+	(*message_out)[size] = 0;
+
+	FS_FreeFile(buf);
+
+	return size + 1;
+}
+
+//TODO: lines starting with ';' char are supposed to be comments?
+static void SplitMessages(char* src, GameMessage_t* dst, const int src_len)
+{
+	const char* end = src + src_len;
+
+	for (int i = 1; src < end; i++)
+	{
+		if (i >= MAX_MESSAGES)
+		{
+			Com_Printf("Too many strings\n");
+			return;
+		}
+
+		dst[i].message = NULL;
+		dst[i].sound_name = NULL;
+
+		char* newline = strchr(src, '\r');
+		if (newline == NULL)
+			return;
+
+		*newline = '\0';
+
+		// # signifies a .wav file
+		char* snd_id = strchr(src, '#');
+		if (snd_id != NULL && snd_id < newline)
+		{
+			dst[i].sound_name = snd_id + 1;
+			*snd_id = '\0';
+		}
+
+		dst[i].message = src;
+
+		// @ signifies a newline
+		while (src != NULL)
+		{
+			src = strchr(src, '@');
+			if (src == NULL)
+				break;
+
+			*src = '\n';
+			src++;
+		}
+
+		src = newline + 2;
+	}
+}
+
 static void InitMessages(void)
 {
-	NOT_IMPLEMENTED
+	const cvar_t* cv = Cvar_Get("file_gamemsg", "gamemsg.txt", CVAR_ARCHIVE);
+	int len = LoadMessages(cv->string, &game_messages_text);
+	SplitMessages(game_messages_text, game_messages, len);
+
+	cv = Cvar_Get("file_levelmsg", "levelmsg.txt", CVAR_ARCHIVE);
+	len = LoadMessages(cv->string, &level_messages_text);
+	SplitMessages(level_messages_text, level_messages, len);
 }
 
 static void ClearGameMessages(void)
