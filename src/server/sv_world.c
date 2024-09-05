0cca539dea12e6a87e0294dbb82b857e1b3e7914
m-x-d <j.maxed@gmail.com>
2024-09-05 19:11:22 +0300

    Implement SV_CreateAreaNode() and ClearLink().

--- src/server/sv_world.c
+++ src/server/sv_world.c
@@ -5,7 +5,9 @@
 //
 
 #include "server.h"
+#include "Vector.h"
 
+#define AREA_DEPTH	4
 #define AREA_NODES	32
 
 typedef struct areanode_s
@@ -20,10 +22,58 @@ typedef struct areanode_s
 areanode_t sv_areanodes[AREA_NODES];
 int sv_numareanodes;
 
-static areanode_t* SV_CreateAreaNode(int depth, vec3_t mins, vec3_t maxs)
+// Q2 counterpart
+// ClearLink is used for new headnodes.
+static void ClearLink(link_t* l)
 {
-	NOT_IMPLEMENTED
-	return NULL;
+	l->next = l;
+	l->prev = l;
+}
+
+// Q2 counterpart
+// Builds a uniformly subdivided tree for the given world size.
+static areanode_t* SV_CreateAreaNode(const int depth, vec3_t mins, vec3_t maxs)
+{
+	vec3_t size;
+	vec3_t mins1;
+	vec3_t maxs1;
+	vec3_t mins2;
+	vec3_t maxs2;
+
+	areanode_t* anode = &sv_areanodes[sv_numareanodes];
+	sv_numareanodes++;
+
+	ClearLink(&anode->trigger_edicts);
+	ClearLink(&anode->solid_edicts);
+
+	if (depth == AREA_DEPTH)
+	{
+		anode->axis = -1;
+		anode->children[0] = NULL;
+		anode->children[1] = NULL;
+
+		return anode;
+	}
+
+	VectorSubtract(maxs, mins, size);
+	if (size[0] > size[1])
+		anode->axis = 0;
+	else
+		anode->axis = 1;
+
+	anode->dist = (maxs[anode->axis] + mins[anode->axis]) * 0.5f;
+	VectorCopy(mins, mins1);
+	VectorCopy(mins, mins2);
+	VectorCopy(maxs, maxs1);
+	VectorCopy(maxs, maxs2);
+
+	maxs1[anode->axis] = anode->dist;
+	mins2[anode->axis] = anode->dist;
+
+	anode->children[0] = SV_CreateAreaNode(depth + 1, mins2, maxs2);
+	anode->children[1] = SV_CreateAreaNode(depth + 1, mins1, maxs1);
+
+	return anode;
 }
 
 // Q2 counterpart
