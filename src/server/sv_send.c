f28ea282c01df9aaa4279178bfb4243dc0774c7d
m-x-d <j.maxed@gmail.com>
2024-09-16 15:21:23 +0300

    Implement SV_SendClientMessages().

--- src/server/sv_send.c
+++ src/server/sv_send.c
@@ -5,6 +5,8 @@
 //
 
 #include "server.h"
+#include "sv_effects.h"
+#include "cl_strings.h"
 
 void SV_BroadcastPrintf(int level, char* fmt, ...)
 {
@@ -123,7 +125,118 @@ void SV_StartSound(vec3_t origin, edict_t* entity, int channel,	int soundindex, 
 	NOT_IMPLEMENTED
 }
 
-void SV_SendClientMessages(qboolean send_client_data)
+#pragma region ========================== FRAME UPDATES ==========================
+
+qboolean SV_SendClientDatagram(client_t* client, qboolean send_client_data)
+{
+	NOT_IMPLEMENTED
+	return false;
+}
+
+static void SV_DemoCompleted(void)
 {
 	NOT_IMPLEMENTED
 }
+
+static qboolean SV_RateDrop(client_t* c)
+{
+	NOT_IMPLEMENTED
+	return false;
+}
+
+void SV_SendClientMessages(const qboolean send_client_data)
+{
+	byte msgbuf[MAX_MSGLEN];
+
+	int msglen = 0;
+
+	// Read the next demo message if needed.
+	if (sv.state == ss_demo && sv.demofile != NULL && !(int)sv_paused->value)
+	{
+		// Get the next message.
+		uint r = fread(&msglen, 4, 1, sv.demofile);
+		if (r != 1 || msglen == -1)
+		{
+			SV_DemoCompleted();
+			return;
+		}
+
+		if (msglen > MAX_MSGLEN)
+			Com_Error(ERR_DROP, "SV_SendClientMessages: msglen > MAX_MSGLEN");
+
+		r = fread(msgbuf, msglen, 1, sv.demofile);
+		if (r != 1)
+		{
+			SV_DemoCompleted();
+			return;
+		}
+	}
+
+	// Send a message to each connected client.
+	int send_mask = 0; // H2
+
+	client_t* c = svs.clients;
+	for (int i = 0; i < (int)maxclients->value; i++, c++)
+	{
+		if (c->state == cs_free)
+			continue;
+
+		// If the reliable message overflowed, drop the client.
+		const qboolean overflowed = c->datagram.overflowed; // H2
+		if (c->netchan.message.overflowed)
+		{
+			SZ_Clear(&c->netchan.message);
+			SZ_Clear(&c->datagram);
+			SV_BroadcastObituary(PRINT_HIGH, GM_OVERFLOW, c->edict->s.number, 0); // H2
+			Com_Printf("WARNING: reliable overflow for %s\n", c->name); // H2
+			SV_DropClient(c);
+		}
+
+		send_mask |= EDICT_MASK(c->edict); // H2
+
+		if (sv.state == ss_cinematic || sv.state == ss_demo)
+		{
+			Netchan_Transmit(&c->netchan, msglen, msgbuf);
+		}
+		else if (c->state == cs_spawned)
+		{
+			// Don't overrun bandwidth
+			if (SV_RateDrop(c))
+				continue;
+
+			SV_SendClientDatagram(c, send_client_data); // H2: new 2-nd arg.
+		}
+		else if (c->netchan.message.cursize > 0 || curtime - c->netchan.last_sent > 1000)
+		{
+			// Just update reliable if needed.
+			Netchan_Transmit(&c->netchan, 0, NULL);
+		}
+
+		if (!overflowed) // H2
+		{
+			const int mask = EDICT_MASK(c->edict);
+			for (int e = 1; e < ge->num_edicts; e++)
+			{
+				edict_t* ent = EDICT_NUM(e);
+				if (ent->just_deleted && (ent->client_sent & mask) != 0)
+					ent->client_sent &= ~mask;
+			}
+		}
+	}
+
+	// H2: clear all client effects.
+	effects_buffer_index = 0;
+	effects_buffer_offset = 0;
+
+	PerEffectsBuffer_t* effect = persistant_effects_array;
+	for (int i = 0; i < MAX_PERSISTANT_EFFECTS; i++, effect++)
+	{
+		if (effect->fx_num == 0 && effect->numEffects != 0 && effect->send_mask == send_mask)
+		{
+			memset(effect, 0, sizeof(PerEffectsBuffer_t));
+			per_effects_buffers_size--;
+		}
+	}
+}
+
+#pragma endregion
