6733a913a9c3649b322b05625583a384a79e5d91
m-x-d <j.maxed@gmail.com>
2024-09-23 15:32:26 +0300

    Implement JoinServer_MenuDraw().

--- src/client/menus/menu_joinserver.c
+++ src/client/menus/menu_joinserver.c
@@ -114,7 +114,27 @@ void JoinServer_MenuInit(void)
 
 static void JoinServer_MenuDraw(void)
 {
-	NOT_IMPLEMENTED
+	char name[MAX_QPATH];
+
+	// Draw menu BG.
+	re.BookDrawPic(0, 0, "book/back/b_conback8.bk", cls.m_menuscale);
+
+	if (cls.m_menualpha == 0.0f)
+		return;
+
+	// Draw menu title.
+	Com_sprintf(name, sizeof(name), "\x03%s", m_banner_join->string);
+	const int x = M_GetMenuLabelX(re.BF_Strlen(name));
+	const int y = M_GetMenuOffsetY(&s_joinserver_menu);
+	re.DrawBigFont(x, y, name, cls.m_menualpha);
+
+	// Update server name items width.
+	for (int i = 0; i < MAX_LOCAL_SERVERS; i++)
+		s_joinserver_server_actions[i].generic.width = re.BF_Strlen(local_server_names[i]);
+
+	// Draw menu items.
+	s_joinserver_menu.x = M_GetMenuLabelX(s_joinserver_menu.width);
+	Menu_Draw(&s_joinserver_menu);
 }
 
 // Q2 counterpart
