97fa22034c7aa6795c7d37227a21a3d26ff36906
m-x-d <j.maxed@gmail.com>
2024-09-24 17:30:36 +0300

    Implement DMOptions_MenuDraw(). Implement DMOptions_MenuKey().

--- src/client/menus/menu_dmoptions.c
+++ src/client/menus/menu_dmoptions.c
@@ -352,13 +352,29 @@ static void DMOptions_MenuInit(void)
 
 static void DMOptions_MenuDraw(void)
 {
-	NOT_IMPLEMENTED
+	char name[MAX_QPATH];
+
+	// Draw menu BG.
+	re.BookDrawPic(0, 0, "book/back/b_conback8.bk", cls.m_menuscale);
+
+	if (cls.m_menualpha == 0.0f)
+		return;
+
+	// Draw menu title.
+	Com_sprintf(name, sizeof(name), "\x03%s", m_banner_dmopt->string);
+	const int x = M_GetMenuLabelX(re.BF_Strlen(name));
+	const int y = M_GetMenuOffsetY(&s_dmoptions_menu);
+	re.DrawBigFont(x, y, name, cls.m_menualpha);
+
+	// Draw menu items.
+	s_dmoptions_menu.x = M_GetMenuLabelX(s_dmoptions_menu.width);
+	Menu_Draw(&s_dmoptions_menu);
 }
 
-static const char* DMOptions_MenuKey(int key)
+// Q2 counterpart
+static const char* DMOptions_MenuKey(const int key)
 {
-	NOT_IMPLEMENTED
-	return NULL;
+	return Default_MenuKey(&s_dmoptions_menu, key);
 }
 
 // Q2 counterpart
