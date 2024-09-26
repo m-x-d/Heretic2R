f1f192a47529fbe0c3a80db75fd9a3b5fd331103
m-x-d <j.maxed@gmail.com>
2024-09-26 18:59:14 +0300

    Implement M_Menu_Misc_f(). Implement Misc_MenuDraw(). Implement Misc_MenuKey().

--- src/client/menus/menu_misc.c
+++ src/client/menus/menu_misc.c
@@ -4,6 +4,7 @@
 // Copyright 1998 Raven Software
 //
 
+#include "client.h"
 #include "menu_misc.h"
 
 cvar_t* m_banner_misc;
@@ -19,7 +20,42 @@ cvar_t* m_item_yawspeed;
 cvar_t* m_item_console;
 cvar_t* m_item_autoweapon;
 
-void M_Menu_Misc_f(void)
+static menuframework_s s_misc_menu;
+
+static void Misc_MenuInit(void) // H2
 {
 	NOT_IMPLEMENTED
+}
+
+static void Misc_MenuDraw(void) // H2
+{
+	char title[MAX_QPATH];
+
+	// Draw menu BG.
+	re.BookDrawPic(0, 0, "book/back/b_conback8.bk", cls.m_menuscale);
+
+	if (cls.m_menualpha == 0.0f)
+		return;
+
+	// Draw menu title.
+	Com_sprintf(title, sizeof(title), "\x03%s", m_banner_misc->string);
+	const int x = M_GetMenuLabelX(re.BF_Strlen(title));
+	const int y = M_GetMenuOffsetY(&s_misc_menu);
+	re.DrawBigFont(x, y, title, cls.m_menualpha);
+
+	// Draw menu items.
+	s_misc_menu.x = M_GetMenuLabelX(s_misc_menu.width);
+	Menu_AdjustCursor(&s_misc_menu, 1);
+	Menu_Draw(&s_misc_menu);
+}
+
+static const char* Misc_MenuKey(const int key) // H2
+{
+	return Default_MenuKey(&s_misc_menu, key);
+}
+
+void M_Menu_Misc_f(void) // H2
+{
+	Misc_MenuInit();
+	M_PushMenu(Misc_MenuDraw, Misc_MenuKey);
 }
