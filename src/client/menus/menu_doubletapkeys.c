6d1b7d5ca914acedde5068d5ff05b0470b315dd4
m-x-d <j.maxed@gmail.com>
2024-09-26 17:33:28 +0300

    Implement M_Menu_DoubletapKeys_f(). Implement DoubletapKeys_MenuDraw().

--- src/client/menus/menu_doubletapkeys.c
+++ src/client/menus/menu_doubletapkeys.c
@@ -4,11 +4,41 @@
 // Copyright 1998 Raven Software
 //
 
+#include "client.h"
 #include "menu_doubletapkeys.h"
+#include "menu_keys.h"
 
 cvar_t* m_banner_dt_keys;
 
+void DoubletapKeys_MenuDraw(void)
+{
+	char name[MAX_QPATH];
+
+	// Draw menu BG.
+	re.BookDrawPic(0, 0, "book/back/b_conback8.bk", cls.m_menuscale);
+
+	if (cls.m_menualpha == 0.0f)
+		return;
+
+	// Draw menu title.
+	m_menu_side = 0;
+	Com_sprintf(name, sizeof(name), "\x03%s", m_banner_dt_keys->string);
+	const int x = M_GetMenuLabelX(re.BF_Strlen(name));
+	const int y = M_GetMenuOffsetY(&s_keys_menu);
+	re.DrawBigFont(x, y, name, cls.m_menualpha);
+
+	// Draw menu items.
+	s_keys_menu.x = M_GetMenuLabelX(0);
+	Menu_AdjustCursor(&s_keys_menu, 1);
+	Menu_Draw(&s_keys_menu);
+}
+
 void M_Menu_DoubletapKeys_f(void)
 {
-	NOT_IMPLEMENTED
+	keys_category_offset = 39;
+	keys_count = 10;
+	use_doublebind = true;
+
+	Keys_MenuInit();
+	M_PushMenu(DoubletapKeys_MenuDraw, Keys_MenuKey);
 }
