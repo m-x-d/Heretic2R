f8457eb98de53593eb05ef0cb77ffd7fdd5773c2
m-x-d <j.maxed@gmail.com>
2024-10-30 13:07:37 +0300

    Cosmetic changes to Message.c. Cosmetic changes to Message.h.

--- src/qcommon/Message.c
+++ src/qcommon/Message.c
@@ -3,217 +3,195 @@
 //
 // Copyright 1998 Raven Software
 //
-// Heretic II
-//
 
 #include "Message.h"
 #include "SinglyLinkedList.h"
 
-void QueueMessage(MsgQueue_t *this, void *msg)
+void QueueMessage(MsgQueue_t* this_ptr, void* msg)
 {
 	GenericUnion4_t temp;
 
 	temp.t_void_p = msg;
-	SLList_Push(&this->msgs, temp);
+	SLList_Push(&this_ptr->msgs, temp);
 }
 
 #ifdef _GAME_DLL
 #include "g_local.h"
-size_t SetParms(SinglyLinkedList_t *this, char *format, va_list marker, qboolean entsAsNames)
+size_t SetParms(SinglyLinkedList_t* this_ptr, const char* format, va_list marker, qboolean entsAsNames)
 #else
-size_t SetParms(SinglyLinkedList_t *this, char *format, va_list marker)
+size_t SetParms(SinglyLinkedList_t* this_ptr, const char* format, va_list marker)
 #endif
 {
 	qboolean append = false;
 	GenericUnion4_t parm;
-	char current;
 	int count = 0;
 	size_t bytesParsed = 0;
 
-	SLList_Front(this);
+	SLList_Front(this_ptr);
 
-	while(current = format[count])
+	while (format[count] != 0)
 	{
-		switch(current)
+		switch (format[count])
 		{
-		case 'b':
-			parm.t_byte = va_arg(marker, byte);
-			bytesParsed += sizeof(byte);
-			break;
-		case 's':
-			parm.t_short = va_arg(marker, short);
-			bytesParsed += sizeof(short);
-			break;
-		case 'i':
-			parm.t_int = va_arg(marker, int);
-			bytesParsed += sizeof(int);
-			break;
-		case 'f':
-			parm.t_float = va_arg(marker, double);
-			bytesParsed += sizeof(double);
-			break;
-		case 'e':	// a pointer is a pointer is a pointer
-//			parm.t_edict_p = va_arg(marker, edict_t*);
-//			break;
+			case 'b':
+				parm.t_byte = va_arg(marker, byte);
+				bytesParsed += sizeof(byte);
+				break;
 
-#ifdef _GAME_DLL	// this is pretty nasty, this may be an indication that something 
-					// needs to be rethought currently this is only used in the 
-					// ICScript_Advance
+			case 's':
+				parm.t_short = va_arg(marker, short);
+				bytesParsed += sizeof(short);
+				break;
 
-					// It does keep the code all in one place, which is nice
+			case 'i':
+				parm.t_int = va_arg(marker, int);
+				bytesParsed += sizeof(int);
+				break;
 
-			if(entsAsNames)	
-			{
-				int j = 0;
-				edict_t *ent = NULL;
-				char entityName[64];
+			case 'f':
+				parm.t_float = va_arg(marker, double);
+				bytesParsed += sizeof(double);
+				break;
 
-				do
+			case 'e': // A pointer is a pointer is a pointer
+#ifdef _GAME_DLL
+				// This is pretty nasty, this may be an indication that something needs to be rethought.
+				// Currently this is only used in the ICScript_Advance.
+				// It does keep the code all in one place, which is nice.
+				if (entsAsNames)
 				{
-					entityName[j] = *(marker + j);
-				}while(*(marker + j++));
+					int j = 0;
+					edict_t* ent = NULL;
+					char entityName[64];
 
-				marker += j;
+					do
+					{
+						entityName[j] = *(marker + j);
+					} while (*(marker + j++));
 
-				ent = G_Find (ent, FOFS(targetname), entityName);
+					marker += j;
+
+					ent = G_Find(ent, FOFS(targetname), entityName);
 
 #ifdef _DEBUG
-				if(ent)
-				{
-					edict_t *ent2 = NULL;
+					if (ent)
+					{
+						edict_t* ent2 = NULL;
 
-					ent2 = G_Find (ent, FOFS(targetname), entityName);
+						ent2 = G_Find(ent, FOFS(targetname), entityName);
 
-					assert(!ent2);	// each entityName should be unique!!!
+						assert(!ent2);	// each entityName should be unique!!!
+					}
+#endif
+					parm.t_void_p = ent;
+					break;
 				}
 #endif
-				parm.t_void_p = ent;
+
+			case 'v': // This better be not be a local variable or this will be bunk when the message is received and parsed.
+			case 'g': // g for generic
+				parm.t_void_p = va_arg(marker, void*);
+				bytesParsed += sizeof(void*);
 				break;
-			}
-#endif
-		case 'v':	// this better be not be a local variable or this will be bunk when
-					// the message is received and parsed
-//			parm.t_float_p = va_arg(marker, float*);
-//			break;
-		case 'g':	// g for generic
-			parm.t_void_p = va_arg(marker, void*);
-			bytesParsed += sizeof(void*);
-			break;
-		case 'c':
-			parm.t_RGBA = va_arg(marker, paletteRGBA_t);
-			bytesParsed += sizeof(paletteRGBA_t);
-			break;
-		default:
-			assert(0);
+
+			case 'c':
+				parm.t_RGBA = va_arg(marker, paletteRGBA_t);
+				bytesParsed += sizeof(paletteRGBA_t);
+				break;
+
+			default:
+				assert(0);
 		}
 
-		if(append)
+		if (append)
 		{
-			SLList_InsertAfter(this, parm);
-			SLList_Increment(this);
+			SLList_InsertAfter(this_ptr, parm);
+			SLList_Increment(this_ptr);
 		}
 		else
 		{
-			SLList_ReplaceCurrent(this, parm);
+			SLList_ReplaceCurrent(this_ptr, parm);
 
-			if(SLList_AtLast(this))
-			{
+			if (SLList_AtLast(this_ptr))
 				append = true;
-			}
 			else
-			{
-				SLList_Increment(this);
-			}
+				SLList_Increment(this_ptr);
 		}
 
-		++count;
+		count++;
 	}
 
 	return bytesParsed;
 }
 
-int GetParms(SinglyLinkedList_t *this, char *format, va_list marker)
+int GetParms(SinglyLinkedList_t* this_ptr, const char* format, va_list marker)
 {
-	qboolean append = false;
-	char current;
 	int count = 0;
-	byte *b;
-	short *s;
-	int *i;
-	float *f, *v;
-//	edict_t **e;
-	void **g;
-	paletteRGBA_t *c;
 
 	assert(format);
 
-	if(!format)
+	if (format == NULL)
 	{
 		Sys_Error("GetParms: null format string");
 		return 0;
 	}
 
-	SLList_Front(this);
+	SLList_Front(this_ptr);
 
-	assert(!SLList_AtEnd(this));
+	assert(!SLList_AtEnd(this_ptr));
 
-	if(SLList_AtEnd(this))
+	if (SLList_AtEnd(this_ptr))
 	{
 		Sys_Error("Getthis: empty parameter list");
 		return 0;
 	}
 
-	while(current = format[count])
+	while (format[count] != 0)
 	{
-		switch(current)
+		switch (format[count])
 		{
+			case 'b':
+				byte* b = va_arg(marker, byte*);
+				*b = SLList_PostIncrement(this_ptr).t_byte;
+				break;
 
-		case 'b':
-			b = va_arg(marker, byte*);
+			case 's':
+				short* s = va_arg(marker, short*);
+				*s = SLList_PostIncrement(this_ptr).t_short;
+				break;
 
-			*b = SLList_PostIncrement(this).t_byte;
-			break;
-		case 's':
-			s = va_arg(marker, short*);
+			case 'i':
+				int* i = va_arg(marker, int*);
+				*i = SLList_PostIncrement(this_ptr).t_int;
+				break;
 
-			*s = SLList_PostIncrement(this).t_short;
-			break;
-		case 'i':
-			i = va_arg(marker, int*);
+			case 'f':
+				float* f = va_arg(marker, float*);
+				*f = SLList_PostIncrement(this_ptr).t_float;
+				break;
 
-			*i = SLList_PostIncrement(this).t_int;
-			break;
-		case 'f':
-			f = va_arg(marker, float*);
+			case 'v':
+				float* v = va_arg(marker, float*);
+				v = SLList_PostIncrement(this_ptr).t_float_p;
+				break;
 
-			*f = SLList_PostIncrement(this).t_float;
-			break;
-		case 'v':
-			v = va_arg(marker, float*);
+			case 'e': // A pointer is a pointer is a pointer.
+			case 'g':
+				void** g = va_arg(marker, void**);
+				*g = SLList_PostIncrement(this_ptr).t_void_p;
+				break;
 
-			v = SLList_PostIncrement(this).t_float_p;
-			break;
-		case 'e':	// a pointer is a pointer is a pointer
-//			e = va_arg(marker, edict_t**);
+			case 'c':
+				paletteRGBA_t* c = va_arg(marker, paletteRGBA_t*);
+				*c = SLList_PostIncrement(this_ptr).t_RGBA;
+				break;
 
-//			*e = SLList_PostIncrement(this).t_edict_p;
-//			break;
-		case 'g':
-			g = va_arg(marker, void**);
-
-			*g = SLList_PostIncrement(this).t_void_p;
-			break;
-		case 'c':
-			c = va_arg(marker, paletteRGBA_t*);
-
-			*c = SLList_PostIncrement(this).t_RGBA;
-			break;
-		default:
-			assert(0);
-			return 0;
+			default:
+				assert(0);
+				return 0;
 		}
 
-		++count;
+		count++;
 	}
 
 	return count;
--- src/qcommon/Message.h
+++ src/qcommon/Message.h
@@ -1,5 +1,10 @@
-#ifndef MESSAGE_H
-#define MESSAGE_H
+//
+// Message.h
+//
+// Copyright 1998 Raven Software
+//
+
+#pragma once
 
 #include "SinglyLinkedList.h"
 
@@ -9,12 +14,10 @@ typedef struct MsgQueue_s
 } MsgQueue_t;
 
 #ifdef _GAME_DLL
-size_t SetParms(SinglyLinkedList_t *this_ptr, char *format, va_list marker, qboolean entsAsNames);
+size_t SetParms(SinglyLinkedList_t *this_ptr, const char *format, va_list marker, qboolean entsAsNames);
 #else
-size_t SetParms(SinglyLinkedList_t *this_ptr, char *format, va_list marker);
+size_t SetParms(SinglyLinkedList_t *this_ptr, const char *format, va_list marker);
 #endif
 
-int GetParms(SinglyLinkedList_t *this_ptr, char *format, va_list marker);
+int GetParms(SinglyLinkedList_t *this_ptr, const char *format, va_list marker);
 void QueueMessage(MsgQueue_t *this_ptr, void *msg);
-
-#endif
