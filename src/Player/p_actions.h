c77aac85f981fa6981f133c6f277570eb80c516a
m-x-d <j.maxed@gmail.com>
2024-12-18 15:19:25 +0300

    p_actions.h: remove unused definitions, fix mismatched function arg names.

--- src/Player/p_actions.h
+++ src/Player/p_actions.h
@@ -1,16 +1,14 @@
 //
-// p_actions2.h
+// p_actions.h
 //
-// Heretic II
 // Copyright 1998 Raven Software
 //
 
-#ifndef _P_ACTIONS2_H_
-#define _P_ACTIONS2_H_
+#pragma once
 
 #include "p_types.h"
-#include "p_anims.h"
 
+//TODO: convert to enum?
 #define SOUND_SWIM_FORWARD	0
 #define SOUND_SWIM_BACK		1
 #define SOUND_SWIM_SIDE		2
@@ -47,12 +45,10 @@ void PlayerActionEndStaffGlow(playerinfo_t* playerinfo, float value);
 qboolean PlayerActionCheckVault(playerinfo_t* playerinfo, float value);
 void PlayerActionSwimIdleSound(playerinfo_t* playerinfo, float value);
 void PlayerActionSwimSound(playerinfo_t* playerinfo, float value);
-//void PlayerActionSwimBackSound(playerinfo_t *playerinfo, float value);
 void PlayerActionClimbWallSound(playerinfo_t* playerinfo, float value);
 void PlayerActionClimbFinishSound(playerinfo_t* playerinfo, float value);
 
 void PlayerActionBreath(playerinfo_t* playerinfo, float value);
-void PlayerActionFireBow(playerinfo_t* playerinfo, float value);
 void PlayerActionFootstep(playerinfo_t* playerinfo, float value);
 void PlayerActionSwim(playerinfo_t* playerinfo, float value);
 void PlayerActionPullup(playerinfo_t* playerinfo, float value);
@@ -68,22 +64,22 @@ void PlayerActionPushLever(playerinfo_t* playerinfo, float value);
 void PlayerActionVaultSound(playerinfo_t* playerinfo, float value);
 void PlayerActionBowReadySound(playerinfo_t* playerinfo, float value);
 void PlayerActionTakePuzzle(playerinfo_t* playerinfo, float value);
-void PlayerActionMoveItem(playerinfo_t* playerinfo, float value);
+void PlayerActionMoveItem(playerinfo_t* playerinfo, float distance);
 
 qboolean PlayerActionCheckPuzzleGrab(playerinfo_t* playerinfo);
 qboolean PlayerActionCheckPushPull(playerinfo_t* playerinfo);
 qboolean PlayerActionCheckPushButton(playerinfo_t* playerinfo);
 qboolean PlayerActionCheckPushLever(playerinfo_t* playerinfo);
-qboolean PlayerActionCheckRopeGrab(playerinfo_t* playerinfo, float v_adjust);
+qboolean PlayerActionCheckRopeGrab(playerinfo_t* playerinfo, float stomp_org);
 
 qboolean PlayerActionUsePuzzle(playerinfo_t* playerinfo);
 
 void PlayerMoveFunc(playerinfo_t* playerinfo, float fwd, float right, float up);
-void PlayerClimbingMoveFunc(playerinfo_t* playerinfo, float fwd, float right, float up);
+void PlayerClimbingMoveFunc(playerinfo_t* playerinfo, float height, float var2, float var3);
 void PlayerMoveUpperFunc(playerinfo_t* playerinfo, float fwd, float right, float up);
-void PlayerMoveForce(playerinfo_t* playerinfo, float fwd, float right, float height);
+void PlayerMoveForce(playerinfo_t* playerinfo, float fwd, float right, float up);
 void PlayerMoveALittle(playerinfo_t* playerinfo, float fwd, float right, float up);
-void PlayerPullupHeight(playerinfo_t* playerinfo, float height, float var2, float var3);
+void PlayerPullupHeight(playerinfo_t* playerinfo, float height, float endseq, float nopushdown);
 
 void PlayerActionFlipForward(playerinfo_t* playerinfo, float value);
 void PlayerActionJump(playerinfo_t* playerinfo, float value);
@@ -91,14 +87,6 @@ void PlayerActionJumpBack(playerinfo_t* playerinfo, float value);
 void PlayerActionPushAway(playerinfo_t* playerinfo, float value);
 void PlayerActionShrineEffect(playerinfo_t* playerinfo, float value);
 
-void PlayerActionStrafe(playerinfo_t* playerinfo, float value);
-
-void PlayerActionCheckWalkStrafe(playerinfo_t* playerinfo);
-void PlayerActionCheckWalkUnStrafe(playerinfo_t* playerinfo);
-void PlayerActionCheckWalkBStrafe(playerinfo_t* playerinfo);	//FIXME: remove
-void PlayerActionCheckWalkBUnStrafe(playerinfo_t* playerinfo);
-
-void PlayerActionCheckRunStrafe(playerinfo_t* playerinfo);
 void PlayerActionCheckRunUnStrafe(playerinfo_t* playerinfo);
 
 void PlayerActionCheckDoubleJump(playerinfo_t* playerinfo);
@@ -106,8 +94,6 @@ void PlayerMoveAdd(playerinfo_t* playerinfo);
 void PlayerActionFlip(playerinfo_t* playerinfo, float value);
 void PlayerActionTurn180(playerinfo_t* playerinfo);
 void PlayerActionSetQTEndTime(playerinfo_t* playerinfo, float QTEndTime);
-void PlayerActionCheckCreepForward(playerinfo_t* playerinfo);
-void PlayerActionCheckCreepBack(playerinfo_t* playerinfo);
 void PlayerActionCheckVaultKick(playerinfo_t* playerinfo);
 
 void PlayerActionDrownFloatUp(playerinfo_t* playerinfo);
@@ -124,7 +110,6 @@ void PlayerActionCheckStrafe(playerinfo_t* playerinfo);
 
 void PlayerJumpNudge(playerinfo_t* playerinfo, float fwd, float right, float up);
 
-void PlayerActionCheckBranchWalking(playerinfo_t* playerinfo);
 void PlayerActionCheckBranchRunningStrafe(playerinfo_t* playerinfo);
 
 PLAYER_API void PlayerReleaseRope(playerinfo_t* playerinfo);
@@ -143,7 +128,6 @@ void PlayerSwimMoveFunc(playerinfo_t* playerinfo, float fwd, float right, float 
 void PlayerActionClimbStartSound(playerinfo_t* playerinfo, float value);
 void PlayerPlaySlide(playerinfo_t* playerinfo);
 
-// JWEIER NEW ACTIONS
 void PlayerActionCheckCreep(playerinfo_t* playerinfo);
 void PlayerActionCheckCreepUnStrafe(playerinfo_t* playerinfo);
 
@@ -157,8 +141,5 @@ void PlayerActionCheckWalkBack(playerinfo_t* playerinfo);
 void PlayerActionCheckWalkBackUnStrafe(playerinfo_t* playerinfo);
 
 void PlayerActionCheckRun(playerinfo_t* playerinfo);
-// JWEIER NEW ACTIONS END
 
 extern PLAYER_API void PlayerInterruptAction(playerinfo_t* playerinfo);
-
-#endif //_P_ACTIONS2_H_
