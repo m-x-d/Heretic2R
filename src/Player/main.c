2406b6abc9806d28f452f54ed252fd6ed2b8c7c5
m-x-d <j.maxed@gmail.com>
2024-12-18 13:35:09 +0300

    Cosmetic changes in Player/main.c.

--- src/Player/main.c
+++ src/Player/main.c
@@ -1,3 +1,9 @@
+//
+// main.c
+//
+// Copyright 1998 Raven Software
+//
+
 #include "Player.h"
 #include "p_anim_data.h"
 
@@ -6,19 +12,16 @@ PLAYER_API void P_Init(void)
 	InitItems();
 }
 
-PLAYER_API void P_Shutdown(void)
-{
-}
+PLAYER_API void P_Shutdown(void) { }
 
 PLAYER_API player_export_t GetPlayerAPI(void)
 {
-	player_export_t playerExport;
+	player_export_t pe;
 
-	playerExport.PlayerSeqData=PlayerSeqData;
-	playerExport.PlayerChickenData=PlayerChickenData;
-	playerExport.p_num_items=p_num_items;
-	playerExport.p_itemlist=p_itemlist;
+	pe.PlayerSeqData = PlayerSeqData;
+	pe.PlayerChickenData = PlayerChickenData;
+	pe.p_num_items = p_num_items;
+	pe.p_itemlist = p_itemlist;
 
-	return(playerExport);
+	return pe;
 }
-
