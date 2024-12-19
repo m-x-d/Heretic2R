9f25fab9d904c99b5f16f523f240ee5cdcb1122e
m-x-d <j.maxed@gmail.com>
2024-12-19 16:05:30 +0300

    p_utility: rename 'event_id' arg for naming consistency.

--- src/Player/p_utility.c
+++ src/Player/p_utility.c
@@ -18,12 +18,12 @@ void P_Trace(const playerinfo_t* info, const vec3_t start, const vec3_t mins, co
 }
 
 //mxd. 'origin', 'leveltime', 'entity', 'attenuation' and 'timeofs' args are always the same in Player.dll, so...
-void P_Sound(const playerinfo_t* info, const byte EventId, const int channel, const char* soundname, const float fvol)
+void P_Sound(const playerinfo_t* info, const byte event_id, const int channel, const char* soundname, const float fvol)
 {
 	if (info->isclient)
-		info->CL_Sound(EventId, info->origin, channel, soundname, fvol, ATTN_NORM, 0);
+		info->CL_Sound(event_id, info->origin, channel, soundname, fvol, ATTN_NORM, 0);
 	else
-		info->G_Sound(EventId, info->leveltime, info->self, channel, info->G_SoundIndex(soundname), fvol, ATTN_NORM, 0);
+		info->G_Sound(event_id, info->leveltime, info->self, channel, info->G_SoundIndex(soundname), fvol, ATTN_NORM, 0);
 }
 
 //mxd. 'entity' arg is always the same in Player.dll, so...
--- src/Player/p_utility.h
+++ src/Player/p_utility.h
@@ -9,12 +9,12 @@
 #include "g_Typedef.h"
 
 void P_Trace(const struct playerinfo_s* info, const vec3_t start, const vec3_t mins, const vec3_t maxs, const vec3_t end, trace_t* trace);
-void P_Sound(const playerinfo_t* info, byte EventId, int channel, const char* soundname, float fvol);
+void P_Sound(const playerinfo_t* info, byte event_id, int channel, const char* soundname, float fvol);
 void P_LocalSound(const playerinfo_t* info, const char* soundname);
 
 //mxd. GROSS HACKS, because one can't just pass optional args from one function to another in C...
-#define P_CreateEffect(info, EventId, owner, type, flags, origin, format, ...) \
+#define P_CreateEffect(info, event_id, owner, type, flags, origin, format, ...) \
 	if (info->isclient) \
-		info->CL_CreateEffect(EventId, owner, (ushort)type, flags, origin, format, ##__VA_ARGS__); \
+		info->CL_CreateEffect(event_id, owner, (ushort)type, flags, origin, format, ##__VA_ARGS__); \
 	else \
-		info->G_CreateEffect(EventId, (owner != NULL ? info->G_GetEntityStatePtr((edict_t *)owner) : NULL), type, flags, origin, format, ##__VA_ARGS__)
+		info->G_CreateEffect(event_id, (owner != NULL ? info->G_GetEntityStatePtr((edict_t *)owner) : NULL), type, flags, origin, format, ##__VA_ARGS__)
