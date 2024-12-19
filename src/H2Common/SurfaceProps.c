ceb133fca8294a07588ebb1634bf79cb7331edc8
m-x-d <j.maxed@gmail.com>
2024-12-19 15:46:36 +0300

    H2Common: properly implement GetClientGroundSurfaceMaterialName() logic.

--- src/H2Common/SurfaceProps.c
+++ src/H2Common/SurfaceProps.c
@@ -5,88 +5,18 @@
 //
 
 #include "SurfaceProps.h"
-#include "q_shared.h" //TODO: remove (referenced in p_types.h)
+#include "p_types.h"
 
-//TODO: Why are you like this?..
-char* SurfaceMaterialNames[MAX_SURFACE_MAT_NAMES] =
+//TODO: mxd. Seems to be used in Player.dll only. Move there?
+H2COMMON_API char* GetClientGroundSurfaceMaterialName(const playerinfo_t* info)
 {
-	"gravel", "metal", "stone",	"wood",
-	"gravel", "metal", "stone", "wood",
-	"gravel", "metal", "stone", "wood",
-	"gravel", "metal", "stone", "wood",
-	"gravel", "metal", "stone", "wood",
-	"gravel", "metal", "stone", "wood",
-	"gravel", "metal", "stone", "wood",
-	"gravel", "metal", "stone", "wood",
-	"gravel", "metal", "stone", "wood",
-	"gravel", "metal", "stone", "wood",
-	"gravel", "metal", "stone", "wood",
-	"gravel", "metal", "stone", "wood",
-	"gravel", "metal", "stone", "wood",
-	"gravel", "metal", "stone", "wood",
-	"gravel", "metal", "stone", "wood",
-	"gravel", "metal", "stone", "wood",
-	"gravel", "metal", "stone",	"wood",
-	"gravel", "metal", "stone", "wood",
-	"gravel", "metal", "stone", "wood",
-	"gravel", "metal", "stone", "wood",
-	"gravel", "metal", "stone", "wood",
-	"gravel", "metal", "stone", "wood",
-	"gravel", "metal", "stone", "wood",
-	"gravel", "metal", "stone", "wood",
-	"gravel", "metal", "stone", "wood",
-	"gravel", "metal", "stone", "wood",
-	"gravel", "metal", "stone", "wood",
-	"gravel", "metal", "stone", "wood",
-	"gravel", "metal", "stone", "wood",
-	"gravel", "metal", "stone", "wood",
-	"gravel", "metal", "stone", "wood",
-	"gravel", "metal", "stone", "wood",
-	"gravel", "metal", "stone",	"wood",
-	"gravel", "metal", "stone", "wood",
-	"gravel", "metal", "stone", "wood",
-	"gravel", "metal", "stone", "wood",
-	"gravel", "metal", "stone", "wood",
-	"gravel", "metal", "stone", "wood",
-	"gravel", "metal", "stone", "wood",
-	"gravel", "metal", "stone", "wood",
-	"gravel", "metal", "stone", "wood",
-	"gravel", "metal", "stone", "wood",
-	"gravel", "metal", "stone", "wood",
-	"gravel", "metal", "stone", "wood",
-	"gravel", "metal", "stone", "wood",
-	"gravel", "metal", "stone", "wood",
-	"gravel", "metal", "stone", "wood",
-	"gravel", "metal", "stone", "wood",
-	"gravel", "metal", "stone",	"wood",
-	"gravel", "metal", "stone", "wood",
-	"gravel", "metal", "stone", "wood",
-	"gravel", "metal", "stone", "wood",
-	"gravel", "metal", "stone", "wood",
-	"gravel", "metal", "stone", "wood",
-	"gravel", "metal", "stone", "wood",
-	"gravel", "metal", "stone", "wood",
-	"gravel", "metal", "stone", "wood",
-	"gravel", "metal", "stone", "wood",
-	"gravel", "metal", "stone", "wood",
-	"gravel", "metal", "stone", "wood",
-	"gravel", "metal", "stone", "wood",
-	"gravel", "metal", "stone", "wood",
-	"gravel", "metal", "stone", "wood",
-	"gravel", "metal", "stone", "wood",
-};
+#define NUM_MATERIALS (sizeof(material_names) / sizeof(material_names[0]))
+	static char* material_names[] = { "gravel", "metal", "stone", "wood" };
 
-//TODO: mxd. Seems to be used from Player.dll only. Move there?
-//TODO: mxd. GROSS HACKS (because I don't want to include Player.dll sources just yet...)
-//H2COMMON_API char* GetClientGroundSurfaceMaterialName(const playerinfo_t* playerinfo)
-H2COMMON_API char* GetClientGroundSurfaceMaterialName(const void* playerinfo)
-{
-	//const csurface_t* surf = playerinfo->GroundSurface;
-	csurface_t* surf = *(csurface_t**)((int)playerinfo + 256);
-	if (surf != NULL)
+	if (info->GroundSurface != NULL)
 	{
-		const int mat_index = surf->flags >> 24;
-		return SurfaceMaterialNames[mat_index];
+		const int mat_index = (info->GroundSurface->flags >> 24);
+		return material_names[mat_index % NUM_MATERIALS];
 	}
 
 	return NULL;
--- src/qcommon/SurfaceProps.h
+++ src/qcommon/SurfaceProps.h
@@ -6,14 +6,6 @@
 
 #pragma once
 
-#include "H2Common.h" //mxd. For H2COMMON_API
-////#include "p_types.h"
-//#include "Player.h"
+#include "H2Common.h"
 
-#define MAX_SURFACE_MAT_NAMES 256
-
-//extern char* SurfaceMaterialNames[MAX_SURFACE_MAT_NAMES]; //TODO: mxd. Disabled. Not exported in original H2Common.dll. Doesn't seem to be used anywhere.
-
-//TODO: fix definition
-//extern H2COMMON_API char* GetClientGroundSurfaceMaterialName(const playerinfo_t* playerinfo);
-extern H2COMMON_API char* GetClientGroundSurfaceMaterialName(const void* playerinfo);
+extern H2COMMON_API char* GetClientGroundSurfaceMaterialName(const void* info);
