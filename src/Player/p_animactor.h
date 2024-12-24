0244ad05dc5b715b8bc326ff5f12f02c84684bed
m-x-d <j.maxed@gmail.com>
2024-12-24 19:08:32 +0300

    p_animactor.h cleanup.

--- src/Player/p_animactor.h
+++ src/Player/p_animactor.h
@@ -1,17 +1,13 @@
 //
 // p_animactor.h
 //
-// Heretic II
 // Copyright 1998 Raven Software
 //
 
-#ifndef _P_ANIMACTOR_H_
-#define _P_ANIMACTOR_H_
+#pragma once
 
 #include "p_types.h"
 
-extern PLAYER_API void TurnOffPlayerEffects(playerinfo_t *playerinfo);
-extern PLAYER_API void AnimUpdateFrame(playerinfo_t *playerinfo);
-extern PLAYER_API void PlayerFallingDamage(playerinfo_t *playerinfo);
-
-#endif // _P_ANIMACTOR_H_
+extern PLAYER_API void TurnOffPlayerEffects(playerinfo_t *info);
+extern PLAYER_API void AnimUpdateFrame(playerinfo_t *info);
+extern PLAYER_API void PlayerFallingDamage(playerinfo_t *info);
