005be2b29b208394733c17d69fd1a3a975e415e6
m-x-d <j.maxed@gmail.com>
2024-12-26 18:48:20 +0300

    PlayerIntLand() cleanup. p_ctrl.h cleanup.

--- src/Player/p_ctrl.c
+++ src/Player/p_ctrl.c
@@ -1,261 +1,188 @@
 //
 // p_ctrl.c
 //
-// Heretic II
 // Copyright 1998 Raven Software
 //
 
-#include "player.h"
-#include "p_types.h"
+#include "Player.h"
 #include "p_actions.h"
 #include "p_anims.h"
 #include "p_ctrl.h"
 #include "p_main.h"
-#include "surfaceprops.h"
-#include "random.h"
-#include "fx.h"
+#include "SurfaceProps.h"
+#include "Random.h"
+#include "FX.h"
+#include "p_utility.h"
 
-PLAYER_API void PlayerIntLand(playerinfo_t *playerinfo, float landspeed)
+PLAYER_API void PlayerIntLand(playerinfo_t* info, const float landspeed)
 {
-	char	LandSound[64];
-	char	*material=NULL;
-	qboolean	hardfall=false;
+	qboolean hardfall = false;
 
 	// Initialise the appropriate landing sound.
+	const char* material = GetClientGroundSurfaceMaterialName(info);
 
-	material=GetClientGroundSurfaceMaterialName(playerinfo);
-	
-	strcpy(LandSound,"player/");
+	char land_sound[64];
+	strcpy_s(land_sound, sizeof(land_sound), "player/"); //mxd. strcpy -> strcpy_s.
 
-	if(material)
-	{	
-		// Okay, lame, but if a material is not found, then it ignores the string being set up.
+	// Okay, lame, but if a material is not found, then it ignores the string being set up.
+	if (material != NULL)
+		strcat_s(land_sound, sizeof(land_sound), material); //mxd. strcat -> strcat_s.
 
-		strcat(LandSound, material);
-	}
-
-	if (playerinfo->edictflags & FL_CHICKEN)
+	if (info->edictflags & FL_CHICKEN)
 	{
-		playerinfo->flags &= ~ PLAYER_FLAG_FALLING;
+		info->flags &= ~PLAYER_FLAG_FALLING;
 	}
-	else if (playerinfo->advancedstaff && playerinfo->seqcmd[ACMDU_ATTACK] && 
-			(playerinfo->upperseq == ASEQ_WSWORD_DOWNSTAB || playerinfo->upperseq == ASEQ_WSWORD_STABHOLD))
+	else if (info->advancedstaff && info->seqcmd[ACMDU_ATTACK] && (info->upperseq == ASEQ_WSWORD_DOWNSTAB || info->upperseq == ASEQ_WSWORD_STABHOLD))
 	{
-		if (landspeed < 600.0)
+		PlayerInterruptAction(info);
+
+		if (landspeed > 600.0f) //mxd. Flipped check.
 		{
-			// Land heavy.
-			PlayerInterruptAction(playerinfo);
-			PlayerAnimSetLowerSeq(playerinfo, ASEQ_WSWORD_LOWERPULLOUT);
-			playerinfo->fwdvel = 0.0;
-			playerinfo->velocity[0] *= 0.5;
-			playerinfo->velocity[1] *= 0.5;
-			strcpy(LandSound,"*offwall.wav");
-		}
-		else 
-		{	
 			// Land hard, so roll.
-			PlayerInterruptAction(playerinfo);
-			PlayerAnimSetLowerSeq(playerinfo, ASEQ_ROLLDIVEF_W);
-			playerinfo->fwdvel = 150;
-			strcat(LandSound,"roll.wav");
+			PlayerAnimSetLowerSeq(info, ASEQ_ROLLDIVEF_W);
+			info->fwdvel = 150.0f;
+			strcat_s(land_sound, sizeof(land_sound), "roll.wav"); //mxd. strcat -> strcat_s.
 			hardfall = true;
 		}
-	}
-	else if (playerinfo->seqcmd[ACMDL_CROUCH])
-	{
-		PlayerInterruptAction(playerinfo);
-		if (landspeed > 50.0)
-		{
-			PlayerAnimSetLowerSeq(playerinfo, ASEQ_ROLLDIVEF_W);
-			strcat(LandSound,"roll.wav");
-		}
 		else
 		{
-			PlayerAnimSetLowerSeq(playerinfo, ASEQ_CROUCH_GO);
-			strcat(LandSound,"land1.wav");
+			// Land heavy.
+			PlayerAnimSetLowerSeq(info, ASEQ_WSWORD_LOWERPULLOUT);
+			info->fwdvel = 0.0f;
+			info->velocity[0] *= 0.5f;
+			info->velocity[1] *= 0.5f;
+			strcpy_s(land_sound, sizeof(land_sound), "*offwall.wav"); //mxd. strcpy -> strcpy_s.
 		}
 	}
-	else if(playerinfo->lowerseq == ASEQ_FORWARD_FLIP_L ||
-		playerinfo->lowerseq == ASEQ_FORWARD_FLIP_R || 
-		playerinfo->upperseq == ASEQ_FORWARD_FLIP_L ||
-		playerinfo->upperseq == ASEQ_FORWARD_FLIP_R)
+	else if (info->seqcmd[ACMDL_CROUCH])
 	{
-		PlayerInterruptAction(playerinfo);
-		if(playerinfo->maxs[2] == 25)//fixme!  have a global CROUCHING_MAX_Z and STANDING_MAX_Z
-		{//already ready to stand up
-			PlayerAnimSetLowerSeq(playerinfo, ASEQ_CROUCH_GO);
-			strcat(LandSound,"land1.wav");
-		}
-		else
-		{//still crouch height
-			PlayerAnimSetLowerSeq(playerinfo, ASEQ_ROLL_FROM_FFLIP);
-			strcat(LandSound,"roll.wav");
-		}
-	}
-	else if (playerinfo->seqcmd[ACMDL_WALK_F])
-	{
-		if (landspeed > 600)
-		{	
-			// Can't avoid heavy fall/rolling.
+		PlayerInterruptAction(info);
 
-			PlayerInterruptAction(playerinfo);
-			PlayerAnimSetLowerSeq(playerinfo, ASEQ_ROLLDIVEF_W);
-			strcat(LandSound,"roll.wav");
+		if (landspeed > 50.0f)
+		{
+			PlayerAnimSetLowerSeq(info, ASEQ_ROLLDIVEF_W);
+			strcat_s(land_sound, sizeof(land_sound), "roll.wav"); //mxd. strcat -> strcat_s.
 		}
 		else
-		{	
+		{
+			PlayerAnimSetLowerSeq(info, ASEQ_CROUCH_GO);
+			strcat_s(land_sound, sizeof(land_sound), "land1.wav"); //mxd. strcat -> strcat_s.
+		}
+	}
+	else if (info->lowerseq == ASEQ_FORWARD_FLIP_L || info->lowerseq == ASEQ_FORWARD_FLIP_R || 
+			 info->upperseq == ASEQ_FORWARD_FLIP_L || info->upperseq == ASEQ_FORWARD_FLIP_R)
+	{
+		PlayerInterruptAction(info);
+
+		if (info->maxs[2] == 25.0f) //TODO: have a global CROUCHING_MAX_Z and STANDING_MAX_Z
+		{
+			// Ready to stand up.
+			PlayerAnimSetLowerSeq(info, ASEQ_CROUCH_GO);
+			strcat_s(land_sound, sizeof(land_sound), "land1.wav"); //mxd. strcat -> strcat_s.
+		}
+		else
+		{
+			// Still crouch height.
+			PlayerAnimSetLowerSeq(info, ASEQ_ROLL_FROM_FFLIP);
+			strcat_s(land_sound, sizeof(land_sound), "roll.wav"); //mxd. strcat -> strcat_s.
+		}
+	}
+	else if (info->seqcmd[ACMDL_WALK_F])
+	{
+		if (landspeed > 600.0f)
+		{
+			// Can't avoid heavy fall/rolling.
+			PlayerInterruptAction(info);
+			PlayerAnimSetLowerSeq(info, ASEQ_ROLLDIVEF_W);
+			strcat_s(land_sound, sizeof(land_sound), "roll.wav"); //mxd. strcat -> strcat_s.
+		}
+		else
+		{
 			// Drop straight into a walk.
-
-			PlayerAnimSetLowerSeq(playerinfo, ASEQ_WALKF);
-			strcat(LandSound,"land1.wav");
+			PlayerAnimSetLowerSeq(info, ASEQ_WALKF);
+			strcat_s(land_sound, sizeof(land_sound), "land1.wav"); //mxd. strcat -> strcat_s.
 		}
 	}
-	else if (playerinfo->seqcmd[ACMDL_RUN_F])
+	else if (info->seqcmd[ACMDL_RUN_F])
 	{
-		if (landspeed > 600)
-		{	
+		if (landspeed > 600.0f)
+		{
 			// Can't avoid heavy fall/rolling.
-			PlayerInterruptAction(playerinfo);
-			PlayerAnimSetLowerSeq(playerinfo, ASEQ_ROLLDIVEF_W);
-			strcat(LandSound,"roll.wav");
+			PlayerInterruptAction(info);
+			PlayerAnimSetLowerSeq(info, ASEQ_ROLLDIVEF_W);
+			strcat_s(land_sound, sizeof(land_sound), "roll.wav"); //mxd. strcat -> strcat_s.
 		}
 		else
-		{	
+		{
 			// Drop straight into a run.
-			PlayerAnimSetLowerSeq(playerinfo, ASEQ_RUNF);
-			strcat(LandSound,"land1.wav");
+			PlayerAnimSetLowerSeq(info, ASEQ_RUNF);
+			strcat_s(land_sound, sizeof(land_sound), "land1.wav"); //mxd. strcat -> strcat_s.
 		}
 	}
-	else if (playerinfo->seqcmd[ACMDL_JUMP])
+	else if (info->seqcmd[ACMDL_JUMP])
 	{
-		if (landspeed > 600)
-		{	
+		if (landspeed > 600.0f)
+		{
 			// Can't avoid heavy fall/rolling.
-			PlayerInterruptAction(playerinfo);
-			PlayerAnimSetLowerSeq(playerinfo, ASEQ_ROLLDIVEF_W);
-			strcat(LandSound,"roll.wav");
+			PlayerInterruptAction(info);
+			PlayerAnimSetLowerSeq(info, ASEQ_ROLLDIVEF_W);
+			strcat_s(land_sound, sizeof(land_sound), "roll.wav"); //mxd. strcat -> strcat_s.
 		}
 		else
-		{	
+		{
 			// Drop straight into another jump.
-			PlayerAnimSetLowerSeq(playerinfo, ASEQ_JUMPSTD_GO);
-			strcat(LandSound,"walk");
-			
-			if (irand(0,1))
-				strcat(LandSound, "1.wav");
-			else
-				strcat(LandSound, "2.wav");
+			PlayerAnimSetLowerSeq(info, ASEQ_JUMPSTD_GO);
+			strcat_s(land_sound, sizeof(land_sound), va("walk%i.wav", irand(1, 2))); //mxd. strcat -> strcat_s.
 		}
 	}
 	else
 	{
-		if (landspeed < 50.0)
+		if (landspeed < 50.0f)
 		{
-			PlayerAnimSetLowerSeq(playerinfo, ASEQ_STAND);
-			playerinfo->fwdvel = 150;
-			strcat(LandSound,"shuffle1.wav");
+			PlayerAnimSetLowerSeq(info, ASEQ_STAND);
+			info->fwdvel = 150.0f;
+			strcat_s(land_sound, sizeof(land_sound), "shuffle1.wav"); //mxd. strcat -> strcat_s.
 		}
-		else if (landspeed < 300.0)
+		else if (landspeed < 300.0f)
 		{
 			// Land light.
-
-			PlayerAnimSetLowerSeq(playerinfo, ASEQ_LANDLIGHT);
-			playerinfo->fwdvel = 0.0;
-			playerinfo->velocity[0] *= 0.5;
-			playerinfo->velocity[1] *= 0.5;
-			strcat(LandSound,"land1.wav");
+			PlayerAnimSetLowerSeq(info, ASEQ_LANDLIGHT);
+			info->fwdvel = 0.0f;
+			info->velocity[0] *= 0.5f;
+			info->velocity[1] *= 0.5f;
+			strcat_s(land_sound, sizeof(land_sound), "land1.wav"); //mxd. strcat -> strcat_s.
 		}
-		else if (landspeed < 600.0)
+		else if (landspeed < 600.0f)
 		{
 			// Land heavy.
-
-			PlayerAnimSetLowerSeq(playerinfo, ASEQ_LANDHEAVY);
-			playerinfo->fwdvel = 0.0;
-			playerinfo->velocity[0] *= 0.5;
-			playerinfo->velocity[1] *= 0.5;
-			strcat(LandSound,"land2.wav");
+			PlayerAnimSetLowerSeq(info, ASEQ_LANDHEAVY);
+			info->fwdvel = 0.0f;
+			info->velocity[0] *= 0.5f;
+			info->velocity[1] *= 0.5f;
+			strcat_s(land_sound, sizeof(land_sound), "land2.wav"); //mxd. strcat -> strcat_s.
 		}
-		else 
-		{	
+		else
+		{
 			// Land hard, so roll.
-
-			PlayerInterruptAction(playerinfo);
-			PlayerAnimSetLowerSeq(playerinfo, ASEQ_ROLLDIVEF_W);
-			playerinfo->fwdvel = 150;
-			strcat(LandSound,"roll.wav");
+			PlayerInterruptAction(info);
+			PlayerAnimSetLowerSeq(info, ASEQ_ROLLDIVEF_W);
+			info->fwdvel = 150.0f;
+			strcat_s(land_sound, sizeof(land_sound), "roll.wav"); //mxd. strcat -> strcat_s.
 			hardfall = true;
 		}
 	}
 
-		// Play the appropriate landing sound.
+	// Play the appropriate landing sound.
+	if (material != NULL)
+		P_Sound(info, SND_PRED_ID51, CHAN_VOICE, land_sound, 1.0f); //mxd
 
-	if(material)
-	{
-		if(playerinfo->isclient)
-		{
-			playerinfo->CL_Sound(SND_PRED_ID51,
-								 playerinfo->origin,
-								 CHAN_VOICE,
-								 LandSound,
-								 1,
-								 ATTN_NORM,
-								 0);
-		}
-		else
-		{
-			playerinfo->G_Sound(SND_PRED_ID51,
-								playerinfo->leveltime,
-								playerinfo->self,
-								CHAN_VOICE,
-								playerinfo->G_SoundIndex(LandSound),
-								1,
-								ATTN_NORM,
-								0);
-		}
-	}
-
+	// Play grunt sound?
 	if (hardfall)
-	{	// Grunt.
-		if(playerinfo->isclient)
-		{
-			playerinfo->CL_Sound(SND_PRED_ID52,
-								 playerinfo->origin,
-								 CHAN_FOOTSTEP,
-								 "*fall.wav",
-								 1,
-								 ATTN_NORM,
-								 0);
-		}
-		else
-		{
-			playerinfo->G_Sound(SND_PRED_ID52,
-								playerinfo->leveltime,
-								playerinfo->self,
-								CHAN_FOOTSTEP,
-								playerinfo->G_SoundIndex("*fall.wav"),
-								1,
-								ATTN_NORM,
-								0);
-		}
-	}
+		P_Sound(info, SND_PRED_ID52, CHAN_FOOTSTEP, "*fall.wav", 1.0f); //mxd
 
-	playerinfo->flags &= ~ PLAYER_FLAG_FALLING;
+	info->flags &= ~PLAYER_FLAG_FALLING;
 
 	// Don't do dust in the water!
-
-	if(playerinfo->waterlevel==0)
-		if(!playerinfo->isclient)
-			playerinfo->G_CreateEffect(EFFECT_PRED_ID15,
-									   playerinfo->G_GetEntityStatePtr(playerinfo->self),
-									   FX_DUST_PUFF,
-									   CEF_OWNERS_ORIGIN, 
-									   playerinfo->origin,
-									   "");
-		else
-			playerinfo->CL_CreateEffect(EFFECT_PRED_ID15,
-										playerinfo->self,
-										FX_DUST_PUFF,
-										CEF_OWNERS_ORIGIN, 
-										playerinfo->origin,
-										"");
+	if (info->waterlevel == 0)
+		P_CreateEffect(info, EFFECT_PRED_ID15, info->self, FX_DUST_PUFF, CEF_OWNERS_ORIGIN, info->origin, ""); //mxd
 }
--- src/Player/p_ctrl.h
+++ src/Player/p_ctrl.h
@@ -1,15 +1,9 @@
 //
-// p_ctrl2.h
+// p_ctrl.h
 //
-// Heretic II
 // Copyright 1998 Raven Software
 //
 
-#ifndef _P_CTRL2_H_
-#define _P_CTRL2_H_
+#pragma once
 
-#include "p_actions.h"
-
-extern PLAYER_API void PlayerIntLand(playerinfo_t *playerinfo_t, float landspeed);
-
-#endif //_P_CTRL2_H_
+extern PLAYER_API void PlayerIntLand(playerinfo_t* info, float landspeed);
