c2e61dcddcf9c71ce03b1da999db8104fa324ba8
m-x-d <j.maxed@gmail.com>
2024-12-27 17:46:58 +0300

    p_weapon.h: cleanup, update function declarations to match definitions in p_weapon.c.

--- src/Player/p_weapon.h
+++ src/Player/p_weapon.h
@@ -1,28 +1,18 @@
 //
-// p_weapon2.h
+// p_weapon.h
 //
-// Heretic II
 // Copyright 1998 Raven Software
 //
 
-#ifndef P_WEAPON2_H
-#define P_WEAPON2_H
+#pragma once
 
 #include "p_types.h"
 
-// ************************************************************************************************
-// Weapon_XXX
-// ----------
-// Applicable to all player-weapon types.
-// ************************************************************************************************
-
-PLAYER_API void Weapon_Ready(playerinfo_t *playerinfo,gitem_t *Weapon);
-PLAYER_API void Weapon_EquipSpell(playerinfo_t *playerinfo,gitem_t *Weapon);
-PLAYER_API void Weapon_EquipSwordStaff(playerinfo_t *playerinfo,gitem_t *Weapon);
-PLAYER_API void Weapon_EquipHellStaff(playerinfo_t *playerinfo,gitem_t *Weapon);
-PLAYER_API void Weapon_EquipBow(playerinfo_t *playerinfo,gitem_t *Weapon);
-PLAYER_API void Weapon_EquipArmor(playerinfo_t *playerinfo, gitem_t *Weapon);
-PLAYER_API int Weapon_CurrentShotsLeft(playerinfo_t *playerinfo);
-PLAYER_API int Defence_CurrentShotsLeft(playerinfo_t *playerinfo, int intent);
-
-#endif // P_WEAPON_H
+PLAYER_API void Weapon_Ready(playerinfo_t* info, gitem_t* weapon);
+PLAYER_API void Weapon_EquipSpell(playerinfo_t* info, gitem_t* weapon);
+PLAYER_API void Weapon_EquipSwordStaff(playerinfo_t* info, gitem_t* weapon);
+PLAYER_API void Weapon_EquipHellStaff(playerinfo_t* info, gitem_t* weapon);
+PLAYER_API void Weapon_EquipBow(playerinfo_t* info, gitem_t* weapon);
+PLAYER_API void Weapon_EquipArmor(playerinfo_t* info, gitem_t* weapon);
+PLAYER_API int Weapon_CurrentShotsLeft(const playerinfo_t* info);
+PLAYER_API int Defence_CurrentShotsLeft(const playerinfo_t* info, int intent);
