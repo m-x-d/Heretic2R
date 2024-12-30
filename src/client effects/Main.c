b00036b07f212f85ba7cc68101af2bd7d3b93828
m-x-d <j.maxed@gmail.com>
2024-12-30 17:26:31 +0300

    AddServerEntities() cleanup.

--- src/client effects/Main.c
+++ src/client effects/Main.c
@@ -433,34 +433,19 @@ static void RemoveEffectsFromCent(centity_t* cent)
 		RemoveOwnedEffectList(cent);
 }
 
-/*
-===============
-AddServerEntities
-
-===============
-*/
-
-void AddServerEntities(frame_t *frame)
+static void AddServerEntities(const frame_t* frame)
 {
-	entity_t			*ent;
-	static entity_t		sv_ents[MAX_SERVER_ENTITIES];
-	static fmnodeinfo_t	sv_ents_fmnodeinfos[MAX_SERVER_ENTITIES][MAX_FM_MESH_NODES];
-	entity_state_t		*s1;
-	float				autorotate, macerotate;
-	int					i;
-	int					pnum;
-	centity_t			*cent;
-	int					autoanim;
-	int					effects, renderfx;
-	int					numEntsToAdd;
-	vec3_t				dist;
-	int					maxclients = atoi(fxi.cl->configstrings[CS_MAXCLIENTS]);
-	clientinfo_t		*ci;
-	int					clientnum;
-	qboolean			isPredictedPlayer;
+	static entity_t sv_ents[MAX_SERVER_ENTITIES];
+	static fmnodeinfo_t sv_ents_fmnodeinfos[MAX_SERVER_ENTITIES][MAX_FM_MESH_NODES];
+
+	int effects;
+	int renderfx;
+	int clientnum;
+	qboolean is_predicted_player;
+
+	const int maxclients = Q_atoi(fxi.cl->configstrings[CS_MAXCLIENTS]);
 
 	// Have to do this here, since the init is loaded once, and the graphics dll might be reloaded.
-
 	ref_soft = (strcmp("soft", vid_ref->string)) ? 0 : 1;
 
 	fxi.cl->PIV = 0;
@@ -469,51 +454,43 @@ void AddServerEntities(frame_t *frame)
 	num_owned_inview = 0;
 
 	// Bonus items rotate at a fixed rate.
-
-	autorotate = anglemod(fxi.cl->time / 10.0);
-	macerotate = anglemod(fxi.cl->time / 700.0);
+	const float autorotate = anglemod((float)fxi.cl->time / 10.0f);
+	const float macerotate = anglemod((float)fxi.cl->time / 700.0f);
 
 	// Brush models can auto animate their frames.
+	const int autoanim = fxi.cl->time / 500;
 
-	autoanim = 2*fxi.cl->time/1000;
+	memset(sv_ents, 0, sizeof(sv_ents));
 
-	memset (sv_ents, 0, sizeof(sv_ents));
-
-	numEntsToAdd = frame->num_entities;
-
-	if(numEntsToAdd > MAX_SERVER_ENTITIES)
+	int num_ents_to_add = frame->num_entities;
+	if (num_ents_to_add > MAX_SERVER_ENTITIES)
 	{
-		Com_Printf("Overflow:  Too many (%d : %d) server entities to add to view\n", numEntsToAdd, MAX_SERVER_ENTITIES);
-		numEntsToAdd = MAX_SERVER_ENTITIES;
+		Com_Printf("Overflow:  Too many (%d : %d) server entities to add to view\n", num_ents_to_add, MAX_SERVER_ENTITIES);
+		num_ents_to_add = MAX_SERVER_ENTITIES;
 	}
 
-	for(pnum = 0, ent = sv_ents; pnum<numEntsToAdd; ++pnum)
+	entity_t* ent = sv_ents;
+	for (int pnum = 0; pnum < num_ents_to_add; pnum++)
 	{
-		s1 = fxi.parse_entities + ((frame->parse_entities+pnum)&(MAX_PARSE_ENTITIES-1));
+		entity_state_t* s1 = &fxi.parse_entities[(frame->parse_entities + pnum) & (MAX_PARSE_ENTITIES - 1)];
+		centity_t* cent = &fxi.server_entities[s1->number];
 
-		cent = fxi.server_entities + s1->number;
+		cent->s1 = s1;
 
-		cent->s1=s1;
-
-		if((fxi.cl_predict->value)&&(s1->number==fxi.cl->playernum+1))
-		{
-			// We are dealing with the client's model under prediction.
-
-			isPredictedPlayer=true;
-		}
+		if ((int)fxi.cl_predict->value && s1->number == fxi.cl->playernum + 1)
+			is_predicted_player = true; // We are dealing with the client's model under prediction.
 		else
-		{
-			// We are dealing with a non predicted model (i.e. everything except the client's model).
-
-			isPredictedPlayer=false;
-		}
+			is_predicted_player = false; // We are dealing with a non predicted model (i.e. everything except the client's model).
 
 		// Setup effects, renderfx, skinnum and clientnum stuff.
-		
-		if(isPredictedPlayer)
+		if (is_predicted_player)
 		{
-			cent->current.effects = effects = fxi.predictinfo->effects;
-			cent->current.renderfx = renderfx = fxi.predictinfo->renderfx;
+			cent->current.effects = fxi.predictinfo->effects;
+			effects = fxi.predictinfo->effects;
+
+			cent->current.renderfx = fxi.predictinfo->renderfx;
+			renderfx = fxi.predictinfo->renderfx;
+
 			ent->skinnum = fxi.predictinfo->skinnum;
 			clientnum = fxi.predictinfo->clientnum;
 		}
@@ -526,7 +503,6 @@ void AddServerEntities(frame_t *frame)
 		}
 
 		// Set frame.
-
 		if (effects & EF_ANIM_ALL)
 			ent->frame = autoanim;
 		else if (effects & EF_ANIM_ALLFAST)
@@ -535,37 +511,30 @@ void AddServerEntities(frame_t *frame)
 			ent->frame = s1->frame;
 
 		// Handle flex-model nodes.
-
 		ent->fmnodeinfo = sv_ents_fmnodeinfos[pnum];
-		
-		if(isPredictedPlayer)
-		{
-			memcpy(ent->fmnodeinfo,fxi.predictinfo->fmnodeinfo,sizeof(s1->fmnodeinfo));
-		}
-		else
-		{
-			memcpy(ent->fmnodeinfo,s1->fmnodeinfo,sizeof(s1->fmnodeinfo));
-		}
 
-		// What's going on here?
-		
-		if(isPredictedPlayer)
+		if (is_predicted_player)
+			memcpy(ent->fmnodeinfo, fxi.predictinfo->fmnodeinfo, sizeof(s1->fmnodeinfo));
+		else
+			memcpy(ent->fmnodeinfo, s1->fmnodeinfo, sizeof(s1->fmnodeinfo));
+
+		//TODO: What's going on here?
+		if (is_predicted_player)
 		{
 			// Deal with predicted origin.
-
-			ent->backlerp = 1.0 - fxi.predictinfo->playerLerp;
+			ent->backlerp = 1.0f - fxi.predictinfo->playerLerp;
 			VectorCopy(cent->origin, ent->origin);
 		}
 		else
 		{
 			ent->oldframe = cent->prev.frame;
-			ent->backlerp = 1.0 - fxi.cl->lerpfrac;
+			ent->backlerp = 1.0f - fxi.cl->lerpfrac;
 
 			// Interpolate origin.
-
+			vec3_t dist;
 			VectorSubtract(cent->current.origin, cent->prev.origin, dist);
 
-			if(DotProduct(dist, dist) <= cl_lerpdist2->value)
+			if (DotProduct(dist, dist) <= cl_lerpdist2->value)
 				VectorMA(cent->prev.origin, 1.0f - ent->backlerp, dist, ent->origin);
 			else
 				VectorCopy(cent->current.origin, ent->origin);
@@ -573,109 +542,85 @@ void AddServerEntities(frame_t *frame)
 			VectorCopy(ent->origin, cent->origin);
 		}
 
-	  	VectorCopy(ent->origin, ent->oldorigin);
+		VectorCopy(ent->origin, ent->oldorigin);
 		VectorCopy(cent->origin, cent->lerp_origin);
 
 		// Set model. 
-
 		if (s1->modelindex == 255)
-		{	
+		{
 			// Use custom model and skin for player.
-
-			ci = &fxi.cl->clientinfo[clientnum];
+			clientinfo_t* ci = &fxi.cl->clientinfo[clientnum];
 
 			ent->model = ci->model;
-			if (ent->skinnum < SKIN_MAX)
-				ent->skin = ci->skin[ent->skinnum];
-			else
-				ent->skin = ci->skin[0];
 
-			// To allow for mutliple skins on various parts, I'm going to send across a pointer to the whole skin array.
+			const int skinnum = (ent->skinnum < SKIN_MAX ? ent->skinnum : 0);
+			ent->skin = ci->skin[skinnum];
 
+			// To allow for multiple skins on various parts, I'm going to send across a pointer to the whole skin array.
 			ent->skins = &ci->skin[0];
 
-			if (!ent->skin || !ent->model)
+			if (ent->skin == NULL || ent->model == NULL)
 			{
 				ent->model = fxi.cl->baseclientinfo.model;
-				if (ent->skinnum < SKIN_MAX)
-					ent->skin = fxi.cl->baseclientinfo.skin[ent->skinnum];
-				else
-					ent->skin = fxi.cl->baseclientinfo.skin[0];
+				ent->skin = fxi.cl->baseclientinfo.skin[skinnum];
 			}
 		}
 		else
 		{
 			ent->model = &fxi.cl->model_draw[s1->modelindex];
-			ent->skin = NULL;			// No custom skin.
+			ent->skin = NULL; // No custom skin.
 		}
 
 		ent->scale = s1->scale;
-
-		if(s1->color.c)
-		{
-			ent->color.c = s1->color.c;
-		}
-		else
-		{
-			ent->color.c = 0xFFFFFFFF;
-		}
+		ent->color.c = (s1->color.c != 0 ? s1->color.c : 0xFFFFFFFF);
 
 		ent->absLight.r = s1->absLight.r;
 		ent->absLight.g = s1->absLight.g;
 		ent->absLight.b = s1->absLight.b;
 
 		// Set render effects (fullbright, translucent, etc).
-
 		ent->flags = renderfx;
 
 		// Calculate angles.
-
 		if (effects & EF_MACE_ROTATE)
-		{	
+		{
 			// Some bonus items auto-rotate.
-
 			ent->angles[0] = macerotate * 2;
 			ent->angles[1] = macerotate;
 			ent->angles[2] = 0;
 		}
 		else if (effects & EF_ROTATE)
-		{	
+		{
 			// Some bonus items auto-rotate.
-
 			ent->angles[0] = 0;
 			ent->angles[1] = autorotate;
 			ent->angles[2] = 0;
 		}
 		else
-		{	
+		{
 			// Interpolate angles.
-
-			float	a1, a2;
-
-			if(isPredictedPlayer)
+			if (is_predicted_player)
 			{
-				// The corect angle values have already been generated by prediction and written
+				// The correct angle values have already been generated by prediction and written
 				// into the client's predictinfo_t structure.
-
-				for (i=0 ; i<3 ; i++)
+				for (int i = 0; i < 3; i++)
 				{
 					cent->current.angles[i] = fxi.predictinfo->currAngles[i];
 					cent->prev.angles[i] = fxi.predictinfo->prevAngles[i];
 
-					a1 = cent->current.angles[i];
-					a2 = cent->prev.angles[i];
-					ent->angles[i] = LerpAngle (a2, a1, fxi.predictinfo->playerLerp);
+					const float a1 = cent->current.angles[i];
+					const float a2 = cent->prev.angles[i];
+					ent->angles[i] = LerpAngle(a2, a1, fxi.predictinfo->playerLerp);
 				}
 			}
 			else
 			{
 				// Get the angle values from the usual source, for all entities, including the player.
-
-				for (i=0 ; i<3 ; i++)
+				for (int i = 0; i < 3; i++)
 				{
-					a1 = cent->current.angles[i];
-					a2 = cent->prev.angles[i];
-					ent->angles[i] = LerpAngle (a2, a1, fxi.cl->lerpfrac);
+					const float a1 = cent->current.angles[i];
+					const float a2 = cent->prev.angles[i];
+					ent->angles[i] = LerpAngle(a2, a1, fxi.cl->lerpfrac);
 				}
 			}
 
@@ -683,145 +628,122 @@ void AddServerEntities(frame_t *frame)
 			VectorCopy(ent->angles, cent->lerp_angles);
 		}
 
-		if(effects & EF_JOINTED)
-		{
-			ent->rootJoint = s1->rootJoint;
-		}
-		else
-		{
-			ent->rootJoint = NULL_ROOT_JOINT;
-		}
+		ent->rootJoint = ((effects & EF_JOINTED) ? s1->rootJoint : NULL_ROOT_JOINT);
 
-		if(isPredictedPlayer)
+		if (is_predicted_player)
 		{
-			// The corect frame and swapframe values have already been generated by prediction
+			// The correct frame and swapframe values have already been generated by prediction
 			// and written into the client's predictinfo_t structure.
-
-			cent->prev.frame = fxi.predictinfo->prevFrame;
-			cent->current.frame = fxi.predictinfo->currFrame;
-			cent->prev.swapFrame = fxi.predictinfo->prevSwapFrame;
-			cent->current.swapFrame = fxi.predictinfo->currSwapFrame;
+			cent->prev.frame = (short)fxi.predictinfo->prevFrame;
+			cent->current.frame = (short)fxi.predictinfo->currFrame;
+			cent->prev.swapFrame = (short)fxi.predictinfo->prevSwapFrame;
+			cent->current.swapFrame = (short)fxi.predictinfo->currSwapFrame;
 
 			ent->oldframe = cent->prev.frame;
 			ent->frame = cent->current.frame;
 
-			if((effects & EF_SWAPFRAME)&&(cent->current.swapFrame!=cent->current.frame))
+			if ((effects & EF_SWAPFRAME) && cent->current.swapFrame != cent->current.frame)
 			{
-				ent->swapFrame=cent->current.swapFrame;
-				ent->oldSwapFrame=cent->prev.swapFrame;
+				ent->swapFrame = cent->current.swapFrame;
+				ent->oldSwapFrame = cent->prev.swapFrame;
 
-				// Yuck... but need to stop crashes for the demo.
-
-				if(ent->oldSwapFrame==NO_SWAP_FRAME)
-					ent->oldSwapFrame=ent->oldframe;
+				// Yuck... but need to stop crashes for the demo. //TODO: no longer needed?
+				if (ent->oldSwapFrame == NO_SWAP_FRAME)
+					ent->oldSwapFrame = ent->oldframe;
 			}
 			else
 			{
-				ent->swapFrame=cent->prev.swapFrame=NO_SWAP_FRAME;
+				ent->swapFrame = NO_SWAP_FRAME;
+				cent->prev.swapFrame = NO_SWAP_FRAME;
 			}
 		}
 		else
 		{
-			// Always get the frame and swapframe values from the usual source, for all entities,
-			// including the player.
-
-			if((effects & EF_SWAPFRAME) && (s1->swapFrame != s1->frame))
+			// Always get the frame and swapframe values from the usual source, for all entities, including the player.
+			if ((effects & EF_SWAPFRAME) && s1->swapFrame != s1->frame)
 			{
 				ent->swapFrame = s1->swapFrame;
 				ent->oldSwapFrame = cent->prev.swapFrame;
 
-				// Yuck... but need to stop crashes for the demo.
-
-				if(ent->oldSwapFrame==NO_SWAP_FRAME)
-					ent->oldSwapFrame=ent->oldframe;
+				// Yuck... but need to stop crashes for the demo. //TODO: no longer needed?
+				if (ent->oldSwapFrame == NO_SWAP_FRAME)
+					ent->oldSwapFrame = ent->oldframe;
 			}
 			else
 			{
-				ent->swapFrame = cent->prev.swapFrame = NO_SWAP_FRAME;
+				ent->swapFrame = NO_SWAP_FRAME;
+				cent->prev.swapFrame = NO_SWAP_FRAME;
 			}
-
 		}
 
 		ent->referenceInfo = cent->referenceInfo;
 
-		if(cent->current.clientEffects.numEffects)
+		if (cent->current.clientEffects.numEffects)
 		{
-			*(fxi.cl_effectpredict)=0;
+			*fxi.cl_effectpredict = 0;
 			ParseEffects(cent);
 		}
 
-		if((s1->number > 0) && (s1->number <= maxclients))
+		if (s1->number > 0 && s1->number <= maxclients)
 		{
 			fxi.cl->PIV |= 1 << (s1->number - 1);
 			VectorCopy(ent->origin, fxi.cl->clientinfo[s1->number - 1].origin);
 		}
 
-		// Add player's packet_entity_t to refresh list of entity_t's and save the entity_t pointer
-		// in PlayerEntPtr.
-
-		if(s1->number==fxi.cl->playernum+1)
+		// Add player's packet_entity_t to refresh list of entity_t's and save the entity_t pointer	in PlayerEntPtr.
+		if (s1->number == fxi.cl->playernum + 1)
 		{
-			if((fxi.cl_predict->value)&&(fxi.clientPredEffects->numEffects))
+			if ((int)fxi.cl_predict->value && fxi.clientPredEffects->numEffects > 0)
 			{
-				*(fxi.cl_effectpredict)=1;
+				*fxi.cl_effectpredict = 1;
 				ParseEffects(cent);
-				*(fxi.cl_effectpredict)=0;
+				*fxi.cl_effectpredict = 0;
 			}
 
 			// This is the player.
-
-			if (*(fxi.PlayerAlpha) < 1.0)
+			if (*fxi.PlayerAlpha < 1.0f)
 			{
-				ent->color.a = (byte)(*(fxi.PlayerAlpha) * (float)(ent->color.a));
+				ent->color.a = (byte)(*fxi.PlayerAlpha * (float)ent->color.a);
 				ent->flags |= RF_TRANSLUCENT;
 			}
 			else
 			{
-				// Color has already been copied from s1
-//				ent->color.a = 255;
+				// Color has already been copied from s1.
 				ent->flags &= ~RF_TRANSLUCENT;
 			}
 
-			if(s1->modelindex)
-			{
+			if (s1->modelindex > 0)
 				AddEntityToView(ent);
-			}
 
-			*(fxi.PlayerEntPtr) = ent;
+			*fxi.PlayerEntPtr = ent;
 
 			// So client effects can play with owners entity.
-
 			cent->entity = ent;
 
-			if(cent->effects && !(effects&(EF_DISABLE_ALL_CFX|EF_ALWAYS_ADD_EFFECTS)))
-			{
+			if (cent->effects && !(effects & (EF_DISABLE_ALL_CFX | EF_ALWAYS_ADD_EFFECTS)))
 				num_owned_inview += AddEffectsToView(&cent->effects, cent);
+
+			ent++;
+		}
+		else
+		{
+			// Cull (any eligible) entire models before they get rendered
+			if (s1->modelindex > 0)
+			{
+				vec3_t dir;
+
+				VectorSubtract(ent->origin, fxi.cl->refdef.vieworg, dir);
+
+				ent->depth = VectorNormalize(dir);
+
+				AddEntityToView(ent);
+				cent->entity = ent; // So client effects can play with owners entity.
+
+				ent++;
 			}
 
-			++ent;
-			continue;
-		}
-
-		// Cull (any elegible) entire models before they get rendered
-		// Don't ask me--I just commented what this does - MW).
-
-		if(s1->modelindex)
-		{
-			vec3_t dir;
-
-			VectorSubtract(ent->origin, fxi.cl->refdef.vieworg, dir);
-
-			ent->depth = VectorNormalize(dir);
-
-			AddEntityToView(ent);
-			cent->entity = ent;					// So client effects can play with owners entity
-
-			++ent;
-		}
-
-		if(cent->effects && !(effects&(EF_DISABLE_ALL_CFX|EF_ALWAYS_ADD_EFFECTS)))
-		{
-			num_owned_inview += AddEffectsToView(&cent->effects, cent);
+			if (cent->effects != NULL && !(effects & (EF_DISABLE_ALL_CFX | EF_ALWAYS_ADD_EFFECTS)))
+				num_owned_inview += AddEffectsToView(&cent->effects, cent);
 		}
 	}
 }
