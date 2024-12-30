be69f3f28878e4e2feb7933ce9506bbf2c619bde
m-x-d <j.maxed@gmail.com>
2024-12-30 17:00:07 +0300

    Add LightStyles.h.

---
+++ src/client effects/LightStyles.h
@@ -0,0 +1,12 @@
+//
+// LightStyles.h -- added to avoid unnecessary forward declarations...
+//
+// Copyright 2024 m-x-d
+//
+
+#pragma once
+
+extern void CL_SetLightstyle(int index);
+extern void CL_AddLightStyles(void);
+extern void CL_ClearLightStyles(void);
+extern void CL_RunLightStyles(void);
