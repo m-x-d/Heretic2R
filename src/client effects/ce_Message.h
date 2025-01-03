8548bcbfabfa4b5e02af39a8c658503b4605684d
m-x-d <j.maxed@gmail.com>
2025-01-03 17:46:04 +0300

    ce_Message.h cleanup.

--- src/client effects/ce_Message.h
+++ src/client effects/ce_Message.h
@@ -1,11 +1,16 @@
-#ifndef CE_MESSAGE_H
-#define CE_MESSAGE_H
+//
+// ce_Message.h
+//
+// Copyright 1998 Raven Software
+//
+
+#pragma once
 
 #include "SinglyLinkedList.h"
 
 typedef enum CE_MsgID_e
 {
-	MSG_COLLISION = 0, // (parm1) trace_t *trace -- the trace matching the collision, valid only on the frame of collision
+	MSG_COLLISION = 0, // (parm1) trace_t* trace -- the trace matching the collision, valid only on the frame of collision.
 	NUM_MESSAGES
 } CE_MsgID_t;
 
@@ -15,15 +20,13 @@ typedef struct Message_s
 	SinglyLinkedList_t parms;
 } CE_Message_t;
 
-typedef void (*CE_MessageHandler_t)(struct client_entity_s *self, CE_Message_t *msg);
-typedef void (*CE_MsgReceiver_t)(struct client_entity_s *self, CE_Message_t *msg);
+typedef void (*CE_MessageHandler_t)(struct client_entity_s* self, CE_Message_t* msg);
+typedef void (*CE_MsgReceiver_t)(struct client_entity_s* self, CE_Message_t* msg);
 
-void InitMsgMngr();
-void ReleaseMsgMngr();
+extern void InitMsgMngr(void);
+extern void ReleaseMsgMngr(void);
 
-void QPostMessage(struct client_entity_s *to, CE_MsgID_t ID, char *format, ...);
-int ParseMsgParms(CE_Message_t *this, char *format, ...);
-void ProcessMessages(struct client_entity_s *this);
-void ClearMessageQueue(struct client_entity_s *this);
-
-#endif
+extern void QPostMessage(struct client_entity_s* to, CE_MsgID_t ID, char* format, ...);
+extern int ParseMsgParms(CE_Message_t* this, char* format, ...);
+extern void ProcessMessages(struct client_entity_s* this);
+extern void ClearMessageQueue(struct client_entity_s* this);
