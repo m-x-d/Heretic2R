46f227caf3c614541338a49bcabc0dffdd2dfb6e
m-x-d <j.maxed@gmail.com>
2025-01-10 16:24:33 +0300

    Utilities.h cleanup.

--- src/client effects/Utilities.h
+++ src/client effects/Utilities.h
@@ -1,45 +1,35 @@
-#ifndef UTILITIES_H
-#define UTILITIES_H
+//
+// Utilities.h
+//
+// Copyright 1998 Raven Software
+//
+
+#pragma once
 
 #include "Client Entities.h"
 
-//------------------------------------------------------------------
-// Update funcs
-//------------------------------------------------------------------
+// Update functions.
+qboolean RemoveSelfAI(client_entity_t* this, centity_t* owner); // Set by default in ClientEntity_new.
+qboolean KeepSelfAI(client_entity_t* this, centity_t* owner);
+qboolean AttemptRemoveSelf(client_entity_t* self, centity_t* owner);
 
-qboolean RemoveSelfAI(client_entity_t *this, centity_t *owner);		// set by default in ClientEntity_new
-qboolean KeepSelfAI(client_entity_t *this, centity_t *owner);
-qboolean AttemptRemoveSelf(client_entity_t *self, centity_t *owner);
+// AddToView functions.
+qboolean LinkedEntityUpdatePlacement(client_entity_t* current, const centity_t* owner);
+qboolean OffsetLinkedEntityUpdatePlacement(client_entity_t* current, const centity_t* owner);
+qboolean ReferenceLinkedEntityUpdatePlacement(struct client_entity_s* self, const centity_t* owner);
 
-//------------------------------------------------------------------
-// AddToView funcs
-//------------------------------------------------------------------
+// Message response helper functions.
+void BecomeStatic(client_entity_t* self);
 
-qboolean LinkedEntityUpdatePlacement(client_entity_t *current, centity_t *owner);
-qboolean OffsetLinkedEntityUpdatePlacement(client_entity_t *current, centity_t *owner);
-qboolean ReferenceLinkedEntityUpdatePlacement(struct client_entity_s *self, centity_t *owner);
-
-//------------------------------------------------------------------
-// Message Response Helper Funcs
-//------------------------------------------------------------------
-
-void BecomeStatic(client_entity_t *self);
-
-//------------------------------------------------------------------
-// Physics Funcs
-//------------------------------------------------------------------
-
-int GetSolidDist(vec3_t origin, vec_t radius, float maxdist, vec_t *dist);
-int GetFallTime(vec3_t origin, vec_t veloc, vec_t accel, vec_t radius, float, trace_t *);
-void AdvanceParticle(struct client_particle_s *p, int ms);
-int GetWaterNormal(vec3_t, float, float, vec3_t, vec_t *);
-qboolean Physics_MoveEnt(client_entity_t *self, float d_time, float d_time2, trace_t *trace);
+// Physics functions.
+int GetSolidDist(vec3_t origin, float radius, float maxdist, float* dist);
+int GetFallTime(vec3_t origin, float velocity, float acceleration, float radius, float, trace_t* trace);
+void AdvanceParticle(struct client_particle_s* p, int ms);
+int GetWaterNormal(vec3_t origin, float radius, float maxdist, vec3_t normal, float* dist);
+qboolean Physics_MoveEnt(client_entity_t* self, float d_time, float d_time2, trace_t* trace);
 
 int GetScaledCount(int count, float refdepend);
-float GetGravity();
+float GetGravity(void);
 
-qboolean ReferencesInitialized(centity_t *owner);
-qboolean RefPointsValid(centity_t *owner);
-
-
-#endif
+qboolean ReferencesInitialized(const centity_t* owner);
+qboolean RefPointsValid(const centity_t* owner);
