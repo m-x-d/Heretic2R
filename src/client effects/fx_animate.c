91a660044530a23dc6d5aaf2aab644c12cb5f263
m-x-d <j.maxed@gmail.com>
2025-01-11 16:31:19 +0300

    fx_animate.c: cleanup includes. Rename FXAnimModel to fx_anim_models.

--- src/client effects/fx_animate.c
+++ src/client effects/fx_animate.c
@@ -1,49 +1,39 @@
 //
 // fx_animate.c
 //
-// Heretic II
 // Copyright 1998 Raven Software
 //
 
-#include "ce_DefaultMessageHandler.h"
 #include "Client Effects.h"
-#include "Client Entities.h"
-#include "Particle.h"
-#include "ResourceManager.h"
-#include "FX.h"
 #include "Vector.h"
 #include "Utilities.h"
-#include "Angles.h"
-#include "random.h"
+#include "Random.h"
 
 typedef struct FXAnimModel
 {
-	char	*ModelName;
-	struct model_s *model;
-	float	radius;
-	float	alpha;
-	int		numframes;
-	int		defaultframe;
+	char* ModelName;
+	struct model_s* model;
+	float radius;
+	float alpha;
+	int numframes;
+	int defaultframe;
 } FXAnimModel_t;
 
-// ************************************************************************************************
-// Animating chunks
-// ************************************************************************************************
-
-FXAnimModel_t FXAnimModel[NUM_FX_ANIM]=
+// Animating chunks.
+static FXAnimModel_t fx_anim_models[NUM_FX_ANIM] =
 {
-	"models/objects/banner/wall/tris.fm",		NULL, 220.0,		1.0,	15,		1,	// FX_ANIM_BANNER
-	"models/objects/lights/candelabrum/tris.fm",NULL, 100.0,		1.0,	7,		1,	// FX_ANIM_CANDELABRUM
-	"models/objects/chandelier/chan2/tris.fm",	NULL, 100.0,		1.0,	7,		7,	// FX_ANIM_CHANDELIER2
-	NULL,									NULL, 100.0,		0.5,	1,		0,	// FX_ANIM_FLAME - NEVER USED
-	NULL,									NULL, 100.0,		0.5,	1,		0, 	// FX_ANIM_FIRE - NEVER USED
-	"models/objects/banner/onpole/tris.fm",		NULL, 100.0,		1.0,	21,		0,	// FX_ANIM_BANNERONPOLE
-	"models/objects/flags/onpole/tris.fm",		NULL, 100.0,		1.0,	80,		0,	// FX_ANIM_FLAGONPOLE
-	"models/objects/eggs/cocoon/tris.fm",		NULL, 100.0,		1.0,	20,		0,	// FX_ANIM_COCOON
-	"models/objects/labs/container1/tris.fm",	NULL, 100.0,		1.0,	30,		0,	// FX_ANIM_LABPARTSCONTAINER
-	"models/objects/labs/tray/tris.fm",			NULL, 100.0,		1.0,	15,		0,	// FX_ANIM_LABTRAY
-	"models/objects/labs/container2/tris.fm",	NULL, 100.0,		1.0,	80,		0,	// FX_ANIM_EYEBALLJAR
-	"models/objects/torture/ogle/tris.fm",		NULL, 100.0,		1.0,	100,	0,	// FX_ANIM_HANGING_OGLE
+	{ "models/objects/banner/wall/tris.fm",		NULL, 220.0f,	1.0f,	15,	1 },	 // FX_ANIM_BANNER
+	{ "models/objects/lights/candelabrum/tris.fm",	NULL, 100.0f,	1.0f,	7,		1 }, // FX_ANIM_CANDELABRUM
+	{ "models/objects/chandelier/chan2/tris.fm",	NULL, 100.0f,	1.0f,	7,		7 },	 // FX_ANIM_CHANDELIER2
+	{ NULL,										NULL, 100.0f,	0.5f,	1,		0 }, // FX_ANIM_FLAME - NEVER USED
+	{ NULL,										NULL, 100.0f,	0.5f,	1,		0 }, // FX_ANIM_FIRE - NEVER USED
+	{ "models/objects/banner/onpole/tris.fm",		NULL, 100.0f,	1.0f,	21,	0 },	 // FX_ANIM_BANNERONPOLE
+	{ "models/objects/flags/onpole/tris.fm",		NULL, 100.0f,	1.0f,	80,	0 }, // FX_ANIM_FLAGONPOLE
+	{ "models/objects/eggs/cocoon/tris.fm",		NULL, 100.0f,	1.0f,	20,	0 }, // FX_ANIM_COCOON
+	{ "models/objects/labs/container1/tris.fm",	NULL, 100.0f,	1.0f,	30,	0 }, // FX_ANIM_LABPARTSCONTAINER
+	{ "models/objects/labs/tray/tris.fm",			NULL, 100.0f,	1.0f,	15,	0 },	 // FX_ANIM_LABTRAY
+	{ "models/objects/labs/container2/tris.fm",	NULL, 100.0f,	1.0f,	80,	0 }, // FX_ANIM_EYEBALLJAR
+	{ "models/objects/torture/ogle/tris.fm",		NULL, 100.0f,	1.0f,	100,	0 }, // FX_ANIM_HANGING_OGLE
 };
 
 void PreCacheFXAnimate(void)
@@ -52,8 +42,8 @@ void PreCacheFXAnimate(void)
 
 	for(i = 0; i < NUM_FX_ANIM; ++i)
 	{
-		if (FXAnimModel[i].ModelName)
-			FXAnimModel[i].model = fxi.RegisterModel(FXAnimModel[i].ModelName);
+		if (fx_anim_models[i].ModelName)
+			fx_anim_models[i].model = fxi.RegisterModel(fx_anim_models[i].ModelName);
 	}
 }
 
@@ -94,14 +84,14 @@ void FXAnimate(centity_t *owner, int type, int flags, vec3_t origin)
 	VectorDegreesToRadians(self->r.angles, self->r.angles);
 	atype = anim & 0x7f;
 
-	self->r.model = &FXAnimModel[atype].model;
+	self->r.model = &fx_anim_models[atype].model;
 	self->r.scale = scale * 0.02;
 	self->r.skinnum = skinnum;
 
-	self->alpha = FXAnimModel[atype].alpha;
-	self->radius = FXAnimModel[atype].radius;
-	self->NoOfAnimFrames = FXAnimModel[atype].numframes;
-	self->r.frame = FXAnimModel[atype].defaultframe;
+	self->alpha = fx_anim_models[atype].alpha;
+	self->radius = fx_anim_models[atype].radius;
+	self->NoOfAnimFrames = fx_anim_models[atype].numframes;
+	self->r.frame = fx_anim_models[atype].defaultframe;
 
 	if(anim & 0x80)
 	{
