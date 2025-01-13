2f738a709d5a6e20ae01c2668b777c531241e5f5
m-x-d <j.maxed@gmail.com>
2025-01-13 15:19:20 +0300

    FXCWBeamUpdate() cleanup.

--- src/client effects/fx_cwatcher.c
+++ src/client effects/fx_cwatcher.c
@@ -42,50 +42,46 @@ void PreCacheCWModels(void)
 	cwmodels[CWM_STAR_TRAIL] = fxi.RegisterModel("sprites/Spells/indigostreak.sp2");
 }
 
-static qboolean FXCWBeamUpdate(struct client_entity_s *self, centity_t *owner)
+static qboolean FXCWBeamUpdate(struct client_entity_s* self, const centity_t* owner)
 {
-	client_entity_t		*spawner;
-	vec3_t				vel = {0, 0, 1};
-	int					i;
+	const vec3_t vel = { 0.0f, 0.0f, 1.0f };
 
 	LinkedEntityUpdatePlacement(self, owner);
 	VectorCopy(self->r.origin, self->r.endpos);
 
-	//Spawn a bunch of lines and sparks
-	i = GetScaledCount(2, 0.85);
+	// Spawn a bunch of lines and sparks.
+	const int count = GetScaledCount(2, 0.85f);
 
-	while (i--)
+	for (int i = 0; i < count; i++)
 	{
-		spawner = ClientEntity_new(FX_CWATCHER, CEF_DONT_LINK, self->r.origin, NULL, 1000);
-		spawner->r.model = cwmodels + CWM_BEAM_LINE;
-		spawner->radius = 400;
-		
-		spawner->r.flags |= RF_TRANSLUCENT | RF_TRANS_ADD | RF_TRANS_ADD_ALPHA;
-		spawner->flags |= CEF_USE_VELOCITY2;
+		client_entity_t* beam = ClientEntity_new(FX_CWATCHER, CEF_DONT_LINK, self->r.origin, NULL, 1000);
+		beam->r.model = &cwmodels[CWM_BEAM_LINE];
+		beam->radius = 400.0f;
 
-		spawner->r.spriteType = SPRITE_LINE;
+		beam->r.flags = RF_TRANSLUCENT | RF_TRANS_ADD | RF_TRANS_ADD_ALPHA;
+		beam->flags |= CEF_USE_VELOCITY2;
 
-		spawner->r.scale=flrand(1.5, 2.5);
-		spawner->alpha=0.75;
-		spawner->d_alpha=-2.5;
-		spawner->d_scale=-2.5;
+		beam->r.spriteType = SPRITE_LINE;
 
-		spawner->r.origin[0] += irand(-2, 2);
-		spawner->r.origin[1] += irand(-2, 2);
+		beam->r.scale = flrand(1.5f, 2.5f);
+		beam->alpha = 0.75f;
+		beam->d_alpha = -2.5f;
+		beam->d_scale = -2.5f;
 
-		VectorRandomCopy(vel, spawner->velocity2, 1.0);
-		VectorCopy(spawner->origin, spawner->r.startpos);
-		VectorMA(spawner->r.startpos, flrand(16,32), spawner->velocity2, spawner->r.endpos);
-		
-		VectorScale(spawner->velocity2, flrand(50,100), spawner->velocity2);
-		VectorClear(spawner->velocity);
+		beam->r.origin[0] += (float)(irand(-2, 2)); //TODO: why not flrand?
+		beam->r.origin[1] += (float)(irand(-2, 2)); //TODO: why not flrand?
 
-		AddEffect(NULL, spawner);
+		VectorRandomCopy(vel, beam->velocity2, 1.0f);
+		VectorCopy(beam->origin, beam->r.startpos);
+		VectorMA(beam->r.startpos, flrand(16.0f, 32.0f), beam->velocity2, beam->r.endpos);
+		VectorScale(beam->velocity2, flrand(50.0f, 100.0f), beam->velocity2);
+
+		AddEffect(NULL, beam);
 	}
 
-	if(++self->LifeTime == 8)
+	if (++self->LifeTime == 8)
 	{
-		fxi.S_StartSound(self->r.origin, -1, CHAN_AUTO, fxi.S_RegisterSound("monsters/elflord/weld.wav"), 0.5, ATTN_IDLE, 0);
+		fxi.S_StartSound(self->r.origin, -1, CHAN_AUTO, fxi.S_RegisterSound("Monsters/elflord/weld.wav"), 0.5f, ATTN_IDLE, 0);
 		self->LifeTime = 0;
 	}
 
