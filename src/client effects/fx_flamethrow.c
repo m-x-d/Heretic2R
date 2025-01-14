6a65ab44986a93c6201e729d4962e43d6851fca0
m-x-d <j.maxed@gmail.com>
2025-01-14 18:05:53 +0300

    FXFlamethrower_trail() cleanup.

--- src/client effects/fx_flamethrow.c
+++ src/client effects/fx_flamethrow.c
@@ -15,14 +15,8 @@
 
 #define FLAME_COUNT		4
 
-qboolean FXFlamethrower_trail(client_entity_t *self, centity_t *owner)
+qboolean FXFlamethrower_trail(client_entity_t* self, centity_t* owner)
 {
-	client_particle_t	*flame;
-	client_entity_t		*light;
-	paletteRGBA_t		color;
-	float				radius;
-	int					count, white;
-
 	if (self->LifeTime < fxi.cl->time)
 	{
 		self->Update = RemoveSelfAI;
@@ -31,48 +25,39 @@ qboolean FXFlamethrower_trail(client_entity_t *self, centity_t *owner)
 		return true;
 	}
 
-	count = GetScaledCount(FLAME_COUNT, 0.9);
+	const int count = GetScaledCount(FLAME_COUNT, 0.9f);
 
-	while (count--)
+	for (int i = 0; i < count; i++)
 	{
-		color.c = 0xFFFFFFFF;
-		
-		flame = ClientParticle_new(irand(PART_16x16_FIRE1, PART_16x16_FIRE3), color, 2000);
-		flame->d_scale = flrand(-10.0, -5.0);
-		flame->d_alpha = flrand(-600.0, -560.0);
-		flame->duration = (255.0 * 1000.0) / -flame->d_alpha;		// time taken to reach zero alpha
-	
-		//radius = self->r.scale * FIRE_SPAWN_RADIUS;
+		client_particle_t* flame = ClientParticle_new(irand(PART_16x16_FIRE1, PART_16x16_FIRE3), color_white, 2000);
+		flame->d_scale = flrand(-10.0f, -5.0f);
+		flame->d_alpha = flrand(-600.0f, -560.0f);
+		flame->duration = (int)(255.0f * 1000.0f / -flame->d_alpha); // Time taken to reach zero alpha.
+
 		VectorCopy(self->direction, flame->velocity);
-		VectorSet(flame->origin, irand(-2,2), irand(-2,2), irand(-2,2));
-		VectorScale(flame->velocity, flrand(0.75, 1), flame->velocity);
+		VectorSet(flame->origin, (float)(irand(-2, 2)), (float)(irand(-2, 2)), (float)(irand(-2, 2)));
+		VectorScale(flame->velocity, flrand(0.75f, 1.0f), flame->velocity);
 
-		flame->scale = flrand(14.0, 20.0);
-
-		flame->velocity[2] += flrand(0, 48);
-		flame->acceleration[2] = 64;
+		flame->scale = flrand(14.0f, 20.0f);
+		flame->velocity[2] += flrand(0.0f, 48.0f);
+		flame->acceleration[2] = 64.0f;
 
 		AddParticleToList(self, flame);
 	}
 
-	if(r_detail->value >= DETAIL_NORMAL)
+	if (r_detail->value >= DETAIL_NORMAL)
 	{
-		//Spawn a dynamic light
-		light = ClientEntity_new(FX_FLAMETHROWER, CEF_NO_DRAW, self->origin, 0, 500);
+		// Spawn a dynamic light.
+		client_entity_t* light = ClientEntity_new(FX_FLAMETHROWER, CEF_NO_DRAW, self->origin, 0, 500);
 		VectorCopy(self->direction, light->velocity);
 
-		white = irand(8, 16);
+		const byte white = (byte)irand(8, 16);
+		const paletteRGBA_t color = { .r = 128 + (byte)irand(108, 127), .g = 64 + white, .b = 16 + white, .a = 64 + (byte)irand(16, 128) };
 
-		color.r = 128 + irand(108, 127);
-		color.g = 64  + white;
-		color.b = 16  + white;
-		color.a = 64  + irand(16, 128);
-
-		light->dlight = CE_DLight_new(color, 150.0F, -100.0F);
+		light->dlight = CE_DLight_new(color, 150.0f, -100.0f);
 
 		AddEffect(NULL, light);
 	}
-
 
 	return true;
 }
