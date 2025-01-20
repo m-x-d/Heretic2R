52bf28b00eedd051fb8165117c4af69768b94489
m-x-d <j.maxed@gmail.com>
2025-01-20 18:40:11 +0300

    FXMeteorBarrierExplode() cleanup.

--- src/client effects/fx_meteorbarrier.c
+++ src/client effects/fx_meteorbarrier.c
@@ -130,45 +130,40 @@ void FXMeteorBarrierTravel(centity_t* owner, const int type, const int flags, co
 	AddEffect(owner, trail);
 }
 
-void FXMeteorBarrierExplode(centity_t *owner, int type, int flags, vec3_t origin)
+void FXMeteorBarrierExplode(centity_t* owner, const int type, const int flags, const vec3_t origin)
 {
-	vec3_t			dir, mins;
-	int				i;
-	client_entity_t	*smokepuff;
-	paletteRGBA_t	LightColor = {64, 255, 76, 255};
-	int				count;
-	client_particle_t	*ce;
-
+	vec3_t dir;
 	fxi.GetEffect(owner, flags, clientEffectSpawners[FX_SPELL_METEORBARRIEREXPLODE].formatString, dir);
-	Vec3ScaleAssign(32.0, dir);
+	Vec3ScaleAssign(32.0f, dir);
 
-	if(flags&CEF_FLAG6)
+	if (flags & CEF_FLAG6)
 	{
 		// Impact explosion: throw off chunks of glowing meteor-rock and puffs of green smoke.
-		vec3_t			DebrisDir;
+		const vec3_t debris_dir = { 0.0f, 0.0f, 1.0f };
+		const vec3_t mins = { 2.0f, 2.0f, 2.0f }; // Because SpawnChunks needs a value for bounding box.
 
-		VectorSet(DebrisDir, 0.0, 0.0, 1.0);
-		VectorSet(mins, 2.0, 2.0, 2.0);	// because SpawnChunks needs a value for bounding box
-		//clear out cef_flag# stuff, means different stuff to debris
-		FXDebris_SpawnChunks(type, flags & ~(CEF_FLAG6|CEF_FLAG7|CEF_FLAG8), origin, 5, MAT_GREYSTONE, DebrisDir, 80000.0f, mins, 1.0, false);
+		// Clear out cef_flag# stuff, means different stuff to debris.
+		FXDebris_SpawnChunks(type, flags & ~(CEF_FLAG6 | CEF_FLAG7 | CEF_FLAG8), origin, 5, MAT_GREYSTONE, debris_dir, 80000.0f, mins, 1.0f, false);
 	}
 
-	smokepuff = ClientEntity_new(type, flags|CEF_NO_DRAW | CEF_ADDITIVE_PARTS, origin, 0, 500);
-	smokepuff->radius = 10.0;
-	if(r_detail->value != DETAIL_LOW)
-		smokepuff->dlight = CE_DLight_new(LightColor, 150.0, 0.0);
+	client_entity_t* smoke_puff = ClientEntity_new(type, (int)(flags | CEF_NO_DRAW | CEF_ADDITIVE_PARTS), origin, NULL, 500);
+	smoke_puff->radius = 10.0f;
 
-	AddEffect(NULL, smokepuff);
+	if (r_detail->value > DETAIL_LOW)
+		smoke_puff->dlight = CE_DLight_new(meteor_dlight_color, 150.0f, 0.0f);
 
-	count = GetScaledCount(NUM_METEOR_CHUNKS, 0.4);
-	for(i = 0; i < count; i++)
+	AddEffect(NULL, smoke_puff);
+
+	const int count = GetScaledCount(NUM_METEOR_CHUNKS, 0.4f);
+	for (int i = 0; i < count; i++)
 	{
-		ce = ClientParticle_new(PART_16x16_SPARK_G, smokepuff->r.color, 500);
-		ce->scale = flrand(11,15);
-		ce->d_scale = -10.0;
-		VectorRandomCopy(dir, ce->velocity, 64.0);
-		ce->acceleration[2] = GetGravity() * 0.3;
-		AddParticleToList(smokepuff, ce);
+		client_particle_t* ce = ClientParticle_new(PART_16x16_SPARK_G, smoke_puff->r.color, 500);
+
+		ce->scale = flrand(11.0f, 15.0f);
+		ce->d_scale = -10.0f;
+		VectorRandomCopy(dir, ce->velocity, 64.0f);
+		ce->acceleration[2] = GetGravity() * 0.3f;
+
+		AddParticleToList(smoke_puff, ce);
 	}
 }
-// end
