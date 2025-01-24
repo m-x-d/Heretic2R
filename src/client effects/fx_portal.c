37b5702f1360d4b28257ff4004f766e4e19c5960
m-x-d <j.maxed@gmail.com>
2025-01-24 16:17:18 +0300

    FXMagicPortalThink() cleanup.

--- src/client effects/fx_portal.c
+++ src/client effects/fx_portal.c
@@ -27,6 +27,8 @@ void PreCachePortal(void)
 	portal_models[2] = fxi.RegisterModel("sprites/spells/patball.sp2");
 }
 
+#define NUM_SPARK_TYPES		(sizeof(random_spark_types) / sizeof(random_spark_types[0]) - 1) //mxd
+
 static int random_spark_types[9] =
 {
 	PART_4x4_WHITE,
@@ -40,184 +42,135 @@ static int random_spark_types[9] =
 	PART_16x16_SPARK_B,
 };
 
-qboolean FXMagicPortalThink(client_entity_t *self, centity_t *owner)
+static qboolean FXMagicPortalThink(client_entity_t* self, centity_t* owner)
 {
-	client_particle_t	*ce;
-	paletteRGBA_t		color;
-	vec3_t				vel, right, up;
-	int					i, part_type;
-	int					count;
-	client_entity_t		*ripple;
-	client_entity_t		*line;
-
+	// Already disabled?
 	if (self->LifeTime == 1)
-	{	// already disabled.
-		if (fxi.cl->time > self->lastThinkTime)
-		{	// Remove it.
-			return(false);
-		}
-		else
-		{
-			return(true);
-		}
-	}
+		return fxi.cl->time <= self->lastThinkTime;
 
-	if(owner->current.effects&EF_DISABLE_EXTRA_FX || (self->SpawnDelay && self->SpawnDelay < fxi.cl->time))
-	{	// Start disabling it, but give it a couple of seconds to fitz out.
+	if ((owner->current.effects & EF_DISABLE_EXTRA_FX) || (self->SpawnDelay > 0 && self->SpawnDelay < fxi.cl->time))
+	{
+		// Start disabling it, but give it a couple of seconds to fitz out.
 		self->LifeTime = 1;
 		self->lastThinkTime = fxi.cl->time + 2000;
 
-		return(true);
+		return true;
 	}
 
+	vec3_t right;
+	vec3_t up;
 	AngleVectors(self->r.angles, NULL, right, up);
-	count = GetScaledCount(NUM_PORTAL_PARTS, 0.7);
-	for(i = 0; i < count; i++)
+
+	const int count = GetScaledCount(NUM_PORTAL_PARTS, 0.7f);
+
+	for (int i = 0; i < count; i++)
 	{
-		VectorScale(right, flrand(-1, 1), vel);
-		VectorMA(vel, flrand(-1, 1), up, vel);
-		if(Vec3IsZero(vel))
-			vel[2] = 1.0;			// Safety in case flrand gens all zeros (VERY unlikely)
+		vec3_t vel;
+		VectorScale(right, flrand(-1.0f, 1.0f), vel);
+		VectorMA(vel, flrand(-1.0f, 1.0f), up, vel);
+
+		if (Vec3IsZero(vel))
+			vel[2] = 1.0f; // Safety in case flrand gens all zeros (VERY unlikely).
+
 		VectorNormalize(vel);
 		VectorScale(vel, PORTAL_RADIUS, vel);
 
-		color.c = 0xffffffff;
+		client_particle_t* ce = ClientParticle_new(random_spark_types[irand(0, NUM_SPARK_TYPES)], color_white, 3000);
 
-		part_type = irand(0, 8);
-		ce = ClientParticle_new(random_spark_types[part_type], color, 3000);
-		VectorCopy(vel, ce->origin);
-
-		VectorScale(vel, flrand(0.0625, 0.125f), ce->velocity);
-		ce->acceleration[2] = flrand(2, 5);
-		ce->scale = flrand(MIN_PARTICLE_SCALE, MAX_PARTICLE_SCALE);
-		ce->d_scale = -0.2F;
-		
-		if(!(self->flags & CEF_NO_DRAW))
-		{
-			ce->color.r = irand(100, 200);
-			ce->color.g = irand(0, 50);
-			ce->color.b = irand(100, 200);
-		}
 		ce->type |= PFL_ADDITIVE;
+		VectorCopy(vel, ce->origin);
+		VectorScale(vel, flrand(0.0625f, 0.125f), ce->velocity);
+		ce->acceleration[2] = flrand(2.0f, 5.0f);
+		ce->scale = flrand(MIN_PARTICLE_SCALE, MAX_PARTICLE_SCALE);
+		ce->d_scale = -0.2f;
+		ce->d_alpha = flrand(320.0f, 480.0f); //mxd. Was irand() in original version.
 
-//		ce->color.a = irand(128, 255);
-		ce->color.a = irand(2, 128);
+		if (!(self->flags & CEF_NO_DRAW))
+			COLOUR_SET(ce->color, irand(100, 200), irand(0, 50), irand(100, 200)); //mxd. Use macro.
 
-//		ce->d_alpha = flrand(ce->color.a * -0.8f, -ce->color.a);
-		ce->d_alpha = irand(320, 480);
+		ce->color.a = (byte)irand(2, 128);
+
 		AddParticleToList(self, ce);
 	}
 
-	if(self->SpawnInfo == 2)
+	if (self->SpawnInfo == 2)
 	{
-		ripple = ClientEntity_new(FX_WATER_ENTRYSPLASH,
-									 0,
-									 owner->current.origin,
-									 self->direction,
-									 4000);
+		self->SpawnInfo = 0;
 
-		ripple->r.model = portal_models;
-		//ripple->r.flags |= RF_TRANS_ADD_ALPHA | RF_TRANS_ADD | RF_TRANSLUCENT | RF_FIXED | RF_TRANS_GHOST;
-		ripple->r.flags |= RF_TRANS_ADD_ALPHA | RF_TRANS_ADD | RF_FIXED | RF_TRANS_GHOST;
+		client_entity_t* ripple = ClientEntity_new(FX_WATER_ENTRYSPLASH, 0, owner->current.origin, self->direction, 4000);
+
+		ripple->r.model = &portal_models[0]; // ripple_add sprite.
+		ripple->r.flags = RF_TRANS_ADD_ALPHA | RF_TRANS_ADD | RF_FIXED | RF_TRANS_GHOST;
 		ripple->r.scale = 0.1f;
 		ripple->d_scale = 1.0f;
-		if(!(self->flags & CEF_NO_DRAW))
+
+		if (!(self->flags & CEF_NO_DRAW))
 		{
-			ripple->r.color.r = irand(10, 100);
-			ripple->r.color.g = irand(0, 50);
-			ripple->r.color.b = irand(10, 100);
-			ripple->alpha = 0.40f;
-			ripple->d_alpha = -0.20f;
+			ripple->alpha = 0.4f;
+			ripple->d_alpha = -0.2f;
+			COLOUR_SET(ripple->r.color, irand(10, 100), irand(0, 50), irand(10, 100)); //mxd. Use macro.
 		}
 		else
 		{
 			ripple->alpha = 0.6f;
-			ripple->d_alpha = -0.30f;
+			ripple->d_alpha = -0.3f;
 		}
 		//FIXME: calculate correct duration
 
-		AddEffect(owner, ripple); 
-
-		self->SpawnInfo = 0;
+		AddEffect(owner, ripple);
 	}
 	else
-		self->SpawnInfo++;
-
-	if(!(self->flags & CEF_NO_DRAW))
 	{
-		self->alpha += flrand(-0.02, 0.02);
+		self->SpawnInfo++;
+	}
 
-		if(1)
+	if (!(self->flags & CEF_NO_DRAW))
+	{
+		self->alpha += flrand(-0.02f, 0.02f);
+
+		for (int i = 0; i < 4; i++)
 		{
-			self->r.verts[0][0] += flrand(-1, 1);
-			self->r.verts[0][1] += flrand(-1, 1);
+			self->r.verts[i][0] += flrand(-1.0f, 1.0f);
+			self->r.verts[i][1] += flrand(-1.0f, 1.0f);
 
-			self->r.verts[1][0] += flrand(-1, 1);
-			self->r.verts[1][1] += flrand(-1, 1);
-
-			self->r.verts[2][0] += flrand(-1, 1);
-			self->r.verts[2][1] += flrand(-1, 1);
-
-			self->r.verts[3][0] += flrand(-1, 1);
-			self->r.verts[3][1] += flrand(-1, 1);
-		}
-
-		if(1)
-		{
 			//FIXME: scroll based on viewangle to camera viewport - but what about alpha channel???
-			self->r.verts[0][2] += flrand(-0.001, 0.001);
-			self->r.verts[0][3] += flrand(-0.001, 0.001);
-
-			self->r.verts[1][2] += flrand(-0.001, 0.001);
-			self->r.verts[1][3] += flrand(-0.001, 0.001);
-
-			self->r.verts[2][2] += flrand(-0.001, 0.001);
-			self->r.verts[2][3] += flrand(-0.001, 0.001);
-
-			self->r.verts[3][2] += flrand(-0.001, 0.001);
-			self->r.verts[3][3] += flrand(-0.001, 0.001);
+			self->r.verts[i][2] += flrand(-0.001f, 0.001f);
+			self->r.verts[i][3] += flrand(-0.001f, 0.001f);
 		}
 	}
 
-	//===================================================
+	// Spawn a hit explosion of lines.
+	if ((int)r_detail->value >= DETAIL_HIGH) //mxd. DETAIL_UBERHIGH only in original version.
+	{
+		const int num_lines = GetScaledCount(16, 0.85f);
 
-	if(r_detail->value > DETAIL_HIGH)
-	{//Spawn a hit explosion of lines
-		i = GetScaledCount(16, 0.85);
-
-		while (i--)
+		for (int i = 0; i < num_lines; i++)
 		{
-			line=ClientEntity_new(FX_WEAPON_STAFF_STRIKE, 0, owner->current.origin, 0, 600);
+			client_entity_t* line = ClientEntity_new(FX_WEAPON_STAFF_STRIKE, 0, owner->current.origin, NULL, 600);
 
-			line->r.model = portal_models + 2;
-			
+			line->r.model = &portal_models[2]; // patball sprite.
 			line->r.spriteType = SPRITE_LINE;
 
-			line->r.flags |= RF_TRANSLUCENT | RF_TRANS_ADD | RF_TRANS_ADD_ALPHA;
-			line->r.color.c = 0xFFFFFFFF;
-			line->r.scale = flrand(1.0, 2.5);
-			line->alpha = flrand(1.0, 0.75);
-			line->d_alpha = -2.0;
-			line->d_scale = -1.0;
+			line->r.flags = RF_TRANSLUCENT | RF_TRANS_ADD | RF_TRANS_ADD_ALPHA;
+			line->r.scale = flrand(1.0f, 2.5f);
+			line->alpha = flrand(0.75f, 1.0f);
+			line->d_alpha = -2.0f;
+			line->d_scale = -1.0f;
+			COLOUR_SETA(line->r.color, irand(128, 255), irand(128, 255), irand(236, 255), irand(80, 192)); //mxd. Use macro.
 
-			line->r.color.r = irand(128, 255);
-			line->r.color.g = irand(128, 255);
-			line->r.color.b = 128 + irand(108, 127);
-			line->r.color.a = 64 + irand(16, 128);
-
-			VectorRandomCopy(self->direction, line->velocity, 1.25);
+			VectorRandomCopy(self->direction, line->velocity, 1.25f);
 
 			VectorCopy(owner->current.origin, line->r.startpos);
-			VectorMA(line->r.startpos, irand(16,48), line->velocity, line->r.endpos);
+			VectorMA(line->r.startpos, flrand(16.0f, 48.0f), line->velocity, line->r.endpos); //mxd. Used irand() in original logic.
 
-			VectorScale(line->velocity, irand(25,100), line->velocity);
-			VectorSet(line->acceleration, line->velocity[0] * 0.1, line->velocity[1] * 0.1, 0);
+			VectorScale(line->velocity, flrand(25.0f, 100.0f), line->velocity); //mxd. Used irand() in original logic.
+			VectorSet(line->acceleration, line->velocity[0] * 0.1f, line->velocity[1] * 0.1f, 0.0f);
 
 			AddEffect(NULL, line);
 		}
 	}
 
-	return(true);
+	return true;
 }
 
 // This is the persistant effect for the teleport pad
