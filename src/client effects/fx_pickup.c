96ebb98ce9cd948739094f17672bbc7b2f65b564
m-x-d <j.maxed@gmail.com>
2025-01-24 13:14:05 +0300

    fx_pickup.c: cleanup header. PreCachePickup() cleanup. Rename pickup_models to pickup_model.

--- src/client effects/fx_pickup.c
+++ src/client effects/fx_pickup.c
@@ -1,30 +1,17 @@
 //
 // fx_pickup.c
 //
-// Heretic II
 // Copyright 1998 Raven Software
 //
 
-#include "ce_DefaultMessageHandler.h"
 #include "Client Effects.h"
-#include "Client Entities.h"
-#include "Particle.h"
-#include "ResourceManager.h"
-#include "FX.h"
-#include "Vector.h"
-#include "Random.h"
-#include "Utilities.h"
-#include "Angles.h"
 
-#define	NUM_PICKUP_MODELS	1
-static struct model_s *pickup_models[NUM_PICKUP_MODELS];
+static struct model_s* pickup_model;
 
-void PreCachePickup()
+void PreCachePickup(void)
 {
-	pickup_models[0] = fxi.RegisterModel("sprites/fx/halo.sp2");
+	pickup_model = fxi.RegisterModel("sprites/fx/halo.sp2");
 }
-
-// --------------------------------------------------------------
 
 void FXPickup(centity_t *owner, int type, int flags, vec3_t origin)
 {
@@ -32,7 +19,7 @@ void FXPickup(centity_t *owner, int type, int flags, vec3_t origin)
 
 	ce = ClientEntity_new(type, flags | CEF_DONT_LINK | CEF_PULSE_ALPHA, origin, NULL, 500);
 
-	ce->r.model = pickup_models;
+	ce->r.model = &pickup_model;
 	ce->r.frame = 1;
 	ce->r.flags = RF_TRANSLUCENT | RF_TRANS_ADD | RF_TRANS_ADD_ALPHA;
 	ce->r.scale = 0.4;
