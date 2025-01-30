f5b01fa9cf48c600c56c28d11512da3b95723ba4
m-x-d <j.maxed@gmail.com>
2025-01-30 19:22:24 +0300

    DoWaterEntrySplash() cleanup.

--- src/client effects/fx_waterentrysplash.c
+++ src/client effects/fx_waterentrysplash.c
@@ -56,161 +56,146 @@ static qboolean WaterEntrySplashThinkerThink(struct client_entity_s* self, centi
 	return true;
 }
 
-// ************************************************************************************************
-// FXWaterEntrySplash
-// ------------------
-// ************************************************************************************************
-
-void DoWaterEntrySplash(int Type,int Flags,vec3_t Origin, byte SplashSize, vec3_t Dir)
+void DoWaterEntrySplash(const int type, const int flags, vec3_t origin, const byte splash_size, vec3_t dir)
 {
-	byte			NoOfRipples;
-	client_entity_t	*EntrySplashThinker,
-					*splash;
-	float				Theta,DeltaTheta;
+	int ripples_count;
 
-	if (SplashSize >= 128)
-		NoOfRipples = 4;
+	if (splash_size >= 128)
+		ripples_count = 4;
+	else if (splash_size >= 96)
+		ripples_count = 3;
+	else if (splash_size >= 64)
+		ripples_count = 2;
 	else
-	if (SplashSize >= 96)
-		NoOfRipples = 3;
-	else
-	if (SplashSize >= 64)
-		NoOfRipples = 2;
-	else
-		NoOfRipples = 1;
+		ripples_count = 1;
 
-
-	if(Flags&CEF_FLAG6)
-	{//they want a sound too
-		fxi.S_StartSound(Origin, -1, CHAN_AUTO,
-				fxi.S_RegisterSound(va("misc/splish%c.wav", irand('2', '3'))), 1, ATTN_STATIC, 0);
-	}
+	// They want a sound too?
+	if (flags & CEF_FLAG6)
+		fxi.S_StartSound(origin, -1, CHAN_AUTO, fxi.S_RegisterSound(va("misc/splish%i.wav", irand(2, 3))), 1.0f, ATTN_STATIC, 0.0f);
 
 	// Create a water entry ripple THINKER that will create the actual water entry ripples.
+	client_entity_t* entry_ripple_thinker = ClientEntity_new(type, flags, origin, dir, 200);
 
-	EntrySplashThinker=ClientEntity_new(Type,Flags,Origin,Dir,200);
-	
-	EntrySplashThinker->flags|=CEF_NO_DRAW;
-	EntrySplashThinker->NoOfAnimFrames=(int)NoOfRipples;
-	EntrySplashThinker->Update=WaterEntrySplashThinkerThink;
-	EntrySplashThinker->d_alpha=-0.8-(0.2-(0.2*((1.0/127.0)*(SplashSize&127))));
+	entry_ripple_thinker->flags |= CEF_NO_DRAW;
+	entry_ripple_thinker->NoOfAnimFrames = ripples_count;
+	entry_ripple_thinker->Update = WaterEntrySplashThinkerThink;
+	entry_ripple_thinker->d_alpha = -0.8f - (0.2f - (0.2f * (1.0f / 127.0f * (float)(splash_size & 127))));
 
-	AddEffect(NULL,EntrySplashThinker);	
+	AddEffect(NULL, entry_ripple_thinker);
 
 	// Create water entry particle splash if required.
-
-	if (SplashSize&128)
+	if (splash_size & 128)
 	{
-		DeltaTheta=(2*M_PI)/12;
-		for(Theta=0.0;Theta<(2*M_PI);Theta+=DeltaTheta)
+		float angle = 0.0f;
+		float delta_angle = ANGLE_360 / 12.0f;
+
+		for (int i = 0; i < 12; i++)
 		{
-			splash = ClientEntity_new(Type,Flags,Origin,Dir,500);
-			splash->r.model = water_models + 1;
-			splash->r.scale = flrand(0.15, 0.25);
+			client_entity_t* splash = ClientEntity_new(type, flags, origin, dir, 500);
+
+			splash->radius = 2.0f;
+			splash->r.model = &water_models[1]; // wfall sprite.
+			splash->r.scale = flrand(0.15f, 0.25f);
 			splash->r.flags = RF_TRANSLUCENT;
 			splash->r.frame = 1;
-			splash->radius = 2.0;
-			splash->d_alpha=-2.0;		
-			splash->d_scale=1.0;
+			splash->d_alpha = -2.0f;
+			splash->d_scale = 1.0f;
 
-			splash->origin[0]=WATER_SPLASH_RADIUS*cos(Theta);
-			splash->origin[1]=WATER_SPLASH_RADIUS*sin(Theta);
-			splash->origin[2]=0.0;
+			splash->origin[0] = WATER_SPLASH_RADIUS * cosf(angle);
+			splash->origin[1] = WATER_SPLASH_RADIUS * sinf(angle);
+			splash->origin[2] = 0.0f;
 
-			splash->velocity[0]=flrand(4.0,6.0)*splash->origin[0];
-			splash->velocity[1]=flrand(4.0,6.0)*splash->origin[1];
-			splash->velocity[2]=flrand(30.0,40.0);
+			splash->velocity[0] = flrand(4.0f, 6.0f) * splash->origin[0];
+			splash->velocity[1] = flrand(4.0f, 6.0f) * splash->origin[1];
+			splash->velocity[2] = flrand(30.0f, 40.0f);
 
-			splash->acceleration[0]=0.0;
-			splash->acceleration[1]=0.0;
-			splash->acceleration[2]=-250;
+			VectorSet(splash->acceleration, 0.0f, 0.0f, -250.0f);
 
 			AddEffect(NULL, splash);
+
+			angle += delta_angle;
 		}
 
-		DeltaTheta=(2*M_PI)/6;
-		for(Theta=0.0;Theta<(2*M_PI);Theta+=DeltaTheta)
+		angle = 0.0f;
+		delta_angle = ANGLE_360 / 6.0f;
+
+		for (int i = 0; i < 6; i++)
 		{
-			splash = ClientEntity_new(Type,Flags,Origin,Dir,800);
-			splash->r.model = water_models + 1;
-			splash->r.scale = flrand(0.1, 0.2);
-			splash->r.flags = RF_TRANSLUCENT;
+			client_entity_t* splash = ClientEntity_new(type, flags, origin, dir, 800);
+
+			splash->radius = 2.0f;
+			splash->r.model = &water_models[1]; // wfall sprite.
 			splash->r.frame = 1;
-			splash->radius = 2.0;
-			splash->d_alpha=-1.0;		
-			splash->d_scale=-0.5;
+			splash->r.flags = RF_TRANSLUCENT;
+			splash->r.scale = flrand(0.1f, 0.2f);
+			splash->d_alpha = -1.0f;
+			splash->d_scale = -0.5f;
 
-			splash->origin[0]=WATER_SPLASH_RADIUS*cos(Theta);
-			splash->origin[1]=WATER_SPLASH_RADIUS*sin(Theta);
-			splash->origin[2]=0.0;
+			splash->origin[0] = WATER_SPLASH_RADIUS * cosf(angle);
+			splash->origin[1] = WATER_SPLASH_RADIUS * sinf(angle);
+			splash->origin[2] = 0.0f;
 
-			splash->velocity[0]=flrand(1.0,2.0)*splash->origin[0];
-			splash->velocity[1]=flrand(1.0,2.0)*splash->origin[1];
-			splash->velocity[2]=flrand(100.0,150.0);
+			splash->velocity[0] = flrand(1.0f, 2.0f) * splash->origin[0];
+			splash->velocity[1] = flrand(1.0f, 2.0f) * splash->origin[1];
+			splash->velocity[2] = flrand(100.0f, 150.0f);
 
-			splash->acceleration[0]=0.0;
-			splash->acceleration[1]=0.0;
-			splash->acceleration[2]=-525;
+			VectorSet(splash->acceleration, 0.0f, 0.0f, -525.0f);
 
 			AddEffect(NULL, splash);
+
+			angle += delta_angle;
 		}
 
 		// Create a water entry ripple.
+		client_entity_t* entry_ripple = ClientEntity_new(FX_WATER_ENTRYSPLASH, flags & (~CEF_OWNERS_ORIGIN), origin, dir, 1200);
 
-		splash=ClientEntity_new(FX_WATER_ENTRYSPLASH, Flags&(~CEF_OWNERS_ORIGIN), Origin, Dir, 1200);
+		entry_ripple->r.model = &water_models[0]; // waterentryripple sprite.
+		entry_ripple->r.flags = RF_FIXED | RF_TRANSLUCENT | RF_ALPHA_TEXTURE;
+		entry_ripple->r.scale = WATER_SPLASH_SCALE * 2.0f;
+		entry_ripple->d_scale = 2.0f;
+		entry_ripple->alpha = 0.6f;
+		entry_ripple->d_alpha = entry_ripple_thinker->d_alpha;
 
-		splash->r.model = water_models;
-		splash->r.scale=WATER_SPLASH_SCALE*2.0;
-		splash->r.frame=0;
-		splash->r.flags |= RF_FIXED|RF_TRANSLUCENT|RF_ALPHA_TEXTURE;
-		splash->alpha=0.6;
-		splash->d_scale=2.0;
-		splash->d_alpha=EntrySplashThinker->d_alpha;
-		
-		AddEffect(NULL,splash);	
+		AddEffect(NULL, entry_ripple);
 	}
-	else if (Flags & CEF_FLAG7 && r_detail->value >= DETAIL_HIGH && Dir[2] >= 0.99)	// Horizontal splash
-	{	// Add little splooshies
-		int parts, i;
-		client_particle_t *part;
-		paletteRGBA_t color={0xff,0xff,0xff,0xff};
-		
+	else if (flags & CEF_FLAG7 && r_detail->value >= DETAIL_HIGH && dir[2] >= 0.99f) // Horizontal splash.
+	{
 		// Create an extra, centered water entry ripple.
-		splash=ClientEntity_new(FX_WATER_ENTRYSPLASH, Flags&(~CEF_OWNERS_ORIGIN), Origin, Dir, 1200);
+		client_entity_t* splash = ClientEntity_new(FX_WATER_ENTRYSPLASH, flags & (~CEF_OWNERS_ORIGIN), origin, dir, 1200);
 
-		splash->r.model = water_models;
-		splash->r.scale=WATER_SPLASH_SCALE;
-		splash->r.frame=0;
-		splash->r.flags |= RF_FIXED|RF_TRANSLUCENT|RF_ALPHA_TEXTURE;
-		splash->alpha=0.6;
-		splash->d_scale=1.0;
-		splash->d_alpha=EntrySplashThinker->d_alpha;
-		
-		AddEffect(NULL,splash);	
+		splash->r.model = &water_models[0]; // waterentryripple sprite.
+		splash->r.flags |= RF_FIXED | RF_TRANSLUCENT | RF_ALPHA_TEXTURE;
+		splash->r.scale = WATER_SPLASH_SCALE;
+		splash->alpha = 0.6f;
+		splash->d_scale = 1.0f;
+		splash->d_alpha = entry_ripple_thinker->d_alpha;
 
-		parts = SplashSize / 12;
+		AddEffect(NULL, splash);
 
-		for (i=0; i<parts; i++)
+		// Add little splooshies.
+		const int particles_count = splash_size / 12;
+
+		for (int i = 0; i < particles_count; i++)
 		{
-			part = ClientParticle_new(PART_32x32_WFALL, color, 750);
-			VectorSet(part->origin, flrand(-2.0,2.0), flrand(-2.0,2.0), 0);
-			VectorScale(part->origin, 8.0, part->velocity);
-			part->velocity[2] = flrand(50.0, 100.0);
-			part->acceleration[2] = -320;
+			client_particle_t* part = ClientParticle_new(PART_32x32_WFALL, color_white, 750);
 
-			part->scale = 4.0;
-			part->d_scale = -5.33;
-			part->d_alpha = -1.3;
+			part->scale = 4.0f;
+			part->d_scale = -5.33f;
+			part->d_alpha = -1.3f;
+
+			VectorSet(part->origin, flrand(-2.0f, 2.0f), flrand(-2.0f, 2.0f), 0.0f);
+			VectorScale(part->origin, 8.0f, part->velocity);
+			part->velocity[2] = flrand(50.0f, 100.0f);
+			part->acceleration[2] = -320.0f;
 
 			AddParticleToList(splash, part);
 		}
 	}
 	else
 	{
-		EntrySplashThinker->flags &= ~CEF_FLAG7;		// Remove that splooshy flag
+		entry_ripple_thinker->flags &= ~CEF_FLAG7; // Remove splooshy flag.
 	}
 }
-
 
 void FXWaterEntrySplash(centity_t *Owner,int Type,int Flags,vec3_t Origin)
 {
