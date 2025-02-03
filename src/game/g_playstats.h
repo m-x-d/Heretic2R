26887258cc07817f3776a1cfebd7a433dab1ebce
m-x-d <j.maxed@gmail.com>
2025-02-03 13:39:16 +0300

    g_playstats.h: change defines type from double to float.

--- src/game/g_playstats.h
+++ src/game/g_playstats.h
@@ -6,7 +6,7 @@
 
 #pragma once
 
-#define DAMAGE_DM_FACTOR				0.67
+#define DAMAGE_DM_FACTOR				0.67f
 
 #pragma region ========================== WEAPON STATS ==========================
 
@@ -20,11 +20,11 @@
 #define SWORD_POWER2_DMG_MIN			45
 #define SWORD_POWER2_DMG_MAX			55
 
-#define SWORD_SPIN_DMG_MOD				1.75
-#define SWORD_BACK_DMG_MOD				0.7
-#define SWORD_STAB_DMG_MOD				2.5
-#define SWORD_BLOCK_DELAY				1.2
-#define SWORD_BLOCK_DURATION			0.4
+#define SWORD_SPIN_DMG_MOD				1.75f
+#define SWORD_BACK_DMG_MOD				0.7f
+#define SWORD_STAB_DMG_MOD				2.5f
+#define SWORD_BLOCK_DELAY				1.2f
+#define SWORD_BLOCK_DURATION			0.4f
 #define SWORD_BLOCK_TIME				(SWORD_BLOCK_DELAY + SWORD_BLOCK_DURATION)
 
 // Flying Fist.
@@ -33,19 +33,19 @@
 #define FIREBALL_DAMAGE_MAX				25
 #define FIREBALL_DAMAGE_MIN_POWER		30
 #define FIREBALL_DAMAGE_MAX_POWER		40
-#define FIREBALL_RADIUS					64.0	// Radius of damage for the powered up version.
+#define FIREBALL_RADIUS					64.0f	// Radius of damage for the powered up version.
 #define FLYING_FIST_SPEED				1000.0f
 
 // Hellstaff.
 #define HELLBOLT_DAMAGE_MIN				7
 #define HELLBOLT_DAMAGE_MAX				10
-#define HELLLASER_DIST					1024.0
+#define HELLLASER_DIST					1024.0f
 #define HELLLASER_DAMAGE_MIN			11
 #define HELLLASER_DAMAGE_MAX			14
 #define HELLBOLT_SPEED					1000.0f
 
 // Blast (Arc of Thunder).
-#define BLAST_DISTANCE					1000.0	// Max range of the blast.
+#define BLAST_DISTANCE					1000.0f	// Max range of the blast.
 #define BLAST_DMG_MIN					10
 #define BLAST_DMG_MAX					15
 #define BLAST_ANGLE_INC					3.5f
@@ -55,23 +55,22 @@
 #define MAGICMISSILE_DAMAGE_MIN			30
 #define MAGICMISSILE_DAMAGE_MAX			40
 #define MAGICMISSILE_DAMAGE_RAD			20
-#define MAGICMISSILE_RADIUS				48
+#define MAGICMISSILE_RADIUS				48.0f
 #define MAGICMISSILE_SPEED				1500.0f
-// #define MAGICMISSILE_DAMAGE_RADIUS		64
 
 // Red Rain Arrows.
 #define RED_RAIN_DMG_ARROW_MIN			35
 #define RED_RAIN_DMG_ARROW_MAX			45
 #define RED_RAIN_DURATION				8.0f
-#define RED_RAIN_DAMAGE_INTERVAL		0.25
+#define RED_RAIN_DAMAGE_INTERVAL		0.25f
 #define RED_RAIN_DAMAGE					10		// 10 points every .25 seconds.
 #define RED_RAIN_RADIUS					60.0f
 #define MAX_REDRAINHEIGHT				200.0f
 #define MAX_REDRAINFALLDIST				1000.0f
-#define RED_RAIN_LIGHTNING_DURATION		(RED_RAIN_DURATION - 1.5)
+#define RED_RAIN_LIGHTNING_DURATION		(RED_RAIN_DURATION - 1.5f)
 #define RED_RAIN_LIGHTNING_CHANCE		6		// 1 in 6 chance per target every .25 seconds
-#define RED_RAIN_LIGHTNING_RADIUS		100.0
-#define RED_RAIN_LIGHTNING_WIDTH		6.0
+#define RED_RAIN_LIGHTNING_RADIUS		100.0f
+#define RED_RAIN_LIGHTNING_WIDTH		6.0f
 #define RED_RAIN_DMG_LIGHTNING_MIN		20
 #define RED_RAIN_DMG_LIGHTNING_MAX		30
 #define NUM_STORMS_PER_PLAYER			3		// Maybe 5, esp in DM?
@@ -83,8 +82,8 @@
 #define POWER_RAIN_DMG_ARROW_MAX		55
 #define POWER_RAIN_DAMAGE				15		// 15 points every .25 seconds.
 #define POWER_RAIN_RADIUS				80.0f
-#define POWER_RAIN_LIGHTNING_RADIUS		120.0
-#define POWER_RAIN_LIGHTNING_WIDTH		40.0
+#define POWER_RAIN_LIGHTNING_RADIUS		120.0f
+#define POWER_RAIN_LIGHTNING_WIDTH		40.0f
 #define POWER_RAIN_DMG_LIGHTNING_MIN	30		// Big nasty beam of thick lightning with radius damage.
 #define POWER_RAIN_DMG_LIGHTNING_MAX	45
 #define POWER_RAIN_DMG_LIGHTNING_RADIUS	100
@@ -93,7 +92,7 @@
 #define FIREBLAST_LIFETIME				5
 #define FIREBLAST_SPEED					480.0f
 #define FIREBLAST_RADIUS				48.0f
-#define FIREBLAST_PROJ_RADIUS			8.0
+#define FIREBLAST_PROJ_RADIUS			8.0f
 #define FIREBLAST_DRADIUS				1.0f
 #define FIREBLAST_VRADIUS				16
 #define FIREBLAST_DAMAGE				48
@@ -104,19 +103,19 @@
 #define FIREWAVE_DOWN					48
 #define FIREWAVE_SPEED					320.0f	// Speed of the firewall.
 #define FIREWAVE_DM_SPEED				450.0f	// Speed of the firewall in deathmatch.
-#define FIREWAVE_PROJ_RADIUS			8.0
+#define FIREWAVE_PROJ_RADIUS			8.0f
 #define FIREWAVE_RADIUS					20.0f	// Size of each element of the firewall.
 #define FIREWAVE_DRADIUS				FIREWAVE_RADIUS
 #define FIREWAVE_DAMAGE					75		// How much damage the wall does every think.
 #define FIREWAVE_DAMAGE_MIN				50
 #define FIREWAVE_WORM_DAMAGE			30
-#define FIREWAVE_WORM_RADIUS			64.0
+#define FIREWAVE_WORM_RADIUS			64.0f
 
 // Phoenix Arrows.
 #define PHOENIX_EXPLODE_DAMAGE			175		// Silver armor + 100 health ends with 13 health. Demo: 225.
-#define PHOENIX_EXPLODE_RADIUS			128
+#define PHOENIX_EXPLODE_RADIUS			128.0f
 #define PHOENIX_EXPLODE_DAMAGE_POWER	300		// Gold armor + 100 health ends with 25 health. Demo: 350.
-#define PHOENIX_EXPLODE_RADIUS_POWER	144
+#define PHOENIX_EXPLODE_RADIUS_POWER	144.0f
 #define PHOENIX_ARROW_SPEED				640.0f
 
 // Sphere of Annihilation.
@@ -127,7 +126,7 @@
 #define SPHERE_MANA_PER_CHARGE			1
 #define SPHERE_MAX_MANA_CHARGE			7		// For a total of 20 mana in a shot.
 #define SPHERE_MAX_CHARGES				(SPHERE_MAX_MANA_CHARGE * 2)	// 2 frames per charge.
-#define SPHERE_FLY_SPEED				600.0
+#define SPHERE_FLY_SPEED				600.0f
 #define SPHERE_LASER_SPEED				320.0f
 
 // Ripper (Iron Doom).
@@ -137,19 +136,19 @@
 #define RIPPER_BALL_ANGLE				(ANGLE_360 / RIPPER_BALLS)
 #define RIPPER_BALL_BANGLE				(256 / RIPPER_BALLS)	// NOTE: Only works if the number of balls is evenly dividable!
 #define RIPPER_EXPLODE_DAMAGE			20
-#define RIPPER_SPEED					2000.0
+#define RIPPER_SPEED					2000.0f
 #define RIPPER_EXPLODE_SPEED			500
 
 // Mace balls (powered up Iron Doom).
 #define MACEBALL_GRAVITY				0.5f
-#define MACEBALL_LIFE					5.0
+#define MACEBALL_LIFE					5.0f
 #define MACEBALL_EXTRALIFE				5
 #define MACEBALL_DAMAGE					1000
 #define MACEBALL_BOSS_DAMAGE			100
 
 // Tornado.
 #define TORN_DUR						0.5f
-#define	SPIN_DUR						15.0
+#define	SPIN_DUR						15.0f
 #define TORN_DAMAGE						20
 
 #define FIRE_LINGER_PLAYER_DMG_MIN		0
@@ -163,11 +162,11 @@
 #pragma region ========================== DEFENSIVE SPELL STATS ==========================
 
 // Delay before you can use a defensive spell again.
-#define DEFENSE_DEBOUNCE		0.5
+#define DEFENSE_DEBOUNCE		0.5f
 
 // Blue Ring of repulsion.
-#define	RING_EFFECT_RADIUS		200.0 
-#define RING_MASS_FACTOR		200.0
+#define	RING_EFFECT_RADIUS		200.0f
+#define RING_MASS_FACTOR		200.0f
 #define RING_KNOCKBACK_BASE		150
 #define RING_KNOCKBACK_SCALE	200
 
@@ -175,7 +174,7 @@
 #define METEOR_DAMAGE_MIN		35
 #define METEOR_DAMAGE_MAX		40
 #define METEOR_SEARCH_CHANCE	12
-#define METEOR_SEARCH_RADIUS	800
+#define METEOR_SEARCH_RADIUS	800.0f
 
 // Morph ovum.
 #define NUM_OF_OVUMS			6
@@ -185,7 +184,7 @@
 #define SHIELD_DAMAGE_MIN		15
 #define SHIELD_DAMAGE_MAX		20
 #define SHIELD_DURATION			23
-#define SHIELD_DAMAGE_RADIUS	96
+#define SHIELD_DAMAGE_RADIUS	96.0f
 #define SHIELD_LIGHTNING_WIDTH	8
 #define SHIELD_ATTACK_CHANCE	3	// This is the chance of searching at all, and the chance of being zapped if found.
 
@@ -193,18 +192,18 @@
 
 #pragma region ========================== OTHER EFFECT STATS ==========================
 
-// ITEM STUFF
+// Item stuff.
 #define BARREL_EXPLODE_DMG_MIN	10
 #define BARREL_EXPLODE_DMG_MAX	100
-#define BARREL_EXPLODE_RADIUS	128
+#define BARREL_EXPLODE_RADIUS	128.0f
 
-// FLAME THROWER STUFF - SERVER SIDE
-#define GRAV_MOV				1.0		// Time to move from min to max grav values - server side.
-#define MAX_GRAV				1.2		// Gravity flamethrower starts with - server side.
-#define MIN_GRAV				0.12	// Gravity flamethrower maxes out at - server side.
-#define ZEN_TIME				3.0		// Time for flamethrower to be at its max - server side - seconds.
-#define FLAME_THROW_DAMAGE		10.0	// Damage flamethrower does at one hit.
-#define FLAME_UPDATE_PER_SEC	0.2		// Frame update speed - server side.
+// Flamethrower stuff - server side.
+#define GRAV_MOV				1.0f	// Time to move from min to max grav values - server side.
+#define MAX_GRAV				1.2f	// Gravity flamethrower starts with - server side.
+#define MIN_GRAV				0.12f	// Gravity flamethrower maxes out at - server side.
+#define ZEN_TIME				3.0f	// Time for flamethrower to be at its max - server side - seconds.
+#define FLAME_THROW_DAMAGE		10.0f	// Damage flamethrower does at one hit.
+#define FLAME_UPDATE_PER_SEC	0.2f	// Frame update speed - server side.
 
 // Count of number of updates per second - server side.
 #define FLAME_NUM_UPDATE_PER_SEC	(GRAV_MOV / FLAME_UPDATE_PER_SEC)
@@ -214,26 +213,26 @@
 #define TOTAL_GRAV_MOV				(MAX_GRAV - MIN_GRAV) / ((float)FLAME_NUM_UPDATE_PER_SEC)
 
 // FLAME THROWER STUFF - CLIENT EFFECTS SIDE
-#define FLAME_GRAV		(-675.0)				// Initial gravity value - client effects side. Should match cl_gravity->value.
+#define FLAME_GRAV		(-675.0f)				// Initial gravity value - client effects side. Should match cl_gravity->value.
 #define MAX_GRAV_FX		FLAME_GRAV * MAX_GRAV	// Gravity flamethrower starts out with - client effects side.
 #define MIN_GRAV_FX		FLAME_GRAV * MIN_GRAV	// Gravity flamethrower maxes out at - client effects side.
-#define ZEN_TIME_FX		(ZEN_TIME * 1000.0)		// Time for flamethrower to be at its max - client effects side - milliseconds.
-#define GRAV_MOV_FX		(GRAV_MOV * 1000.0)		// Time to move from min to max grav values - client effects side - milliseconds.
+#define ZEN_TIME_FX		(ZEN_TIME * 1000.0f)	// Time for flamethrower to be at its max - client effects side - milliseconds.
+#define GRAV_MOV_FX		(GRAV_MOV * 1000.0f)	// Time to move from min to max grav values - client effects side - milliseconds.
 #define TOTAL_DIST_FX	((float)MAX_GRAV_FX - (float)MIN_GRAV_FX)	// Total distance from min to max grav.
 
-// SCREEN SHAKE DEFINES
+// Screen shake defines.
 #define SHAKE_LATERAL	1
 #define SHAKE_VERTICAL	2
 #define SHAKE_DEPTH		4
 #define SHAKE_ALL_DIR	7
 
-// DISTANCE DEFINES
-#define RUN_MULT		1.65
+// Distance defines.
+#define RUN_MULT		1.65f
 
 #define FAR_CLIP_DIST		"4096.0"
-#define FAR_CLIP_DIST_VAL	4096.0
+#define FAR_CLIP_DIST_VAL	4096.0f
 #define NEAR_CLIP_DIST		"64.0"
-#define NEAR_CLIP_DIST_VAL	64.0
+#define NEAR_CLIP_DIST_VAL	64.0f
 
 #define DETAIL_LOW		0
 #define DETAIL_NORMAL	1
@@ -250,8 +249,8 @@
 #define VIOLENCE_DEFAULT		VIOLENCE_NORMAL
 #define VIOLENCE_DEFAULT_STR	"2"
 
-// BREATH HOLDING DURATIONS (IN SECONDS)
-#define HOLD_BREATH_TIME	20.0
-#define M_HOLD_BREATH_TIME	12.0
+// Breath holding durations (in seconds).
+#define HOLD_BREATH_TIME	20.0f
+#define M_HOLD_BREATH_TIME	12.0f
 
 #pragma endregion
