6aaae364a325767659dea12157e5713f0931b732
m-x-d <j.maxed@gmail.com>
2025-02-06 13:05:59 +0300

    Add Killed() declaration to g_combat.h.

--- src/game/g_combat.h
+++ src/game/g_combat.h
@@ -10,3 +10,5 @@
 
 extern gitem_armor_t silver_armor_info;
 extern gitem_armor_t gold_armor_info;
+
+extern void Killed(edict_t* targ, edict_t* inflictor, edict_t* attacker, int damage, vec3_t point, int mod);
