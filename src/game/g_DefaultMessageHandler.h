0c111d132d512f950da117fa5bcfd66b6a65c03f
m-x-d <j.maxed@gmail.com>
2025-02-07 13:20:25 +0300

    g_DefaultMessageHandler.h cleanup.

--- src/game/g_DefaultMessageHandler.h
+++ src/game/g_DefaultMessageHandler.h
@@ -1,5 +1,10 @@
-#ifndef	G_DEFAULTMESSAGEHANDLER_H
-#define	G_DEFAULTMESSAGEHANDLER_H
+//
+// g_DefaultMessageHandler.h
+//
+// Copyright 1998 Raven Software
+//
+
+#pragma once
 
 #include "g_Message.h"
 
@@ -7,11 +12,8 @@ extern G_MsgReceiver_t DefaultMessageReceivers[NUM_MESSAGES];
 extern void DefaultMsgHandler(struct edict_s *self, G_Message_t *msg);
 extern void DyingMsgHandler(struct edict_s *self, G_Message_t *msg);
 
-
-void DefaultReceiver_Repulse(struct edict_s *self, G_Message_t *msg);
-void DefaultReceiver_SetAnim(struct edict_s *self, G_Message_t *msg);
-void DefaultReceiver_RemoveSelf(struct edict_s *self, G_Message_t *msg);
-void DefaultReceiver_Suspend(struct edict_s *self, G_Message_t *msg);
-void DefaultReceiver_Unsuspend(struct edict_s *self, G_Message_t *msg);
-
-#endif
+extern void DefaultReceiver_Repulse(struct edict_s *self, G_Message_t *msg);
+extern void DefaultReceiver_SetAnim(struct edict_s *self, G_Message_t *msg);
+extern void DefaultReceiver_RemoveSelf(struct edict_s *self, G_Message_t *msg);
+extern void DefaultReceiver_Suspend(struct edict_s *self, G_Message_t *msg);
+extern void DefaultReceiver_Unsuspend(struct edict_s *self, G_Message_t *msg);
