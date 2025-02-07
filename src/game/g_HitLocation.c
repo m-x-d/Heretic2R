b0429ce2d07b505c9cf0e0ea2507d94ce0c86a38
m-x-d <j.maxed@gmail.com>
2025-02-07 17:22:02 +0300

    MG_GetHitLocation() cleanup.

--- src/game/g_HitLocation.c
+++ src/game/g_HitLocation.c
@@ -51,178 +51,130 @@ static HitLocation_t hit_location_for_vfl_zone[] =
 	hl_Half_FT_ULA,		hl_TorsoFront,		hl_TorsoFront,		hl_TorsoFront,		hl_Half_FT_URA,		// Between 80% and 100% from back to front (Forward)
 };
 
-HitLocation_t MG_GetHitLocation(edict_t *target, edict_t *inflictor, vec3_t ppoint, vec3_t pdir)
+HitLocation_t MG_GetHitLocation(const edict_t* target, const edict_t* inflictor, const vec3_t p_point, const vec3_t p_dir)
 {
-	vec3_t			dir, point, point_dir;
-	vec3_t			forward, right, up;
-	vec3_t			tangles, tcenter;
-	float			tradius, hdist;
-	float			udot, fdot, rdot;
-	int				Vertical, Forward, Lateral;
-	HitLocation_t	HitLoc;
+	// Get target forward, right and up.
+	vec3_t tgt_angles;
+	if (target->client != NULL)
+		VectorSet(tgt_angles, 0.0f, target->s.angles[YAW], 0.0f); // Ignore player's pitch and roll.
+	else
+		VectorCopy(target->s.angles, tgt_angles);
 
-//get target forward, right and up
-	if(target->client)
-	{//ignore player's pitch and roll
-		VectorSet(tangles, 0, target->s.angles[YAW], 0);
+	vec3_t v_fwd;
+	vec3_t v_right;
+	vec3_t v_up;
+	AngleVectors(tgt_angles, v_fwd, v_right, v_up);
+
+	// Get center of target.
+	vec3_t tgt_center;
+	VectorAdd(target->absmin, target->absmax, tgt_center);
+	Vec3ScaleAssign(0.5f, tgt_center);
+
+	// Get radius width of target.
+	const float tgt_radius = (fabsf(target->maxs[0]) + fabsf(target->maxs[1]) + fabsf(target->mins[0]) + fabsf(target->mins[1])) / 4.0f;
+
+	// Get impact point.
+	vec3_t point;
+	if (p_point != NULL && Vec3NotZero(p_point))
+		VectorCopy(p_point, point);
+	else
+		VectorCopy(inflictor->s.origin, point); // This is bad!
+
+	// Get impact dir.
+	vec3_t dir;
+	if (p_dir != NULL && Vec3NotZero(p_dir))
+	{
+		VectorCopy(p_dir, dir);
 	}
 	else
-		VectorCopy(target->s.angles, tangles);
-
-	AngleVectors(tangles, forward, right, up);
-
-//get center of target
-	VectorAdd(target->absmin, target->absmax, tcenter);
-	Vec3ScaleAssign(0.5, tcenter);
-
-//get radius width of target
-	tradius = (fabs(target->maxs[0]) + fabs(target->maxs[1]) + fabs(target->mins[0]) + fabs(target->mins[1]))/4;
-
-//get impact point
-	if(ppoint && Vec3NotZero(ppoint))
-		VectorCopy(ppoint, point);
-	else
-		VectorCopy(inflictor->s.origin, point);//this is bad!
-
-//get impact dir
-	if(pdir && Vec3NotZero(pdir))
-		VectorCopy(pdir, dir);
-	else
-	{//take the inflictor's last origin to current to get dir
+	{
+		// Take the inflictor's last origin to current to get dir.
 		VectorSubtract(inflictor->s.origin, inflictor->s.old_origin, dir);
-		if(Vec3IsZero(dir))
-		{//ok, that didn't work, make dir to target center, ignoring z
+
+		if (Vec3IsZero(dir))
+		{
+			// OK, that didn't work, make dir to target center, ignoring z.
 			VectorSubtract(target->s.origin, inflictor->s.origin, dir);
-			dir[2] = 0;
+			dir[2] = 0.0f;
 		}
+
 		VectorNormalize(dir);
 	}
 
-//put point at controlled distance from center
-	hdist = vhlen(point, tcenter);
+	// Put point at controlled distance from center.
+	const float hor_dist = vhlen(point, tgt_center);
 
-	VectorMA(point, hdist - tradius, dir, point);
-	//now a point on the surface of a cylinder with a radius of tradius
-	
-	VectorSubtract(point, tcenter, point_dir);
+	// Now a point on the surface of a cylinder with a radius of tgt_radius.
+	VectorMA(point, hor_dist - tgt_radius, dir, point);
+
+	vec3_t point_dir;
+	VectorSubtract(point, tgt_center, point_dir);
 	VectorNormalize(point_dir);
 
-	//Get bottom to top (Vertical) position index
-	udot = DotProduct(up, point_dir);
-	if(udot>.666)
-		Vertical = 4;
-	else if(udot>.333)
-		Vertical = 3;
-	else if(udot>-.333)
-		Vertical = 2;
-	else if(udot>-.666)
-		Vertical = 1;
+	// Get bottom to top (Vertical) position index.
+	const float up_dot = DotProduct(v_up, point_dir);
+
+	int vertical;
+	if (up_dot > 0.666f)
+		vertical = 4;
+	else if (up_dot > 0.333f)
+		vertical = 3;
+	else if (up_dot > -0.333f)
+		vertical = 2;
+	else if (up_dot > -0.666f)
+		vertical = 1;
 	else
-		Vertical = 0;
+		vertical = 0;
 
 	//Get back to front (Forward) position index
-	fdot = DotProduct(forward, point_dir);
-	if(fdot>.666)
-		Forward = 4;
-	else if(fdot>.333)
-		Forward = 3;
-	else if(fdot>-.333)
-		Forward = 2;
-	else if(fdot>-.666)
-		Forward = 1;
+	const float fwd_dot = DotProduct(v_fwd, point_dir);
+
+	int forward;
+	if (fwd_dot > 0.666f)
+		forward = 4;
+	else if (fwd_dot > 0.333f)
+		forward = 3;
+	else if (fwd_dot > -0.333f)
+		forward = 2;
+	else if (fwd_dot > -0.666f)
+		forward = 1;
 	else
-		Forward = 0;
+		forward = 0;
 
-	//Get left to right (Lateral) position index
-	rdot = DotProduct(right, point_dir);
-	if(rdot>.666)
-		Lateral = 4;
-	else if(rdot>.333)
-		Lateral = 3;
-	else if(rdot>-.333)
-		Lateral = 2;
-	else if(rdot>-.666)
-		Lateral = 1;
+	// Get left to right (Lateral) position index.
+	const float right_dot = DotProduct(v_right, point_dir);
+
+	int lateral;
+	if (right_dot > 0.666f)
+		lateral = 4;
+	else if (right_dot > 0.333f)
+		lateral = 3;
+	else if (right_dot > -0.333f)
+		lateral = 2;
+	else if (right_dot > -0.666f)
+		lateral = 1;
 	else
-		Lateral = 0;
+		lateral = 0;
 
-//FIXME: make one for horizonal bodies (harpies, corpses)
-	HitLoc = hit_location_for_vfl_zone[Vertical * 25 + Forward * 5 + Lateral];
+	//FIXME: make one for horizontal bodies (harpies, corpses).
+	const HitLocation_t hit_loc = hit_location_for_vfl_zone[vertical * 25 + forward * 5 + lateral];
 
-	switch(HitLoc)
+	switch (hit_loc)
 	{
-		case hl_Half_LLL_LRL://left lower leg and right lower leg:
-			if(!irand(0,1))
-				return hl_LegLowerLeft;
-			else
-				return hl_LegUpperRight;
-			break;
-		case hl_Half_ULL_URL://left upper leg and right upper leg:
-			if(!irand(0,1))
-				return hl_LegUpperLeft;
-			else
-				return hl_LegLowerRight;
-			break;
-		case hl_Half_FT_BT://front and back torso:
-			if(!irand(0,1))
-				return hl_TorsoFront;
-			else
-				return hl_TorsoBack;
-			break;
-		case hl_Half_FT_URA://front torso and upper right arm:
-			if(!irand(0,1))
-				return hl_TorsoFront;
-			else
-				return hl_ArmUpperRight;
-			break;
-		case hl_Half_FT_ULA://front torso and upper left arm:
-			if(!irand(0,1))
-				return hl_TorsoFront;
-			else
-				return hl_ArmUpperLeft;
-			break;
-		case hl_Half_FT_LRA://front torso and lower right arm:
-			if(!irand(0,1))
-				return hl_TorsoFront;
-			else
-				return hl_ArmLowerRight;
-			break;
-		case hl_Half_FT_LLA://front torso and lower left arm:
-			if(!irand(0,1))
-				return hl_TorsoFront;
-			else
-				return hl_ArmLowerLeft;
-			break;
-		case hl_Half_BT_URA://back torso and upper right arm:
-			if(!irand(0,1))
-				return hl_TorsoBack;
-			else
-				return hl_ArmUpperRight;
-			break;
-		case hl_Half_BT_ULA://back torso and upper left arm:
-			if(!irand(0,1))
-				return hl_TorsoBack;
-			else
-				return hl_ArmUpperLeft;
-			break;
-		case hl_Half_BT_LRA://back torso and lower right arm:
-			if(!irand(0,1))
-				return hl_TorsoBack;
-			else
-				return hl_ArmLowerRight;
-			break;
-		case hl_Half_BT_LLA://back torso and lower left arm:
-			if(!irand(0,1))
-				return hl_TorsoBack;
-			else
-				return hl_ArmLowerLeft;
-			break;
-		default:
-			return HitLoc;
-			break;
-	}
-	//never happens:
-	return hl_NoneSpecific;
-}
+		case hl_Half_LLL_LRL:	return (irand(0, 1) ? hl_LegUpperRight : hl_LegLowerLeft);	// Left lower leg and right lower leg.
+		case hl_Half_ULL_URL:	return (irand(0, 1) ? hl_LegLowerRight : hl_LegUpperLeft);	// Left upper leg and right upper leg.
 
-// end
+		case hl_Half_FT_BT:		return (irand(0, 1) ? hl_TorsoBack     : hl_TorsoFront);	// Front and back torso.
+		case hl_Half_FT_URA:	return (irand(0, 1) ? hl_ArmUpperRight : hl_TorsoFront);	// Front torso and upper right arm.
+		case hl_Half_FT_ULA:	return (irand(0, 1) ? hl_ArmUpperLeft  : hl_TorsoFront);	// Front torso and upper left arm.
+		case hl_Half_FT_LRA:	return (irand(0, 1) ? hl_ArmLowerRight : hl_TorsoFront);	// Front torso and lower right arm.
+		case hl_Half_FT_LLA:	return (irand(0, 1) ? hl_ArmLowerLeft  : hl_TorsoFront);	// Front torso and lower left arm.
+
+		case hl_Half_BT_URA:	return (irand(0, 1) ? hl_ArmUpperRight : hl_TorsoBack);		// Back torso and upper right arm.
+		case hl_Half_BT_ULA:	return (irand(0, 1) ? hl_ArmUpperLeft  : hl_TorsoBack);		// Back torso and upper left arm.
+		case hl_Half_BT_LRA:	return (irand(0, 1) ? hl_ArmLowerRight : hl_TorsoBack);		// Back torso and lower right arm.
+		case hl_Half_BT_LLA:	return (irand(0, 1) ? hl_ArmLowerLeft  : hl_TorsoBack);		// Back torso and lower left arm.
+
+		default: return hit_loc;
+	}
+}
