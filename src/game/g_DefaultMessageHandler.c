567c4f32b1404d11fd049f6813ce03abe512c6da
m-x-d <j.maxed@gmail.com>
2025-02-07 13:22:02 +0300

    g_DefaultMessageHandler.c: cleanup includes.

--- src/game/g_DefaultMessageHandler.c
+++ src/game/g_DefaultMessageHandler.c
@@ -1,54 +1,58 @@
+//
+// g_DefaultMessageHandler.c
+//
+// Copyright 1998 Raven Software
+//
+
 #include "g_DefaultMessageHandler.h"
-#include "g_Physics.h"
 #include "g_local.h"
-#include "Vector.h"
 #include "Utilities.h"
-#include "g_HitLocation.h"
 
 G_MsgReceiver_t DefaultMessageReceivers[NUM_MESSAGES] =
 {
-	NULL,								// MSG_STAND
-	NULL,								// MSG_CROUCH  
-	NULL,								// MSG_DUCKDOWN,
-	NULL,								// MSG_DUCKHOLD,
-	NULL,								// MSG_DUCKUP,
-	NULL,								// MSG_WALK    
-	NULL,								// MSG_RUN      
-	NULL,								// MSG_JUMP     
-	NULL,								// MSG_MELEE    
-	NULL,								// MSG_MISSILE  
-	NULL,								// MSG_WATCH    
-	NULL,								// MSG_EAT      
-	NULL,								// MSG_PAIN     
-	NULL,								// MSG_DEATH    
-	NULL,								// MSG_FLY      
-	NULL,								// MSG_FLYBACK  
-	NULL,								// MSG_HOVER    
-	NULL,								// MSG_FLEE     
-	NULL,								// MSG_FLYATTACK
-	DefaultReceiver_Repulse,			// MSG_REPULSE  
-	NULL,								// MSG_IDLE,
-	NULL,								// MSG_TOUCH,
-	NULL,								// MSG_FALLBACK,
-	NULL,								// MSG_SEARCH,
-	NULL,								// MSG_DODGE,
-	NULL,								// MSG_ATTACK,
-	NULL,								// MSG_SIGHT,
-	NULL,								// MSG_TURN,
-	NULL,								// MSG_TURNLEFT,
-	NULL,								// MSG_TURNRIGHT,
-	NULL, 								// MSG_BLOCKED,
-	NULL,								// G_MSG_KNOCKEDBACK,
-	NULL,								// G_MSG_RESTSTATE,
-	DefaultReceiver_SetAnim,			// G_MSG_SET_ANIM,
-	DefaultReceiver_RemoveSelf,			// G_MSG_REMOVESELF,
-	DefaultReceiver_Suspend,			// G_MSG_SUSPEND,
-	DefaultReceiver_Unsuspend,			// G_MSG_UNSUSPEND,
+	NULL,						// MSG_STAND
+	NULL,						// MSG_CROUCH
+	NULL,						// MSG_DUCKDOWN
+	NULL,						// MSG_DUCKHOLD
+	NULL,						// MSG_DUCKUP
+	NULL,						// MSG_WALK
+	NULL,						// MSG_RUN
+	NULL,						// MSG_JUMP
+	NULL,						// MSG_MELEE
+	NULL,						// MSG_MISSILE
+	NULL,						// MSG_WATCH
+	NULL,						// MSG_EAT
+	NULL,						// MSG_PAIN
+	NULL,						// MSG_DEATH
+	NULL,						// MSG_FLY
+	NULL,						// MSG_FLYBACK
+	NULL,						// MSG_HOVER
+	NULL,						// MSG_FLEE
+	NULL,						// MSG_FLYATTACK
+	DefaultReceiver_Repulse,	// MSG_REPULSE
+	NULL,						// MSG_IDLE
+	NULL,						// MSG_TOUCH
+	NULL,						// MSG_FALLBACK
+	NULL,						// MSG_SEARCH
+	NULL,						// MSG_DODGE
+	NULL,						// MSG_ATTACK
+	NULL,						// MSG_SIGHT
+	NULL,						// MSG_TURN
+	NULL,						// MSG_TURNLEFT
+	NULL,						// MSG_TURNRIGHT
+	NULL, 						// MSG_BLOCKED
 
-	NULL,								// MSG_VOICE_SIGHT,
-	NULL,								// MSG_VOICE_POLL,
-	NULL,								// MSG_VOICE_PUPPET,
-	NULL,								// MSG_CHECK_MOOD,
+	NULL,						// G_MSG_KNOCKEDBACK
+	NULL,						// G_MSG_RESTSTATE
+	DefaultReceiver_SetAnim,	// G_MSG_SET_ANIM
+	DefaultReceiver_RemoveSelf,	// G_MSG_REMOVESELF
+	DefaultReceiver_Suspend,	// G_MSG_SUSPEND
+	DefaultReceiver_Unsuspend,	// G_MSG_UNSUSPEND
+
+	NULL,						// MSG_VOICE_SIGHT
+	NULL,						// MSG_VOICE_POLL
+	NULL,						// MSG_VOICE_PUPPET
+	NULL,						// MSG_CHECK_MOOD
 };
 
 void DefaultMsgHandler(edict_t *self, G_Message_t *msg)
