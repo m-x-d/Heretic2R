026573b025e5d1e669894221d97dfde3181c6605
m-x-d <j.maxed@gmail.com>
2025-02-10 14:34:07 +0300

    g_itemstats.h: change defines type to float where applicable.

--- src/game/g_itemstats.h
+++ src/game/g_itemstats.h
@@ -8,22 +8,22 @@
 
 #pragma region ========================== SHRINE STATS ==========================
 
-#define LIGHT_DURATION				90	// Duration of shrine light on player - in seconds.
-#define REFLECT_DURATION_SINGLE		30	// Duration of shrine reflect on player - in seconds.
-#define REFLECT_DURATION_DEATHMATCH	20	// Duration of shrine reflect on player - in seconds.
-#define GHOST_DURATION				60	// Duration of shrine ghosting on player - in seconds.
-#define POWERUP_DURATION			30	// Duration of shrine powerup on player weapons - in seconds.
-#define LUNGS_DURATION				20	// Duration of shrine lungs on player - in seconds.
-#define SHRINE_DELAY				45	// Time till Shrine is useable again if so toggled - in seconds.
-#define SPEED_DURATION				25	// Duration of shrine speed on player - in seconds.
+#define LIGHT_DURATION				90.0f	// Duration of shrine light on player - in seconds.
+#define REFLECT_DURATION_SINGLE		30.0f	// Duration of shrine reflect on player - in seconds.
+#define REFLECT_DURATION_DEATHMATCH	20.0f	// Duration of shrine reflect on player - in seconds.
+#define GHOST_DURATION				60.0f	// Duration of shrine ghosting on player - in seconds.
+#define POWERUP_DURATION			30.0f	// Duration of shrine powerup on player weapons - in seconds.
+#define LUNGS_DURATION				20.0f	// Duration of shrine lungs on player - in seconds.
+#define SHRINE_DELAY				45.0f	// Time till Shrine is useable again if so toggled - in seconds.
+#define SPEED_DURATION				25.0f	// Duration of shrine speed on player - in seconds.
 
 // Amount of armor you get at a shrine - note support is in for gold armor - you just can't actually get it at this stage.
-#define MAX_GOLD_ARMOR		250		// Amount of damage it can absorb.
-#define MAX_SILVER_ARMOR	100		// Amount of damage it can absorb.
-#define SILVER_HIT_MULT		0.5		// Percentage of hit damage we take on a physical hit - silver armor.
-#define SILVER_SPELL_MULT	0.5		// Percentage of hit damage we take on a spell hit - silver armor.
-#define GOLD_HIT_MULT		0.25	// Percentage of hit damage we take on a physical hit - gold armor.
-#define GOLD_SPELL_MULT		0.25	// Percentage of hit damage we take on a spell hit - gold armor.
+#define MAX_GOLD_ARMOR		250.0f	// Amount of damage it can absorb.
+#define MAX_SILVER_ARMOR	100.0f	// Amount of damage it can absorb.
+#define SILVER_HIT_MULT		0.5f	// Percentage of hit damage we take on a physical hit - silver armor.
+#define SILVER_SPELL_MULT	0.5f	// Percentage of hit damage we take on a spell hit - silver armor.
+#define GOLD_HIT_MULT		0.25f	// Percentage of hit damage we take on a physical hit - gold armor.
+#define GOLD_SPELL_MULT		0.25f	// Percentage of hit damage we take on a spell hit - gold armor.
 
 #define SHRINE_HEALTH		100
 #define SHRINE_MAX_HEALTH	150
@@ -45,15 +45,16 @@
 
 #pragma region ========================== SPELL ITEM STATS ==========================
 
-#define RESPAWN_TIME_WEAPON		30	// Seconds
-#define RESPAWN_TIME_MACEBALL	60	// Seconds
-#define RESPAWN_TIME_DEFENSE	30	// Seconds
-#define RESPAWN_TIME_RING		20	// Seconds
-#define RESPAWN_TIME_TELEPORT	25	// Seconds
-#define RESPAWN_TIME_MORPH		40	// Seconds
-#define RESPAWN_TIME_AMMO		20	// Seconds
-#define RESPAWN_TIME_ARROWS		30	// Seconds
-#define RESPAWN_TIME_MISC		20	// Seconds
+// In seconds.
+#define RESPAWN_TIME_WEAPON		30.0f
+#define RESPAWN_TIME_MACEBALL	60.0f
+#define RESPAWN_TIME_DEFENSE	30.0f
+#define RESPAWN_TIME_RING		20.0f
+#define RESPAWN_TIME_TELEPORT	25.0f
+#define RESPAWN_TIME_MORPH		40.0f
+#define RESPAWN_TIME_AMMO		20.0f
+#define RESPAWN_TIME_ARROWS		30.0f
+#define RESPAWN_TIME_MISC		20.0f
 
 // Ammunition usage
 #define AMMO_USE_FIREBALL		2
@@ -70,7 +71,7 @@
 #define MANA_USE_SHIELD			20
 #define MANA_USE_TELEPORT		15
 #define MANA_USE_POLYMORPH		30
-#define MANA_USE_METEORS		5	// Five PER meteor: Total of 20
+#define MANA_USE_METEORS		5	// Five PER meteor: Total of 20.
 #define MANA_USE_TORNADO		30
 
 #define AMMO_COUNT_MOST			20
