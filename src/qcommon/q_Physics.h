fa1cea69ed03529b7f7fe7febf408f13cbbe15d4
m-x-d <j.maxed@gmail.com>
2025-02-11 18:09:51 +0300

    Remove ELASTICITY_ENTITY_BOUNCE define (unused). Remove ELASTICITY_BOUNCE define (unused). Remove ELASTICITY_REFLECT define (unused). Remove FRICTION define (unused). Remove MAX_VELOCITY define (unused). Remove PPF_INFO_GRAB define (unused). Remove PRF_COLLISION define (unused). Remove PRF_EXPANSION_BLOCKED define (unused).

--- src/qcommon/q_Physics.h
+++ src/qcommon/q_Physics.h
@@ -12,27 +12,20 @@
 
 #define ELASTICITY_NONE				0.0f
 #define ELASTICITY_SLIDE			1.0001f
-#define ELASTICITY_ENTITY_BOUNCE	1.1f
-#define ELASTICITY_BOUNCE			1.5f
-#define ELASTICITY_REFLECT			2.0f
 #define ELASTICITY_MACEBALL			2.0f
 
 #define GROUND_NORMAL				0.7f
-
 #define GRAVITY						675.0f
-#define GRAVITY_STRING				"675.0" // To set cvars with
 
-#define	FRICTION					1600.0f
-#define FRICTION_STRING				"1600.0" // To set cvars with
+#define GRAVITY_STRING				"675.0" // To set cvars with.
+#define FRICTION_STRING				"1600.0" // To set cvars with.
+#define MAX_VELOCITY_STRING			"2000.0" // To set cvars with.
 
-#define	STOP_EPSILON				0.1f
+#define STOP_EPSILON				0.1f
 
-#define MAX_VELOCITY				2000
-#define MAX_VELOCITY_STRING			"2000"
-
-#define	PHYSICS_Z_FUDGE				0.5f
-#define	CHECK_BELOW_DIST			0.5f
-#define Z_VEL_NOT_ONGROUND			100
+#define PHYSICS_Z_FUDGE				0.5f
+#define CHECK_BELOW_DIST			0.5f
+#define Z_VEL_NOT_ONGROUND			100.0f
 
 typedef struct FormMove_s
 {
@@ -50,17 +43,6 @@ typedef struct FormMove_s
 	int processFlags;	// Filled in prior to passing in the FormMove to a high level physics call //TODO: mxd. Checked, but never set?
 	int resultFlags;	// Will eventually be filled in by physics //TODO: unused
 } FormMove_t;
-
-// Physics Process Flags //TODO: mxd. Never set (PPF_INFO_GRAB) / unused (PRF_COLLISION, PRF_EXPANSION_BLOCKED)
-
-// Indicates the function is being only for informational purposes.
-// The entities position will not be modified and it will not be linked.
-// This currently only works with DiscreteMove_Step.
-#define PPF_INFO_GRAB			0x00000001
-
-// Physics Result Flags
-#define PRF_COLLISION			0x00000001	// Indicates there was a collision
-#define PRF_EXPANSION_BLOCKED	0x00000002	// Indicates an expansion was blocked
 
 H2COMMON_API void BounceVelocity(const vec3_t in, const vec3_t normal, vec3_t out, float elasticity);
 H2COMMON_API qboolean BoundVelocity(float *vel);
