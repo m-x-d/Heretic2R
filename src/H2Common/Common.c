969fa5b04ab9714a564599285928cfcc0fe074f8
m-x-d <j.maxed@gmail.com>
2025-02-13 13:26:10 +0300

    Cosmetic changes in COM_SkipPath(). Cosmetic changes in COM_StripExtension(). Cosmetic changes in COM_FileExtension(). Cosmetic changes in COM_Parse(). Cosmetic changes in Com_PageInMemory().

--- src/H2Common/Common.c
+++ src/H2Common/Common.c
@@ -12,7 +12,7 @@ H2COMMON_API char* COM_SkipPath(char* pathname)
 {
 	char* last = pathname;
 
-	while (*pathname)
+	while (*pathname != 0)
 	{
 		if (*pathname == '/')
 			last = pathname + 1;
@@ -26,18 +26,18 @@ H2COMMON_API char* COM_SkipPath(char* pathname)
 // Q2 counterpart
 H2COMMON_API void COM_StripExtension(const char* in, char* out)
 {
-	while (*in && *in != '.')
+	while (*in != 0 && *in != '.')
 		*out++ = *in++;
 
 	*out = 0;
 }
 
-// Q2 counterpart, replaced with YQ2 version //TODO: not included in H2 toolkit headers. Included where?
+// Q2 counterpart, replaced with YQ2 version. //TODO: not included in H2 toolkit headers. Included where?
 H2COMMON_API const char* COM_FileExtension(const char* in)
 {
 	const char* ext = strrchr(in, '.');
 
-	if (!ext || ext == in)
+	if (ext == NULL || ext == in)
 		return "";
 
 	return ext + 1;
@@ -66,7 +66,7 @@ H2COMMON_API void COM_FileBase(const char* in, char* out)
 	}
 }
 
-// Q2 counterpart. Returns the path up to, but not including the last '/'
+// Q2 counterpart. Returns the path up to, but not including the last '/'.
 H2COMMON_API void COM_FilePath(const char* in, char* out)
 {
 	const char* s = in + strlen(in) - 1;
@@ -80,12 +80,12 @@ H2COMMON_API void COM_FilePath(const char* in, char* out)
 // Q2 counterpart
 H2COMMON_API void COM_DefaultExtension(char* path, const char* extension)
 {
-	// If path doesn't have a .EXT, append extension (extension should include the '.')
+	// If path doesn't have a .EXT, append extension (extension should include the '.').
 	const char* src = path + strlen(path) - 1;
 	while (*src != '/' && src != path)
 	{
 		if (*src == '.')
-			return; // It has an extension
+			return; // It has an extension.
 
 		src--;
 	}
@@ -93,25 +93,25 @@ H2COMMON_API void COM_DefaultExtension(char* path, const char* extension)
 	strcat(path, extension);
 }
 
-char com_token[MAX_TOKEN_CHARS];
-
-// Q2 counterpart. Parse a token out of a string
+// Q2 counterpart. Parse a token out of a string.
 H2COMMON_API char* COM_Parse(char** data_p)
 {
+	static char com_token[MAX_TOKEN_CHARS]; //mxd. Made local static.
+
 	char c; //mxd. int -> char
 
 	char* data = *data_p;
 	int len = 0;
 	com_token[0] = 0;
 
-	if (!data)
+	if (data == 0)
 	{
 		*data_p = NULL;
 		return "";
 	}
 
 skipwhite:
-	// Skip whitespace
+	// Skip whitespace.
 	while ((c = *data) <= ' ')
 	{
 		if (c == 0)
@@ -123,7 +123,7 @@ skipwhite:
 		data++;
 	}
 
-	// Skip '//' comments
+	// Skip '//' comments.
 	if (c == '/' && data[1] == '/')
 	{
 		while (*data && *data != '\n')
@@ -132,7 +132,7 @@ skipwhite:
 		goto skipwhite;
 	}
 
-	// Handle quoted strings specially
+	// Handle quoted strings specially.
 	if (c == '\"')
 	{
 		data++;
@@ -141,7 +141,7 @@ skipwhite:
 		{
 			c = *data++;
 
-			if (c == '\"' || !c)
+			if (c == 0 || c == '\"')
 				goto done;
 
 			if (len < MAX_TOKEN_CHARS)
@@ -152,7 +152,7 @@ skipwhite:
 		}
 	}
 
-	// Parse a regular word
+	// Parse a regular word.
 	do
 	{
 		if (len < MAX_TOKEN_CHARS)
@@ -186,14 +186,14 @@ H2COMMON_API void Com_sprintf(char* dest, const int size, const char* fmt, ...)
 
 	//TODO: if triggered, this will cause an exception when called before Sys_Init() is called in quake2.dll...
 	if (len >= size)
-		(*com_printf)("Com_sprintf: overflow of %i in %i\n", len, size); //mxd. Q2 uses regular Com_Printf here
+		(*com_printf)("Com_sprintf: overflow of %i in %i\n", len, size); //mxd. Q2 uses regular Com_Printf here.
 }
-
-static int paged_total = 0;
 
 // Q2 counterpart. //TODO: Ancient win32 logic to fetch data from swap file to RAM. No longer needed. Remove?
 H2COMMON_API void Com_PageInMemory(const byte* buffer, const int size)
 {
+	static int paged_total = 0; //mxd. Made local static.
+
 	for (int i = size - 1; i > 0; i -= 4096)
 		paged_total += buffer[i];
 }
