9bd08bf0075681bb0dafc4623ada92a26931dd31
m-x-d <j.maxed@gmail.com>
2025-02-13 15:37:03 +0300

    Rename shrine_powerup_touch() to ShrinePowerupTouch(). ShrinePowerupTouch() cleanup.

--- src/game/g_shrine.c
+++ src/game/g_shrine.c
@@ -1071,41 +1071,22 @@ static void ShrinePowerupCore(edict_t* other) //mxd. Named 'shrine_powerup_core'
 	ShrineRestorePlayer(other);
 }
 
-void shrine_powerup_touch (edict_t *self, edict_t *other, cplane_t *plane, csurface_t *surf)
+static void ShrinePowerupTouch(edict_t* self, edict_t* other, cplane_t* plane, csurface_t* surf) //mxd. Named 'shrine_powerup_touch' in original version.
 {
 	// If we aren't a player, forget it.
-
-	if (!other->client)
+	if (other->client == NULL)
 		return;
 
 	ShrinePowerupCore(other);
-
 	gi.gamemsg_centerprintf(other, GM_S_POWERUP);
 
-	// If we are in death match, don't make us go through the shrine anim, just/ start the effect,
-	// give us whatever, and leave it at that.
-
-	if (deathmatch->value || (other->flags & FL_CHICKEN) || (other->client->playerinfo.flags & PLAYER_FLAG_WATER))
-	{
+	// If we are in deathmatch, don't make us go through the shrine anim, just start the effect, give us whatever, and leave it at that.
+	if (DEATHMATCH || (other->flags & FL_CHICKEN) || (other->client->playerinfo.flags & PLAYER_FLAG_WATER))
 		PlayerShrinePowerupEffect(other);
-	}
 	else
-	{
-		// Tell us what sort of shrine we just hit.
-
-		other->shrine_type = SHRINE_POWERUP;
-
-		// Initialise the shrine animation.
-
-		P_PlayerAnimSetLowerSeq(&other->client->playerinfo, ASEQ_SHRINE);
-
-		// Make us invunerable for a couple of seconds.
-
-		other->client->shrine_framenum = level.time + INVUNERABILITY_TIME;
-	}
+		PlayerShrineStartUseAnimation(other, SHRINE_POWERUP); //mxd
 
 	// Decide whether to delete this shrine or disable it for a while.
-
 	UpdateShrineNode(self);
 }
 
@@ -1121,7 +1102,7 @@ void SP_shrine_powerup_trigger (edict_t *ent)
 	ent->classname = powerup_text;
 
 	if (!deathmatch->value || (deathmatch->value && !((int)dmflags->value & DF_NO_SHRINE)))
-		ent->touch = shrine_powerup_touch;
+		ent->touch = ShrinePowerupTouch;
 
 	if(deathmatch->value && ((int)dmflags->value & DF_SHRINE_CHAOS) && !((int)dmflags->value & DF_NO_SHRINE))
 	{
