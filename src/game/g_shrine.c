5057ae98b5d523f72e603d8614562ac19d1ce3a6
m-x-d <j.maxed@gmail.com>
2025-02-13 15:04:28 +0300

    SP_shrine_armor_silver_trigger() cleanup.

--- src/game/g_shrine.c
+++ src/game/g_shrine.c
@@ -446,10 +446,8 @@ static void ShrineArmorSilverTouch(edict_t* self, edict_t* other, cplane_t* plan
 	UpdateShrineNode(self);
 }
 
-/*QUAKED shrine_armor (.5 .3 .5) ? PERMANENT
-*/
-
-void SP_shrine_armor_silver_trigger (edict_t *ent)
+// QUAKED shrine_armor (.5 .3 .5) ? PERMANENT
+void SP_shrine_armor_silver_trigger(edict_t* ent) //mxd. Named 'shrine_armor' in original version.
 {
 	ent->movetype = PHYSICSTYPE_NONE;
 	ent->svflags |= SVF_NOCLIENT;
@@ -457,17 +455,17 @@ void SP_shrine_armor_silver_trigger (edict_t *ent)
 	ent->shrine_type = SHRINE_ARMOR_SILVER;
 	ent->classname = armor_silver_text;
 
-	if (!deathmatch->value || (deathmatch->value && !((int)dmflags->value & DF_NO_SHRINE)))
+	if (!DEATHMATCH || (DEATHMATCH && !(DMFLAGS & DF_NO_SHRINE)))
 		ent->touch = ShrineArmorSilverTouch;
 
-	if(deathmatch->value && ((int)dmflags->value & DF_SHRINE_CHAOS) && !((int)dmflags->value & DF_NO_SHRINE))
+	if (DEATHMATCH && (DMFLAGS & DF_SHRINE_CHAOS) && !(DMFLAGS & DF_NO_SHRINE))
 	{
 		ent->shrine_type = SHRINE_RANDOM;
 		ent->touch = ShrineRandomTouch;
 	}
 
 	gi.setmodel(ent, ent->model);
-	gi.linkentity (ent);
+	gi.linkentity(ent);
 }
 
 #pragma endregion
