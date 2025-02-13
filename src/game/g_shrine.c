8f675996a5d0b91bcaca61761078792368e36218
m-x-d <j.maxed@gmail.com>
2025-02-13 15:27:15 +0300

    Rename shrine_mana_touch() to ShrineManaTouch(). ShrineManaTouch() cleanup.

--- src/game/g_shrine.c
+++ src/game/g_shrine.c
@@ -829,42 +829,22 @@ static void ShrineManaCore(edict_t* other) //mxd. Named 'shrine_mana_core' in or
 }
 
 // We hit the mana shrine pad, give us some manna, do the animation.
-
-void shrine_mana_touch	(edict_t *self, edict_t *other, cplane_t *plane, csurface_t *surf)
+static void ShrineManaTouch(edict_t* self, edict_t* other, cplane_t* plane, csurface_t* surf) //mxd. Named 'shrine_mana_touch' in original version.
 {
 	// If we aren't a player, forget it.
-
-	if (!other->client)
+	if (other->client == NULL)
 		return;
 
 	ShrineManaCore(other);
-
 	gi.gamemsg_centerprintf(other, GM_S_MANA);
 
-	// If we are in death match, don't make us go through the shrine anim, just start the effect,
-	// give us whatever, and leave it at that.
-
-	if (deathmatch->value || (other->flags & FL_CHICKEN) || (other->client->playerinfo.flags & PLAYER_FLAG_WATER))
-	{
+	// If we are in deathmatch, don't make us go through the shrine anim, just start the effect, give us whatever, and leave it at that.
+	if (DEATHMATCH || (other->flags & FL_CHICKEN) || (other->client->playerinfo.flags & PLAYER_FLAG_WATER))
 		PlayerShrineManaEffect(other);
-	}
 	else
-	{
-		// Tell us what sort of shrine we just hit.
-
-		other->shrine_type = SHRINE_MANA;
-
-		// Initialise the shrine animation.
-
-		P_PlayerAnimSetLowerSeq(&other->client->playerinfo, ASEQ_SHRINE);
-
-		// Make us invunerable for a couple of seconds.
-
-		other->client->shrine_framenum = level.time + INVUNERABILITY_TIME;
-	}
+		PlayerShrineStartUseAnimation(other, SHRINE_MANA); //mxd
 
 	// Decide whether to delete this shrine or disable it for a while.
-
 	UpdateShrineNode(self);
 }
 
@@ -880,7 +860,7 @@ void SP_shrine_mana_trigger (edict_t *ent)
 	ent->classname = mana_text;
 
 	if (!deathmatch->value || (deathmatch->value && !((int)dmflags->value & DF_NO_SHRINE)))
-		ent->touch = shrine_mana_touch;
+		ent->touch = ShrineManaTouch;
 
 	if(deathmatch->value && ((int)dmflags->value & DF_SHRINE_CHAOS) && !((int)dmflags->value & DF_NO_SHRINE))
 	{
