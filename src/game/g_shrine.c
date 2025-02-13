4d4b15cd2ee178eaf26639fc73a8661003486a32
m-x-d <j.maxed@gmail.com>
2025-02-13 15:29:36 +0300

    Rename shrine_ghost_core() to ShrineGhostCore(). ShrineGhostCore(): cleanup, remove 'self' arg (unused).

--- src/game/g_shrine.c
+++ src/game/g_shrine.c
@@ -883,31 +883,28 @@ static void PlayerShrineGhostEffect(edict_t* self) //mxd. Named 'player_shrine_g
 	gi.sound(self, CHAN_ITEM, gi.soundindex("items/shrine6.wav"), 1.0f, ATTN_NORM, 0.0f);
 }
 
-void shrine_ghost_core(edict_t *self,edict_t *other)
+static void ShrineGhostCore(edict_t* other) //mxd. Named 'shrine_ghost_core' in original version.
 {
 	if (other->deadflag != DEAD_NO)
 		return;
 
-	// If we are a chicken, lets make us a player again.  Don't give him anything else.
+	// If we are a chicken, lets make us a player again. Don't give him anything else.
 	if (other->flags & FL_CHICKEN)
 	{
-		other->morph_timer = level.time - 0.1;
+		other->morph_timer = (int)level.time - 1; //BUGFIX: mxd. 'level.time - 0.1f' in original version.
 		return;
 	}
 
 	// Add some time in on the timer for the ghost effect.
-
 	other->client->playerinfo.ghost_timer = level.time + GHOST_DURATION;
 
 	// Update the model attributes for ghosting.
-	
 	SetupPlayerinfo_effects(other);
 	P_PlayerUpdateModelAttributes(&other->client->playerinfo);
 	WritePlayerinfo_effects(other);
 
-	// restore dismemberment, and stop us being on fire
+	// Restore dismemberment, and stop us being on fire.
 	ShrineRestorePlayer(other);
-
 }
 
 // Fire off an effect and give us a ghosting for a while powerup.
@@ -919,7 +916,7 @@ void shrine_ghost_touch	(edict_t *self, edict_t *other, cplane_t *plane, csurfac
 	if (!other->client)
 		return;
 
-	shrine_ghost_core(self,other);
+	ShrineGhostCore(other);
 
 	gi.gamemsg_centerprintf(other, GM_S_GHOST);
 
@@ -1467,7 +1464,7 @@ void ShrineRandomTouch (edict_t *self, edict_t *other, cplane_t *plane, csurface
 
 		case SHRINE_GHOST:
 
-			shrine_ghost_core(self,other);
+			ShrineGhostCore(other);
 			gi.gamemsg_centerprintf(other, GM_CS_GHOST);
 
 			break;
