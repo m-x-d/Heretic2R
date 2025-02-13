b6d94816552dfeaeeb282522e4535ff0189828ed
m-x-d <j.maxed@gmail.com>
2025-02-13 15:21:04 +0300

    SP_shrine_lungs_trigger() cleanup.

--- src/game/g_shrine.c
+++ src/game/g_shrine.c
@@ -691,9 +691,8 @@ static void ShrineLungsTouch(edict_t* self, edict_t* other, cplane_t* plane, csu
 	UpdateShrineNode(self);
 }
 
-/*QUAKED shrine_lung (.5 .3 .5) ? PERMANENT
-*/
-void SP_shrine_lungs_trigger (edict_t *ent)
+// QUAKED shrine_lung (.5 .3 .5) ? PERMANENT
+void SP_shrine_lungs_trigger(edict_t* ent) //mxd. Named 'shrine_lung' in original version.
 {
 	ent->movetype = PHYSICSTYPE_NONE;
 	ent->svflags |= SVF_NOCLIENT;
@@ -701,17 +700,17 @@ void SP_shrine_lungs_trigger (edict_t *ent)
 	ent->shrine_type = SHRINE_LUNGS;
 	ent->classname = lungs_text;
 
-	if(!deathmatch->value || (deathmatch->value && !((int)dmflags->value & DF_NO_SHRINE)))
+	if (!DEATHMATCH || (DEATHMATCH && !(DMFLAGS & DF_NO_SHRINE)))
 		ent->touch = ShrineLungsTouch;
 
-	if(deathmatch->value && ((int)dmflags->value & DF_SHRINE_CHAOS) && !((int)dmflags->value & DF_NO_SHRINE))
+	if (DEATHMATCH && (DMFLAGS & DF_SHRINE_CHAOS) && !(DMFLAGS & DF_NO_SHRINE))
 	{
 		ent->shrine_type = SHRINE_RANDOM;
 		ent->touch = ShrineRandomTouch;
 	}
 
 	gi.setmodel(ent, ent->model);
-	gi.linkentity (ent);
+	gi.linkentity(ent);
 }
 
 #pragma endregion
