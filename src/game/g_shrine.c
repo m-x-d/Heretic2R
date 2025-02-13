cb7c323122263e9bca60debc40f7fb8664e24179
m-x-d <j.maxed@gmail.com>
2025-02-13 15:20:26 +0300

    Rename shrine_lung_touch() to ShrineLungsTouch(). ShrineLungsTouch() cleanup.

--- src/game/g_shrine.c
+++ src/game/g_shrine.c
@@ -672,42 +672,22 @@ static void ShrineLungsCore(edict_t* other) //mxd. Named 'shrine_lung_core' in o
 }
 
 // Fire off an effect and give us lung power.
-
-void shrine_lung_touch	(edict_t *self, edict_t *other, cplane_t *plane, csurface_t *surf)
+static void ShrineLungsTouch(edict_t* self, edict_t* other, cplane_t* plane, csurface_t* surf) //mxd. Named 'shrine_lung_touch' in original version.
 {
 	// If we aren't a player, forget it.
-
-	if (!other->client)
+	if (other->client == NULL)
 		return;
 
 	ShrineLungsCore(other);
-
 	gi.gamemsg_centerprintf(other, GM_S_LUNGS);
 
-	// If we are in death match, don't make us go through the shrine anim, just start the effect,
-	// give us whatever, and leave it at that.
-
-	if (deathmatch->value || (other->flags & FL_CHICKEN) || (other->client->playerinfo.flags & PLAYER_FLAG_WATER))
-	{
+	// If we are in deathmatch, don't make us go through the shrine anim, just start the effect, give us whatever, and leave it at that.
+	if (DEATHMATCH || (other->flags & FL_CHICKEN) || (other->client->playerinfo.flags & PLAYER_FLAG_WATER))
 		PlayerShrineLungsEffect(other);
-	}
 	else
-	{
-		// Tell us what sort of shrine we just hit.
-
-		other->shrine_type = SHRINE_LUNGS;
-
-		// Initialise the shrine animation.
-
-		P_PlayerAnimSetLowerSeq(&other->client->playerinfo, ASEQ_SHRINE);
-
-		// Make us invulnerable for a couple of seconds.
-
-		other->client->shrine_framenum = level.time + INVUNERABILITY_TIME;
-	}
+		PlayerShrineStartUseAnimation(other, SHRINE_LUNGS); //mxd
 
 	// Decide whether to delete this shrine or disable it for a while.
-
 	UpdateShrineNode(self);
 }
 
@@ -722,7 +702,7 @@ void SP_shrine_lungs_trigger (edict_t *ent)
 	ent->classname = lungs_text;
 
 	if(!deathmatch->value || (deathmatch->value && !((int)dmflags->value & DF_NO_SHRINE)))
-		ent->touch = shrine_lung_touch;
+		ent->touch = ShrineLungsTouch;
 
 	if(deathmatch->value && ((int)dmflags->value & DF_SHRINE_CHAOS) && !((int)dmflags->value & DF_NO_SHRINE))
 	{
