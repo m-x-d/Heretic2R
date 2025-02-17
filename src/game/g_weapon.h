8bfc149d626046b12b53cd7aa219411fa0b763a7
m-x-d <j.maxed@gmail.com>
2025-02-17 17:00:19 +0300

    g_weapon.h cleanup.

--- src/game/g_weapon.h
+++ src/game/g_weapon.h
@@ -1,30 +1,22 @@
 //
-// g_weapon.h //TODO: mxd. Rename to p_weapon.h ?
+// g_weapon.h
 //
-// Heretic II
 // Copyright 1998 Raven Software
 //
 
-#ifndef P_WEAPON_H
-#define P_WEAPON_H
+#pragma once
 
-// ************************************************************************************************
-// WeaponThink_XXX
-// ---------------
-// Player-weapon think functions, each corresponding to one weapon type. Made visible here so that
-// they can be referenced from 'g_items.c'.
-// ************************************************************************************************
+#include "q_Typedef.h" //mxd
 
-void WeaponThink_SwordStaff(edict_t *Caster,char *Format,...);
-void WeaponThink_FlyingFist(edict_t *Caster,char *Format,...);
-void WeaponThink_MagicMissileSpread(edict_t *Caster,char *Format,...);
-void WeaponThink_SphereOfAnnihilation(edict_t *Caster,char *Format,...);
-void WeaponThink_Maceballs(edict_t *Caster,char *Format,...);
-void WeaponThink_Firewall(edict_t *Caster,char *Format,...);
-void WeaponThink_Ripper(edict_t *Caster,char *Format,...);
-void WeaponThink_Blast(edict_t *Caster,char *Format,...);
-void WeaponThink_RedRainBow(edict_t *Caster, char *Format,...);
-void WeaponThink_PhoenixBow(edict_t *Caster, char *Format,...);
-void WeaponThink_HellStaff(edict_t *Caster, char *Format,...);
-
-#endif // P_WEAPON_H
+// Player-weapon think functions, each corresponding to one weapon type.
+// Made visible here so that they can be referenced from 'g_items.c'.
+void WeaponThink_SwordStaff(edict_t* Caster, char* Format, ...);
+void WeaponThink_FlyingFist(edict_t* Caster, char* Format, ...);
+void WeaponThink_MagicMissileSpread(edict_t* Caster, char* Format, ...);
+void WeaponThink_SphereOfAnnihilation(edict_t* Caster, char* Format, ...);
+void WeaponThink_Maceballs(edict_t* Caster, char* Format, ...);
+void WeaponThink_Firewall(edict_t* Caster, char* Format, ...);
+void WeaponThink_Blast(edict_t* Caster, char* Format, ...);
+void WeaponThink_RedRainBow(edict_t* Caster, char* Format, ...);
+void WeaponThink_PhoenixBow(edict_t* Caster, char* Format, ...);
+void WeaponThink_HellStaff(edict_t* Caster, char* Format, ...);
