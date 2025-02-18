791eb8a4ceb3d61b3d4609330ad9571eb4a1a1d9
m-x-d <j.maxed@gmail.com>
2025-02-18 13:11:44 +0300

    g_weapon.h: update arg names to match with function definitions.

--- src/game/g_weapon.h
+++ src/game/g_weapon.h
@@ -10,13 +10,13 @@
 
 // Player-weapon think functions, each corresponding to one weapon type.
 // Made visible here so that they can be referenced from 'g_items.c'.
-void WeaponThink_SwordStaff(edict_t* Caster, char* Format, ...);
-void WeaponThink_FlyingFist(edict_t* Caster, char* Format, ...);
-void WeaponThink_MagicMissileSpread(edict_t* Caster, char* Format, ...);
-void WeaponThink_SphereOfAnnihilation(edict_t* Caster, char* Format, ...);
-void WeaponThink_Maceballs(edict_t* Caster, char* Format, ...);
-void WeaponThink_Firewall(edict_t* Caster, char* Format, ...);
-void WeaponThink_Blast(edict_t* Caster, char* Format, ...);
-void WeaponThink_RedRainBow(edict_t* Caster, char* Format, ...);
-void WeaponThink_PhoenixBow(edict_t* Caster, char* Format, ...);
-void WeaponThink_HellStaff(edict_t* Caster, char* Format, ...);
+void WeaponThink_SwordStaff(edict_t* caster, char* format, ...);
+void WeaponThink_FlyingFist(edict_t* caster, char* format, ...);
+void WeaponThink_MagicMissileSpread(edict_t* caster, char* format, ...);
+void WeaponThink_SphereOfAnnihilation(edict_t* caster, char* format, ...);
+void WeaponThink_Maceballs(edict_t* caster, char* format, ...);
+void WeaponThink_Firewall(edict_t* caster, char* format, ...);
+void WeaponThink_Blast(edict_t* caster, char* format, ...);
+void WeaponThink_RedRainBow(edict_t* caster, char* format, ...);
+void WeaponThink_PhoenixBow(edict_t* caster, char* format, ...);
+void WeaponThink_HellStaff(edict_t* caster, char* format, ...);
