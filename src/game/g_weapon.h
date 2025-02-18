47f9e3ada6a413ec408673ae95d1f1e2ec62e34b
m-x-d <j.maxed@gmail.com>
2025-02-18 13:12:49 +0300

    g_weapon.h: add 'extern' keyword.

--- src/game/g_weapon.h
+++ src/game/g_weapon.h
@@ -10,13 +10,13 @@
 
 // Player-weapon think functions, each corresponding to one weapon type.
 // Made visible here so that they can be referenced from 'g_items.c'.
-void WeaponThink_SwordStaff(edict_t* caster, char* format, ...);
-void WeaponThink_FlyingFist(edict_t* caster, char* format, ...);
-void WeaponThink_MagicMissileSpread(edict_t* caster, char* format, ...);
-void WeaponThink_SphereOfAnnihilation(edict_t* caster, char* format, ...);
-void WeaponThink_Maceballs(edict_t* caster, char* format, ...);
-void WeaponThink_Firewall(edict_t* caster, char* format, ...);
-void WeaponThink_Blast(edict_t* caster, char* format, ...);
-void WeaponThink_RedRainBow(edict_t* caster, char* format, ...);
-void WeaponThink_PhoenixBow(edict_t* caster, char* format, ...);
-void WeaponThink_HellStaff(edict_t* caster, char* format, ...);
+extern void WeaponThink_SwordStaff(edict_t* caster, char* format, ...);
+extern void WeaponThink_FlyingFist(edict_t* caster, char* format, ...);
+extern void WeaponThink_MagicMissileSpread(edict_t* caster, char* format, ...);
+extern void WeaponThink_SphereOfAnnihilation(edict_t* caster, char* format, ...);
+extern void WeaponThink_Maceballs(edict_t* caster, char* format, ...);
+extern void WeaponThink_Firewall(edict_t* caster, char* format, ...);
+extern void WeaponThink_Blast(edict_t* caster, char* format, ...);
+extern void WeaponThink_RedRainBow(edict_t* caster, char* format, ...);
+extern void WeaponThink_PhoenixBow(edict_t* caster, char* format, ...);
+extern void WeaponThink_HellStaff(edict_t* caster, char* format, ...);
