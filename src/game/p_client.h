eb87778c060d0945aff5bd5a081003fe5e4135bc
m-x-d <j.maxed@gmail.com>
2025-02-18 13:22:30 +0300

    p_client.h: add 'extern' keyword.

--- src/game/p_client.h
+++ src/game/p_client.h
@@ -9,13 +9,13 @@
 #include "q_shared.h"
 #include "q_Typedef.h"
 
-qboolean ClientConnect(edict_t* ent, char* userinfo);
-void ClientThink(edict_t* ent, usercmd_t* cmd);
-void ClientUserinfoChanged(edict_t* ent, char* userinfo);
-void ClientDisconnect(edict_t* ent);
-void ClientBegin(edict_t* ent);
+extern qboolean ClientConnect(edict_t* ent, char* userinfo);
+extern void ClientThink(edict_t* ent, usercmd_t* cmd);
+extern void ClientUserinfoChanged(edict_t* ent, char* userinfo);
+extern void ClientDisconnect(edict_t* ent);
+extern void ClientBegin(edict_t* ent);
 
-void SP_info_player_start(edict_t* self);
-void SP_info_player_deathmatch(edict_t* self);
-void SP_info_player_coop(edict_t* self);
-void SP_info_player_intermission(edict_t* self);
+extern void SP_info_player_start(edict_t* self);
+extern void SP_info_player_deathmatch(edict_t* self);
+extern void SP_info_player_coop(edict_t* self);
+extern void SP_info_player_intermission(edict_t* self);
