4711aadfc94ae9fab456f03727646158858e7be8
m-x-d <j.maxed@gmail.com>
2025-02-20 16:03:57 +0300

    p_funcs.h cleanup.

--- src/game/p_funcs.h
+++ src/game/p_funcs.h
@@ -1,52 +1,45 @@
+//
 // p_funcs.h
 //
 // Heretic II - Raven software
 //
-// Written by Marcus Whitlock
-//
 
-#ifndef _P_FUNCS_H_
-#define _P_FUNCS_H_
+#pragma once
 
-#include "g_local.h"
+#include "p_types.h"
 
-extern entity_state_t *G_GetEntityStatePtr(edict_t *entity);
-extern int G_BranchLwrClimbing(playerinfo_t *playerinfo);
-extern qboolean G_PlayerActionCheckRopeGrab(playerinfo_t *playerinfo, float stomp_org);
-extern void G_PlayerClimbingMoveFunc(playerinfo_t *playerinfo, float height, float var2, float var3);
-extern qboolean G_PlayerActionCheckPuzzleGrab(playerinfo_t *playerinfo);
-extern void G_PlayerActionTakePuzzle(playerinfo_t *playerinfo);
-extern qboolean G_PlayerActionUsePuzzle(playerinfo_t *playerinfo);
-extern qboolean G_PlayerActionCheckPushPull_Ent(void *ent);
-extern void PushPull_stop(edict_t *self);
-extern void G_PlayerActionMoveItem(playerinfo_t *playerinfo,float distance);
-extern qboolean G_PlayerActionCheckPushButton(playerinfo_t *playerinfo);
-extern void G_PlayerActionPushButton(playerinfo_t *playerinfo);
-extern qboolean G_PlayerActionCheckPushLever(playerinfo_t *playerinfo);
-extern void G_PlayerActionPushLever(playerinfo_t *playerinfo);
-extern qboolean G_HandleTeleport(playerinfo_t *playerinfo);
-extern void G_SetJointAngles(playerinfo_t *playerinfo);
-extern void G_ResetJointAngles(playerinfo_t *playerinfo);
-extern void G_PlayerActionChickenBite(playerinfo_t *playerinfo);
-extern void G_PlayerFallingDamage(playerinfo_t *playerinfo,float delta);
-extern void G_PlayerActionSwordAttack(playerinfo_t *playerinfo,int value);
-extern void G_PlayerActionSpellFireball(playerinfo_t *playerinfo);
-extern void G_PlayerActionSpellBlast(playerinfo_t *playerinfo);
-extern void G_PlayerActionSpellArray(playerinfo_t *playerinfo,int value);
-extern void G_PlayerActionSpellSphereCreate(playerinfo_t *playerinfo,qboolean *Charging);
-extern void G_PlayerActionSpellBigBall(playerinfo_t *playerinfo);
-extern void G_PlayerActionSpellFirewall(playerinfo_t *playerinfo);
-extern void G_PlayerActionRedRainBowAttack(playerinfo_t *playerinfo);
-extern void G_PlayerActionPhoenixBowAttack(playerinfo_t *playerinfo);
-extern void G_PlayerActionHellstaffAttack(playerinfo_t *playerinfo);
-extern void G_PlayerActionSpellDefensive(playerinfo_t *playerinfo);
-extern void G_PlayerActionChickenBite(playerinfo_t *playerinfo);
-extern void G_PlayerSpellShieldAttack(playerinfo_t *playerinfo);
-extern void G_PlayerSpellStopShieldAttack(playerinfo_t *playerinfo);
-extern void G_PlayerVaultKick(playerinfo_t *playerinfo);
-extern void G_PlayerActionCheckRopeMove(playerinfo_t *playerinfo);
-extern qboolean G_EntIsAButton(edict_t *ent);
-void Updatefmnodeinfo(edict_t *ent);
-void Setfmnodeinfo(edict_t *ent);
+extern entity_state_t* G_GetEntityStatePtr(edict_t* entity);
+extern int G_BranchLwrClimbing(playerinfo_t* info);
+extern qboolean G_PlayerActionCheckRopeGrab(playerinfo_t* info, float stomp_org);
+extern void G_PlayerClimbingMoveFunc(playerinfo_t* info, float height, float var2, float var3);
+extern qboolean G_PlayerActionCheckPuzzleGrab(playerinfo_t* info);
+extern void G_PlayerActionTakePuzzle(const playerinfo_t* info);
+extern qboolean G_PlayerActionUsePuzzle(const playerinfo_t* info);
+extern qboolean G_PlayerActionCheckPushPull_Ent(const void* ent);
+extern void G_PlayerActionMoveItem(const playerinfo_t* info, float distance);
+extern qboolean G_PlayerActionCheckPushButton(const playerinfo_t* info);
+extern void G_PlayerActionPushButton(const playerinfo_t* info);
+extern qboolean G_PlayerActionCheckPushLever(const playerinfo_t* info);
+extern void G_PlayerActionPushLever(const playerinfo_t* info);
+extern qboolean G_HandleTeleport(const playerinfo_t* info);
+extern void G_SetJointAngles(const playerinfo_t* info);
+extern void G_ResetJointAngles(const playerinfo_t* info);
+extern void G_PlayerActionChickenBite(const playerinfo_t* info);
+extern void G_PlayerFallingDamage(const playerinfo_t* info, float delta);
+extern void G_PlayerActionSwordAttack(const playerinfo_t* info, int value);
+extern void G_PlayerActionSpellFireball(const playerinfo_t* info);
+extern void G_PlayerActionSpellBlast(const playerinfo_t* info);
+extern void G_PlayerActionSpellArray(const playerinfo_t* info, int value);
+extern void G_PlayerActionSpellSphereCreate(const playerinfo_t* info, qboolean* charging);
+extern void G_PlayerActionSpellBigBall(const playerinfo_t* info);
+extern void G_PlayerActionSpellFirewall(const playerinfo_t* info);
+extern void G_PlayerActionRedRainBowAttack(const playerinfo_t* info);
+extern void G_PlayerActionPhoenixBowAttack(const playerinfo_t* info);
+extern void G_PlayerActionHellstaffAttack(const playerinfo_t* info);
+extern void G_PlayerActionSpellDefensive(playerinfo_t* info);
+extern void G_PlayerSpellShieldAttack(const playerinfo_t* info);
+extern void G_PlayerSpellStopShieldAttack(const playerinfo_t* info);
+extern void G_PlayerVaultKick(const playerinfo_t* info);
+extern void G_PlayerActionCheckRopeMove(playerinfo_t* info);
+extern qboolean G_EntIsAButton(const edict_t* ent);
 extern void PlayerChickenDeath(edict_t* self); //mxd
-#endif // _P_FUNCS_H_
