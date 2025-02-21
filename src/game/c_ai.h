b4e9f8c83cff583cfc93f113c1e86f078d42b8cd
m-x-d <j.maxed@gmail.com>
2025-02-21 16:56:00 +0300

    c_ai.h: cleanup, remove ai_c_wav() declaration (unused).

--- src/game/c_ai.h
+++ src/game/c_ai.h
@@ -1,22 +1,19 @@
 //
 // c_ai.h
 //
-// Heretic II
 // Copyright 1998 Raven Software
 //
 
-#ifndef	C_AI_H
-#define C_AI_H
+#pragma once
 
-void ai_c_cycleend (edict_t *self);
-void ai_c_gib(edict_t *self, G_Message_t *msg);
-void ai_c_move (edict_t *self,float x,float y,float z);
-void ai_c_readmessage(edict_t *self, G_Message_t *msg);
-void ai_c_stand (edict_t *self,float forward,float right,float up);
-void ai_c_wav (edict_t *self,float wav_num);
+#include "g_Message.h" //mxd
+#include "q_Typedef.h" //mxd
 
-void c_corvus_init(edict_t *self,int classId);
-void c_character_init(edict_t *self,int classId);
+void ai_c_cycleend(edict_t* self);
+void ai_c_gib(edict_t* self, G_Message_t* msg);
+void ai_c_move(edict_t* self, float forward, float right, float up);
+void ai_c_readmessage(edict_t* self, G_Message_t* msg);
+void ai_c_stand(edict_t* self, float forward, float right, float up);
 
-
-#endif
+void c_corvus_init(edict_t* self, int class_id);
+void c_character_init(edict_t* self, int class_id);
