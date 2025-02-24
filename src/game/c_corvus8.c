c8968d1c2d64e3447d3803399820602119a35cc1
m-x-d <j.maxed@gmail.com>
2025-02-24 14:54:29 +0300

    c_corvus8.c cleanup. c_corvus8.h cleanup. c_corvus8_anim.c cleanup. c_corvus8_anim.h cleanup.

--- src/game/c_corvus8.c
+++ src/game/c_corvus8.c
@@ -1,32 +1,17 @@
-/*-------------------------------------------------------------------
-c_corvus8.c
-
-Heretic II
-Copyright 1998 Raven Software
-
-  AI:
-
-
--------------------------------------------------------------------*/
-#include "g_local.h"
-#include "Utilities.h"
-#include "g_DefaultMessageHandler.h"
-#include "g_monster.h"
-#include "fx.h"
-#include "random.h"
-#include "buoy.h"
-#include "vector.h"
+//
+// c_corvus8.c
+//
+// Copyright 1998 Raven Software
+//
 
 #include "c_corvus8.h"
 #include "c_corvus8_anim.h"
 #include "c_ai.h"
+#include "Utilities.h"
 
-/*----------------------------------------------------------------------
-  plagueElf Base Info
------------------------------------------------------------------------*/
-static animmove_t *animations[ NUM_ANIMS] =
+// Corvus8 cinematic actions.
+static animmove_t* animations[NUM_ANIMS] =
 {
-	// Cinematics
 	&corvus8_move_c_action1,
 	&corvus8_move_c_action2,
 	&corvus8_move_c_action3,
@@ -64,175 +49,197 @@ static animmove_t *animations[ NUM_ANIMS] =
 	&corvus8_move_c_pivotrightstop
 };
 
-static ClassResourceInfo_t resInfo;
-
-
-/*-------------------------------------------------------------------------
-	corvus8_c_anims
--------------------------------------------------------------------------*/
-void corvus8_c_anims(edict_t *self, G_Message_t *msg)
+static void corvus8_c_anims(edict_t* self, G_Message_t* msg)
 {
-	int int_msg;
 	int curr_anim;
 
 	ai_c_readmessage(self, msg);
-	int_msg = (int) msg->ID;
+	self->monsterinfo.c_anim_flag = 0;
 
-	self->monsterinfo.c_anim_flag = 0; 
-
-	switch(int_msg)
+	switch (msg->ID)
 	{
-		case MSG_C_ACTION1 :
+		case MSG_C_ACTION1:
 			self->monsterinfo.c_anim_flag |= C_ANIM_REPEAT;
 			curr_anim = ANIM_C_ACTION1;
 			break;
-		case MSG_C_ACTION2 :
+
+		case MSG_C_ACTION2:
 			self->monsterinfo.c_anim_flag |= C_ANIM_REPEAT;
 			curr_anim = ANIM_C_ACTION2;
 			break;
-		case MSG_C_ACTION3 :
+
+		case MSG_C_ACTION3:
 			self->monsterinfo.c_anim_flag |= C_ANIM_REPEAT;
 			curr_anim = ANIM_C_ACTION3;
 			break;
-		case MSG_C_ACTION4 :
+
+		case MSG_C_ACTION4:
 			self->monsterinfo.c_anim_flag |= C_ANIM_REPEAT;
 			curr_anim = ANIM_C_ACTION4;
 			break;
-		case MSG_C_ACTION5 :
+
+		case MSG_C_ACTION5:
 			self->monsterinfo.c_anim_flag |= C_ANIM_REPEAT;
 			curr_anim = ANIM_C_ACTION5;
 			break;
-		case MSG_C_ACTION6 :
+
+		case MSG_C_ACTION6:
 			self->monsterinfo.c_anim_flag |= C_ANIM_REPEAT;
 			curr_anim = ANIM_C_ACTION6;
 			break;
-		case MSG_C_ACTION7 :
+
+		case MSG_C_ACTION7:
 			self->monsterinfo.c_anim_flag |= C_ANIM_REPEAT;
 			curr_anim = ANIM_C_ACTION7;
 			break;
-		case MSG_C_ACTION8 :
+
+		case MSG_C_ACTION8:
 			self->monsterinfo.c_anim_flag |= C_ANIM_REPEAT;
 			curr_anim = ANIM_C_ACTION8;
 			break;
-		case MSG_C_ACTION9 :
+
+		case MSG_C_ACTION9:
 			self->monsterinfo.c_anim_flag |= C_ANIM_REPEAT;
 			curr_anim = ANIM_C_ACTION9;
 			break;
-		case MSG_C_ACTION10 :
+
+		case MSG_C_ACTION10:
 			self->monsterinfo.c_anim_flag |= C_ANIM_REPEAT;
 			curr_anim = ANIM_C_ACTION10;
 			break;
-		case MSG_C_ACTION11 :
+
+		case MSG_C_ACTION11:
 			self->monsterinfo.c_anim_flag |= C_ANIM_REPEAT;
 			curr_anim = ANIM_C_ACTION11;
 			break;
-		case MSG_C_ACTION12 :
+
+		case MSG_C_ACTION12:
 			self->monsterinfo.c_anim_flag |= C_ANIM_REPEAT;
 			curr_anim = ANIM_C_ACTION12;
 			break;
-		case MSG_C_ACTION13 :
+
+		case MSG_C_ACTION13:
 			self->monsterinfo.c_anim_flag |= C_ANIM_REPEAT;
 			curr_anim = ANIM_C_ACTION13;
 			break;
-		case MSG_C_ACTION14 :
+
+		case MSG_C_ACTION14:
 			self->monsterinfo.c_anim_flag |= C_ANIM_REPEAT;
 			curr_anim = ANIM_C_ACTION14;
 			break;
-		case MSG_C_ACTION15 :
+
+		case MSG_C_ACTION15:
 			self->monsterinfo.c_anim_flag |= C_ANIM_REPEAT;
 			curr_anim = ANIM_C_ACTION15;
 			break;
-		case MSG_C_ACTION16 :
+
+		case MSG_C_ACTION16:
 			self->monsterinfo.c_anim_flag |= C_ANIM_REPEAT;
 			curr_anim = ANIM_C_ACTION16;
 			break;
-		case MSG_C_ACTION17 :
+
+		case MSG_C_ACTION17:
 			self->monsterinfo.c_anim_flag |= C_ANIM_REPEAT;
 			curr_anim = ANIM_C_ACTION17;
 			break;
-		case MSG_C_ACTION18 :
+
+		case MSG_C_ACTION18:
 			self->monsterinfo.c_anim_flag |= C_ANIM_REPEAT;
 			curr_anim = ANIM_C_ACTION18;
 			break;
-		case MSG_C_ACTION19 :
+
+		case MSG_C_ACTION19:
 			self->monsterinfo.c_anim_flag |= C_ANIM_REPEAT;
 			curr_anim = ANIM_C_ACTION19;
 			break;
-		case MSG_C_ACTION20 :
+
+		case MSG_C_ACTION20:
 			self->monsterinfo.c_anim_flag |= C_ANIM_REPEAT;
 			curr_anim = ANIM_C_ACTION20;
 			break;
+
 		case MSG_C_IDLE1:
 			self->monsterinfo.c_anim_flag |= C_ANIM_REPEAT | C_ANIM_IDLE;
 			curr_anim = ANIM_C_IDLE1;
 			break;
+
 		case MSG_C_IDLE2:
 			self->monsterinfo.c_anim_flag |= C_ANIM_REPEAT;
 			curr_anim = ANIM_C_IDLE2;
 			break;
+
 		case MSG_C_PIVOTLEFTGO:
 			self->monsterinfo.c_anim_flag |= C_ANIM_REPEAT;
 			curr_anim = ANIM_C_PIVOTLEFTGO;
 			break;
+
 		case MSG_C_PIVOTLEFT:
 			self->monsterinfo.c_anim_flag |= C_ANIM_MOVE;
 			curr_anim = ANIM_C_PIVOTLEFT;
 			break;
+
 		case MSG_C_PIVOTLEFTSTOP:
 			self->monsterinfo.c_anim_flag |= C_ANIM_REPEAT;
 			curr_anim = ANIM_C_PIVOTLEFTSTOP;
 			break;
+
 		case MSG_C_PIVOTRIGHTGO:
 			self->monsterinfo.c_anim_flag |= C_ANIM_REPEAT;
 			curr_anim = ANIM_C_PIVOTRIGHTGO;
 			break;
+
 		case MSG_C_PIVOTRIGHT:
 			self->monsterinfo.c_anim_flag |= C_ANIM_MOVE;
 			curr_anim = ANIM_C_PIVOTRIGHT;
 			break;
+
 		case MSG_C_PIVOTRIGHTSTOP:
 			self->monsterinfo.c_anim_flag |= C_ANIM_REPEAT;
 			curr_anim = ANIM_C_PIVOTRIGHTSTOP;
 			break;
+
 		case MSG_C_WALKSTART:
 			self->monsterinfo.c_anim_flag |= C_ANIM_REPEAT;
 			curr_anim = ANIM_C_WALKSTART;
 			break;
+
 		case MSG_C_WALK1:
 			self->monsterinfo.c_anim_flag |= C_ANIM_MOVE;
 			curr_anim = ANIM_C_WALK1;
 			break;
+
 		case MSG_C_WALK2:
 			self->monsterinfo.c_anim_flag |= C_ANIM_MOVE;
 			curr_anim = ANIM_C_WALK2;
 			break;
+
 		case MSG_C_WALK3:
 			self->monsterinfo.c_anim_flag |= C_ANIM_MOVE;
 			curr_anim = ANIM_C_WALK3;
 			break;
+
 		case MSG_C_WALKSTOP1:
 			self->monsterinfo.c_anim_flag |= C_ANIM_REPEAT;
 			curr_anim = ANIM_C_WALKSTOP1;
 			break;
+
 		case MSG_C_WALKSTOP2:
 			self->monsterinfo.c_anim_flag |= C_ANIM_REPEAT;
 			curr_anim = ANIM_C_WALKSTOP2;
 			break;
+
 		default:
 			self->monsterinfo.c_anim_flag |= C_ANIM_MOVE;
 			curr_anim = ANIM_C_IDLE1;
 			break;
-	} 
+	}
 
 	SetAnim(self, curr_anim);
 }
 
-
-/*-------------------------------------------------------------------------
-	StaticsInit
--------------------------------------------------------------------------*/
-void Corvus8StaticsInit()
+void Corvus8StaticsInit(void)
 {
+	static ClassResourceInfo_t res_info; //mxd. Made local static.
 
 	classStatics[CID_CORVUS8].msgReceivers[MSG_C_ACTION1] = corvus8_c_anims;
 	classStatics[CID_CORVUS8].msgReceivers[MSG_C_ACTION2] = corvus8_c_anims;
@@ -269,20 +276,16 @@ void Corvus8StaticsInit()
 	classStatics[CID_CORVUS8].msgReceivers[MSG_C_PIVOTRIGHT] = corvus8_c_anims;
 	classStatics[CID_CORVUS8].msgReceivers[MSG_C_PIVOTRIGHTSTOP] = corvus8_c_anims;
 
-	resInfo.numAnims = NUM_ANIMS;
-	resInfo.animations = animations;
-	
-	//note that the name is different in the path
-	resInfo.modelIndex = gi.modelindex("models/player/siernan_scene/tris.fm");
+	res_info.numAnims = NUM_ANIMS;
+	res_info.animations = animations;
+	res_info.modelIndex = gi.modelindex("models/player/siernan_scene/tris.fm"); // Note that the name is different in the path.
 
-	classStatics[CID_CORVUS8].resInfo = &resInfo;
-
+	classStatics[CID_CORVUS8].resInfo = &res_info;
 }
 
-/*QUAKED character_corvus8 (1 .5 0) (-17 -25 -32) (22 12 32) INVISIBLE
-The cinematic corvus for the Siernan scenes
-*/
-void SP_character_corvus8 (edict_t *self)
+// QUAKED character_corvus8 (1 .5 0) (-17 -25 -32) (22 12 32) INVISIBLE
+// The cinematic corvus for the Siernan scenes.
+void SP_character_corvus8(edict_t* self)
 {
-	c_corvus_init(self,CID_CORVUS8);
+	c_corvus_init(self, CID_CORVUS8);
 }
