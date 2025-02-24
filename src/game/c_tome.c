49f4436b1e9addf6d586141c3023fbe8748805fa
m-x-d <j.maxed@gmail.com>
2025-02-24 18:04:10 +0300

    c_tome.c cleanup. c_tome.h cleanup. c_tome_anim.c cleanup. c_tome_anim.h cleanup.

--- src/game/c_tome.c
+++ src/game/c_tome.c
@@ -1,97 +1,70 @@
-/*-------------------------------------------------------------------
-c_tome.c
-
-Heretic II
-Copyright 1998 Raven Software
-
-  AI:
-
-
--------------------------------------------------------------------*/
-#include "g_local.h"
-#include "Utilities.h"
-#include "g_DefaultMessageHandler.h"
-#include "g_monster.h"
-#include "fx.h"
-#include "random.h"
-#include "vector.h"
+//
+// c_tome.c
+//
+// Copyright 1998 Raven Software
+//
 
 #include "c_tome.h"
 #include "c_tome_anim.h"
 #include "c_ai.h"
+#include "Utilities.h"
+#include "Vector.h"
 
-/*----------------------------------------------------------------------
-  plagueElf Base Info
------------------------------------------------------------------------*/
-static animmove_t *animations[ NUM_ANIMS] =
+// Tome of Power cinematic actions.
+static animmove_t* animations[NUM_ANIMS] =
 {
-	// Cinematics
 	&tome_move_c_idle1,
 	&tome_move_c_idle2,
 };
 
-static ClassResourceInfo_t resInfo;
-
-
-/*-------------------------------------------------------------------------
-	tome_c_anims
--------------------------------------------------------------------------*/
-void tome_c_anims(edict_t *self, G_Message_t *msg)
+static void tome_c_anims(edict_t* self, G_Message_t* msg)
 {
-	int int_msg;
 	int curr_anim;
 
 	ai_c_readmessage(self, msg);
-	int_msg = (int) msg->ID;
+	self->monsterinfo.c_anim_flag = 0;
 
-	self->monsterinfo.c_anim_flag = 0; 
-
-	switch(int_msg)
+	switch (msg->ID)
 	{
 		case MSG_C_IDLE1:
-			self->monsterinfo.c_anim_flag |= C_ANIM_REPEAT | C_ANIM_IDLE;
+			self->monsterinfo.c_anim_flag |= (C_ANIM_REPEAT | C_ANIM_IDLE);
 			curr_anim = ANIM_C_IDLE1;
 			break;
+
 		case MSG_C_IDLE2:
 			self->monsterinfo.c_anim_flag |= C_ANIM_REPEAT;
 			curr_anim = ANIM_C_IDLE2;
 			break;
+
 		default:
 			self->monsterinfo.c_anim_flag |= C_ANIM_MOVE;
 			curr_anim = ANIM_C_IDLE1;
 			break;
-	} 
+	}
 
 	SetAnim(self, curr_anim);
 }
 
-
-/*-------------------------------------------------------------------------
-	PlagueElfStaticsInit
--------------------------------------------------------------------------*/
-void TomeStaticsInit()
+void TomeStaticsInit(void)
 {
+	static ClassResourceInfo_t res_info; //mxd. Made local static.
 
 	classStatics[CID_C_TOME].msgReceivers[MSG_C_IDLE1] = tome_c_anims;
 	classStatics[CID_C_TOME].msgReceivers[MSG_C_IDLE2] = tome_c_anims;
 
-	resInfo.numAnims = NUM_ANIMS;
-	resInfo.animations = animations;
-	
-	resInfo.modelIndex = gi.modelindex("models/spells/book/tris.fm");
+	res_info.numAnims = NUM_ANIMS;
+	res_info.animations = animations;
+	res_info.modelIndex = gi.modelindex("models/spells/book/tris.fm");
 
-	classStatics[CID_C_TOME].resInfo = &resInfo;
-
+	classStatics[CID_C_TOME].resInfo = &res_info;
 }
 
-
-/*QUAKED character_tome (1 .5 0) (-4 -8 -12) (4 8 12) INVISIBLE
-The talking tome of power (sounds like a hot babe)
-*/
-void SP_character_tome (edict_t *self)
+// QUAKED character_tome (1 .5 0) (-4 -8 -12) (4 8 12) INVISIBLE
+// The talking tome of power (sounds like a hot babe).
+void SP_character_tome(edict_t* self)
 {
-	VectorSet (self->mins, -4, -8, -12);
-	VectorSet (self->maxs, 4, 8, 12);	
+	VectorSet(self->mins, -4.0f, -8.0f, -12.0f);
+	VectorSet(self->maxs,  4.0f,  8.0f,  12.0f);
 
-	c_character_init(self,CID_C_TOME);
+	c_character_init(self, CID_C_TOME);
 }
--- src/game/c_tome.h
+++ src/game/c_tome.h
@@ -1,9 +1,12 @@
-typedef enum AnimID_e
-{	
-	ANIM_C_IDLE1,
-	ANIM_C_IDLE2,
-	NUM_ANIMS
-} AnimID_t;
+//
+// c_tome.h
+//
+// Copyright 1998 Raven Software
+//
 
-extern animmove_t tome_move_c_idle1; 
-extern animmove_t tome_move_c_idle2; 
+#pragma once
+
+#include "q_Typedef.h"
+
+extern void TomeStaticsInit(void); //mxd
+extern void SP_character_tome(edict_t* self); //mxd
