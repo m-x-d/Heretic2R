6740bcb4d0079262c91648ae1b7b239819eba41e
m-x-d <j.maxed@gmail.com>
2025-02-24 13:16:32 +0300

    c_corvus3.c cleanup. c_corvus3.h cleanup. c_corvus3_anim.c cleanup. c_corvus3_anim.h cleanup.

--- src/game/c_corvus3.c
+++ src/game/c_corvus3.c
@@ -1,32 +1,20 @@
-/*-------------------------------------------------------------------
-c_corvus.c
-
-Heretic II
-Copyright 1998 Raven Software
-
-  AI:
-
-
--------------------------------------------------------------------*/
-#include "g_local.h"
-#include "Utilities.h"
-#include "g_DefaultMessageHandler.h"
-#include "g_monster.h"
-#include "fx.h"
-#include "random.h"
-#include "buoy.h"
-#include "vector.h"
+//
+// c_corvus3.c
+//
+// Copyright 1998 Raven Software
+//
 
 #include "c_corvus3.h"
 #include "c_corvus3_anim.h"
 #include "c_ai.h"
+#include "FX.h"
+#include "Random.h"
+#include "Utilities.h"
+#include "Vector.h"
 
-/*----------------------------------------------------------------------
-  plagueElf Base Info
------------------------------------------------------------------------*/
-static animmove_t *animations[NUM_ANIMS] =
+// Corvus3 cinematic actions.
+static animmove_t* animations[NUM_ANIMS] =
 {
-	// Cinematics
 	&corvus3_move_c_action1,
 	&corvus3_move_c_action2,
 	&corvus3_move_c_action3,
@@ -64,194 +52,221 @@ static animmove_t *animations[NUM_ANIMS] =
 	&corvus3_move_c_pivotrightstop
 };
 
-static ClassResourceInfo_t resInfo;
-
-void corvus3_invisible (edict_t *self)
+void corvus3_invisible(edict_t* self)
 {
 	self->s.modelindex = 0;
 }
 
-void corvus3_visible (edict_t *self)
+void corvus3_visible(edict_t* self)
 {
-	self->s.modelindex = self->count;
+	self->s.modelindex = (byte)self->count;
 }
 
-void corvus3_teleportsmalleffect (edict_t *self)
+void corvus3_teleportsmalleffect(edict_t* self)
 {
-	vec3_t holdorigin,forward,right;
-	int holdfor;
+	vec3_t hold_origin;
+	VectorCopy(self->s.origin, hold_origin);
 
-	VectorCopy(self->s.origin,holdorigin);
-
+	vec3_t forward;
+	vec3_t right;
 	AngleVectors(self->s.angles, forward, right, NULL);
 
-	holdfor = irand(5,10);
-	VectorMA(holdorigin, holdfor, forward, holdorigin);
-	holdfor = irand(-5,5);
-	VectorMA(holdorigin, holdfor, right, holdorigin);
-	holdorigin[2] += flrand(-25,5);
+	float holdfor = flrand(5.0f, 10.0f); //mxd. int/irand() in original version.
+	VectorMA(hold_origin, holdfor, forward, hold_origin);
 
-	gi.sound(self, CHAN_WEAPON, gi.soundindex("player/picup.wav"), 1, ATTN_NORM, 0);
-	gi.CreateEffect(NULL, FX_PICKUP, 0, holdorigin, NULL);
+	holdfor = flrand(-5.0f, 5.0f); //mxd. irand() in original version.
+	VectorMA(hold_origin, holdfor, right, hold_origin);
+	hold_origin[2] += flrand(-25.0f, 5.0f);
+
+	gi.sound(self, CHAN_WEAPON, gi.soundindex("player/picup.wav"), 1.0f, ATTN_NORM, 0.0f);
+	gi.CreateEffect(NULL, FX_PICKUP, 0, hold_origin, NULL);
 }
 
-void corvus3_teleporteffect (edict_t *self)
+void corvus3_teleporteffect(edict_t* self)
 {
-	gi.sound(self,CHAN_WEAPON,gi.soundindex("weapons/teleport.wav"),1,ATTN_NORM,0);
+	gi.sound(self, CHAN_WEAPON, gi.soundindex("weapons/teleport.wav"), 1.0f, ATTN_NORM, 0.0f);
 	gi.CreateEffect(&self->s, FX_PLAYER_TELEPORT_IN, CEF_OWNERS_ORIGIN, self->s.origin, NULL);
 }
 
-/*-------------------------------------------------------------------------
-	corvus3_c_anims
--------------------------------------------------------------------------*/
-void corvus3_c_anims(edict_t *self, G_Message_t *msg)
+static void corvus3_c_anims(edict_t* self, G_Message_t* msg)
 {
-	int int_msg;
 	int curr_anim;
 
 	ai_c_readmessage(self, msg);
-	int_msg = (int) msg->ID;
+	self->monsterinfo.c_anim_flag = 0;
 
-	self->monsterinfo.c_anim_flag = 0; 
-
-	switch(int_msg)
+	switch (msg->ID)
 	{
 		case MSG_C_ACTION1:
 			self->monsterinfo.c_anim_flag |= C_ANIM_REPEAT;
 			curr_anim = ANIM_C_ACTION1;
 			break;
+
 		case MSG_C_ACTION2:
 			self->monsterinfo.c_anim_flag |= C_ANIM_REPEAT;
 			curr_anim = ANIM_C_ACTION2;
 			break;
+
 		case MSG_C_ACTION3:
 			self->monsterinfo.c_anim_flag |= C_ANIM_REPEAT;
 			curr_anim = ANIM_C_ACTION3;
 			break;
+
 		case MSG_C_ACTION4:
 			self->monsterinfo.c_anim_flag |= C_ANIM_REPEAT;
 			curr_anim = ANIM_C_ACTION4;
 			break;
+
 		case MSG_C_ACTION5:
 			self->monsterinfo.c_anim_flag |= C_ANIM_REPEAT;
 			curr_anim = ANIM_C_ACTION5;
 			break;
+
 		case MSG_C_ACTION6:
 			self->monsterinfo.c_anim_flag |= C_ANIM_REPEAT;
 			curr_anim = ANIM_C_ACTION6;
 			break;
+
 		case MSG_C_ACTION7:
 			self->monsterinfo.c_anim_flag |= C_ANIM_REPEAT;
 			curr_anim = ANIM_C_ACTION7;
 			break;
+
 		case MSG_C_ACTION8:
 			self->monsterinfo.c_anim_flag |= C_ANIM_REPEAT;
 			curr_anim = ANIM_C_ACTION8;
 			break;
+
 		case MSG_C_ACTION9:
 			self->monsterinfo.c_anim_flag |= C_ANIM_REPEAT;
 			curr_anim = ANIM_C_ACTION9;
 			break;
+
 		case MSG_C_ACTION10:
 			self->monsterinfo.c_anim_flag |= C_ANIM_REPEAT;
 			curr_anim = ANIM_C_ACTION10;
 			break;
+
 		case MSG_C_ACTION11:
 			self->monsterinfo.c_anim_flag |= C_ANIM_REPEAT;
 			curr_anim = ANIM_C_ACTION11;
 			break;
+
 		case MSG_C_ACTION12:
 			self->monsterinfo.c_anim_flag |= C_ANIM_REPEAT;
 			curr_anim = ANIM_C_ACTION12;
 			break;
+
 		case MSG_C_ACTION13:
 			self->monsterinfo.c_anim_flag |= C_ANIM_REPEAT;
 			curr_anim = ANIM_C_ACTION13;
 			break;
+
 		case MSG_C_ACTION14:
 			self->monsterinfo.c_anim_flag |= C_ANIM_REPEAT;
 			curr_anim = ANIM_C_ACTION14;
 			break;
+
 		case MSG_C_ACTION15:
 			self->monsterinfo.c_anim_flag |= C_ANIM_REPEAT;
 			curr_anim = ANIM_C_ACTION15;
 			break;
+
 		case MSG_C_ACTION16:
 			self->monsterinfo.c_anim_flag |= C_ANIM_REPEAT;
 			curr_anim = ANIM_C_ACTION16;
 			break;
+
 		case MSG_C_ACTION17:
 			self->monsterinfo.c_anim_flag |= C_ANIM_REPEAT;
 			curr_anim = ANIM_C_ACTION17;
 			break;
+
 		case MSG_C_ACTION18:
 			self->monsterinfo.c_anim_flag |= C_ANIM_REPEAT;
 			curr_anim = ANIM_C_ACTION18;
 			break;
+
 		case MSG_C_ACTION19:
 			self->monsterinfo.c_anim_flag |= C_ANIM_REPEAT;
 			curr_anim = ANIM_C_ACTION19;
 			break;
+
 		case MSG_C_ACTION20:
 			self->monsterinfo.c_anim_flag |= C_ANIM_REPEAT;
 			curr_anim = ANIM_C_ACTION20;
 			break;
+
 		case MSG_C_IDLE1:
 			self->monsterinfo.c_anim_flag |= C_ANIM_REPEAT | C_ANIM_IDLE;
 			curr_anim = ANIM_C_IDLE1;
 			break;
+
 		case MSG_C_IDLE2:
 			self->monsterinfo.c_anim_flag |= C_ANIM_REPEAT | C_ANIM_IDLE;
 			curr_anim = ANIM_C_IDLE2;
 			break;
+
 		case MSG_C_IDLE3:
 			self->monsterinfo.c_anim_flag |= C_ANIM_REPEAT | C_ANIM_IDLE;
 			curr_anim = ANIM_C_IDLE3;
 			break;
+
 		case MSG_C_PIVOTLEFTGO:
 			self->monsterinfo.c_anim_flag |= C_ANIM_REPEAT;
 			curr_anim = ANIM_C_PIVOTLEFTGO;
 			break;
+
 		case MSG_C_PIVOTLEFT:
 			self->monsterinfo.c_anim_flag |= C_ANIM_MOVE;
 			curr_anim = ANIM_C_PIVOTLEFT;
 			break;
+
 		case MSG_C_PIVOTLEFTSTOP:
 			self->monsterinfo.c_anim_flag |= C_ANIM_REPEAT;
 			curr_anim = ANIM_C_PIVOTLEFTSTOP;
 			break;
+
 		case MSG_C_PIVOTRIGHTGO:
 			self->monsterinfo.c_anim_flag |= C_ANIM_REPEAT;
 			curr_anim = ANIM_C_PIVOTRIGHTGO;
 			break;
+
 		case MSG_C_PIVOTRIGHT:
 			self->monsterinfo.c_anim_flag |= C_ANIM_MOVE;
 			curr_anim = ANIM_C_PIVOTRIGHT;
 			break;
+
 		case MSG_C_PIVOTRIGHTSTOP:
 			self->monsterinfo.c_anim_flag |= C_ANIM_REPEAT;
 			curr_anim = ANIM_C_PIVOTRIGHTSTOP;
 			break;
+
 		case MSG_C_WALKSTART:
 			self->monsterinfo.c_anim_flag |= C_ANIM_REPEAT;
 			curr_anim = ANIM_C_WALKSTART;
 			break;
+
 		case MSG_C_WALK1:
 			self->monsterinfo.c_anim_flag |= C_ANIM_MOVE;
 			curr_anim = ANIM_C_WALK1;
 			break;
+
 		case MSG_C_WALK2:
 			self->monsterinfo.c_anim_flag |= C_ANIM_MOVE;
 			curr_anim = ANIM_C_WALK2;
 			break;
+
 		case MSG_C_WALK3:
 			self->monsterinfo.c_anim_flag |= C_ANIM_MOVE;
 			curr_anim = ANIM_C_WALK3;
 			break;
+
 		case MSG_C_WALKSTOP1:
 			self->monsterinfo.c_anim_flag |= C_ANIM_REPEAT;
 			curr_anim = ANIM_C_WALKSTOP1;
 			break;
+
 		case MSG_C_WALKSTOP2:
 			self->monsterinfo.c_anim_flag |= C_ANIM_REPEAT;
 			curr_anim = ANIM_C_WALKSTOP2;
@@ -261,17 +276,15 @@ void corvus3_c_anims(edict_t *self, G_Message_t *msg)
 			self->monsterinfo.c_anim_flag |= C_ANIM_MOVE;
 			curr_anim = ANIM_C_IDLE1;
 			break;
-	} 
+	}
 
 	SetAnim(self, curr_anim);
 }
 
-
-/*-------------------------------------------------------------------------
-	PlagueElfStaticsInit
--------------------------------------------------------------------------*/
-void Corvus3StaticsInit()
+void Corvus3StaticsInit(void)
 {
+	static ClassResourceInfo_t res_info; //mxd. Made local static.
+
 	classStatics[CID_CORVUS3].msgReceivers[MSG_C_ACTION1] = corvus3_c_anims;
 	classStatics[CID_CORVUS3].msgReceivers[MSG_C_ACTION2] = corvus3_c_anims;
 	classStatics[CID_CORVUS3].msgReceivers[MSG_C_ACTION3] = corvus3_c_anims;
@@ -308,20 +321,16 @@ void Corvus3StaticsInit()
 	classStatics[CID_CORVUS3].msgReceivers[MSG_C_PIVOTRIGHT] = corvus3_c_anims;
 	classStatics[CID_CORVUS3].msgReceivers[MSG_C_PIVOTRIGHTSTOP] = corvus3_c_anims;
 
-	resInfo.numAnims = NUM_ANIMS;
-	resInfo.animations = animations;
-	
-	resInfo.modelIndex = gi.modelindex("models/player/highpriestess_scene/tris.fm");
+	res_info.numAnims = NUM_ANIMS;
+	res_info.animations = animations;
+	res_info.modelIndex = gi.modelindex("models/player/highpriestess_scene/tris.fm");
 
-	classStatics[CID_CORVUS3].resInfo = &resInfo;
-
+	classStatics[CID_CORVUS3].resInfo = &res_info;
 }
 
-
-/*QUAKED character_corvus3 (1 .5 0) (-17 -25 -32) (22 12 32) INVISIBLE
-The cinematic Corvus for the high priestess scene
-*/
-void SP_character_corvus3 (edict_t *self)
+// QUAKED character_corvus3 (1 .5 0) (-17 -25 -32) (22 12 32) INVISIBLE
+// The cinematic Corvus for the high priestess scene.
+void SP_character_corvus3(edict_t* self)
 {
-	c_corvus_init(self,CID_CORVUS3);
+	c_corvus_init(self, CID_CORVUS3);
 }
