5b95a02ee7ae1d64d954447da57b7bc4a29e85a1
m-x-d <j.maxed@gmail.com>
2025-02-24 14:21:50 +0300

    c_corvus5.c cleanup. c_corvus5.h cleanup. c_corvus5_anim.c cleanup. c_corvus5_anim.h cleanup.

--- src/game/c_corvus5.c
+++ src/game/c_corvus5.c
@@ -1,32 +1,17 @@
-/*-------------------------------------------------------------------
-c_corvus.c
-
-Heretic II
-Copyright 1998 Raven Software
-
-  AI:
-
-
--------------------------------------------------------------------*/
-#include "g_local.h"
-#include "Utilities.h"
-#include "g_DefaultMessageHandler.h"
-#include "g_monster.h"
-#include "fx.h"
-#include "random.h"
-#include "buoy.h"
-#include "vector.h"
+//
+// c_corvus5.c
+//
+// Copyright 1998 Raven Software
+//
 
 #include "c_corvus5.h"
 #include "c_corvus5_anim.h"
 #include "c_ai.h"
+#include "Utilities.h"
 
-/*----------------------------------------------------------------------
-  plagueElf Base Info
------------------------------------------------------------------------*/
-static animmove_t *animations[NUM_ANIMS] =
+// Corvus5 cinematic actions.
+static animmove_t* animations[NUM_ANIMS] =
 {
-	// Cinematics
 	&corvus5_move_c_action1,
 	&corvus5_move_c_action2,
 	&corvus5_move_c_action3,
@@ -51,108 +36,120 @@ static animmove_t *animations[NUM_ANIMS] =
 	&corvus5_move_c_pivotrightstop
 };
 
-static ClassResourceInfo_t resInfo;
-
-
-/*-------------------------------------------------------------------------
-	corvus5_c_anims
--------------------------------------------------------------------------*/
-void corvus5_c_anims(edict_t *self, G_Message_t *msg)
+static void corvus5_c_anims(edict_t* self, G_Message_t* msg)
 {
-	int int_msg;
 	int curr_anim;
 
 	ai_c_readmessage(self, msg);
-	int_msg = (int) msg->ID;
+	self->monsterinfo.c_anim_flag = 0;
 
-	self->monsterinfo.c_anim_flag = 0; 
-
-	switch(int_msg)
+	switch (msg->ID)
 	{
 		case MSG_C_ACTION1:
 			self->monsterinfo.c_anim_flag |= C_ANIM_REPEAT;
 			curr_anim = ANIM_C_ACTION1;
 			break;
+
 		case MSG_C_ACTION2:
 			self->monsterinfo.c_anim_flag |= C_ANIM_REPEAT;
 			curr_anim = ANIM_C_ACTION2;
 			break;
+
 		case MSG_C_ACTION3:
 			self->monsterinfo.c_anim_flag |= C_ANIM_REPEAT;
 			curr_anim = ANIM_C_ACTION3;
 			break;
+
 		case MSG_C_ACTION4:
 			self->monsterinfo.c_anim_flag |= C_ANIM_REPEAT;
 			curr_anim = ANIM_C_ACTION4;
 			break;
+
 		case MSG_C_ACTION5:
 			self->monsterinfo.c_anim_flag |= C_ANIM_REPEAT;
 			curr_anim = ANIM_C_ACTION5;
 			break;
+
 		case MSG_C_ACTION6:
 			self->monsterinfo.c_anim_flag |= C_ANIM_REPEAT;
 			curr_anim = ANIM_C_ACTION6;
 			break;
+
 		case MSG_C_ACTION7:
 			self->monsterinfo.c_anim_flag |= C_ANIM_REPEAT;
 			curr_anim = ANIM_C_ACTION7;
 			break;
+
 		case MSG_C_ACTION8:
 			self->monsterinfo.c_anim_flag |= C_ANIM_REPEAT;
 			curr_anim = ANIM_C_ACTION8;
 			break;
+
 		case MSG_C_IDLE1:
 			self->monsterinfo.c_anim_flag |= C_ANIM_REPEAT | C_ANIM_IDLE;
 			curr_anim = ANIM_C_IDLE1;
 			break;
+
 		case MSG_C_IDLE2:
 			self->monsterinfo.c_anim_flag |= C_ANIM_REPEAT;
 			curr_anim = ANIM_C_IDLE2;
 			break;
+
 		case MSG_C_IDLE3:
 			self->monsterinfo.c_anim_flag |= C_ANIM_REPEAT;
 			curr_anim = ANIM_C_IDLE3;
 			break;
+
 		case MSG_C_PIVOTLEFTGO:
 			self->monsterinfo.c_anim_flag |= C_ANIM_REPEAT;
 			curr_anim = ANIM_C_PIVOTLEFTGO;
 			break;
+
 		case MSG_C_PIVOTLEFT:
 			self->monsterinfo.c_anim_flag |= C_ANIM_MOVE;
 			curr_anim = ANIM_C_PIVOTLEFT;
 			break;
+
 		case MSG_C_PIVOTLEFTSTOP:
 			self->monsterinfo.c_anim_flag |= C_ANIM_REPEAT;
 			curr_anim = ANIM_C_PIVOTLEFTSTOP;
 			break;
+
 		case MSG_C_PIVOTRIGHTGO:
 			self->monsterinfo.c_anim_flag |= C_ANIM_REPEAT;
 			curr_anim = ANIM_C_PIVOTRIGHTGO;
 			break;
+
 		case MSG_C_PIVOTRIGHT:
 			self->monsterinfo.c_anim_flag |= C_ANIM_MOVE;
 			curr_anim = ANIM_C_PIVOTRIGHT;
 			break;
+
 		case MSG_C_PIVOTRIGHTSTOP:
 			self->monsterinfo.c_anim_flag |= C_ANIM_REPEAT;
 			curr_anim = ANIM_C_PIVOTRIGHTSTOP;
 			break;
+
 		case MSG_C_WALKSTART:
 			self->monsterinfo.c_anim_flag |= C_ANIM_REPEAT;
 			curr_anim = ANIM_C_WALKSTART;
 			break;
+
 		case MSG_C_WALK1:
 			self->monsterinfo.c_anim_flag |= C_ANIM_MOVE;
 			curr_anim = ANIM_C_WALK1;
 			break;
+
 		case MSG_C_WALK2:
 			self->monsterinfo.c_anim_flag |= C_ANIM_MOVE;
 			curr_anim = ANIM_C_WALK2;
 			break;
+
 		case MSG_C_WALKSTOP1:
 			self->monsterinfo.c_anim_flag |= C_ANIM_REPEAT;
 			curr_anim = ANIM_C_WALKSTOP1;
 			break;
+
 		case MSG_C_WALKSTOP2:
 			self->monsterinfo.c_anim_flag |= C_ANIM_REPEAT;
 			curr_anim = ANIM_C_WALKSTOP2;
@@ -162,17 +159,15 @@ void corvus5_c_anims(edict_t *self, G_Message_t *msg)
 			self->monsterinfo.c_anim_flag |= C_ANIM_MOVE;
 			curr_anim = ANIM_C_IDLE1;
 			break;
-	} 
+	}
 
 	SetAnim(self, curr_anim);
 }
 
-
-/*-------------------------------------------------------------------------
-	PlagueElfStaticsInit
--------------------------------------------------------------------------*/
-void Corvus5StaticsInit()
+void Corvus5StaticsInit(void)
 {
+	static ClassResourceInfo_t res_info; //mxd. Made local static.
+
 	classStatics[CID_CORVUS5].msgReceivers[MSG_C_ACTION1] = corvus5_c_anims;
 	classStatics[CID_CORVUS5].msgReceivers[MSG_C_ACTION2] = corvus5_c_anims;
 	classStatics[CID_CORVUS5].msgReceivers[MSG_C_ACTION3] = corvus5_c_anims;
@@ -196,20 +191,16 @@ void Corvus5StaticsInit()
 	classStatics[CID_CORVUS5].msgReceivers[MSG_C_PIVOTRIGHT] = corvus5_c_anims;
 	classStatics[CID_CORVUS5].msgReceivers[MSG_C_PIVOTRIGHTSTOP] = corvus5_c_anims;
 
-	resInfo.numAnims = NUM_ANIMS;
-	resInfo.animations = animations;
-	
-	resInfo.modelIndex = gi.modelindex("models/player/dranor_scene/tris.fm");
+	res_info.numAnims = NUM_ANIMS;
+	res_info.animations = animations;
+	res_info.modelIndex = gi.modelindex("models/player/dranor_scene/tris.fm");
 
-	classStatics[CID_CORVUS5].resInfo = &resInfo;
-
+	classStatics[CID_CORVUS5].resInfo = &res_info;
 }
 
-
-/*QUAKED character_corvus5 (1 .5 0) (-16 -16 -34) (16 16 25) INVISIBLE
-The cinematic Corvus for the Dranor scene
-*/
-void SP_character_corvus5 (edict_t *self)
+// QUAKED character_corvus5 (1 .5 0) (-16 -16 -34) (16 16 25) INVISIBLE
+// The cinematic Corvus for the Dranor scene.
+void SP_character_corvus5(edict_t* self)
 {
-	c_corvus_init(self,CID_CORVUS5);
+	c_corvus_init(self, CID_CORVUS5);
 }
