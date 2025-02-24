eea504b79c73faf125f1365c7b4099f07fd0ea0e
m-x-d <j.maxed@gmail.com>
2025-02-24 16:19:00 +0300

    c_morcalavin.c cleanup. c_morcalavin.h cleanup. c_morcalavin_anim.c cleanup. c_morcalavin_anim.h cleanup.

--- src/game/c_morcalavin.h
+++ src/game/c_morcalavin.h
@@ -1,20 +1,12 @@
-typedef enum AnimID_e
-{	
-	ANIM_C_ACTION1,
-	ANIM_C_ACTION2,
-	ANIM_C_ACTION3,
-	ANIM_C_IDLE1,
-	ANIM_C_IDLE2,
-	ANIM_C_IDLE3,
-	ANIM_C_IDLE4,
-	NUM_ANIMS
-} AnimID_t;
+//
+// c_morcalavin.h
+//
+// Copyright 1998 Raven Software
+//
 
-extern animmove_t morcalavin_move_c_action1; 
-extern animmove_t morcalavin_move_c_action2; 
-extern animmove_t morcalavin_move_c_action3; 
-extern animmove_t morcalavin_move_c_idle1; 
-extern animmove_t morcalavin_move_c_idle2; 
-extern animmove_t morcalavin_move_c_idle3; 
-extern animmove_t morcalavin_move_c_idle4; 
+#pragma once
 
+#include "q_Typedef.h"
+
+extern void MorcalavinCinStaticsInit(void); //mxd
+extern void SP_character_morcalavin(edict_t* self); //mxd
