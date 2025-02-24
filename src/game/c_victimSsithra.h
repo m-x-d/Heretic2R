d0217092ef03fc71cf7564115ba9bc1020b7bb50
m-x-d <j.maxed@gmail.com>
2025-02-24 17:38:36 +0300

    c_victimSsithra.c cleanup. c_victimSsithra.h cleanup. c_victimSsithra_anim.c cleanup. c_victimSsithra_anim.h cleanup.

--- src/game/c_victimSsithra.h
+++ src/game/c_victimSsithra.h
@@ -1,28 +1,12 @@
-//c_victimSsithra.h
+//
+// c_victimSsithra.h
+//
+// Copyright 1998 Raven Software
+//
 
+#pragma once
 
-typedef enum AnimID_e
-{
-	ANIM_C_ACTION1,
-	ANIM_C_ACTION2,
-	ANIM_C_ACTION3,
-	ANIM_C_ACTION4,
-	ANIM_C_ACTION5,
-	ANIM_C_ACTION6,
-	NUM_ANIMS
-} AnimID_t;
+#include "q_Typedef.h"
 
-typedef enum SoundID_e
-{
-	SND_PAIN1,
-	NUM_SOUNDS
-} SoundID_t;
-
-#define MODEL_SCALE		1.000000
-
-extern animmove_t victimSsithra_move_c_action1; 
-extern animmove_t victimSsithra_move_c_action2; 
-extern animmove_t victimSsithra_move_c_action3; 
-extern animmove_t victimSsithra_move_c_action4; 
-extern animmove_t victimSsithra_move_c_action5; 
-extern animmove_t victimSsithra_move_c_action6; 
+extern void VictimSsithraStaticsInit(void); //mxd
+void SP_character_ssithra_victim(edict_t* self); //mxd
