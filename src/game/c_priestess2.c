cdac799ca07485fa3aa878d353b00669e70d460e
m-x-d <j.maxed@gmail.com>
2025-02-24 16:49:46 +0300

    c_priestess2.c cleanup. c_priestess2.h cleanup. c_priestess2_anim.c cleanup. c_priestess2_anim.h cleanup.

--- src/game/c_priestess2.c
+++ src/game/c_priestess2.c
@@ -1,31 +1,18 @@
-/*-------------------------------------------------------------------
-c_priestess2.c
-
-Heretic II
-Copyright 1998 Raven Software
-
-  AI:
-
-
--------------------------------------------------------------------*/
-#include "g_local.h"
-#include "Utilities.h"
-#include "g_DefaultMessageHandler.h"
-#include "g_monster.h"
-#include "fx.h"
-#include "random.h"
-#include "vector.h"
+//
+// c_priestess2.c
+//
+// Copyright 1998 Raven Software
+//
 
 #include "c_priestess2.h"
 #include "c_priestess2_anim.h"
 #include "c_ai.h"
+#include "Utilities.h"
+#include "Vector.h"
 
-/*----------------------------------------------------------------------
-  plagueElf Base Info
------------------------------------------------------------------------*/
-static animmove_t *animations[NUM_ANIMS] =
+// High priestess cinematic actions.
+static animmove_t* animations[NUM_ANIMS] =
 {
-	// Cinematics
 	&priestess2_move_c_action1,
 	&priestess2_move_c_action2,
 	&priestess2_move_c_action3,
@@ -36,77 +23,74 @@ static animmove_t *animations[NUM_ANIMS] =
 	&priestess2_move_c_idle1,
 };
 
-static ClassResourceInfo_t resInfo;
-
-
-/*-------------------------------------------------------------------------
-	priestess2_c_anims
--------------------------------------------------------------------------*/
-void priestess2_c_anims(edict_t *self, G_Message_t *msg)
+static void priestess2_c_anims(edict_t* self, G_Message_t* msg)
 {
-	int int_msg;
 	int curr_anim;
 
 	ai_c_readmessage(self, msg);
-	int_msg = (int) msg->ID;
+	self->monsterinfo.c_anim_flag = 0;
 
-	self->monsterinfo.c_anim_flag = 0; 
-
-	switch(int_msg)
+	switch (msg->ID)
 	{
-		case MSG_C_ACTION1 :
+		case MSG_C_ACTION1:
 			self->monsterinfo.c_anim_flag |= C_ANIM_REPEAT;
 			self->monsterinfo.c_dist = 40;
 			curr_anim = ANIM_C_ACTION1;
 			break;
-		case MSG_C_ACTION2 :
+
+		case MSG_C_ACTION2:
 			self->monsterinfo.c_anim_flag |= C_ANIM_REPEAT;
 			self->monsterinfo.c_dist = 16;
 			curr_anim = ANIM_C_ACTION2;
 			break;
-		case MSG_C_ACTION3 :
+
+		case MSG_C_ACTION3:
 			self->monsterinfo.c_anim_flag |= C_ANIM_REPEAT;
 			self->monsterinfo.c_dist = 158;
 			curr_anim = ANIM_C_ACTION3;
 			break;
-		case MSG_C_ACTION4 :
+
+		case MSG_C_ACTION4:
 			self->monsterinfo.c_anim_flag |= C_ANIM_REPEAT;
 			self->monsterinfo.c_dist = 96;
 			curr_anim = ANIM_C_ACTION4;
 			break;
-		case MSG_C_ACTION5 :
+
+		case MSG_C_ACTION5:
 			self->monsterinfo.c_anim_flag |= C_ANIM_REPEAT;
 			self->monsterinfo.c_dist = 158;
 			curr_anim = ANIM_C_ACTION5;
 			break;
-		case MSG_C_ACTION6 :
+
+		case MSG_C_ACTION6:
 			self->monsterinfo.c_anim_flag |= C_ANIM_REPEAT;
 			self->monsterinfo.c_dist = 72;
 			curr_anim = ANIM_C_ACTION6;
 			break;
-		case MSG_C_ACTION7 :
+
+		case MSG_C_ACTION7:
 			self->monsterinfo.c_anim_flag |= C_ANIM_REPEAT;
 			curr_anim = ANIM_C_ACTION7;
 			break;
+
 		case MSG_C_IDLE1:
 			self->monsterinfo.c_anim_flag |= C_ANIM_REPEAT | C_ANIM_IDLE;
 			curr_anim = ANIM_C_IDLE1;
 			break;
+
 		default:
 			self->monsterinfo.c_anim_flag |= C_ANIM_MOVE;
 			curr_anim = ANIM_C_IDLE1;
 			break;
-	} 
+	}
 
 	SetAnim(self, curr_anim);
 }
 
-
-/*-------------------------------------------------------------------------
-	StaticsInit
--------------------------------------------------------------------------*/
-void Priestess2CStaticsInit()
+void Priestess2CStaticsInit(void)
 {
+	static ClassResourceInfo_t res_info; //mxd. Made local static.
+
 	classStatics[CID_C_HIGHPRIESTESS2].msgReceivers[MSG_C_ACTION1] = priestess2_c_anims;
 	classStatics[CID_C_HIGHPRIESTESS2].msgReceivers[MSG_C_ACTION2] = priestess2_c_anims;
 	classStatics[CID_C_HIGHPRIESTESS2].msgReceivers[MSG_C_ACTION3] = priestess2_c_anims;
@@ -115,24 +99,20 @@ void Priestess2CStaticsInit()
 	classStatics[CID_C_HIGHPRIESTESS2].msgReceivers[MSG_C_ACTION6] = priestess2_c_anims;
 	classStatics[CID_C_HIGHPRIESTESS2].msgReceivers[MSG_C_ACTION7] = priestess2_c_anims;
 	classStatics[CID_C_HIGHPRIESTESS2].msgReceivers[MSG_C_IDLE1] = priestess2_c_anims;
-	resInfo.numAnims = NUM_ANIMS;
-	resInfo.animations = animations;
-	
-	resInfo.modelIndex = gi.modelindex("models/monsters/highpriestess/pod_scene/tris.fm");
 
-	classStatics[CID_C_HIGHPRIESTESS2].resInfo = &resInfo;
+	res_info.numAnims = NUM_ANIMS;
+	res_info.animations = animations;
+	res_info.modelIndex = gi.modelindex("models/monsters/highpriestess/pod_scene/tris.fm");
 
+	classStatics[CID_C_HIGHPRIESTESS2].resInfo = &res_info;
 }
 
-/*QUAKED character_highpriestess2(1 .5 0) (-24 -24 -36) (24 24 36)  INVISIBLE
-The cinematic High Priestess for the Pod Scene
---------SPAWNFLAG-------------
-INVISIBLE - you won't see her
-*/
-void SP_character_highpriestess2 (edict_t *self)
+// QUAKED character_highpriestess2(1 .5 0) (-24 -24 -36) (24 24 36)  INVISIBLE
+// The cinematic High Priestess for the Pod Scene.
+void SP_character_highpriestess2(edict_t* self)
 {
-	VectorSet (self->mins, -16, -16, -38);
-	VectorSet (self->maxs, 16, 16, 38);	
+	VectorSet(self->mins, -16.0f, -16.0f, -38.0f);
+	VectorSet(self->maxs,  16.0f,  16.0f,  38.0f);
 
-	c_character_init(self,CID_C_HIGHPRIESTESS2);
+	c_character_init(self, CID_C_HIGHPRIESTESS2);
 }
