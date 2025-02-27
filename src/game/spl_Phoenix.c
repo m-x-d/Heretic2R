a584b66d515271adc5f770626a9a9853322681cf
m-x-d <j.maxed@gmail.com>
2025-02-27 16:50:23 +0300

    Move PhoenixMissileTouch() above PhoenixMissileReflect() to avoid using forward declaration. PhoenixMissileTouch() cleanup.

--- src/game/spl_Phoenix.c
+++ src/game/spl_Phoenix.c
@@ -18,6 +18,72 @@
 
 void create_phoenix(edict_t *phoenix);
 
+static void PhoenixMissileTouch(edict_t* self, edict_t* other, cplane_t* plane, csurface_t* surface)
+{
+	// Did we hit the sky? 
+	if (surface != NULL && (surface->flags & SURF_SKY))
+	{
+		SkyFly(self);
+		return;
+	}
+
+	// Are we reflecting?
+	if (self->reflect_debounce_time && EntReflecting(other, true, true))
+	{
+		Create_rand_relect_vect(self->velocity, self->velocity);
+		Vec3ScaleAssign(PHOENIX_ARROW_SPEED / 2.0f, self->velocity);
+		PhoenixMissileReflect(self, other, self->velocity);
+
+		return;
+	}
+
+	// Kill the travel sound.
+	gi.sound(self, CHAN_WEAPON, gi.soundindex("misc/null.wav"), 1.0f, ATTN_NORM, 0.0f);
+
+	// I'm gonna set the enemy to what I hit... The DAMAGE_ENEMY_MAX assures that what I hit takes full damage.
+	// I do this rather than do direct damage AND radius because that tends to do double damage to what is hit...
+	self->enemy = ((other != NULL && other->takedamage != DAMAGE_NO) ? other : NULL);
+
+	if (self->health == 1)
+	{
+		// Must be powered up version. Storing in health is not so good, though...
+		// Powered up Phoenix will NOT damage the shooter. //TODO: check this. Shouldn't 'ignore' arg be set for this to work?
+		T_DamageRadius(self, self->owner, NULL, PHOENIX_EXPLODE_RADIUS_POWER,
+			PHOENIX_EXPLODE_DAMAGE_POWER, PHOENIX_EXPLODE_DAMAGE_POWER >> 4,
+			DAMAGE_NORMAL | DAMAGE_FIRE | DAMAGE_EXTRA_KNOCKBACK | DAMAGE_POWERPHOENIX | DAMAGE_ENEMY_MAX | DAMAGE_PHOENIX,
+			MOD_P_PHOENIX_SPL);
+	}
+	else
+	{
+		T_DamageRadius(self, self->owner, NULL, PHOENIX_EXPLODE_RADIUS,
+			PHOENIX_EXPLODE_DAMAGE, PHOENIX_EXPLODE_DAMAGE >> 4,
+			DAMAGE_NORMAL | DAMAGE_FIRE | DAMAGE_EXTRA_KNOCKBACK | DAMAGE_ENEMY_MAX | DAMAGE_PHOENIX,
+			MOD_PHOENIX_SPL);
+	}
+
+	AlertMonsters(self, self->owner, 3.0f, false);
+
+	// Attempt to apply a scorchmark decal to the thing I hit.
+	int scorch_flag = 0;
+	vec3_t plane_dir;
+	if (IsDecalApplicable(other, self->s.origin, surface, plane, plane_dir))
+		scorch_flag = CEF_FLAG8;
+
+	VectorNormalize2(self->velocity, self->movedir);
+
+	// Start the explosion.
+	if (plane->normal) //TODO: this is always true. Should use Vec3NotZero() instead?
+		gi.CreateEffect(&self->s, FX_WEAPON_PHOENIXEXPLODE, CEF_BROADCAST | (self->health << 5) | scorch_flag, self->s.origin, "td", plane->normal, self->movedir);
+	else
+		gi.CreateEffect(&self->s, FX_WEAPON_PHOENIXEXPLODE, CEF_BROADCAST | (self->health << 5) | scorch_flag, self->s.origin, "td", self->movedir, self->movedir);
+
+	VectorClear(self->velocity);
+
+	// Turn off the client effect.
+	gi.RemoveEffects(&self->s, FX_WEAPON_PHOENIXMISSILE);
+
+	G_SetToFree(self);
+}
 
 // ****************************************************************************
 // PhoenixMissile Reflect
@@ -54,82 +120,6 @@ edict_t *PhoenixMissileReflect(edict_t *self, edict_t *other, vec3_t vel)
 	gi.CreateEffect(&phoenix->s, FX_LIGHTNING_HIT, CEF_OWNERS_ORIGIN, NULL, "t", phoenix->velocity);
 
 	return(phoenix);
-}
-
-
-// ****************************************************************************
-// PhoenixMissile touch
-// ****************************************************************************
-void PhoenixMissileTouch(edict_t *self, edict_t *other, cplane_t *plane, csurface_t *surface)
-{
-	int			makeScorch;
-	vec3_t		planedir;
-			    
-	// did we hit the sky ? 
-	if(surface && (surface->flags & SURF_SKY))
-	{
-		SkyFly(self);
-		return;
-	}
-
-	// are we reflecting ?
-	if (self->reflect_debounce_time)
-	{
-		if(EntReflecting(other, true, true))
-		{
-			Create_rand_relect_vect(self->velocity, self->velocity);
-			Vec3ScaleAssign(PHOENIX_ARROW_SPEED/2,self->velocity);
-			PhoenixMissileReflect(self, other, self->velocity);
-			return;
-		}
-	}
-
-	// kill the travel sound
-	gi.sound(self, CHAN_WEAPON, gi.soundindex("misc/null.wav"), 1, ATTN_NORM, 0);
-
-	// I'm gonna set the enemy to what I hit...  The DAMAGE_ENEMY_MAX assures that what I hit takes full damage.
-	// I do this rather than do direct damage AND radius because that tends to do double damage to what is hit...
-	if (other && other->takedamage)
-		self->enemy = other;
-	else
-		self->enemy = NULL;
-	if(self->health == 1)
-	{	// must be powered up version - storing in health is not so good, though
-		// Powered up Phoenix will NOT damage the shooter.
-		T_DamageRadius(self, self->owner, NULL, PHOENIX_EXPLODE_RADIUS_POWER, 
-						PHOENIX_EXPLODE_DAMAGE_POWER, PHOENIX_EXPLODE_DAMAGE_POWER>>4, 
-						DAMAGE_NORMAL|DAMAGE_FIRE|DAMAGE_EXTRA_KNOCKBACK|DAMAGE_POWERPHOENIX|DAMAGE_ENEMY_MAX|DAMAGE_PHOENIX,
-						MOD_P_PHOENIX_SPL);
-	}
-	else
-	{
-		T_DamageRadius(self, self->owner, NULL, PHOENIX_EXPLODE_RADIUS, 
-						PHOENIX_EXPLODE_DAMAGE, PHOENIX_EXPLODE_DAMAGE>>4, 
-						DAMAGE_NORMAL|DAMAGE_FIRE|DAMAGE_EXTRA_KNOCKBACK|DAMAGE_ENEMY_MAX|DAMAGE_PHOENIX,
-						MOD_PHOENIX_SPL);
-	}
-	
-	AlertMonsters (self, self->owner, 3, false);
-
-	// Attempt to apply a scorchmark decal to the thing I hit.
-	makeScorch = 0;
-	if(IsDecalApplicable(other, self->s.origin, surface, plane, planedir))
-	{
-		makeScorch = CEF_FLAG8;
-	}
-
-	VectorNormalize2(self->velocity,self->movedir);
-	// Start the explosion
-	if (plane->normal)
-		gi.CreateEffect(&self->s, FX_WEAPON_PHOENIXEXPLODE, CEF_BROADCAST |(self->health << 5)| makeScorch, self->s.origin, "td", plane->normal, self->movedir);
-	else
-		gi.CreateEffect(&self->s, FX_WEAPON_PHOENIXEXPLODE, CEF_BROADCAST |(self->health << 5)| makeScorch, self->s.origin, "td", self->movedir, self->movedir);
-
-	VectorClear(self->velocity);
-   // Turn off the client effect
-	gi.RemoveEffects(&self->s, FX_WEAPON_PHOENIXMISSILE);
-
-	G_SetToFree(self);
 }
 
 // ****************************************************************************
