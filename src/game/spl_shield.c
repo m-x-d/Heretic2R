3c56b4ba1306c741e9a9e7132b9734f078430a36
m-x-d <j.maxed@gmail.com>
2025-02-28 14:37:23 +0300

    SpellLightningShieldAttack() cleanup.

--- src/game/spl_shield.c
+++ src/game/spl_shield.c
@@ -26,69 +26,51 @@ void SpellCastShield(edict_t* caster, vec3_t startpos, vec3_t aimangles, vec3_t 
 	caster->s.sound_data = (255 & ENT_VOL_MASK) | ATTN_NORM;
 }
 
-
-
-void SpellLightningShieldAttack(edict_t *self)
+void SpellLightningShieldAttack(edict_t* self)
 {
-	edict_t *found=NULL;
-	vec3_t	dir;
-	int		damage;
-	qboolean	onefound=false;
+	assert(self->client != NULL);
 
-	assert(self->client);
+	// Find all the entities in the area.
+	edict_t* ent = NULL;
+	qboolean ent_found = false;
 
-	// find all the foundities in the area
-	while((found = FindInRadius(found, self->s.origin, SHIELD_DAMAGE_RADIUS)))
-	{	// Only attack monsters and players in deathmatch
-		if (found == self)		// don't hurt yourself
-			continue;
-		
-
-		// don't target team members in team deathmatching, if they are on the same team, and friendly fire is not enabled.
-		if ((found->client && (int)(dmflags->value) & (DF_MODELTEAMS | DF_SKINTEAMS)) && !((int)dmflags->value & DF_HURT_FRIENDS) && deathmatch->value)
-		{
-			if (OnSameTeam(found, self))
-			continue;
-		}
-
-		if ((!(found->svflags & SVF_MONSTER) || (found->svflags & SVF_DEADMONSTER)) &&	// If we have a monster here.
-				!(found->client && deathmatch->value))									// Only attack players in deathmatch
+	while ((ent = FindInRadius(ent, self->s.origin, SHIELD_DAMAGE_RADIUS)) != NULL)
+	{
+		if (ent == self) // Don't hurt yourself.
 			continue;
 
-		// if we have reflection on, then no damage
-		if(EntReflecting(found, true, true))
+		// Don't target team members in team deathmatch, if they are on the same team, and friendly fire is not enabled.
+		if (DEATHMATCH && (DMFLAGS & (DF_MODELTEAMS | DF_SKINTEAMS)) && !(DMFLAGS & DF_HURT_FRIENDS) && ent->client != NULL && OnSameTeam(ent, self))
 			continue;
 
-		if (onefound)
-		{
-			if (irand(0, (SHIELD_ATTACK_CHANCE-1)) != 0)									// Don't attack everything we find.
-				continue;				
-		}
-		else
-		{	// More likely to find one if none found yet.
-			if (irand(0, (SHIELD_ATTACK_CHANCE-2)) != 0)									// Don't attack everything we find.
-				continue;				
-		}
+		// Only attack [monsters] or [players in deathmatch].
+		if ((!(ent->svflags & SVF_MONSTER) || (ent->svflags & SVF_DEADMONSTER)) && (!DEATHMATCH || ent->client == NULL))
+			continue;
 
-		VectorSubtract(found->s.origin, self->s.origin, dir);
+		// If we have reflection on, then no damage.
+		if (EntReflecting(ent, true, true))
+			continue;
+
+		// More likely to find one if none found yet.
+		const int chance_mod = (ent_found ? 1 : 2); //mxd
+		if (irand(0, (SHIELD_ATTACK_CHANCE - chance_mod)) != 0) // Don't attack everything we find.
+			continue;
+
+		// Start lightning effect.
+		vec3_t dir;
+		VectorSubtract(ent->s.origin, self->s.origin, dir);
 		VectorNormalize(dir);
-		damage = irand(SHIELD_DAMAGE_MIN, SHIELD_DAMAGE_MAX);
 
-		T_Damage(found, self, self, dir, vec3_origin, vec3_origin, damage, 0, DAMAGE_SPELL,MOD_SHIELD); 
-		
-		gi.CreateEffect(NULL, FX_LIGHTNING, 0, 
-				self->s.origin, "vbb", found->s.origin, (byte)SHIELD_LIGHTNING_WIDTH, (byte)0);
+		const int damage = irand(SHIELD_DAMAGE_MIN, SHIELD_DAMAGE_MAX);
+		T_Damage(ent, self, self, dir, vec3_origin, vec3_origin, damage, 0, DAMAGE_SPELL, MOD_SHIELD);
 
-		// Do a nasty looking blast at the impact point
-		gi.CreateEffect(&found->s, FX_LIGHTNING_HIT, CEF_OWNERS_ORIGIN, NULL, "t", dir);
+		gi.CreateEffect(NULL, FX_LIGHTNING, 0, self->s.origin, "vbb", ent->s.origin, (byte)SHIELD_LIGHTNING_WIDTH, (byte)0);
 
-		gi.sound(self, CHAN_WEAPON, gi.soundindex("weapons/ShieldAttack.wav"), 2, ATTN_NORM,0);
+		// Do a nasty looking blast at the impact point.
+		gi.CreateEffect(&ent->s, FX_LIGHTNING_HIT, CEF_OWNERS_ORIGIN, NULL, "t", dir);
 
-		onefound = true;
+		gi.sound(self, CHAN_WEAPON, gi.soundindex("weapons/ShieldAttack.wav"), 2.0f, ATTN_NORM, 0.0f); //TODO: why 2.0 volume?
+
+		ent_found = true;
 	}
-}	
-
-
-
-
-
+}
