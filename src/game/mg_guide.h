730efbc3a7750b16fc92ec4f3ebd0f6285a26478
m-x-d <j.maxed@gmail.com>
2025-03-05 13:23:26 +0300

    Update mg_guide.h to match changes in mg_guide.c.

--- src/game/mg_guide.h
+++ src/game/mg_guide.h
@@ -11,16 +11,16 @@
 #define MONSTER_SEARCH_TIME		10 // Monsters search for player for 10 seconds after losing him before giving up.
 
 extern void MG_InitMoods(edict_t* self); //mxd
-extern qboolean clear_visible_pos(edict_t* self, vec3_t spot2); //mxd
+extern qboolean clear_visible_pos(const edict_t* self, const vec3_t spot); //mxd
 extern void MG_Pathfind(edict_t* self, qboolean check_clear_path); //mxd
 extern int MG_SetFirstBuoy(edict_t* self); //mxd
 extern void MG_BuoyNavigate(edict_t* self); //mxd
 extern qboolean MG_GoToRandomBuoy(edict_t* self); //mxd
-extern qboolean MG_ReachedBuoy(edict_t* self, vec3_t pspot);
-extern void MG_RemoveBuoyEffects(edict_t* self); //mxd
+extern qboolean MG_ReachedBuoy(const edict_t* self, const vec3_t p_spot); //mxd
+extern void MG_RemoveBuoyEffects(const edict_t* self); //mxd
 extern qboolean MG_CheckClearPathToEnemy(edict_t* self); //mxd
-extern qboolean MG_MonsterAttemptTeleport(edict_t* self, vec3_t destination, qboolean ignoreLOS); //mxd
-extern qboolean MG_MakeConnection(edict_t* self, buoy_t* first_buoy, qboolean skipjump); //mxd
+extern qboolean MG_MonsterAttemptTeleport(edict_t* self, const vec3_t destination, qboolean ignore_los); //mxd
+extern qboolean MG_MakeConnection(edict_t* self, const buoy_t* first_buoy, qboolean skip_jump); //mxd
 
 //mxd. Local forward declarations for mg_guide.c:
-static qboolean MG_CheckClearPathToSpot(edict_t* self, vec3_t spot);
+static qboolean MG_CheckClearPathToSpot(edict_t* self, const vec3_t spot);
