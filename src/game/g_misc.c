55c1df7ce29be7e6a34b9d7ae9d71850e8adf64c
m-x-d <j.maxed@gmail.com>
2025-03-06 15:40:32 +0300

    SP_info_null() cleanup. SP_info_notnull() cleanup.

--- src/game/g_misc.c
+++ src/game/g_misc.c
@@ -536,30 +536,27 @@ void SP_point_combat(edict_t* self)
 
 #pragma endregion
 
-/*QUAKED info_null (0 0.5 0) (-4 -4 -4) (4 4 4)
-	
-	Used as a positional target for spotlights, etc.
-*/
-void SP_info_null (edict_t *self)
+#pragma region ========================== info_null, info_notnull  ==========================
+
+// QUAKED info_null (0 0.5 0) (-4 -4 -4) (4 4 4)
+// Used as a positional target for spotlights, etc.
+void SP_info_null(edict_t* self)
 {
-	G_FreeEdict (self);
-};
+	G_FreeEdict(self);
+}
 
-
-/*QUAKED info_notnull (0 0.5 0) (-4 -4 -4) (4 4 4)
-
-	Used as a positional target for lightning.
-*/
-void SP_info_notnull (edict_t *self)
+// QUAKED info_notnull (0 0.5 0) (-4 -4 -4) (4 4 4)
+// Used as a positional target for lightning.
+void SP_info_notnull(edict_t* self)
 {
-	VectorCopy (self->s.origin, self->absmin);
-	VectorCopy (self->s.origin, self->absmax);
-
 	self->solid = SOLID_TRIGGER;
 	self->movetype = PHYSICSTYPE_NONE;
 
-};
+	VectorCopy(self->s.origin, self->absmin);
+	VectorCopy(self->s.origin, self->absmax);
+}
 
+#pragma endregion
 
 /*QUAKED func_wall (0 .5 .8) ? TRIGGER_SPAWN TOGGLE START_ON ANIMATED ANIMATED_FAST
 	
