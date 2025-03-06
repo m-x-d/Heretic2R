23c5034c5dea1fc4807e7e5e40debe0135b9bb43
m-x-d <j.maxed@gmail.com>
2025-03-06 16:29:49 +0300

    g_misc.h: cleanup, update to match with g_misc.c logic.

--- src/game/g_misc.h
+++ src/game/g_misc.h
@@ -1,20 +1,17 @@
 //
 // g_misc.h
 //
-// Heretic II
 // Copyright 1998 Raven Software
 //
 
-#ifndef	G_MISC_H
-#define G_MISC_H
+#pragma once
 
 #include "q_Typedef.h"
 
-void BecomeDebris(edict_t *Self);
-void SprayDebris(edict_t *self, vec3_t spot, byte NoOfChunks, float damage);
-void ThrowBodyPart(edict_t *self, vec3_t *spot, int BodyPart, float damage, int frame);
-void ThrowWeapon(edict_t *self, vec3_t *spot, int BodyPart, float damage, int frame);
-void DefaultObjectDieHandler(edict_t *self, struct G_Message_s *msg);
-void BboxYawAndScale(edict_t *self);
+extern void BecomeDebris(edict_t* self);
+extern void SprayDebris(const edict_t* self, const vec3_t spot, byte num_chunks, float damage);
+extern void ThrowBodyPart(const edict_t* self, const vec3_t* spot, int body_part, float damage, int frame);
+extern void ThrowWeapon(const edict_t* self, const vec3_t* spot, int body_part, float damage, int frame);
+extern void DefaultObjectDieHandler(edict_t* self, struct G_Message_s* msg);
 
-#endif
+void BboxYawAndScale(edict_t* self); //TODO: move to g_obj.h
