0002a950e622014153e16cadc1d3afde76a8ca5c
m-x-d <j.maxed@gmail.com>
2025-03-10 15:50:22 +0300

    Remove func_conveyor logic (initially disabled).

--- src/game/g_func.c
+++ src/game/g_func.c
@@ -2256,49 +2256,6 @@ void SP_func_timer(edict_t* self)
 
 #pragma endregion
 
-/*QUAK-ED func_conveyor (0 .5 .8) ? START_ON TOGGLE
-Conveyors are stationary brushes that move what's on them.
-The brush should be have a surface with at least one current content enabled.
-speed	default 100
-*/
-/*
-void func_conveyor_use (edict_t *self, edict_t *other, edict_t *activator)
-{
-	if (self->spawnflags & 1)
-	{
-		self->speed = 0;
-		self->spawnflags &= ~1;
-	}
-	else
-	{
-		self->speed = self->count;
-		self->spawnflags |= 1;
-	}
-
-	if (!(self->spawnflags & 2))
-		self->count = 0;
-}
-*/
-/*
-void SP_func_conveyor (edict_t *self)
-{
-	if (!self->speed)
-		self->speed = 100;
-
-	if (!(self->spawnflags & 1))
-	{
-		self->count = self->speed;
-		self->speed = 0;
-	}
-
-	self->use = func_conveyor_use;
-
-	gi.setmodel (self, self->model);
-	self->solid = SOLID_BSP;
-	gi.linkentity (self);
-}
-*/
-
 /*QUAKED func_door_secret (0 .5 .8) ? always_shoot 1st_left 1st_down
 A secret door.  Slide back and then to the side.
 
