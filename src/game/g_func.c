2510a62d6bd73a3eb2b2cf491feb64cc5bd2b0d7
m-x-d <j.maxed@gmail.com>
2025-03-10 15:49:46 +0300

    SP_func_timer() cleanup.

--- src/game/g_func.c
+++ src/game/g_func.c
@@ -2223,10 +2223,18 @@ static void FuncTimerUse(edict_t* self, edict_t* other, edict_t* activator) //mx
 		FuncTimerThink(self);
 }
 
-void SP_func_timer (edict_t *self)
+// QUAKED func_timer (0.3 0.1 0.6) (-8 -8 -8) (8 8 8) START_ON
+// These can used but not touched.
+
+// Variables:
+// wait			- Base time between triggering all targets (default 1).
+// random		- Wait variance (default 0). So, the basic time between firing is a random time between (wait - random) and (wait + random).
+// delay		- Delay before first firing when turned on (default  0).
+// pausetime	- Additional delay used only the very first time and only if spawned with START_ON.
+void SP_func_timer(edict_t* self)
 {
-	if (!self->wait)
-		self->wait = 1.0;
+	if (self->wait == 0.0f)
+		self->wait = 1.0f;
 
 	self->use = FuncTimerUse;
 	self->think = FuncTimerThink;
@@ -2237,9 +2245,9 @@ void SP_func_timer (edict_t *self)
 		gi.dprintf("func_timer at %s has random >= wait\n", vtos(self->s.origin));
 	}
 
-	if (self->spawnflags & 1)
+	if (self->spawnflags & SF_START_ON)
 	{
-		self->nextthink = level.time + 1.0 + st.pausetime + self->delay + self->wait + flrand(-self->random, self->random);
+		self->nextthink = level.time + 1.0f + st.pausetime + self->delay + self->wait + flrand(-self->random, self->random);
 		self->activator = self;
 	}
 
