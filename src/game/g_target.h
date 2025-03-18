4088c3f0e901a7d89202827e8cb7ed5020acdb9d
m-x-d <j.maxed@gmail.com>
2025-03-18 16:13:03 +0300

    Add SP_target_NNN function declarations to g_target.h, use where appropriate. Add target_temp_entity to spawns list.

--- src/game/g_target.h
+++ src/game/g_target.h
@@ -11,3 +11,9 @@
 extern void TargetChangelevelUse(edict_t* self, edict_t* other, edict_t* activator);
 
 extern void SP_target_temp_entity(edict_t* ent);
+extern void SP_target_explosion(edict_t* ent);
+extern void SP_target_changelevel(edict_t* ent);
+extern void SP_target_crosslevel_trigger(edict_t* self);
+extern void SP_target_crosslevel_target(edict_t* self);
+extern void SP_target_lightramp(edict_t* self);
+extern void SP_target_earthquake(edict_t* self);
