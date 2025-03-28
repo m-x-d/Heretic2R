5802bb3ef777bebbb1288e7cc933a5db51e4b377
m-x-d <j.maxed@gmail.com>
2025-03-28 14:23:40 +0300

    M_CheckBottom() cleanup. Add SimpleBottomCornersCheck().

--- src/game/m_move.c
+++ src/game/m_move.c
@@ -9,76 +9,72 @@
 #include "Vector.h"
 #include "g_local.h"
 
-/*
-=============
-M_CheckBottom
-
-Returns false if any part of the bottom of the entity is off an edge that
-is not a staircase.
-
-=============
-*/
-int c_yes, c_no;
-
-qboolean M_CheckBottom (edict_t *ent)
+static qboolean SimpleBottomCornersCheck(const vec3_t mins, const vec3_t maxs) //mxd. Added to simplify logic a bit.
 {
-	vec3_t	mins, maxs, start, stop;
-	trace_t	trace;
-	int		x, y;
-	float	mid, bottom;
-	
-	VectorAdd (ent->s.origin, ent->mins, mins);
-	VectorAdd (ent->s.origin, ent->maxs, maxs);
+	vec3_t pos;
+	pos[2] = mins[2] - 1.0f;
 
-// if all of the points under the corners are solid world, don't bother
-// with the tougher checks
-// the corners must be within 16 of the midpoint
-	start[2] = mins[2] - 1;
-	for	(x=0 ; x<=1 ; x++)
-		for	(y=0 ; y<=1 ; y++)
+	for (int x = 0; x < 2; x++)
+	{
+		for (int y = 0; y < 2; y++)
 		{
-			start[0] = x ? maxs[0] : mins[0];
-			start[1] = y ? maxs[1] : mins[1];
-			if (gi.pointcontents (start) != CONTENTS_SOLID)
-				goto realcheck;
-		}
+			pos[0] = (x == 1 ? maxs[0] : mins[0]);
+			pos[1] = (y == 1 ? maxs[1] : mins[1]);
 
-	c_yes++;
-	return true;		// we got out easy
-
-realcheck:
-	c_no++;
-//
-// check it for real...
-//
-	start[2] = mins[2];
-	
-// the midpoint must be within 16 of the bottom
-	start[0] = stop[0] = (mins[0] + maxs[0])*0.5;
-	start[1] = stop[1] = (mins[1] + maxs[1])*0.5;
-	stop[2] = start[2] - 2*STEP_SIZE;
-	gi.trace (start, vec3_origin, vec3_origin, stop, ent, MASK_MONSTERSOLID,&trace);
-
-	if (trace.fraction == 1.0)
-		return false;
-	mid = bottom = trace.endpos[2];
-	
-// the corners must be within 16 of the midpoint	
-	for	(x=0 ; x<=1 ; x++)
-		for	(y=0 ; y<=1 ; y++)
-		{
-			start[0] = stop[0] = x ? maxs[0] : mins[0];
-			start[1] = stop[1] = y ? maxs[1] : mins[1];
-			
-			gi.trace (start, vec3_origin, vec3_origin, stop, ent, MASK_MONSTERSOLID,&trace);
-			
-			if (trace.fraction != 1.0 && trace.endpos[2] > bottom)
-				bottom = trace.endpos[2];
-			if (trace.fraction == 1.0 || mid - trace.endpos[2] > STEP_SIZE)
+			if (gi.pointcontents(pos) != CONTENTS_SOLID)
 				return false;
 		}
+	}
 
-	c_yes++;
+	return true;
+}
+
+// Returns false if any part of the bottom of the entity is off an edge that is not a staircase.
+static qboolean M_CheckBottom(const edict_t* ent)
+{
+	vec3_t mins;
+	vec3_t maxs;
+	VectorAdd(ent->s.origin, ent->mins, mins);
+	VectorAdd(ent->s.origin, ent->maxs, maxs);
+
+	// If all of the points under the corners are solid world, don't bother with the tougher checks.
+	// The corners must be within 16 of the midpoint.
+	if (SimpleBottomCornersCheck(mins, maxs)) //mxd
+		return true; // We got out easy.
+
+	// Check it for real... The midpoint must be within 16 of the bottom.
+	vec3_t start = { (mins[0] + maxs[0]) * 0.5f, (mins[1] + maxs[1]) * 0.5f, mins[2] };
+	vec3_t stop =  { start[0], start[1], start[2] - STEP_SIZE * 2.0f };
+
+	trace_t	trace;
+	gi.trace(start, vec3_origin, vec3_origin, stop, ent, MASK_MONSTERSOLID, &trace);
+
+	if (trace.fraction == 1.0f)
+		return false;
+
+	const float mid = trace.endpos[2];
+	float bottom = mid;
+
+	// The corners must be within 16 of the midpoint
+	for (int x = 0; x < 2; x++)
+	{
+		for (int y = 0; y < 2; y++)
+		{
+			start[0] = (x == 1 ? maxs[0] : mins[0]);
+			start[1] = (y == 1 ? maxs[1] : mins[1]);
+			stop[0] = start[0];
+			stop[1] = start[1];
+
+			gi.trace(start, vec3_origin, vec3_origin, stop, ent, MASK_MONSTERSOLID, &trace);
+
+			if (trace.fraction != 1.0f && trace.endpos[2] > bottom)
+				bottom = trace.endpos[2];
+
+			if (trace.fraction == 1.0f || mid - trace.endpos[2] > STEP_SIZE)
+				return false;
+		}
+	}
+
 	return true;
 }
 
