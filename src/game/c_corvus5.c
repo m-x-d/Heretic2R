a22ccc2fba6eadf0fafab6bea865cb2110a04607
m-x-d <j.maxed@gmail.com>
2025-04-01 17:44:01 +0300

    c_corvus5.c: mark animations var as const. c_corvus5_anim.c: mark all animframe_t and animmove_t vars as const.

--- src/game/c_corvus5.c
+++ src/game/c_corvus5.c
@@ -10,7 +10,7 @@
 #include "Utilities.h"
 
 // Corvus5 cinematic actions.
-static animmove_t* animations[NUM_ANIMS] =
+static const animmove_t* animations[NUM_ANIMS] =
 {
 	&corvus5_move_c_action1,
 	&corvus5_move_c_action2,
