d92a529f321a1fc1ec3233e1de92f125faafee06
m-x-d <j.maxed@gmail.com>
2025-04-11 18:48:37 +0300

    m_gkrokon.h: include g_Edict.h.

--- src/game/m_gkrokon.h
+++ src/game/m_gkrokon.h
@@ -6,6 +6,8 @@
 
 #pragma once
 
+#include "g_Edict.h"
+
 void SP_Monster_Gkrokon(edict_t* self);
 edict_t* GkrokonSpooReflect(edict_t* self, edict_t* other, vec3_t vel);
 void GkrokonStaticsInit(void);
