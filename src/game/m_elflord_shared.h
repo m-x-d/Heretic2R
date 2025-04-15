f4875799c4595455a1028d0d5b650a679f8c285b
m-x-d <j.maxed@gmail.com>
2025-04-15 13:14:55 +0300

    elflord_decelerate(): fix function declaration arg name mismatch.

--- src/game/m_elflord_shared.h
+++ src/game/m_elflord_shared.h
@@ -69,7 +69,7 @@ extern const animmove_t elflord_move_move;
 extern const animmove_t elflord_move_wait;
 extern const animmove_t elflord_move_come_to_life;
 
-void elflord_decelerate(edict_t* self, float value);
+void elflord_decelerate(edict_t* self, float scaler);
 void elflord_flymove(edict_t* self, float dist);
 void elflord_check_attack(edict_t* self);
 void elflord_finish_death(edict_t* self); //mxd
