a8d6ff676feeb9fb668fece7103cae459e579d12
m-x-d <j.maxed@gmail.com>
2025-04-21 17:39:17 +0300

    Rename morcalavin_proj3_blocked() to MorcalavinProjectile3Blocked(). MorcalavinProjectile3Blocked() cleanup.

--- src/game/m_morcalavin.c
+++ src/game/m_morcalavin.c
@@ -23,7 +23,7 @@
 
 static void create_morcalavin_proj(edict_t* self, edict_t* proj); //TODO: remove.
 static void morcalavin_proj1_blocked(edict_t* self, trace_t* trace); //TODO: remove.
-static void morcalavin_proj3_blocked(edict_t* self, trace_t* trace); //TODO: remove.
+static void MorcalavinProjectile3Blocked(edict_t* self, trace_t* trace); //TODO: remove.
 static void morcalavin_init_phase_out(edict_t* self); //TODO: remove.
 static void morcalavin_attack_fade_out(edict_t* self); //TODO: remove.
 
@@ -106,9 +106,9 @@ void morcalavin_big_shot(edict_t* self)
 	proj->owner = self;
 	self->target_ent = proj;
 
-	proj->isBlocked = morcalavin_proj3_blocked;
-	proj->isBlocking = morcalavin_proj3_blocked;
-	proj->bounced = morcalavin_proj3_blocked;
+	proj->isBlocked = MorcalavinProjectile3Blocked;
+	proj->isBlocking = MorcalavinProjectile3Blocked;
+	proj->bounced = MorcalavinProjectile3Blocked;
 
 	proj->think = MorcalavinLightning2Think;
 	proj->nextthink = level.time + FRAMETIME; //mxd. Use define.
@@ -287,67 +287,45 @@ static void MorcalavinProjectile2Blocked(edict_t* self, trace_t* trace) //mxd. N
 	self->nextthink = level.time + FRAMETIME; //mxd. Use define.
 }
 
-/*-----------------------------------------------
-	morcalavin_proj3_blocked
------------------------------------------------*/
-
-static void morcalavin_proj3_blocked( edict_t *self, trace_t *trace )
-{	
-	edict_t	*proj;
-	vec3_t	hitDir;
-	byte	exp;
-
+static void MorcalavinProjectile3Blocked(edict_t* self, trace_t* trace) //mxd. Named 'morcalavin_proj3_blocked' in original logic.
+{
 	if (trace->ent == self->owner)
 		return;
 
-	//Reflection stuff
-	if(EntReflecting(trace->ent, true, true))
+	// Reflection stuff.
+	if (EntReflecting(trace->ent, true, true))
 	{
-		proj = G_Spawn();
+		edict_t* proj = G_Spawn();
 
-		create_morcalavin_proj(self,proj);
+		create_morcalavin_proj(self, proj);
 		proj->owner = self->owner;
 		proj->ideal_yaw = self->ideal_yaw;
 
 		Create_rand_relect_vect(self->velocity, proj->velocity);
-		Vec3ScaleAssign(proj->ideal_yaw,proj->velocity);
+		Vec3ScaleAssign(proj->ideal_yaw, proj->velocity);
 		vectoangles(proj->velocity, proj->s.angles);
 
-		exp = HPMISSILE1_EXPLODE;
-
-		gi.CreateEffect(&self->s,
-					FX_HP_MISSILE,
-					CEF_OWNERS_ORIGIN,
-					self->s.origin,
-					"vb",
-					vec3_origin,
-					(unsigned char) exp);
-
-		gi.linkentity(proj); 
+		gi.CreateEffect(&self->s, FX_HP_MISSILE, CEF_OWNERS_ORIGIN, self->s.origin, "vb", vec3_origin, HPMISSILE1_EXPLODE); //TODO: play SND_HOMEHIT sound?
+		gi.linkentity(proj);
 
 		G_SetToFree(self);
 
 		return;
 	}
 
-	if ( trace->ent->takedamage )
+	if (trace->ent->takedamage != DAMAGE_NO)
 	{
-		VectorCopy( self->velocity, hitDir );
-		VectorNormalize( hitDir );
+		vec3_t hit_dir;
+		VectorCopy(self->velocity, hit_dir);
+		VectorNormalize(hit_dir);
 
-		T_Damage( trace->ent, self, self->owner, hitDir, self->s.origin, trace->plane.normal, irand(3,5), 0, DAMAGE_SPELL | DAMAGE_NO_KNOCKBACK,MOD_DIED );
+		T_Damage(trace->ent, self, self->owner, hit_dir, self->s.origin, trace->plane.normal, irand(3, 5), 0, DAMAGE_SPELL | DAMAGE_NO_KNOCKBACK, MOD_DIED); //TODO: 'damage' arg value is the only difference between this and MorcalavinProjectile2Blocked().
 	}
 
-	gi.CreateEffect(NULL, 
-					FX_M_EFFECTS, 
-					0, 
-					self->s.origin, 
-					"bv", 
-					FX_MORK_MISSILE_HIT, 
-					trace->plane.normal);
+	gi.CreateEffect(NULL, FX_M_EFFECTS, 0, self->s.origin, "bv", FX_MORK_MISSILE_HIT, trace->plane.normal); //TODO: play SND_HOMEHIT sound?
 
 	self->think = G_FreeEdict;
-	self->nextthink = level.time + 0.1;
+	self->nextthink = level.time + FRAMETIME; //mxd. Use define.
 }
 
 void morcalavin_check_lightning(edict_t *self)
