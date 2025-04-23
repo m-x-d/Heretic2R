551f98971a78d6ea5d3a0f90afe936eaf94f4431
m-x-d <j.maxed@gmail.com>
2025-04-23 15:17:19 +0300

    m_mssithra.c: group functions into regions.

--- src/game/m_mssithra.c
+++ src/game/m_mssithra.c
@@ -17,9 +17,6 @@
 #include "Vector.h"
 #include "g_local.h"
 
-static void MssithraArrowInit(edict_t* arrow); //TODO: remove?
-static qboolean MssithraCheckMood(edict_t* self); //TODO: remove?
-
 #pragma region ========================== Mutant Ssithra base info ==========================
 
 static const animmove_t* animations[NUM_ANIMS] =
@@ -44,6 +41,89 @@ static const animmove_t* animations[NUM_ANIMS] =
 static int sounds[NUM_SOUNDS];
 
 #pragma endregion
+
+#pragma region ========================== Utility functions =========================
+
+// Create the guts of the ssithra arrow.
+static void MssithraArrowInit(edict_t* arrow) //mxd. Named 'create_ssithra_arrow' in original logic.
+{
+	arrow->s.modelindex = (byte)gi.modelindex("models/objects/exarrow/tris.fm");
+
+	arrow->movetype = MOVETYPE_FLYMISSILE;
+	arrow->solid = SOLID_BBOX;
+	arrow->classname = "mssithra_Arrow";
+	arrow->clipmask = MASK_SHOT;
+	arrow->s.effects |= (EF_ALWAYS_ADD_EFFECTS | EF_CAMERA_NO_CLIP);
+	arrow->s.scale = 1.5f;
+	arrow->svflags |= SVF_ALWAYS_SEND;
+
+	arrow->touch = MssithraArrowTouch;
+
+	VectorSet(arrow->mins, -1.0f, -1.0f, -1.0f);
+	VectorSet(arrow->maxs,  1.0f,  1.0f,  1.0f);
+}
+
+edict_t* MssithraArrowReflect(edict_t* self, edict_t* other, vec3_t vel) //mxd. Named 'MssithraAlphaArrowReflect' in original logic.
+{
+	edict_t* arrow = G_Spawn();
+
+	MssithraArrowInit(arrow);
+	VectorCopy(self->s.origin, arrow->s.origin);
+	arrow->owner = self->owner;
+
+	arrow->think = G_FreeEdict;
+	arrow->nextthink = self->nextthink;
+
+	VectorCopy(vel, arrow->velocity);
+	VectorNormalize2(vel, arrow->movedir);
+	AnglesFromDir(arrow->movedir, arrow->s.angles);
+	arrow->s.angles[YAW] += 90.0f;
+
+	arrow->reflect_debounce_time = self->reflect_debounce_time - 1;
+	arrow->reflected_time = self->reflected_time;
+
+	G_LinkMissile(arrow);
+	G_SetToFree(self);
+
+	gi.CreateEffect(&arrow->s, FX_M_EFFECTS, CEF_OWNERS_ORIGIN | CEF_FLAG6, NULL, "bv", FX_MSSITHRA_ARROW, arrow->velocity);
+	gi.CreateEffect(&arrow->s, FX_LIGHTNING_HIT, CEF_OWNERS_ORIGIN, NULL, "t", vel);
+
+	return arrow;
+}
+
+static qboolean MssithraCheckMood(edict_t* self) //mxd. Named 'mssithraCheckMood' in original logic.
+{
+	if (self->monsterinfo.aiflags & AI_OVERRIDE_GUIDE)
+		return false;
+
+	self->mood_think(self);
+
+	switch (self->ai_mood)
+	{
+		case AI_MOOD_ATTACK:
+			QPostMessage(self, ((self->ai_mood_flags & AI_MOOD_FLAG_MISSILE) ? MSG_MISSILE : MSG_MELEE), PRI_DIRECTIVE, NULL);
+			return true;
+
+		case AI_MOOD_PURSUE:
+		case AI_MOOD_NAVIGATE:
+			QPostMessage(self, MSG_RUN, PRI_DIRECTIVE, NULL);
+			return true;
+
+		case AI_MOOD_WALK:
+			QPostMessage(self, MSG_WALK, PRI_DIRECTIVE, NULL);
+			return true;
+
+		case AI_MOOD_STAND:
+			QPostMessage(self, MSG_STAND, PRI_DIRECTIVE, NULL);
+			return true;
+	}
+
+	return false;
+}
+
+#pragma endregion
+
+#pragma region ========================== Message handlers ==========================
 
 static void MssithraStandMsgHandler(edict_t* self, G_Message_t* msg) //mxd. Named 'mssithra_stand' in original logic.
 {
@@ -91,19 +171,6 @@ static void MssithraDeathMsgHandler(edict_t* self, G_Message_t* msg) //mxd. Name
 	self->next_post_think = -1.0f;
 }
 
-void mssithra_dead(edict_t* self)
-{
-	//FIXME: maybe allow dead bodies to be chopped? Make BBOX small?
-	self->msgHandler = DeadMsgHandler;
-	self->deadState = DEAD_DEAD;
-	self->flags |= FL_DONTANIMATE;
-
-	self->think = NULL;
-	self->nextthink = -1.0f; //mxd. '0' in original logic. Changed for consistency sake.
-
-	gi.linkentity(self);
-}
-
 static void MssithraMeleeMsgHandler(edict_t* self, G_Message_t* msg) //mxd. Named 'mssithra_melee' in original logic.
 {
 	if (!M_ValidTarget(self, self->enemy))
@@ -137,6 +204,89 @@ static void MssithraMissileMsgHandler(edict_t* self, G_Message_t* msg) //mxd. Na
 		SetAnim(self, ANIM_SHOOT_TRANS);
 	else
 		SetAnim(self, ANIM_IDLE1);
+}
+
+#pragma endregion
+
+#pragma region ========================== Edict callbacks ===========================
+
+static void MssithraArrowExplodeThink(edict_t* self) //mxd. Named 'mssithra_missile_explode' in original logic.
+{
+	gi.CreateEffect(NULL, FX_M_EFFECTS, 0, self->s.origin, "bv", FX_MSSITHRA_EXPLODE, self->movedir);
+
+	const float damage = flrand(8.0f, 16.0f); //mxd. int / irand() in original logic.
+	T_DamageRadius(self, self->owner, self->owner, MSSITHRA_ARROW_DMG_RADIUS, damage, damage / 2.0f, DAMAGE_ATTACKER_IMMUNE, MOD_DIED);
+
+	G_FreeEdict(self);
+}
+
+static void MssithraArrowTouch(edict_t* self, edict_t* other, cplane_t* plane, csurface_t* surface) //mxd. Named 'mssithraAlphaArrowTouch' in original logic.
+{
+	// Are we reflecting?
+	if (self->reflect_debounce_time > 0 && EntReflecting(other, true, true))
+	{
+		Create_rand_relect_vect(self->velocity, self->velocity);
+		Vec3ScaleAssign(MSSITHRA_ARROW_SPEED / 2.0f, self->velocity);
+		MssithraArrowReflect(self, other, self->velocity);
+
+		return;
+	}
+
+	if (plane != NULL) //mxd. Original logic null-checks plane->normal (always true).
+		VectorCopy(plane->normal, self->movedir);
+
+	if (other->takedamage != DAMAGE_NO)
+	{
+		self->dmg = irand(MSSITHRA_DMG_MIN * 2, MSSITHRA_DMG_MAX * 2);
+		MssithraArrowExplodeThink(self);
+	}
+	else
+	{
+		gi.sound(self, CHAN_WEAPON, sounds[SND_INWALL], 0.5f, ATTN_NORM, 0.0f);
+
+		VectorClear(self->velocity);
+		self->s.effects |= EF_ALTCLIENTFX;
+		self->dmg = irand(MSSITHRA_DMG_MIN, MSSITHRA_DMG_MAX);
+
+		self->think = MssithraArrowExplodeThink;
+		self->nextthink = level.time + flrand(0.5f, 1.5f);
+	}
+}
+
+static void MssithraPostThink(edict_t* self) //mxd. Named 'mssithra_postthink' in original logic.
+{
+	// Only display a life-meter if we have an enemy.
+	if (self->enemy != NULL)
+	{
+		if (self->mssithra_healthbar_buildup < self->max_health)
+		{
+			M_ShowLifeMeter(self->mssithra_healthbar_buildup, self->mssithra_healthbar_buildup);
+			self->mssithra_healthbar_buildup += self->max_health / 10; //mxd. '+= 50' in original logic (way too slow).
+		}
+		else
+		{
+			M_ShowLifeMeter(self->health, self->max_health);
+		}
+	}
+
+	self->next_post_think = level.time + 0.05f;
+}
+
+#pragma endregion
+
+#pragma region ========================== Action functions ==========================
+
+void mssithra_dead(edict_t* self)
+{
+	//FIXME: maybe allow dead bodies to be chopped? Make BBOX small?
+	self->msgHandler = DeadMsgHandler;
+	self->deadState = DEAD_DEAD;
+	self->flags |= FL_DONTANIMATE;
+
+	self->think = NULL;
+	self->nextthink = -1.0f; //mxd. '0' in original logic. Changed for consistency sake.
+
+	gi.linkentity(self);
 }
 
 void mssithra_swipe(edict_t* self) //mxd. Named 'mssithraSwipe' in original logic.
@@ -175,96 +325,6 @@ void mssithra_swipe(edict_t* self) //mxd. Named 'mssithraSwipe' in original logi
 	{
 		gi.sound(self, CHAN_WEAPON, sounds[SND_SWIPE], 1.0f, ATTN_NORM, 0.0f);
 	}
-}
-
-static void MssithraArrowExplodeThink(edict_t* self) //mxd. Named 'mssithra_missile_explode' in original logic.
-{
-	gi.CreateEffect(NULL, FX_M_EFFECTS, 0, self->s.origin, "bv", FX_MSSITHRA_EXPLODE, self->movedir);
-
-	const float damage = flrand(8.0f, 16.0f); //mxd. int / irand() in original logic.
-	T_DamageRadius(self, self->owner, self->owner, MSSITHRA_ARROW_DMG_RADIUS, damage, damage / 2.0f, DAMAGE_ATTACKER_IMMUNE, MOD_DIED);
-
-	G_FreeEdict(self);
-}
-
-edict_t* MssithraArrowReflect(edict_t* self, edict_t* other, vec3_t vel) //mxd. Named 'MssithraAlphaArrowReflect' in original logic.
-{
-	edict_t* arrow = G_Spawn();
-
-	MssithraArrowInit(arrow);
-	VectorCopy(self->s.origin, arrow->s.origin);
-	arrow->owner = self->owner;
-
-	arrow->think = G_FreeEdict;
-	arrow->nextthink = self->nextthink;
-
-	VectorCopy(vel, arrow->velocity);
-	VectorNormalize2(vel, arrow->movedir);
-	AnglesFromDir(arrow->movedir, arrow->s.angles);
-	arrow->s.angles[YAW] += 90.0f;
-
-	arrow->reflect_debounce_time = self->reflect_debounce_time - 1;
-	arrow->reflected_time = self->reflected_time;
-
-	G_LinkMissile(arrow);
-	G_SetToFree(self);
-
-	gi.CreateEffect(&arrow->s, FX_M_EFFECTS, CEF_OWNERS_ORIGIN | CEF_FLAG6, NULL, "bv", FX_MSSITHRA_ARROW, arrow->velocity);
-	gi.CreateEffect(&arrow->s, FX_LIGHTNING_HIT, CEF_OWNERS_ORIGIN, NULL, "t", vel);
-
-	return arrow;
-}
-
-static void MssithraArrowTouch(edict_t* self, edict_t* other, cplane_t* plane, csurface_t* surface) //mxd. Named 'mssithraAlphaArrowTouch' in original logic.
-{
-	// Are we reflecting?
-	if (self->reflect_debounce_time > 0 && EntReflecting(other, true, true))
-	{
-		Create_rand_relect_vect(self->velocity, self->velocity);
-		Vec3ScaleAssign(MSSITHRA_ARROW_SPEED / 2.0f, self->velocity);
-		MssithraArrowReflect(self, other, self->velocity);
-
-		return;
-	}
-
-	if (plane != NULL) //mxd. Original logic null-checks plane->normal (always true).
-		VectorCopy(plane->normal, self->movedir);
-
-	if (other->takedamage != DAMAGE_NO)
-	{
-		self->dmg = irand(MSSITHRA_DMG_MIN * 2, MSSITHRA_DMG_MAX * 2);
-		MssithraArrowExplodeThink(self);
-	}
-	else
-	{
-		gi.sound(self, CHAN_WEAPON, sounds[SND_INWALL], 0.5f, ATTN_NORM, 0.0f);
-
-		VectorClear(self->velocity);
-		self->s.effects |= EF_ALTCLIENTFX;
-		self->dmg = irand(MSSITHRA_DMG_MIN, MSSITHRA_DMG_MAX);
-
-		self->think = MssithraArrowExplodeThink;
-		self->nextthink = level.time + flrand(0.5f, 1.5f);
-	}
-}
-
-// Create the guts of the ssithra arrow.
-static void MssithraArrowInit(edict_t* arrow) //mxd. Named 'create_ssithra_arrow' in original logic.
-{
-	arrow->s.modelindex = (byte)gi.modelindex("models/objects/exarrow/tris.fm");
-
-	arrow->movetype = MOVETYPE_FLYMISSILE;
-	arrow->solid = SOLID_BBOX;
-	arrow->classname = "mssithra_Arrow";
-	arrow->clipmask = MASK_SHOT;
-	arrow->s.effects |= (EF_ALWAYS_ADD_EFFECTS | EF_CAMERA_NO_CLIP);
-	arrow->s.scale = 1.5f;
-	arrow->svflags |= SVF_ALWAYS_SEND;
-
-	arrow->touch = MssithraArrowTouch;
-
-	VectorSet(arrow->mins, -1.0f, -1.0f, -1.0f);
-	VectorSet(arrow->maxs,  1.0f,  1.0f,  1.0f);
 }
 
 void mssithra_arrow(edict_t* self) //mxd. Named 'mssithraArrow' in original logic.
@@ -376,58 +436,9 @@ void mssithra_arrow(edict_t* self) //mxd. Named 'mssithraArrow' in original logi
 	}
 }
 
-static qboolean MssithraCheckMood(edict_t* self) //mxd. Named 'mssithraCheckMood' in original logic.
-{
-	if (self->monsterinfo.aiflags & AI_OVERRIDE_GUIDE)
-		return false;
-
-	self->mood_think(self);
-
-	switch (self->ai_mood)
-	{
-		case AI_MOOD_ATTACK:
-			QPostMessage(self, ((self->ai_mood_flags & AI_MOOD_FLAG_MISSILE) ? MSG_MISSILE : MSG_MELEE), PRI_DIRECTIVE, NULL);
-			return true;
-
-		case AI_MOOD_PURSUE:
-		case AI_MOOD_NAVIGATE:
-			QPostMessage(self, MSG_RUN, PRI_DIRECTIVE, NULL);
-			return true;
-
-		case AI_MOOD_WALK:
-			QPostMessage(self, MSG_WALK, PRI_DIRECTIVE, NULL);
-			return true;
-
-		case AI_MOOD_STAND:
-			QPostMessage(self, MSG_STAND, PRI_DIRECTIVE, NULL);
-			return true;
-	}
-
-	return false;
-}
-
 void mssithra_check_mood(edict_t* self) //mxd. Added action function version.
 {
 	MssithraCheckMood(self);
-}
-
-static void MssithraPostThink(edict_t* self) //mxd. Named 'mssithra_postthink' in original logic.
-{
-	// Only display a life-meter if we have an enemy.
-	if (self->enemy != NULL)
-	{
-		if (self->mssithra_healthbar_buildup < self->max_health)
-		{
-			M_ShowLifeMeter(self->mssithra_healthbar_buildup, self->mssithra_healthbar_buildup);
-			self->mssithra_healthbar_buildup += self->max_health / 10; //mxd. '+= 50' in original logic (way too slow).
-		}
-		else
-		{
-			M_ShowLifeMeter(self->health, self->max_health);
-		}
-	}
-
-	self->next_post_think = level.time + 0.05f;
 }
 
 void mssithra_growl(edict_t* self) //mxd. Named 'mmssithraRandomGrowlSound' in original logic.
@@ -446,6 +457,8 @@ void mssithra_check_shoot_loop(edict_t* self) //mxd. Named 'mssithraCheckShotLoo
 	if (irand(0, (SKILL + 1) * 2) == 0) // Otherwise, just keep playing current animation.
 		SetAnim(self, ANIM_SHOOT_DETRANS); //TODO: never do this when health < 50% (or 33%)?
 }
+
+#pragma endregion
 
 void MssithraStaticsInit(void)
 {
--- src/game/m_mssithra.h
+++ src/game/m_mssithra.h
@@ -11,3 +11,6 @@
 extern void SP_monster_mssithra(edict_t* self);
 extern edict_t* MssithraArrowReflect(edict_t* self, edict_t* other, vec3_t vel);
 extern void MssithraStaticsInit(void);
+
+// Local forward declarations for m_mssithra.c.
+static void MssithraArrowTouch(edict_t* self, edict_t* other, cplane_t* plane, csurface_t* surface);
