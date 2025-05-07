4b9ac4e069a7880bbfe045202db1d8d804a6d269
m-x-d <j.maxed@gmail.com>
2025-05-07 16:42:20 +0300

    m_seraph.c: group functions into regions.

--- src/game/m_seraph.c
+++ src/game/m_seraph.c
@@ -18,8 +18,6 @@
 #include "g_monster.h"
 #include "g_local.h"
 
-static void SeraphDropWeapon(edict_t* self); //TODO: remove.
-
 #define OVERLORD_ENFORCE_RADIUS	1000.0f //FIXME: Tweak out. //mxd. Named 'OVERLORD_RADIUS' in original logic.
 
 #pragma region ========================== Seraph Base Info ==========================
@@ -60,180 +58,7 @@ static int sounds[NUM_SOUNDS];
 
 #pragma endregion
 
-void seraph_jump(edict_t* self) //mxd. Named 'seraphApplyJump' in original logic.
-{
-	self->jump_time = level.time + 0.75f;
-	VectorCopy(self->movedir, self->velocity);
-	VectorNormalize(self->movedir);
-}
-
-void seraph_dead(edict_t* self)
-{
-	self->health = 0;
-	self->solid = SOLID_NOT;
-
-	M_EndDeath(self);
-}
-
-void seraph_death_loop(edict_t* self)
-{
-	SetAnim(self, ANIM_DEATH2_LOOP);
-}
-
-void seraph_check_land(edict_t* self)
-{
-	M_ChangeYaw(self);
-
-	vec3_t end_pos;
-	VectorCopy(self->s.origin, end_pos);
-	end_pos[2] -= 48.0f;
-
-	trace_t trace;
-	gi.trace(self->s.origin, self->mins, self->maxs, end_pos, self, MASK_MONSTERSOLID, &trace);
-
-	if ((trace.fraction < 1.0f || trace.allsolid || trace.startsolid) && self->curAnimID != ANIM_DEATH2_END && self->curAnimID != ANIM_DEATH2_GO)
-	{
-		self->elasticity = 1.25f;
-		self->friction = 0.5f;
-		SetAnim(self, ANIM_DEATH2_END);
-	}
-}
-
-void seraph_sound_startle(edict_t* self)
-{
-	gi.sound(self, CHAN_VOICE, sounds[SND_STARTLE], 1.0f, ATTN_NORM, 0.0f);
-}
-
-void seraph_sound_slap(edict_t* self)
-{
-	gi.sound(self, CHAN_WEAPON, sounds[SND_SLAP], 1.0f, ATTN_NORM, 0.0f);
-}
-
-void seraph_sound_scold(edict_t* self)
-{
-	gi.sound(self, CHAN_VOICE, sounds[SND_SCOLD3], 1.0f, ATTN_NORM, 0.0f);
-}
-
-void seraph_sound_scold2(edict_t* self)
-{
-	gi.sound(self, CHAN_VOICE, sounds[irand(SND_SCOLD1, SND_SCOLD2)], 1.0f, ATTN_NORM, 0.0f);
-}
-
-void seraph_sound_yell(edict_t* self)
-{
-	gi.sound(self, CHAN_VOICE, sounds[SND_SCARE], 1.0f, ATTN_NORM, 0.0f);
-}
-
-void seraph_sound_whip(edict_t* self)
-{
-	gi.sound(self, CHAN_WEAPON, sounds[SND_ATTACK], 1.0f, ATTN_NORM, 0.0f);
-}
-
-// Seraph has finished his startle, either track down the enemy, or go back to normal.
-void seraph_done_startle(edict_t* self)
-{
-	if (!FindTarget(self))
-		QPostMessage(self, MSG_STAND, PRI_DIRECTIVE, NULL);
-}
-
-// Seraph has finished ANIM_GET2WORK, and must reset its enemy.
-void seraph_done_get2work(edict_t* self)
-{
-	if (self->enemy != NULL) //TODO: also check if self->enemy is CID_OGLE?
-		self->enemy->targeted = false;
-
-	self->enemy = NULL;
-	self->ai_mood = AI_MOOD_STAND;
-	self->ai_mood_flags &= ~AI_MOOD_FLAG_IGNORE;
-
-	seraph_pause(self);
-}
-
-// Special walk code for going to yell at an ogle.
-void seraph_ai_walk(edict_t* self, float distance)
-{
-	if (self->enemy != NULL && M_DistanceToTarget(self, self->enemy) < 72.0f) //TODO: also check if self->enemy is CID_OGLE?
-	{
-		self->ai_mood = AI_MOOD_STAND;
-		SetAnim(self, irand(ANIM_GET2WORK, ANIM_GET2WORK2));
-	}
-	else
-	{
-		MG_MoveToGoal(self, distance);
-	}
-}
-
-void seraph_stand(edict_t* self) //mxd. Named 'seraph2idle' in original logic.
-{
-	SetAnim(self, ANIM_STAND1);
-}
-
-// Upper level AI interfacing.
-void seraph_pause(edict_t* self)
-{
-	self->mood_think(self);
-
-	if (self->enemy != NULL && self->enemy->classID == CID_OGLE && (self->ai_mood_flags & AI_MOOD_FLAG_IGNORE))
-		return;
-
-	switch (self->ai_mood)
-	{
-		case AI_MOOD_ATTACK:
-			QPostMessage(self, MSG_MELEE, PRI_DIRECTIVE, NULL);
-			break;
-
-		case AI_MOOD_PURSUE:
-		case AI_MOOD_NAVIGATE:
-		case AI_MOOD_WALK:
-			QPostMessage(self, MSG_RUN, PRI_DIRECTIVE, NULL);
-			break;
-
-		case AI_MOOD_STAND:
-			if (self->enemy == NULL) //TODO: else what?
-				QPostMessage(self, MSG_STAND, PRI_DIRECTIVE, NULL);
-			break;
-
-		case AI_MOOD_WANDER:
-			SetAnim(self, ANIM_WALK1);
-			break;
-
-		case AI_MOOD_POINT_NAVIGATE:
-			SetAnim(self, ANIM_WALK2);
-			break;
-
-		case AI_MOOD_JUMP:
-			SetAnim(self, ANIM_FJUMP);
-			break;
-
-		default:
-			break;
-	}
-}
-
-static void SeraphCheckMoodMsgHandler(edict_t* self, G_Message_t* msg) //mxd. Named 'seraph_check_mood' in original logic.
-{
-	ParseMsgParms(msg, "i", &self->ai_mood);
-	seraph_pause(self);
-}
-
-// Targets a specific ogle and puts it back to work.
-void seraph_enforce_ogle(edict_t* self)
-{
-	if (self->enemy != NULL) //TODO: also check if self->enemy is CID_OGLE?
-	{
-		self->enemy->use(self->enemy, self, self);
-		gi.sound(self, CHAN_VOICE, sounds[irand(SND_SCOLD1, SND_SCOLD2)], 1.0f, ATTN_NORM, 0.0f);
-	}
-}
-
-// Targets all idle ogles and puts them back to work.
-void seraph_enforce(edict_t* self)
-{
-	edict_t* ogle = NULL;
-	while ((ogle = FindInRadius(ogle, self->s.origin, OVERLORD_ENFORCE_RADIUS)) != NULL)
-		if (ogle->classID == CID_OGLE && ogle->ai_mood == AI_MOOD_REST && ogle->targetEnt == self)
-			ogle->use(ogle, self, self); // Setup within the ogle code.
-}
+#pragma region ========================== Utility functions =========================
 
 // Check to see if you can make it to an idle ogle and scare them.
 static qboolean SeraphCheckScare(edict_t* self, edict_t* ogle) //mxd. Named 'seraph_checkscare' in original logic.
@@ -292,98 +117,63 @@ static void SeraphOversee(edict_t* self) //mxd. Named 'seraph_oversee' in origin
 	}
 }
 
-// Cycles through the various idle animations.
-void seraph_idle(edict_t* self)
+static qboolean SeraphCanThrowNode(edict_t* self, int node_id, int* throw_nodes) //mxd. Named 'canthrownode_so' in original logic.
 {
-	SeraphOversee(self);
-
-	// Check to see if we were supposed to point at an ogle.
-	if (self->ai_mood == AI_MOOD_ATTACK && (self->ai_mood_flags & AI_MOOD_FLAG_WHIP) && self->curAnimID != ANIM_POINT1)
+	static const int bit_for_mesh_node[NUM_MESH_NODES] = //mxd. Made local static.
 	{
-		SetAnim(self, ANIM_POINT1);
-		self->ai_mood = AI_MOOD_STAND;
-		self->ai_mood_flags &= ~AI_MOOD_FLAG_WHIP;
+		BIT_BASEBIN,
+		BIT_PITHEAD, // Overlord head.
+		BIT_SHOULDPAD,
+		BIT_GUARDHEAD, // Guard head.
+		BIT_LHANDGRD, // Left hand guard.
+		BIT_LHANDBOSS, // Left hand overlord.
+		BIT_RHAND, // Right hand.
+		BIT_FRTORSO,
+		BIT_ARMSPIKES,
+		BIT_LFTUPARM,
+		BIT_RTLEG,
+		BIT_RTARM,
+		BIT_LFTLEG,
+		BIT_BKTORSO,
+		BIT_AXE,
+		BIT_WHIP
+	};
 
-		return;
+	// See if it's on, if so, add it to throw_nodes. Turn it off on thrower.
+	if (!(self->s.fmnodeinfo[node_id].flags & FMNI_NO_DRAW))
+	{
+		*throw_nodes |= bit_for_mesh_node[node_id];
+		self->s.fmnodeinfo[node_id].flags |= FMNI_NO_DRAW;
+
+		return true;
 	}
 
-	// See if we're going to go scare an ogle.
-	if (self->ai_mood == AI_MOOD_POINT_NAVIGATE)
-	{
-		SetAnim(self, ANIM_WALK2);
-		return;
-	}
-
-	const int chance = irand(0, 100);
-
-	switch (self->curAnimID)
-	{
-		case ANIM_STAND1:
-			if (chance < 20)
-				SetAnim(self, ANIM_STAND1_TR);
-			else if (chance < 40)
-				SetAnim(self, ANIM_STAND1_TL);
-			break;
-
-			// Right.
-		case ANIM_POINT1:
-		case ANIM_STAND1_TR:
-			SetAnim(self, ANIM_STAND1_R);
-			break;
-
-		case ANIM_STAND1_R:
-			if (chance < 50)
-				SetAnim(self, ANIM_STAND1_TRC);
-			break;
-
-		case ANIM_STAND1_TLC:
-		case ANIM_STAND1_TRC:
-			SetAnim(self, ANIM_STAND1);
-			break;
-
-			// Left.
-		case ANIM_STAND1_TL:
-			SetAnim(self, ANIM_STAND1_L);
-			break;
-
-		case ANIM_STAND1_L:
-			if (chance < 50)
-				SetAnim(self, ANIM_STAND1_TLC);
-			break;
-
-		default:
-			break;
-	}
+	return false;
 }
 
-// Check to do damage with a whip.
-void seraph_strike(edict_t* self, float damage, float a, float b)
+// Throws weapon, turns off those nodes, sets that weapon as gone.
+static void SeraphDropWeapon(edict_t* self) //mxd. Named 'seraph_dropweapon' in original logic.
 {
-	const vec3_t start_offset = { 16.0f, 16.0f, 32.0f };
-	const vec3_t end_offset = { 80.0f, 16.0f, 8.0f };
+	if (self->s.fmnodeinfo[MESH__WHIP].flags & FMNI_NO_DRAW)
+		return; // Already dropped.
 
-	const vec3_t mins = { -2.0f, -2.0f, -2.0f };
-	const vec3_t maxs = {  2.0f,  2.0f,  2.0f };
+	vec3_t forward;
+	vec3_t right;
+	vec3_t up;
+	AngleVectors(self->s.angles, forward, right, up);
 
-	trace_t	trace;
-	vec3_t direction;
-	edict_t* victim = M_CheckMeleeLineHit(self, start_offset, end_offset, mins, maxs, &trace, direction);
+	vec3_t hand_pos = { 0 };
+	VectorMA(hand_pos, 8.0f, forward, hand_pos);
+	VectorMA(hand_pos, 5.0f, right, hand_pos);
+	VectorMA(hand_pos, 12.0f, up, hand_pos);
 
-	gi.sound(self, CHAN_WEAPON, sounds[SND_ATTACK], 1.0f, ATTN_NORM, 0.0f);
-
-	if (victim == NULL) // Missed.
-		return; //TODO: play swoosh sound instead?
-
-	if (victim == self) // Hit wall.
-		return; //TODO: Create a puff effect.
-
-	// Hurt whatever we were whacking away at.
-	vec3_t blood_dir;
-	VectorSubtract(start_offset, end_offset, blood_dir);
-	VectorNormalize(blood_dir);
-
-	T_Damage(victim, self, self, direction, trace.endpos, blood_dir, (int)damage, (int)damage, DAMAGE_EXTRA_BLOOD | DAMAGE_EXTRA_KNOCKBACK, MOD_DIED);
+	ThrowWeapon(self, &hand_pos, BIT_WHIP, 0, FRAME_partfly);
+	self->s.fmnodeinfo[MESH__WHIP].flags |= FMNI_NO_DRAW;
 }
+
+#pragma endregion
+
+#pragma region ========================== Message handlers ==========================
 
 static void SeraphDeathPainMsgHandler(edict_t* self, G_Message_t* msg) //mxd. Named 'seraph_death_pain' in original logic.
 {
@@ -536,6 +326,21 @@ static void SeraphPainMsgHandler(edict_t* self, G_Message_t* msg) //mxd. Named '
 	}
 }
 
+static void SeraphVoiceSightMsgHandler(edict_t* self, G_Message_t* msg) //mxd. Named 'ser_ovl_SightSound' in original logic.
+{
+	gi.sound(self, CHAN_VOICE, sounds[irand(SND_SIGHT1, SND_SIGHT3)], 1.0f, ATTN_NORM, 0.0f);
+}
+
+static void SeraphCheckMoodMsgHandler(edict_t* self, G_Message_t* msg) //mxd. Named 'seraph_check_mood' in original logic.
+{
+	ParseMsgParms(msg, "i", &self->ai_mood);
+	seraph_pause(self);
+}
+
+#pragma endregion
+
+#pragma region ========================== Edict callbacks ===========================
+
 static qboolean SeraphAlert(edict_t* self, alertent_t* alerter, edict_t* enemy) //mxd. Named 'seraphAlerted' in original logic.
 {
 	if (self->alert_time < level.time)
@@ -562,69 +367,6 @@ static qboolean SeraphAlert(edict_t* self, alertent_t* alerter, edict_t* enemy) 
 	AI_FoundTarget(self, true);
 
 	return true;
-}
-
-void seraph_back(edict_t* self, float distance)
-{
-	if (!MG_TryWalkMove(self, self->s.angles[YAW] + 180.0f, distance, true) && irand(0, 1000) == 0) // 0.0009% chance to run away when MG_TryWalkMove() fails. //TODO: chance seems way too low. Scale with difficulty?
-	{
-		self->monsterinfo.aiflags |= AI_COWARD;
-		SetAnim(self, ANIM_RUN1);
-	}
-}
-
-static qboolean SeraphCanThrowNode(edict_t* self, int node_id, int* throw_nodes) //mxd. Named 'canthrownode_so' in original logic.
-{
-	static const int bit_for_mesh_node[NUM_MESH_NODES] = //mxd. Made local static.
-	{
-		BIT_BASEBIN,
-		BIT_PITHEAD, // Overlord head.
-		BIT_SHOULDPAD,
-		BIT_GUARDHEAD, // Guard head.
-		BIT_LHANDGRD, // Left hand guard.
-		BIT_LHANDBOSS, // Left hand overlord.
-		BIT_RHAND, // Right hand.
-		BIT_FRTORSO,
-		BIT_ARMSPIKES,
-		BIT_LFTUPARM,
-		BIT_RTLEG,
-		BIT_RTARM,
-		BIT_LFTLEG,
-		BIT_BKTORSO,
-		BIT_AXE,
-		BIT_WHIP
-	};
-
-	// See if it's on, if so, add it to throw_nodes. Turn it off on thrower.
-	if (!(self->s.fmnodeinfo[node_id].flags & FMNI_NO_DRAW))
-	{
-		*throw_nodes |= bit_for_mesh_node[node_id];
-		self->s.fmnodeinfo[node_id].flags |= FMNI_NO_DRAW;
-
-		return true;
-	}
-
-	return false;
-}
-
-// Throws weapon, turns off those nodes, sets that weapon as gone.
-static void SeraphDropWeapon(edict_t* self) //mxd. Named 'seraph_dropweapon' in original logic.
-{
-	if (self->s.fmnodeinfo[MESH__WHIP].flags & FMNI_NO_DRAW)
-		return; // Already dropped.
-
-	vec3_t forward;
-	vec3_t right;
-	vec3_t up;
-	AngleVectors(self->s.angles, forward, right, up);
-
-	vec3_t hand_pos = { 0 };
-	VectorMA(hand_pos, 8.0f, forward, hand_pos);
-	VectorMA(hand_pos, 5.0f, right, hand_pos);
-	VectorMA(hand_pos, 12.0f, up, hand_pos);
-
-	ThrowWeapon(self, &hand_pos, BIT_WHIP, 0, FRAME_partfly);
-	self->s.fmnodeinfo[MESH__WHIP].flags |= FMNI_NO_DRAW;
 }
 
 static qboolean SeraphThrowHead(edict_t* self, float damage, const qboolean dismember_ok) //mxd. Added to simplify logic.
@@ -803,10 +545,282 @@ static void SeraphDismember(edict_t* self, int damage, HitLocation_t hl) //mxd. 
 	}
 }
 
-static void SeraphVoiceSightMsgHandler(edict_t* self, G_Message_t* msg) //mxd. Named 'ser_ovl_SightSound' in original logic.
+#pragma endregion
+
+#pragma region ========================== Action functions ==========================
+
+void seraph_jump(edict_t* self) //mxd. Named 'seraphApplyJump' in original logic.
 {
-	gi.sound(self, CHAN_VOICE, sounds[irand(SND_SIGHT1, SND_SIGHT3)], 1.0f, ATTN_NORM, 0.0f);
+	self->jump_time = level.time + 0.75f;
+	VectorCopy(self->movedir, self->velocity);
+	VectorNormalize(self->movedir);
 }
+
+void seraph_dead(edict_t* self)
+{
+	self->health = 0;
+	self->solid = SOLID_NOT;
+
+	M_EndDeath(self);
+}
+
+void seraph_death_loop(edict_t* self)
+{
+	SetAnim(self, ANIM_DEATH2_LOOP);
+}
+
+void seraph_check_land(edict_t* self)
+{
+	M_ChangeYaw(self);
+
+	vec3_t end_pos;
+	VectorCopy(self->s.origin, end_pos);
+	end_pos[2] -= 48.0f;
+
+	trace_t trace;
+	gi.trace(self->s.origin, self->mins, self->maxs, end_pos, self, MASK_MONSTERSOLID, &trace);
+
+	if ((trace.fraction < 1.0f || trace.allsolid || trace.startsolid) && self->curAnimID != ANIM_DEATH2_END && self->curAnimID != ANIM_DEATH2_GO)
+	{
+		self->elasticity = 1.25f;
+		self->friction = 0.5f;
+		SetAnim(self, ANIM_DEATH2_END);
+	}
+}
+
+void seraph_sound_startle(edict_t* self)
+{
+	gi.sound(self, CHAN_VOICE, sounds[SND_STARTLE], 1.0f, ATTN_NORM, 0.0f);
+}
+
+void seraph_sound_slap(edict_t* self)
+{
+	gi.sound(self, CHAN_WEAPON, sounds[SND_SLAP], 1.0f, ATTN_NORM, 0.0f);
+}
+
+void seraph_sound_scold(edict_t* self)
+{
+	gi.sound(self, CHAN_VOICE, sounds[SND_SCOLD3], 1.0f, ATTN_NORM, 0.0f);
+}
+
+void seraph_sound_scold2(edict_t* self)
+{
+	gi.sound(self, CHAN_VOICE, sounds[irand(SND_SCOLD1, SND_SCOLD2)], 1.0f, ATTN_NORM, 0.0f);
+}
+
+void seraph_sound_yell(edict_t* self)
+{
+	gi.sound(self, CHAN_VOICE, sounds[SND_SCARE], 1.0f, ATTN_NORM, 0.0f);
+}
+
+void seraph_sound_whip(edict_t* self)
+{
+	gi.sound(self, CHAN_WEAPON, sounds[SND_ATTACK], 1.0f, ATTN_NORM, 0.0f);
+}
+
+// Seraph has finished his startle, either track down the enemy, or go back to normal.
+void seraph_done_startle(edict_t* self)
+{
+	if (!FindTarget(self))
+		QPostMessage(self, MSG_STAND, PRI_DIRECTIVE, NULL);
+}
+
+// Seraph has finished ANIM_GET2WORK, and must reset its enemy.
+void seraph_done_get2work(edict_t* self)
+{
+	if (self->enemy != NULL) //TODO: also check if self->enemy is CID_OGLE?
+		self->enemy->targeted = false;
+
+	self->enemy = NULL;
+	self->ai_mood = AI_MOOD_STAND;
+	self->ai_mood_flags &= ~AI_MOOD_FLAG_IGNORE;
+
+	seraph_pause(self);
+}
+
+// Special walk code for going to yell at an ogle.
+void seraph_ai_walk(edict_t* self, float distance)
+{
+	if (self->enemy != NULL && M_DistanceToTarget(self, self->enemy) < 72.0f) //TODO: also check if self->enemy is CID_OGLE?
+	{
+		self->ai_mood = AI_MOOD_STAND;
+		SetAnim(self, irand(ANIM_GET2WORK, ANIM_GET2WORK2));
+	}
+	else
+	{
+		MG_MoveToGoal(self, distance);
+	}
+}
+
+void seraph_stand(edict_t* self) //mxd. Named 'seraph2idle' in original logic.
+{
+	SetAnim(self, ANIM_STAND1);
+}
+
+// Upper level AI interfacing.
+void seraph_pause(edict_t* self)
+{
+	self->mood_think(self);
+
+	if (self->enemy != NULL && self->enemy->classID == CID_OGLE && (self->ai_mood_flags & AI_MOOD_FLAG_IGNORE))
+		return;
+
+	switch (self->ai_mood)
+	{
+		case AI_MOOD_ATTACK:
+			QPostMessage(self, MSG_MELEE, PRI_DIRECTIVE, NULL);
+			break;
+
+		case AI_MOOD_PURSUE:
+		case AI_MOOD_NAVIGATE:
+		case AI_MOOD_WALK:
+			QPostMessage(self, MSG_RUN, PRI_DIRECTIVE, NULL);
+			break;
+
+		case AI_MOOD_STAND:
+			if (self->enemy == NULL) //TODO: else what?
+				QPostMessage(self, MSG_STAND, PRI_DIRECTIVE, NULL);
+			break;
+
+		case AI_MOOD_WANDER:
+			SetAnim(self, ANIM_WALK1);
+			break;
+
+		case AI_MOOD_POINT_NAVIGATE:
+			SetAnim(self, ANIM_WALK2);
+			break;
+
+		case AI_MOOD_JUMP:
+			SetAnim(self, ANIM_FJUMP);
+			break;
+
+		default:
+			break;
+	}
+}
+
+// Targets a specific ogle and puts it back to work.
+void seraph_enforce_ogle(edict_t* self)
+{
+	if (self->enemy != NULL) //TODO: also check if self->enemy is CID_OGLE?
+	{
+		self->enemy->use(self->enemy, self, self);
+		gi.sound(self, CHAN_VOICE, sounds[irand(SND_SCOLD1, SND_SCOLD2)], 1.0f, ATTN_NORM, 0.0f);
+	}
+}
+
+// Targets all idle ogles and puts them back to work.
+void seraph_enforce(edict_t* self)
+{
+	edict_t* ogle = NULL;
+	while ((ogle = FindInRadius(ogle, self->s.origin, OVERLORD_ENFORCE_RADIUS)) != NULL)
+		if (ogle->classID == CID_OGLE && ogle->ai_mood == AI_MOOD_REST && ogle->targetEnt == self)
+			ogle->use(ogle, self, self); // Setup within the ogle code.
+}
+
+// Cycles through the various idle animations.
+void seraph_idle(edict_t* self)
+{
+	SeraphOversee(self);
+
+	// Check to see if we were supposed to point at an ogle.
+	if (self->ai_mood == AI_MOOD_ATTACK && (self->ai_mood_flags & AI_MOOD_FLAG_WHIP) && self->curAnimID != ANIM_POINT1)
+	{
+		SetAnim(self, ANIM_POINT1);
+		self->ai_mood = AI_MOOD_STAND;
+		self->ai_mood_flags &= ~AI_MOOD_FLAG_WHIP;
+
+		return;
+	}
+
+	// See if we're going to go scare an ogle.
+	if (self->ai_mood == AI_MOOD_POINT_NAVIGATE)
+	{
+		SetAnim(self, ANIM_WALK2);
+		return;
+	}
+
+	const int chance = irand(0, 100);
+
+	switch (self->curAnimID)
+	{
+		case ANIM_STAND1:
+			if (chance < 20)
+				SetAnim(self, ANIM_STAND1_TR);
+			else if (chance < 40)
+				SetAnim(self, ANIM_STAND1_TL);
+			break;
+
+			// Right.
+		case ANIM_POINT1:
+		case ANIM_STAND1_TR:
+			SetAnim(self, ANIM_STAND1_R);
+			break;
+
+		case ANIM_STAND1_R:
+			if (chance < 50)
+				SetAnim(self, ANIM_STAND1_TRC);
+			break;
+
+		case ANIM_STAND1_TLC:
+		case ANIM_STAND1_TRC:
+			SetAnim(self, ANIM_STAND1);
+			break;
+
+			// Left.
+		case ANIM_STAND1_TL:
+			SetAnim(self, ANIM_STAND1_L);
+			break;
+
+		case ANIM_STAND1_L:
+			if (chance < 50)
+				SetAnim(self, ANIM_STAND1_TLC);
+			break;
+
+		default:
+			break;
+	}
+}
+
+// Check to do damage with a whip.
+void seraph_strike(edict_t* self, float damage, float a, float b)
+{
+	const vec3_t start_offset = { 16.0f, 16.0f, 32.0f };
+	const vec3_t end_offset = { 80.0f, 16.0f, 8.0f };
+
+	const vec3_t mins = { -2.0f, -2.0f, -2.0f };
+	const vec3_t maxs = {  2.0f,  2.0f,  2.0f };
+
+	trace_t	trace;
+	vec3_t direction;
+	edict_t* victim = M_CheckMeleeLineHit(self, start_offset, end_offset, mins, maxs, &trace, direction);
+
+	gi.sound(self, CHAN_WEAPON, sounds[SND_ATTACK], 1.0f, ATTN_NORM, 0.0f);
+
+	if (victim == NULL) // Missed.
+		return; //TODO: play swoosh sound instead?
+
+	if (victim == self) // Hit wall.
+		return; //TODO: Create a puff effect.
+
+	// Hurt whatever we were whacking away at.
+	vec3_t blood_dir;
+	VectorSubtract(start_offset, end_offset, blood_dir);
+	VectorNormalize(blood_dir);
+
+	T_Damage(victim, self, self, direction, trace.endpos, blood_dir, (int)damage, (int)damage, DAMAGE_EXTRA_BLOOD | DAMAGE_EXTRA_KNOCKBACK, MOD_DIED);
+}
+
+void seraph_back(edict_t* self, float distance)
+{
+	if (!MG_TryWalkMove(self, self->s.angles[YAW] + 180.0f, distance, true) && irand(0, 1000) == 0) // 0.0009% chance to run away when MG_TryWalkMove() fails. //TODO: chance seems way too low. Scale with difficulty?
+	{
+		self->monsterinfo.aiflags |= AI_COWARD;
+		SetAnim(self, ANIM_RUN1);
+	}
+}
+
+#pragma endregion
 
 void SeraphOverlordStaticsInit(void)
 {
