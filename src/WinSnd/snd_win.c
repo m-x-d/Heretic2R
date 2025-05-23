00e3a951901a4fa86dc2b8d554620c864b2ec636
m-x-d <j.maxed@gmail.com>
2025-05-23 15:15:12 +0300

    Implement S_Activate().

--- src/WinSnd/snd_win.c
+++ src/WinSnd/snd_win.c
@@ -4,9 +4,35 @@
 // Copyright 1998 Raven Software
 //
 
+#include <dsound.h>
 #include "snd_win.h"
+#include "sys_win.h"
 
-void S_Activate(qboolean active)
+static qboolean snd_isdirect;
+
+static LPDIRECTSOUND pDS;
+
+static qboolean DS_CreateBuffers(void)
 {
 	NOT_IMPLEMENTED
+	return false;
+}
+
+static void DS_DestroyBuffers(void)
+{
+	NOT_IMPLEMENTED
+}
+
+// Q2 counterpart.
+// Called when the main window gains or loses focus.
+// The window have been destroyed and recreated between a deactivate and an activate.
+void S_Activate(const qboolean active)
+{
+	if (pDS != NULL && cl_hwnd != NULL && snd_isdirect)
+	{
+		if (active)
+			DS_CreateBuffers();
+		else
+			DS_DestroyBuffers();
+	}
 }
