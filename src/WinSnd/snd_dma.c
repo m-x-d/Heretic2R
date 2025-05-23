55c23137c86e6972e05d9f7fb8a21a720207a335
m-x-d <j.maxed@gmail.com>
2025-05-23 14:59:27 +0300

    Implement S_StopAllSounds().

--- src/WinSnd/snd_dma.c
+++ src/WinSnd/snd_dma.c
@@ -6,6 +6,16 @@
 
 #include "snd_dma.h"
 
+// Internal sound data & structures.
+channel_t channels[MAX_CHANNELS];
+
+static qboolean sound_started = false; //mxd. int in Q2.
+
+#define MAX_PLAYSOUNDS	128
+static playsound_t s_playsounds[MAX_PLAYSOUNDS];
+static playsound_t s_freeplays;
+playsound_t s_pendingplays;
+
 void S_Init(void)
 {
 	NOT_IMPLEMENTED
@@ -51,7 +61,28 @@ void S_StartLocalSound(const char* sound)
 
 void S_StopAllSounds(void)
 {
-	NOT_IMPLEMENTED
+	if (!sound_started)
+		return;
+
+	// Clear all the playsounds.
+	memset(s_playsounds, 0, sizeof(s_playsounds));
+
+	s_freeplays.next = &s_freeplays;
+	s_freeplays.prev = &s_freeplays;
+	s_pendingplays.next = &s_pendingplays;
+	s_pendingplays.prev = &s_pendingplays;
+
+	for (int i = 0; i < MAX_PLAYSOUNDS; i++)
+	{
+		s_playsounds[i].prev = &s_freeplays;
+		s_playsounds[i].next = s_freeplays.next;
+		s_playsounds[i].prev->next = &s_playsounds[i];
+		s_playsounds[i].next->prev = &s_playsounds[i];
+	}
+
+	// Clear all the channels.
+	memset(channels, 0, sizeof(channels));
+	// H2: no S_ClearBuffer() call.
 }
 
 void S_StopAllSounds_Sounding(void) // H2
