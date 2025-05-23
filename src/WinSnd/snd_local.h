443cf64cb33ab7c0b72fa2a3142be5ff2a123421
m-x-d <j.maxed@gmail.com>
2025-05-23 14:48:56 +0300

    Move sfx_t typedef to snd_local.h. Move sfxcache_t typedef to snd_local.h. Remove snd_loc.h.

--- src/WinSnd/snd_local.h
+++ src/WinSnd/snd_local.h
@@ -6,9 +6,29 @@
 
 #pragma once
 
-#include "Debug.h" //TODO: remove
+#include "q_shared.h"
 
 #define SNDLIB_DECLSPEC __declspec(dllexport)
+
+//mxd. Moved from snd_loc.h. Not used outside of sound library.
+typedef struct sfxcache_s
+{
+	int length;
+	int loopstart;
+	int speed; // Not needed, because converted on load?
+	int width;
+	int stereo;
+	byte data[1]; // Variable-sized.
+} sfxcache_t;
+
+//mxd. Moved from snd_loc.h.
+typedef struct sfx_s
+{
+	char name[MAX_QPATH];
+	int registration_sequence;
+	sfxcache_t* cache;
+	//char* truename; //mxd. Missing in H2.
+} sfx_t;
 
 typedef struct
 {
