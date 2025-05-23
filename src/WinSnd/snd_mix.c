3dc3ba925b975d84d380e61c2ac5e6d2f9459226
m-x-d <j.maxed@gmail.com>
2025-05-23 17:19:40 +0300

    Implement S_InitScaletable().

--- src/WinSnd/snd_mix.c
+++ src/WinSnd/snd_mix.c
@@ -5,9 +5,21 @@
 //
 
 #include "snd_mix.h"
+#include "snd_dma.h"
 #include "snd_local.h"
 
+static int snd_scaletable[32][256];
+
+// Q2 counterpart.
 void S_InitScaletable(void)
 {
-	NOT_IMPLEMENTED
+	s_volume->modified = false;
+
+	for (int i = 0; i < 32; i++)
+	{
+		const int scale = (int)((float)i * 8.0f * 256.0f * s_volume->value);
+
+		for (int j = 0; j < 256; j++)
+			snd_scaletable[i][j] = (char)j * scale;
+	}
 }
