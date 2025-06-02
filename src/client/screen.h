294c27f4a6cce5a3e4fb2aec9e6ab696d49ab25b
m-x-d <j.maxed@gmail.com>
2025-06-02 15:34:22 +0300

    screen.h: remove scr_conlines var declaration (unused). screen.h: remove sb_lines var declaration (unused). screen.h: add 'extern' keyword to function declarations.

--- src/client/screen.h
+++ src/client/screen.h
@@ -9,34 +9,31 @@
 #include "q_shared.h" //mxd
 #include "vid.h" //mxd
 
-void SCR_Init(void);
-void SCR_UpdateScreen(void);
+extern void SCR_Init(void);
+extern void SCR_UpdateScreen(void);
 
-void SCR_BeginLoadingPlaque(void);
-void SCR_EndLoadingPlaque(void);
-void SCR_UpdateProgressbar(int unused, int section); //mxd
-void SCR_DebugGraph(float value, uint color); //mxd. Re-added
+extern void SCR_BeginLoadingPlaque(void);
+extern void SCR_EndLoadingPlaque(void);
+extern void SCR_UpdateProgressbar(int unused, int section); //mxd
+extern void SCR_DebugGraph(float value, uint color); //mxd. Re-added.
 
 extern qboolean scr_draw_loading_plaque; //mxd
 
 extern float scr_con_current;
-extern float scr_conlines;	// Lines of console to display
-
-extern int sb_lines;
 
 extern cvar_t* scr_viewsize;
 extern cvar_t* scr_centertime; //mxd
 extern cvar_t* crosshair;
 
-extern vrect_t scr_vrect;	// Position of render window
+extern vrect_t scr_vrect; // Position of render window.
 
-void SCR_AddDirtyPoint(int x, int y);
-void SCR_DirtyScreen(void);
+extern void SCR_AddDirtyPoint(int x, int y);
+extern void SCR_DirtyScreen(void);
 
 // cl_smk.c
-void SCR_PlayCinematic(const char *name);
-void SCR_DrawCinematic(void); //mxd. Removed unused qboolean return type.
-void SCR_RunCinematic(void);
-void SCR_FinishCinematic(void);
-void SCR_StopCinematic(void);
-void SMK_Shutdown(void); //mxd
+extern void SCR_PlayCinematic(const char *name);
+extern void SCR_DrawCinematic(void); //mxd. Removed unused qboolean return type.
+extern void SCR_RunCinematic(void);
+extern void SCR_FinishCinematic(void);
+extern void SCR_StopCinematic(void);
+extern void SMK_Shutdown(void); //mxd
