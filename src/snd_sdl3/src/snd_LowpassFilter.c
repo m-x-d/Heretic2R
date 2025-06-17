5050bef251ac4547c5ad952e0a229e2293938419
m-x-d <j.maxed@gmail.com>
2025-06-17 19:17:30 +0300

    Implement LPF_UpdateSamples().

--- src/snd_sdl3/src/snd_LowpassFilter.c
+++ src/snd_sdl3/src/snd_LowpassFilter.c
@@ -7,7 +7,7 @@
 
 #include "snd_LowpassFilter.h"
 
-void LPF_Initialize(LpfContext_t* lpf_context, const float gain_hf, const int target_frequency)
+void LPF_Initialize(LpfContext_t* lpf_context, const float gain_hf, const int target_frequency) // YQ2 (lpf_initialize).
 {
 #define K_TO_PI	6.283185307f
 
@@ -26,7 +26,53 @@ void LPF_Initialize(LpfContext_t* lpf_context, const float gain_hf, const int ta
 	lpf_context->is_history_initialized = false;
 }
 
-void LPF_UpdateSamples(LpfContext_t* lpf_context, int sample_count, portable_samplepair_t* samples)
+void LPF_UpdateSamples(LpfContext_t* lpf_context, const int sample_count, portable_samplepair_t* samples) // YQ2 (lpf_update_samples).
 {
-	NOT_IMPLEMENTED
+	assert(lpf_context != NULL);
+	assert(sample_count >= 0);
+	assert(samples != NULL);
+
+	if (sample_count <= 0)
+		return;
+
+	const float a = lpf_context->a;
+	portable_samplepair_t* history = lpf_context->history;
+
+	if (!lpf_context->is_history_initialized)
+	{
+		lpf_context->is_history_initialized = true;
+
+		for (int s = 0; s < 2; s++)
+		{
+			history[s].left = 0;
+			history[s].right = 0;
+		}
+	}
+
+	for (int s = 0; s < sample_count; s++)
+	{
+		portable_samplepair_t y;
+
+		// Update left channel.
+		y.left = samples[s].left;
+
+		y.left = (int)((float)y.left + a * (float)(history[0].left - y.left));
+		history[0].left = y.left;
+
+		y.left = (int)((float)y.left + a * (float)(history[1].left - y.left));
+		history[1].left = y.left;
+
+
+		// Update right channel.
+		y.right = samples[s].right;
+
+		y.right = (int)((float)y.right + a * (float)(history[0].right - y.right));
+		history[0].right = y.right;
+
+		y.right = (int)((float)y.right + a * (float)(history[1].right - y.right));
+		history[1].right = y.right;
+
+		// Update sample.
+		samples[s] = y;
+	}
 }
