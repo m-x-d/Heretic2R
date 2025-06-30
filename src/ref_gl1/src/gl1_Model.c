b8f10b9b79fc382cee00b465f911437f6bf48c01
m-x-d <j.maxed@gmail.com>
2025-06-30 17:02:47 +0300

    ref_gl1: implement Mod_FreeAll().

--- src/ref_gl1/src/gl1_Model.c
+++ src/ref_gl1/src/gl1_Model.c
@@ -7,6 +7,10 @@
 #include "gl1_Local.h"
 #include "gl1_Model.h"
 
+#define MAX_MOD_KNOWN 512
+static model_t mod_known[MAX_MOD_KNOWN];
+static int mod_numknown;
+
 void Mod_Modellist_f(void)
 {
 	NOT_IMPLEMENTED
@@ -15,6 +19,19 @@ void Mod_Modellist_f(void)
 void Mod_Init(void)
 {
 	NOT_IMPLEMENTED
+}
+
+static void Mod_Free(model_t* mod)
+{
+	NOT_IMPLEMENTED
+}
+
+// Q2 counterpart
+void Mod_FreeAll(void)
+{
+	for (int i = 0; i < mod_numknown; i++)
+		if (mod_known[i].extradatasize)
+			Mod_Free(&mod_known[i]);
 }
 
 void R_BeginRegistration(const char* model)
@@ -29,11 +46,6 @@ struct model_s* R_RegisterModel(const char* name)
 }
 
 void R_EndRegistration(void)
-{
-	NOT_IMPLEMENTED
-}
-
-void Mod_FreeAll(void)
 {
 	NOT_IMPLEMENTED
 }
