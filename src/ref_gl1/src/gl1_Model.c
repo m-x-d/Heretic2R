73ff23be56a40490b0e5b1f917f18ec93857c367
m-x-d <j.maxed@gmail.com>
2025-07-08 07:30:33 +0300

    ref_gl1: implement Mod_LoadSpriteModel().

--- src/ref_gl1/src/gl1_Model.c
+++ src/ref_gl1/src/gl1_Model.c
@@ -82,7 +82,35 @@ static void Mod_LoadBookModel(model_t* mod, const void* buffer) // H2
 
 static void Mod_LoadSpriteModel(model_t* mod, const void* buffer)
 {
-	NOT_IMPLEMENTED
+	char sprite_name[MAX_OSPATH]; // H2
+
+	const dsprite_t* sprin = buffer;
+	dsprite_t* sprout = Hunk_Alloc(modfilelen);
+
+	sprout->ident = LittleLong(sprin->ident);
+	sprout->version = LittleLong(sprin->version);
+	sprout->numframes = LittleLong(sprin->numframes);
+
+	if (sprout->version != SPRITE_VERSION)
+		ri.Sys_Error(ERR_DROP, "'%s' has wrong version number (%i should be %i)", mod->name, sprout->version, SPRITE_VERSION);
+
+	if (sprout->numframes > MAX_FRAMES)
+		ri.Sys_Error(ERR_DROP, "'%s' has too many frames (%i > %i)", mod->name, sprout->numframes, MAX_FRAMES);
+
+	// Byte-swap everything.
+	for (int i = 0; i < sprout->numframes; i++)
+	{
+		sprout->frames[i].width = LittleLong(sprin->frames[i].width);
+		sprout->frames[i].height = LittleLong(sprin->frames[i].height);
+		sprout->frames[i].origin_x = LittleLong(sprin->frames[i].origin_x);
+		sprout->frames[i].origin_y = LittleLong(sprin->frames[i].origin_y);
+		memcpy(sprout->frames[i].name, sprin->frames[i].name, MAX_FRAMENAME);
+
+		Com_sprintf(sprite_name, sizeof(sprite_name), "Sprites/%s", sprout->frames->name); // H2
+		mod->skins[i] = R_FindImage(sprite_name, it_sprite);
+	}
+
+	mod->type = mod_sprite;
 }
 
 #pragma region ========================== BRUSHMODEL LOADING ==========================
