d064cdbd082e3194d908daffb3e4a683f8565b00
m-x-d <j.maxed@gmail.com>
2025-08-04 17:58:10 +0300

    Draw_BookPic(): preserve aspect ratio in widescreen. Draw_BigFont(): preserve aspect ratio in widescreen.

--- src/ref_gl1/src/gl1_DrawBook.c
+++ src/ref_gl1/src/gl1_DrawBook.c
@@ -19,14 +19,14 @@ static const glxy_t* GetCharDef(const byte c, const glxy_t* font)
 }
 
 //mxd. Not part of original logic.
-static void DrawBigFontChar(const int x, int y, const glxy_t* char_def)
+static void DrawBigFontChar(const int x, int y, const int offset_x, const int offset_y, const int width, const int height, const glxy_t* char_def)
 {
 	y -= char_def->baseline;
 
-	const int xl = viddef.width * x / DEF_WIDTH;
-	const int xr = viddef.width * (char_def->w + x) / DEF_WIDTH;
-	const int yt = viddef.height * y / DEF_HEIGHT;
-	const int yb = viddef.height * (char_def->h + y) / DEF_HEIGHT;
+	const int xl = (width * x / DEF_WIDTH) + offset_x;
+	const int xr = (width * (char_def->w + x) / DEF_WIDTH) + offset_x;
+	const int yt = (height * y / DEF_HEIGHT) + offset_y;
+	const int yb = (height * (char_def->h + y) / DEF_HEIGHT) + offset_y;
 
 	glBegin(GL_QUADS);
 
@@ -63,6 +63,18 @@ void Draw_BigFont(const int x, const int y, const char* text, const float alpha)
 	int ox = x;
 	int oy = y;
 
+	int vid_w = viddef.width;
+	int vid_h = viddef.height;
+
+	int offset_x = 0;
+	int offset_y = 0;
+
+	if ((float)vid_w * 0.75f > (float)vid_h) //mxd. Setup for widescreen aspect ratio.
+	{
+		vid_w = vid_h * 4 / 3;
+		offset_x = (viddef.width - vid_w) / 2;
+	}
+
 	while (true)
 	{
 		const byte c = *text++;
@@ -72,8 +84,8 @@ void Draw_BigFont(const int x, const int y, const char* text, const float alpha)
 
 		switch (c)
 		{
-			case 1:
-				ox = (viddef.width - BF_Strlen(text)) / 2;
+			case 1: //TODO: unused?
+				ox = (DEF_WIDTH - BF_Strlen(text)) / 2; //mxd. viddef.width instead of DEF_WIDTH in original logic.
 				break;
 
 			case 2:
@@ -105,7 +117,7 @@ void Draw_BigFont(const int x, const int y, const char* text, const float alpha)
 				if (c > 32)
 				{
 					const glxy_t* char_def = GetCharDef(c, cur_font);
-					DrawBigFontChar(ox, oy, char_def); //mxd. Logic split into helper function.
+					DrawBigFontChar(ox, oy, offset_x, offset_y, vid_w, vid_h, char_def); //mxd. Logic split into helper function.
 					ox += char_def->w;
 				}
 				break;
@@ -175,14 +187,23 @@ void Draw_BookPic(const char* name, const float scale)
 
 	book_t* book = mod->extradata;
 
-	const float vid_w = (float)viddef.width;
-	const float vid_h = (float)viddef.height;
-
 	const float header_w = (float)book->bheader.total_w;
 	const float header_h = (float)book->bheader.total_h;
 
-	const int offset_x = (const int)((header_w - header_w * scale) * 0.5f * (vid_w / header_w));
-	const int offset_y = (const int)((header_h - header_h * scale) * 0.5f * (vid_h / header_h));
+	float vid_w = (float)viddef.width;
+	float vid_h = (float)viddef.height;
+
+	int offset_x = 0;
+	int offset_y = 0;
+
+	if (vid_w * 0.75f > vid_h) //mxd. Setup for widescreen aspect ratio.
+	{
+		vid_w = vid_h * 4 / 3;
+		offset_x = (viddef.width - (int)vid_w) / 2;
+	}
+
+	offset_x += (const int)((header_w - header_w * scale) * 0.5f * (vid_w / header_w));
+	offset_y += (const int)((header_h - header_h * scale) * 0.5f * (vid_h / header_h));
 
 	bookframe_t* bframe = &book->bframes[0];
 	for (int i = 0; i < book->bheader.num_segments; i++, bframe++)
