m-x-d/Heretic2R

commit 70aba32d6b2605674c32c02041d77c741e10b551

client.h: add kbutton_state_t enum, change kbutton_t.state type to kbutton_state_t. Update input logic to use kbutton_state_t values instead of integers.

src/client/cl_input.c
@@ -73,7 +73,7 @@ static void KeyUp(kbutton_t* b)
 		// Typed manually at the console, assume for unsticking, so clear all.
 		b->down[0] = 0;
 		b->down[1] = 0;
-		b->state = 4; // Impulse up.
+		b->state = KS_IMPULSE_UP; // Impulse up.
 
 		return;
 	}
@@ -87,7 +87,7 @@ static void KeyUp(kbutton_t* b)
 	else
 		return; // Key up without corresponding down (menu pass through).
 
-	if (b->down[0] || b->down[1] || !(b->state & 1))
+	if (b->down[0] || b->down[1] || !(b->state & KS_DOWN))
 		return; // Some other key is still holding it down or still up (this should not happen).
 
 	// Save timestamp.
@@ -97,8 +97,8 @@ static void KeyUp(kbutton_t* b)
 	else
 		b->msec += 10;
 
-	b->state &= ~1; // Now up.
-	b->state |= 4; // Impulse up.
+	b->state &= ~KS_DOWN; // Now up.
+	b->state |= KS_IMPULSE_UP; // Impulse up.
 }
 
 // Q2 counterpart
@@ -129,7 +129,7 @@ static void KeyDown(kbutton_t* b)
 		return;
 	}
 
-	if (b->state & 1)
+	if (b->state & KS_DOWN)
 		return; // Still down.
 
 	// Save timestamp.
@@ -139,7 +139,7 @@ static void KeyDown(kbutton_t* b)
 	if (b->downtime == 0)
 		b->downtime = sys_frame_time - 100;
 
-	b->state |= 1 + 2; // Down + impulse down.
+	b->state |= (KS_DOWN | KS_IMPULSE_DOWN); // Down + impulse down.
 }
 
 // Q2 counterpart
@@ -474,7 +474,7 @@ static void CL_UpdateClientAngles(void)
 	static qboolean st_unknown5;
 	static qboolean st_unknown6;
 
-	const qboolean do_lookaround = (in_lookaround.state & 1);
+	const qboolean do_lookaround = (in_lookaround.state & KS_DOWN);
 
 	// Look around key pressed?
 	if (do_lookaround)
@@ -719,7 +719,7 @@ void CL_RefreshCmd(void) // YQ2. Called on packetframe or renderframe.
 	old_sys_frame_time = sys_frame_time;
 
 	// Important events are send immediately.
-	if ((in_attack.state & 2) || (in_defend.state & 2) || (in_action.state & 2))
+	if ((in_attack.state & KS_IMPULSE_DOWN) || (in_defend.state & KS_IMPULSE_DOWN) || (in_action.state & KS_IMPULSE_DOWN))
 		cls.force_packet = true;
 }
 
@@ -747,12 +747,12 @@ void CL_RefreshMove(void) // YQ2. Called on packetframe or renderframe.
 // Returns the fraction of the frame that the key was down.
 static float CL_KeyState(kbutton_t* key)
 {
-	key->state &= 1; // Clear impulses.
+	key->state &= ~(KS_IMPULSE_DOWN | KS_IMPULSE_UP); // Clear impulses.
 
 	uint msec = key->msec;
 	key->msec = 0;
 
-	if (key->state) // Still down.
+	if (key->state == KS_DOWN) // Still down.
 	{
 		msec += sys_frame_time - key->downtime;
 		key->downtime = sys_frame_time;
@@ -767,12 +767,12 @@ static void CL_AdjustAngles(void)
 	float speed;
 	float scaler;
 
-	if (in_speed.state & 1)
+	if (in_speed.state & KS_DOWN)
 		speed = cls.nframetime * cl_anglespeedkey->value;
 	else
 		speed = cls.nframetime;
 
-	if (!(in_strafe.state & 1))
+	if (!(in_strafe.state & KS_DOWN))
 	{
 		scaler = speed * cl_yawspeed->value;
 
@@ -782,7 +782,7 @@ static void CL_AdjustAngles(void)
 
 	scaler = speed * cl_pitchspeed->value;
 
-	if (in_klook.state & 1)
+	if (in_klook.state & KS_DOWN)
 	{
 		cl.delta_inputangles[PITCH] -= CL_KeyState(&in_forward) * scaler;
 		cl.delta_inputangles[PITCH] += CL_KeyState(&in_back) * scaler;
@@ -802,7 +802,7 @@ void CL_BaseMove(usercmd_t* cmd)
 
 	CL_AdjustAngles();
 
-	if (in_strafe.state & 1)
+	if (in_strafe.state & KS_DOWN)
 	{
 		cmd->sidemove += (short)(CL_KeyState(&in_right) * MOVE_SCALER);
 		cmd->sidemove -= (short)(CL_KeyState(&in_left) * MOVE_SCALER);
@@ -814,7 +814,7 @@ void CL_BaseMove(usercmd_t* cmd)
 	cmd->upmove += (short)(CL_KeyState(&in_up) * MOVE_SCALER);
 	cmd->upmove -= (short)(CL_KeyState(&in_down) * MOVE_SCALER);
 
-	if (!(in_klook.state & 1))
+	if (!(in_klook.state & KS_DOWN))
 	{
 		cmd->forwardmove += (short)(CL_KeyState(&in_forward) * MOVE_SCALER);
 		cmd->forwardmove -= (short)(CL_KeyState(&in_back) * MOVE_SCALER);
@@ -826,57 +826,57 @@ static void CL_FinishMove(usercmd_t* cmd) // Called on packetframe.
 	// Figure button bits.
 
 	// He attac.
-	if (in_attack.state & 3)
+	if (in_attack.state & (KS_DOWN | KS_IMPULSE_DOWN))
 		cmd->buttons |= BUTTON_ATTACK;
-	in_attack.state &= ~2;
+	in_attack.state &= ~KS_IMPULSE_DOWN;
 
 	// But also protec.
-	if (in_defend.state & 2)
+	if (in_defend.state & KS_DOWN) //TODO: why no KS_IMPULSE_DOWN check here?..
 		cmd->buttons |= BUTTON_DEFEND;
-	in_defend.state &= ~2;
+	in_defend.state &= ~KS_IMPULSE_DOWN;
 
 	// Action.
-	if (in_action.state & 3 && !cl.frame.playerstate.cinematicfreeze)
+	if ((in_action.state & (KS_DOWN | KS_IMPULSE_DOWN)) && !cl.frame.playerstate.cinematicfreeze)
 		cmd->buttons |= BUTTON_ACTION;
-	in_action.state &= ~2;
+	in_action.state &= ~KS_IMPULSE_DOWN;
 
 	// Run.
-	const qboolean speed_state = (in_speed.state & 3);
+	const qboolean speed_state = (in_speed.state & (KS_DOWN | KS_IMPULSE_DOWN));
 	const qboolean run = (int)cl_run->value;
 	if ((speed_state || run) && ((speed_state && !run) || speed_state == (cmd->forwardmove < -10)))
 		cmd->buttons |= BUTTON_RUN;
-	in_speed.state &= ~2;
+	in_speed.state &= ~KS_IMPULSE_DOWN;
 
 	// Crouch.
-	if (in_creep.state & 3)
+	if (in_creep.state & (KS_DOWN | KS_IMPULSE_DOWN))
 		cmd->buttons |= BUTTON_CREEP;
-	in_creep.state &= ~2;
+	in_creep.state &= ~KS_IMPULSE_DOWN;
 
 	// Autoaim.
-	if ((in_autoaim.state & 3) != (int)cl_doautoaim->value)
+	if ((in_autoaim.state & (KS_DOWN | KS_IMPULSE_DOWN)) != (int)cl_doautoaim->value)
 		cmd->buttons |= BUTTON_AUTOAIM;
-	in_autoaim.state &= ~2;
+	in_autoaim.state &= ~KS_IMPULSE_DOWN;
 
 	in_do_autoaim = (cmd->buttons & BUTTON_AUTOAIM);
 
 	// TR-style look around.
-	if (in_lookaround.state & 3)
+	if (in_lookaround.state & (KS_DOWN | KS_IMPULSE_DOWN))
 		cmd->buttons |= BUTTON_LOOKAROUND;
-	in_lookaround.state &= ~2;
+	in_lookaround.state &= ~KS_IMPULSE_DOWN;
 
 	// TR-style quick-turn.
-	if (in_quickturn.state & 3)
+	if (in_quickturn.state & (KS_DOWN | KS_IMPULSE_DOWN))
 	{
 		cmd->buttons |= BUTTON_QUICKTURN;
 		if (quickturn_time == 0.0f)
 			quickturn_time = 0.5f;
 	}
-	in_quickturn.state &= ~2;
+	in_quickturn.state &= ~KS_IMPULSE_DOWN;
 
 	// Open inventory.
-	if (in_inventory.state & 3)
+	if (in_inventory.state & (KS_DOWN | KS_IMPULSE_DOWN))
 		cmd->buttons |= BUTTON_INVENTORY;
-	in_inventory.state &= ~2;
+	in_inventory.state &= ~KS_IMPULSE_DOWN;
 
 	if (anykeydown > 0 && cls.key_dest == key_game)
 		cmd->buttons |= BUTTON_ANY;

src/client/cl_entities.c
@@ -1393,7 +1393,7 @@ static void CL_UpdateCameraOrientation(float viewheight, const qboolean interpol
 		}
 	}
 
-	if (waterlevel == 0 || (water_flags & ~WF_SWIMFREE) || (water_flags == 0 && in_down.state == 0))
+	if (waterlevel == 0 || (water_flags & ~WF_SWIMFREE) || (water_flags == 0 && in_down.state == KS_NONE))
 	{
 		const float roll_scaler = 1.0f - fabsf(look_angles[PITCH] / 89.0f);
 		const vec3_t v = { mins[0], mins[1], -1.0f - roll_scaler * 2.0f };
@@ -1521,7 +1521,7 @@ static void CL_CalcViewValues(void)
 
 			VectorCopy(cl.predicted_angles, look_angles);
 		}
-		else if (in_lookaround.state & 1)
+		else if (in_lookaround.state & KS_DOWN)
 		{
 			for (int i = 0; i < 3; i++)
 				look_angles[i] = cl.lookangles[i] + (float)ps->pmove.delta_angles[i] * SHORT_TO_ANGLE;

src/client/client.h
@@ -630,12 +630,20 @@ extern void CL_PingServers_f(void);
 extern void CL_Snd_Restart_f(void);
 
 // cl_input.c
+typedef enum kbutton_state_e //mxd
+{
+	KS_NONE = 0,			// Not pressed.
+	KS_DOWN = 1,			// Pressed.
+	KS_IMPULSE_DOWN = 2,	// Pressed this frame.
+	KS_IMPULSE_UP = 4,		// Released this frame.
+} kbutton_state_t;
+
 typedef struct
 {
 	int down[2];	// Key nums holding it down
 	uint downtime;	// Msec timestamp
 	uint msec;		// Msec down this frame
-	int state;
+	kbutton_state_t state; //mxd. Int in original logic.
 } kbutton_t;
 
 extern kbutton_t in_klook;

src/client/input_sdl3.c
@@ -175,12 +175,12 @@ static void IN_MouseMove(usercmd_t* cmd)
 	mouse_y *= mouse_sensitivity_y->value; // 'sensitivity' cvar in Q2
 
 	// Add mouse X/Y movement to cmd.
-	if ((in_strafe.state & 1) || ((int)lookstrafe->value && mlooking)) //TODO: remove 'lookstrafe' cvar, always freelook.
+	if ((in_strafe.state & KS_DOWN) || ((int)lookstrafe->value && mlooking)) //TODO: remove 'lookstrafe' cvar, always freelook.
 		cmd->sidemove += (short)(mouse_x * m_side->value);
 	else
 		cl.delta_inputangles[YAW] -= mouse_x * m_yaw->value;
 
-	if (!(in_strafe.state & 1) || ((int)freelook->value && mlooking)) // H2: no 'else' case //TODO: remove freelook cvar, always freelook.
+	if (!(in_strafe.state & KS_DOWN) || ((int)freelook->value && mlooking)) // H2: no 'else' case //TODO: remove freelook cvar, always freelook.
 		cl.delta_inputangles[PITCH] += mouse_y * m_pitch->value;
 
 	// Reset mouse position.
@@ -220,7 +220,7 @@ void IN_Init(void)
 	IN_InitController();
 
 	if (!SDL_WasInit(SDL_INIT_EVENTS) && !SDL_Init(SDL_INIT_EVENTS))
-		Com_Error(ERR_FATAL, "Couldn't initialize SDL event subsystem:%s\n", SDL_GetError());
+		Com_Error(ERR_FATAL, "Couldn't initialize SDL event subsystem: %s\n", SDL_GetError());
 
 	IN_StartupController();
 }

commit 42bf125268d9cfbadd6a99264407fa3653cabacc

Fix logic error in CL_FinishMove() (introduced in 70aba32).

src/client/cl_input.c
@@ -831,7 +831,7 @@ static void CL_FinishMove(usercmd_t* cmd) // Called on packetframe.
 	in_attack.state &= ~KS_IMPULSE_DOWN;
 
 	// But also protec.
-	if (in_defend.state & KS_DOWN) //TODO: why no KS_IMPULSE_DOWN check here?..
+	if (in_defend.state & KS_IMPULSE_DOWN) //TODO: why no KS_DOWN check here?..
 		cmd->buttons |= BUTTON_DEFEND;
 	in_defend.state &= ~KS_IMPULSE_DOWN;
 