c0db46deb1bab88a5e41af029d1302f7eaedd686
m-x-d <j.maxed@gmail.com>
2025-09-09 02:32:26 +0300

    HealthPickupThink(): update rotation/bobbing logic to work at current framerate instead of fixed 20 FPS.

--- src/client effects/fx_HealthPickup.c
+++ src/client effects/fx_HealthPickup.c
@@ -9,9 +9,11 @@
 #include "Vector.h"
 #include "Random.h"
 
-#define BOB_HEIGHT		6.0f
-#define BOB_SPEED		ANGLE_10
-#define HEALTH_RADIUS	6.0f
+#define BOB_HEIGHT			6.0f
+#define BOB_SPEED			ANGLE_10
+#define HEALTH_RADIUS		6.0f
+#define HEALTH_SMALL_RADIUS	3.0f //mxd
+#define ANIMATION_SPEED		50 //mxd
 
 static struct model_s* health_models[2];
 
@@ -23,15 +25,25 @@ void PreCacheHealth(void)
 
 static qboolean HealthPickupThink(struct client_entity_s* self, centity_t* owner)
 {
-	paletteRGBA_t color;
-
 	// Rotate and bob.
-	self->r.angles[YAW] += ANGLE_15;
+	const int step = fxi.cl->time - self->nextThinkTime; //mxd
+	const float lerp = (float)step / ANIMATION_SPEED; //mxd
+	self->LifeTime += step; //mxd
+
+	self->r.angles[YAW] += ANGLE_15 * lerp;
 	VectorCopy(owner->current.origin, self->r.origin);
 	self->r.origin[2] += cosf(self->SpawnData) * BOB_HEIGHT;
-	self->SpawnData += BOB_SPEED;
+	self->SpawnData += BOB_SPEED * lerp;
+
+	//mxd. Spawn particles at original rate.
+	if (self->LifeTime < ANIMATION_SPEED)
+		return true;
+
+	self->LifeTime %= ANIMATION_SPEED;
 
 	// Spawn particles.
+	paletteRGBA_t color;
+
 	if (self->flags & CEF_FLAG6)
 	{
 		color.g = (byte)irand(80, 120);
@@ -48,7 +60,17 @@ static qboolean HealthPickupThink(struct client_entity_s* self, centity_t* owner
 	color.a = 255;
 	client_particle_t* p = ClientParticle_new(PART_4x4_WHITE | PFL_SOFT_MASK, color, 600);
 
-	VectorSet(p->origin, flrand(-HEALTH_RADIUS, HEALTH_RADIUS), flrand(-HEALTH_RADIUS, HEALTH_RADIUS), 0.0f);
+	if (self->flags & CEF_FLAG6)
+	{
+		VectorSet(p->origin, flrand(-HEALTH_RADIUS, HEALTH_RADIUS), flrand(-HEALTH_RADIUS, HEALTH_RADIUS), 0.0f);
+	}
+	else //mxd. Align with the bottle cap.
+	{
+		const float yaw = -self->r.angles[YAW] - ANGLE_180;
+		VectorSet(p->origin, sinf(yaw) * 3.0f + flrand(-HEALTH_SMALL_RADIUS, HEALTH_SMALL_RADIUS), 
+							 cosf(yaw) * 3.0f + flrand(-HEALTH_SMALL_RADIUS, HEALTH_SMALL_RADIUS), 6.0f);
+	}
+	
 	VectorSet(p->velocity, 0.0f, 0.0f, flrand(20.0f, 40.0f));
 	p->acceleration[2] = 20.0f;
 	AddParticleToList(self, p);
@@ -59,13 +81,13 @@ static qboolean HealthPickupThink(struct client_entity_s* self, centity_t* owner
 void FXHealthPickup(centity_t* owner, const int type, int flags, vec3_t origin)
 {
 	flags &= ~CEF_OWNERS_ORIGIN;
-	flags |= CEF_DONT_LINK | CEF_CHECK_OWNER | CEF_VIEWSTATUSCHANGED;
-	client_entity_t* ce = ClientEntity_new(type, flags, origin, NULL, 50);
+	flags |= (CEF_DONT_LINK | CEF_CHECK_OWNER | CEF_VIEWSTATUSCHANGED);
+	client_entity_t* ce = ClientEntity_new(type, flags, origin, NULL, 0); //mxd. next_think_time 50 in original logic. Set to 0, so self->nextThinkTime holds previous update time in AmmoPickupThink()...
 
 	ce->radius = 10.0f;
 	const int model_index = (flags & CEF_FLAG6) >> 5; // 0 - small, 1 - big.
 	ce->r.model = &health_models[model_index];
-	ce->r.flags = RF_GLOW | RF_TRANSLUCENT | RF_TRANS_ADD;
+	ce->r.flags = (RF_GLOW | RF_TRANSLUCENT | RF_TRANS_ADD);
 	ce->alpha = 0.8f;
 
 	if (model_index == 0) // Bigger scale for Half Health.
