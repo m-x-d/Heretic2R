e77af553548a23da01c945113136072623a19a86
m-x-d <j.maxed@gmail.com>
2025-09-09 18:38:29 +0300

    WeaponPickupThink(): update bobbing logic to work at current framerate instead of fixed 20 FPS. WeaponPickupThink(): randomize particle scale a bit. WeaponPickupThink(): make particle vertical offset animation a bit more pronounced.

--- src/client effects/fx_WeaponPickup.c
+++ src/client effects/fx_WeaponPickup.c
@@ -13,6 +13,7 @@
 #define BOB_HEIGHT			6.0f
 #define BOB_SPEED			ANGLE_10
 #define WP_PARTICLE_RADIUS	16.0f
+#define ANIMATION_SPEED		50 //mxd
 
 static struct model_s* weapon_models[7];
 
@@ -29,44 +30,54 @@ void PreCacheItemWeapons(void)
 
 static qboolean WeaponPickupThink(struct client_entity_s* self, centity_t* owner)
 {
+	// Rotate and bob.
+	const int step = fxi.cl->time - self->nextThinkTime; //mxd
+	const float lerp = (float)step / ANIMATION_SPEED; //mxd
+	self->LifeTime += step; //mxd
+
+	self->r.angles[YAW] += ANGLE_15 * lerp;
+	VectorCopy(owner->current.origin, self->r.origin);
+	self->r.origin[2] += cosf(self->SpawnData) * BOB_HEIGHT;
+	self->SpawnData += BOB_SPEED * lerp;
+
+	//mxd. Spawn particles at original rate.
+	if (self->LifeTime < ANIMATION_SPEED)
+		return true;
+
+	self->LifeTime %= ANIMATION_SPEED;
+
 	int part;
 	paletteRGBA_t color;
 
-	// Rotate and bob.
-	self->r.angles[YAW] += ANGLE_15;
-	VectorCopy(owner->current.origin, self->r.origin);
-	self->r.origin[2] += cosf(self->SpawnData) * BOB_HEIGHT;
-	self->SpawnData += BOB_SPEED;
-
 	switch (self->SpawnInfo)
 	{
-		case 0: // Hellstaff
-		case 2: // Red rain bow
+		case ITEM_WEAPON_HELLSTAFF: // Hellstaff.
+		case ITEM_WEAPON_REDRAINBOW: // Red rain bow.
 			part = PART_16x16_SPARK_R;
 			color.c = 0xff0000ff;
 			break;
 
-		case 1: // Magic Missile
+		case ITEM_WEAPON_MAGICMISSILE: // Magic Missile.
 			part = PART_16x16_SPARK_I;
 			color.c = 0xff00ff00;
 			break;
 
-		case 3: // Sphere
+		case ITEM_WEAPON_SPHEREOFANNIHILATION: // Sphere.
 			part = PART_16x16_SPARK_B;
 			color.c = 0xffff0000;
 			break;
 
-		case 4: // Phoenix bow
+		case ITEM_WEAPON_PHOENIXBOW: // Phoenix bow.
 			part = irand(PART_32x32_FIRE0, PART_32x32_FIRE2);
 			color.c = 0xff0080ff;
 			break;
 
-		case 5: // Maceballs
+		case ITEM_WEAPON_MACEBALLS: // Maceballs.
 			part = PART_16x16_SPARK_G;
 			color.c = 0xff00ff00;
 			break;
 
-		case 6: // Firewall
+		case ITEM_WEAPON_FIREWALL: // Firewall.
 			part = irand(PART_16x16_FIRE1, PART_16x16_FIRE3);
 			color.c = 0xff0080ff;
 			break;
@@ -78,14 +89,14 @@ static qboolean WeaponPickupThink(struct client_entity_s* self, centity_t* owner
 	if (ref_soft)
 		part |= PFL_SOFT_MASK;
 	else
-		color.c = 0xffffffff;
+		color.c = color_white.c;
 
 	client_particle_t* spark = ClientParticle_new(part, color, 500);
 
-	spark->scale = 6.0f;
+	spark->scale = flrand(5.0f, 7.0f); //mxd. Randomize scale a bit.
 	spark->origin[0] = cosf(self->SpawnData * 4.0f) * WP_PARTICLE_RADIUS;
 	spark->origin[1] = sinf(self->SpawnData * 4.0f) * WP_PARTICLE_RADIUS;
-	spark->origin[2] = -cosf(self->SpawnData) * BOB_HEIGHT;
+	spark->origin[2] = -cosf(self->SpawnData) * BOB_HEIGHT * 1.8f; //mxd. Scale a bit to make the effect more pronounced.
 	spark->acceleration[2] = flrand(128.0f, 256.0f);
 
 	AddParticleToList(self, spark);
@@ -103,16 +114,15 @@ void FXWeaponPickup(centity_t* owner, const int type, int flags, vec3_t origin)
 		return;
 
 	flags &= ~CEF_OWNERS_ORIGIN;
-	flags |= CEF_DONT_LINK | CEF_CHECK_OWNER | CEF_ADDITIVE_PARTS | CEF_VIEWSTATUSCHANGED;
-	client_entity_t* ce = ClientEntity_new(type, flags, origin, NULL, 50);
+	flags |= (CEF_DONT_LINK | CEF_CHECK_OWNER | CEF_ADDITIVE_PARTS | CEF_VIEWSTATUSCHANGED);
+	client_entity_t* ce = ClientEntity_new(type, flags, origin, NULL, 0); //mxd. next_think_time 50 in original logic. Set to 0, so self->nextThinkTime holds previous update time in AmmoPickupThink()...
 
 	ce->radius = 10.0f;
-	ce->SpawnInfo = tag - 2;
-	ce->r.model = &weapon_models[ce->SpawnInfo];
-	ce->r.flags = RF_TRANSLUCENT | RF_GLOW;
+	ce->SpawnInfo = tag;
+	ce->r.model = &weapon_models[tag - 2];
+	ce->r.flags = RF_GLOW; //mxd. Remove RF_TRANSLUCENT flag and 0.8 alpha (looks broken with those enabled).
 	ce->r.scale = ((tag == ITEM_WEAPON_FIREWALL) ? 1.0f : 0.5f);
 	ce->r.skinnum = ((tag == ITEM_WEAPON_PHOENIXBOW) ? 1 : 0);
-	ce->alpha = 0.8f;
 	ce->Update = WeaponPickupThink;
 
 	AddEffect(owner, ce);
