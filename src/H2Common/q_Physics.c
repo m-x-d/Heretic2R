67b7011e911994d4bd4a918eea4499a0ca5d101b
m-x-d <j.maxed@gmail.com>
2025-09-23 18:46:40 +0300

    BounceVelocity(): update logic to match commonly used vector reflection logic. Old logic occasionally resulted in rapid velocity growth.

--- src/H2Common/q_Physics.c
+++ src/H2Common/q_Physics.c
@@ -7,10 +7,14 @@
 #include "q_Physics.h"
 #include "Vector.h"
 
+// https://3dkingdoms.com/weekly/weekly.php?a=2
 H2COMMON_API void BounceVelocity(const vec3_t in, const vec3_t normal, vec3_t out, const float elasticity)
 {
-	const float len = DotProduct(in, normal) * elasticity;
-	VectorMA(in, -len, normal, out);
+	const float dot = DotProduct(in, normal);
+
+	//mxd. Original logic multiplies 'dot' by '-elasticity' instead of '-2.0f'. This occasionally results in rapid velocity growth...
+	for (int i = 0; i < 3; i++)
+		out[i] = (elasticity * 0.5f) * (-2.0f * dot * normal[i] + in[i]); //mxd. Scale elasticity by 0.5 to compensate for -2.0 scaler...
 }
 
 H2COMMON_API qboolean BoundVelocity(vec3_t vel)
