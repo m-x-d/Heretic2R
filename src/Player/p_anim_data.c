m-x-d/Heretic2R

commit 218594f1692a056209550293c205ea7340e6aeaa

SeqCtrl[]: change ASEQ_DASH_LEFT and ASEQ_DASH_RIGHT branchfunc to BranchLwrRunning (fixes jumping from stand-still position from these animations).

src/Player/p_anim_data.c
@@ -393,7 +393,7 @@ seqctrl_t SeqCtrl[ASEQ_MAX] =
 	{ ACMDL_CROUCH,		ASEQ_CROUCH_PIVOTR,			ASEQ_CROUCH_END,		BranchLwrCrouching },		// ASEQ_CROUCH_PIVOTR
 	{ ACMDL_STRAFE_L,	ASEQ_STRAFEL,				ASEQ_STRAFEL_END,		BranchLwrStanding },		// ASEQ_STRAFEL
 	{ ACMD_NONE,		ASEQ_STAND,					ASEQ_STAND,				BranchLwrStanding },		// ASEQ_STRAFEL_END
-	{ ACMDL_STRAFE_R,	ASEQ_STRAFER,				ASEQ_STRAFER_END,		BranchLwrStanding },		// ASEQ_STRAFER1
+	{ ACMDL_STRAFE_R,	ASEQ_STRAFER,				ASEQ_STRAFER_END,		BranchLwrStanding },		// ASEQ_STRAFER
 	{ ACMD_NONE,		ASEQ_STAND,					ASEQ_STAND,				BranchLwrStanding },		// ASEQ_STRAFER_END
 	{ ACMD_NONE,		ASEQ_JUMPUP,				ASEQ_JUMPUP,			BranchLwrJumping },			// ASEQ_JUMPSTD_GO
 	{ ACMD_NONE,		ASEQ_JUMPFWD,				ASEQ_JUMPFWD,			NULL },						// ASEQ_JUMPFWD_SGO
@@ -534,9 +534,9 @@ seqctrl_t SeqCtrl[ASEQ_MAX] =
 	{ ACMD_NONE,		ASEQ_FALL,					ASEQ_FALL,				NULL },						// ASEQ_JUMPRIGHTFLIP
 	{ ACMD_NONE,		ASEQ_DROWN_IDLE,			ASEQ_DROWN_IDLE,		NULL },						// ASEQ_DROWN_IDLE
 	{ ACMDL_STRAFE_L,	ASEQ_DASH_LEFT,				ASEQ_STAND,				NULL },						// ASEQ_DASH_LEFT_GO
-	{ ACMDL_STRAFE_L,	ASEQ_DASH_LEFT,				ASEQ_STAND,				BranchLwrStanding },		// ASEQ_DASH_LEFT
+	{ ACMDL_STRAFE_L,	ASEQ_DASH_LEFT,				ASEQ_STAND,				BranchLwrRunning },			// ASEQ_DASH_LEFT //mxd. BranchLwrStanding in original logic. Fixes jumping from stand-still position during side-dash.
 	{ ACMDL_STRAFE_R,	ASEQ_DASH_RIGHT,			ASEQ_STAND,				NULL },						// ASEQ_DASH_RIGHT_GO
-	{ ACMDL_STRAFE_R,	ASEQ_DASH_RIGHT,			ASEQ_STAND,				BranchLwrStanding },		// ASEQ_DASH_RIGHT
+	{ ACMDL_STRAFE_R,	ASEQ_DASH_RIGHT,			ASEQ_STAND,				BranchLwrRunning },			// ASEQ_DASH_RIGHT //mxd. BranchLwrStanding in original logic. Fixes jumping from stand-still position during side-dash.
 	{ ACMDL_BACK,		ASEQ_CSTRAFEB_LEFT,			ASEQ_STAND,				BranchLwrShortstep },		// ASEQ_CSTRAFEB_LEFT
 	{ ACMDL_BACK,		ASEQ_CSTRAFEB_RIGHT,		ASEQ_STAND,				BranchLwrShortstep },		// ASEQ_CSTRAFEB_RIGHT
 	{ ACMDL_BACK,		ASEQ_WSTRAFEB_LEFT, 		ASEQ_STAND,				BranchLwrWalking },			// ASEQ_WSTRAFEB_LEFT

commit 645fc82f067f25776b978d8adc87147496bf34fb

player_frames_dash_left[]: add extra frame to make it loop properly (broken in 218594f, because animation no longer goes through ASEQ_DASH_LEFT_GO state when looping). player_frames_dash_right[]: add extra frame to make it loop properly (broken in 218594f, because animation no longer goes through ASEQ_DASH_RIGHT_GO state when looping).

src/Player/p_anim_data.c
@@ -4980,10 +4980,11 @@ static panimframe_t player_frames_dash_left[] =
 	{ FRAME_fastleft2, PlayerMoveFunc, 0, -200 * PHYS_SCALER, 0, NULL, 0, PlayerActionCheckBranchRunningStrafe },
 	{ FRAME_fastleft3, PlayerMoveFunc, 0, -200 * PHYS_SCALER, 0, NULL, 0, PlayerActionCheckBranchRunningStrafe },
 	{ FRAME_fastleft4, PlayerMoveFunc, 0, -200 * PHYS_SCALER, 0, NULL, 0, PlayerActionCheckBranchRunningStrafe },
-	{ FRAME_fastleft5, PlayerMoveFunc, 0, -200 * PHYS_SCALER, 0, PlayerActionFootstep, STEP_RUN, PlayerActionCheckBranchRunningStrafe },
+	{ FRAME_fastleft5, PlayerMoveFunc, 0, -200 * PHYS_SCALER, 0, PlayerActionFootstep, STEP_RUN2, PlayerActionCheckBranchRunningStrafe },
 	{ FRAME_fastleft6, PlayerMoveFunc, 0, -200 * PHYS_SCALER, 0, NULL, 0, PlayerActionCheckBranchRunningStrafe },
 	{ FRAME_fastleft7, PlayerMoveFunc, 0, -200 * PHYS_SCALER, 0, NULL, 0, PlayerActionCheckBranchRunningStrafe },
 	{ FRAME_fastleft8, PlayerMoveFunc, 0, -200 * PHYS_SCALER, 0, NULL, 0, PlayerActionCheckBranchRunningStrafe },
+	{ FRAME_fastleft1, PlayerMoveFunc, 0, -200 * PHYS_SCALER, 0, PlayerActionFootstep, STEP_RUN, PlayerActionCheckBranchRunningStrafe }, //mxd. Added to make it loop properly...
 };
 panimmove_t player_move_dash_left = PANIMMOVE(player_frames_dash_left, PlayerAnimLowerUpdate);
 
@@ -5000,10 +5001,11 @@ static panimframe_t player_frames_dash_right[] =
 	{ FRAME_fastrite2, PlayerMoveFunc, 0, 200 * PHYS_SCALER, 0, NULL, 0, PlayerActionCheckBranchRunningStrafe },
 	{ FRAME_fastrite3, PlayerMoveFunc, 0, 200 * PHYS_SCALER, 0, NULL, 0, PlayerActionCheckBranchRunningStrafe },
 	{ FRAME_fastrite4, PlayerMoveFunc, 0, 200 * PHYS_SCALER, 0, NULL, 0, PlayerActionCheckBranchRunningStrafe },
-	{ FRAME_fastrite5, PlayerMoveFunc, 0, 200 * PHYS_SCALER, 0, PlayerActionFootstep, STEP_RUN, PlayerActionCheckBranchRunningStrafe },
+	{ FRAME_fastrite5, PlayerMoveFunc, 0, 200 * PHYS_SCALER, 0, PlayerActionFootstep, STEP_RUN2, PlayerActionCheckBranchRunningStrafe },
 	{ FRAME_fastrite6, PlayerMoveFunc, 0, 200 * PHYS_SCALER, 0, NULL, 0, PlayerActionCheckBranchRunningStrafe },
 	{ FRAME_fastrite7, PlayerMoveFunc, 0, 200 * PHYS_SCALER, 0, NULL, 0, PlayerActionCheckBranchRunningStrafe },
 	{ FRAME_fastrite8, PlayerMoveFunc, 0, 200 * PHYS_SCALER, 0, NULL, 0, PlayerActionCheckBranchRunningStrafe },
+	{ FRAME_fastrite1, PlayerMoveFunc, 0, 200 * PHYS_SCALER, 0, PlayerActionFootstep, STEP_RUN, PlayerActionCheckBranchRunningStrafe }, //mxd. Added to make it loop properly...
 };
 panimmove_t player_move_dash_right = PANIMMOVE(player_frames_dash_right, PlayerAnimLowerUpdate);
 