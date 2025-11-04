0cb8ea87593ad9823cde0c70504ffaa9718cd7e3
m-x-d <j.maxed@gmail.com>
2025-11-04 13:26:50 +0300

    cl_skeletons.h: add 'extern' keyword to function declarations.

--- src/client/cl_skeletons.h
+++ src/client/cl_skeletons.h
@@ -13,8 +13,8 @@
 extern CL_SkeletalJoint_t skeletal_joints[MAX_ARRAYED_SKELETAL_JOINTS];
 extern ArrayedListNode_t joint_nodes[MAX_ARRAYED_JOINT_NODES];
 
-void SK_UpdateSkeletons(void);
-void SK_ClearJoints(int joint_index);
-void SK_SetJointAngles(const playerinfo_t* playerinfo);
-void SK_ResetJointAngles(const playerinfo_t* playerinfo);
-void SK_CreateSkeleton(int structure, int root_index);
+extern void SK_UpdateSkeletons(void);
+extern void SK_ClearJoints(int joint_index);
+extern void SK_SetJointAngles(const playerinfo_t* playerinfo);
+extern void SK_ResetJointAngles(const playerinfo_t* playerinfo);
+extern void SK_CreateSkeleton(int structure, int root_index);
