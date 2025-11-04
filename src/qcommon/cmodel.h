b1c9bf08de6791b929d7f1b703771c4c6da57c9d
m-x-d <j.maxed@gmail.com>
2025-11-04 13:53:53 +0300

    cmodel.h: add 'extern' keyword to function declarations.

--- src/qcommon/cmodel.h
+++ src/qcommon/cmodel.h
@@ -19,39 +19,39 @@ extern int c_brush_traces;
 extern int numtexinfo;
 extern csurface_t map_surfaces[MAX_MAP_TEXINFO]; // 65536 bytes
 
-cmodel_t* CM_LoadMap(const char* name, qboolean clientload, uint* checksum);
-cmodel_t* CM_InlineModel(const char* name); // *1, *2, etc
+extern cmodel_t* CM_LoadMap(const char* name, qboolean clientload, uint* checksum);
+extern cmodel_t* CM_InlineModel(const char* name); // *1, *2, etc
 
-int CM_NumClusters(void);
-int CM_NumInlineModels(void);
-char* CM_EntityString(void);
+extern int CM_NumClusters(void);
+extern int CM_NumInlineModels(void);
+extern char* CM_EntityString(void);
 
-// Creates a clipping hull for an arbitrary box
-int CM_HeadnodeForBox(const vec3_t mins, const vec3_t maxs);
+// Creates a clipping hull for an arbitrary box.
+extern int CM_HeadnodeForBox(const vec3_t mins, const vec3_t maxs);
 
-// Returns an ORed contents mask
-int CM_PointContents(const vec3_t p, int headnode);
-int CM_TransformedPointContents(const vec3_t p, int headnode, const vec3_t origin, const vec3_t angles);
+// Returns an ORed contents mask.
+extern int CM_PointContents(const vec3_t p, int headnode);
+extern int CM_TransformedPointContents(const vec3_t p, int headnode, const vec3_t origin, const vec3_t angles);
 
-void CM_BoxTrace(const vec3_t start, const vec3_t end, const vec3_t mins, const vec3_t maxs, int headnode, uint brushmask, trace_t* return_trace);
-void CM_TransformedBoxTrace(const vec3_t start, const vec3_t end, const vec3_t mins, const vec3_t maxs, int headnode, uint brushmask, const vec3_t origin, const vec3_t angles, trace_t* return_trace);
+extern void CM_BoxTrace(const vec3_t start, const vec3_t end, const vec3_t mins, const vec3_t maxs, int headnode, uint brushmask, trace_t* return_trace);
+extern void CM_TransformedBoxTrace(const vec3_t start, const vec3_t end, const vec3_t mins, const vec3_t maxs, int headnode, uint brushmask, const vec3_t origin, const vec3_t angles, trace_t* return_trace);
 
-byte* CM_ClusterPVS(int cluster);
-byte* CM_ClusterPHS(int cluster);
+extern byte* CM_ClusterPVS(int cluster);
+extern byte* CM_ClusterPHS(int cluster);
 
-int CM_PointLeafnum(const vec3_t p);
+extern int CM_PointLeafnum(const vec3_t p);
 
 // Call with topnode set to the headnode, returns with topnode set to the first node that splits the box.
-int CM_BoxLeafnums(vec3_t mins, vec3_t maxs, int* list, int listsize, int* topnode);
+extern int CM_BoxLeafnums(vec3_t mins, vec3_t maxs, int* list, int listsize, int* topnode);
 
-int CM_LeafCluster(int leafnum);
-int CM_LeafArea(int leafnum);
+extern int CM_LeafCluster(int leafnum);
+extern int CM_LeafArea(int leafnum);
 
-void CM_SetAreaPortalState(int portalnum, qboolean open);
-qboolean CM_AreasConnected(int area1, int area2);
+extern void CM_SetAreaPortalState(int portalnum, qboolean open);
+extern qboolean CM_AreasConnected(int area1, int area2);
 
-int CM_WriteAreaBits(byte* buffer, int area);
-qboolean CM_HeadnodeVisible(int headnode, byte* visbits);
+extern int CM_WriteAreaBits(byte* buffer, int area);
+extern qboolean CM_HeadnodeVisible(int headnode, byte* visbits);
 
-void CM_WritePortalState(FILE* f);
-void CM_ReadPortalState(FILE* f);
+extern void CM_WritePortalState(FILE* f);
+extern void CM_ReadPortalState(FILE* f);
