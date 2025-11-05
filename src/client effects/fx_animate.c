e04cbec9d8b1d774b7ee1057f239531f33dfd74d
m-x-d <j.maxed@gmail.com>
2025-11-05 19:01:53 +0300

    FXAnimate(): update logic to use CEF_FRAME_LERP flag.

--- src/client effects/fx_animate.c
+++ src/client effects/fx_animate.c
@@ -17,23 +17,143 @@ typedef struct FXAnimModel
 	float alpha;
 	int num_frames;
 	int default_frame;
+	int update_time; //mxd. In ms.
 } FXAnimModel_t;
 
 // Animating chunks.
 static FXAnimModel_t fx_anim_models[NUM_FX_ANIM] =
 {
-	{ "models/objects/banner/wall/tris.fm",		NULL, 220.0f,	1.0f,	15,	1 },	 // FX_ANIM_BANNER
-	{ "models/objects/lights/candelabrum/tris.fm",	NULL, 100.0f,	1.0f,	7,		1 }, // FX_ANIM_CANDELABRUM
-	{ "models/objects/chandelier/chan2/tris.fm",	NULL, 100.0f,	1.0f,	7,		7 },	 // FX_ANIM_CHANDELIER2
-	{ NULL,										NULL, 100.0f,	0.5f,	1,		0 }, // FX_ANIM_FLAME - NEVER USED
-	{ NULL,										NULL, 100.0f,	0.5f,	1,		0 }, // FX_ANIM_FIRE - NEVER USED
-	{ "models/objects/banner/onpole/tris.fm",		NULL, 100.0f,	1.0f,	21,	0 },	 // FX_ANIM_BANNERONPOLE
-	{ "models/objects/flags/onpole/tris.fm",		NULL, 100.0f,	1.0f,	80,	0 }, // FX_ANIM_FLAGONPOLE
-	{ "models/objects/eggs/cocoon/tris.fm",		NULL, 100.0f,	1.0f,	20,	0 }, // FX_ANIM_COCOON
-	{ "models/objects/labs/container1/tris.fm",	NULL, 100.0f,	1.0f,	30,	0 }, // FX_ANIM_LABPARTSCONTAINER
-	{ "models/objects/labs/tray/tris.fm",			NULL, 100.0f,	1.0f,	15,	0 },	 // FX_ANIM_LABTRAY
-	{ "models/objects/labs/container2/tris.fm",	NULL, 100.0f,	1.0f,	80,	0 }, // FX_ANIM_EYEBALLJAR
-	{ "models/objects/torture/ogle/tris.fm",		NULL, 100.0f,	1.0f,	100,	0 }, // FX_ANIM_HANGING_OGLE
+	// FX_ANIM_BANNER
+	{
+		.model_name = "models/objects/banner/wall/tris.fm",
+		.model = NULL,
+		.radius = 220.0f,
+		.alpha = 1.0f,
+		.num_frames = 15,
+		.default_frame = 1,
+		.update_time = 500 //mxd
+	},
+
+	// FX_ANIM_CANDELABRUM
+	{
+		.model_name = "models/objects/lights/candelabrum/tris.fm",
+		.model = NULL,
+		.radius = 100.0f,
+		.alpha = 1.0f,
+		.num_frames = 7,
+		.default_frame = 1,
+		.update_time = 25 //mxd
+	},
+
+	// FX_ANIM_CHANDELIER2
+	{
+		.model_name = "models/objects/chandelier/chan2/tris.fm",
+		.model = NULL,
+		.radius = 100.0f,
+		.alpha = 1.0f,
+		.num_frames = 7,
+		.default_frame = 7,
+		.update_time = 100 //mxd
+	},
+
+	// FX_ANIM_FLAME - NEVER USED
+	{
+		.model_name = NULL,
+		.model = NULL,
+		.radius = 100.0f,
+		.alpha = 0.5f,
+		.num_frames = 1,
+		.default_frame = 0,
+		.update_time = 100 //mxd
+	},
+
+	// FX_ANIM_FIRE - NEVER USED
+	{
+		.model_name = NULL,
+		.model = NULL,
+		.radius = 100.0f,
+		.alpha = 0.5f,
+		.num_frames = 1,
+		.default_frame = 0,
+		.update_time = 100 //mxd
+	},
+
+	// FX_ANIM_BANNERONPOLE
+	{
+		.model_name = "models/objects/banner/onpole/tris.fm",
+		.model = NULL,
+		.radius = 100.0f,
+		.alpha = 1.0f,
+		.num_frames = 21,
+		.default_frame = 0,
+		.update_time = 100 //mxd
+	},
+
+	// FX_ANIM_FLAGONPOLE
+	{
+		.model_name = "models/objects/flags/onpole/tris.fm",
+		.model = NULL,
+		.radius = 100.0f,
+		.alpha = 1.0f,
+		.num_frames = 80,
+		.default_frame = 0,
+		.update_time = 100 //mxd
+	},
+
+	// FX_ANIM_COCOON
+	{
+		.model_name = "models/objects/eggs/cocoon/tris.fm",
+		.model = NULL,
+		.radius = 100.0f,
+		.alpha = 1.0f,
+		.num_frames = 20,
+		.default_frame = 0,
+		.update_time = 1000 //mxd
+	},
+
+	// FX_ANIM_LABPARTSCONTAINER
+	{
+		.model_name = "models/objects/labs/container1/tris.fm",
+		.model = NULL,
+		.radius = 100.0f,
+		.alpha = 1.0f,
+		.num_frames = 30,
+		.default_frame = 0,
+		.update_time = 100 //mxd //TODO: speed-up animation (to 25?), add random delays between heartbeats (via custom Update handler)?
+	},
+
+	// FX_ANIM_LABTRAY
+	{
+		.model_name = "models/objects/labs/tray/tris.fm",
+		.model = NULL,
+		.radius = 100.0f,
+		.alpha = 1.0f,
+		.num_frames = 15,
+		.default_frame = 0,
+		.update_time = 200 //mxd
+	},
+
+	// FX_ANIM_EYEBALLJAR
+	{
+		.model_name = "models/objects/labs/container2/tris.fm",
+		.model = NULL,
+		.radius = 100.0f,
+		.alpha = 1.0f,
+		.num_frames = 80,
+		.default_frame = 0,
+		.update_time = 100 //mxd
+	},
+
+	// FX_ANIM_HANGING_OGLE
+	{
+		.model_name = "models/objects/torture/ogle/tris.fm",
+		.model = NULL,
+		.radius = 100.0f,
+		.alpha = 1.0f,
+		.num_frames = 100,
+		.default_frame = 0,
+		.update_time = 100 //mxd
+	} 
 };
 
 void PreCacheFXAnimate(void)
@@ -43,29 +163,16 @@ void PreCacheFXAnimate(void)
 			fx_anim_models[i].model = fxi.RegisterModel(fx_anim_models[i].model_name);
 }
 
-static qboolean AnimateGo(struct client_entity_s* self, centity_t* owner)
+static qboolean AnimationUpdate(struct client_entity_s* self, centity_t* owner)
 {
+	//mxd. Update frame interpolation settings.
+	self->framelerp_time = fxi.cl->time;
+	self->r.oldframe = self->r.frame;
+
 	self->r.frame++;
 
 	if (self->r.frame >= self->NoOfAnimFrames)
 		self->r.frame = 0;
-
-	return true;
-}
-
-static qboolean AnimateRandomGo(struct client_entity_s* self, centity_t* owner)
-{
-	self->r.frame++;
-
-	if (self->r.frame == self->NoOfAnimFrames)
-	{
-		self->r.frame = 0;
-		self->updateTime = irand(500, 5000);
-	}
-	else
-	{
-		self->updateTime = 100;
-	}
 
 	return true;
 }
@@ -75,27 +182,29 @@ void FXAnimate(centity_t* owner, const int type, const int flags, vec3_t origin)
 	byte anim;
 	byte scale;
 	byte skinnum;
+	vec3_t angles;
+	fxi.GetEffect(owner, flags, clientEffectSpawners[FX_ANIMATE].formatString, &anim, &scale, &skinnum, angles);
 
-	client_entity_t* self = ClientEntity_new(type, flags, origin, NULL, 100);
+	const byte anim_type = (anim & 0x7f);
+	FXAnimModel_t* anim_settings = &fx_anim_models[anim_type];
 
-	fxi.GetEffect(owner, flags, clientEffectSpawners[FX_ANIMATE].formatString, &anim, &scale, &skinnum, self->r.angles);
-	VectorDegreesToRadians(self->r.angles, self->r.angles);
-	const byte atype = anim & 0x7f;
+	client_entity_t* self = ClientEntity_new(type, flags | CEF_FRAME_LERP, origin, NULL, anim_settings->update_time); //mxd. +CEF_FRAME_LERP; 100 -> update_time.
+	VectorDegreesToRadians(angles, self->r.angles);
 
-	self->r.model = &fx_anim_models[atype].model;
+	self->r.model = &anim_settings->model;
 	self->r.scale = (float)scale * 0.02f;
 	self->r.skinnum = skinnum;
 
-	self->alpha = fx_anim_models[atype].alpha;
-	self->radius = fx_anim_models[atype].radius;
-	self->NoOfAnimFrames = fx_anim_models[atype].num_frames;
-	self->r.frame = fx_anim_models[atype].default_frame;
+	self->alpha = anim_settings->alpha;
+	self->radius = anim_settings->radius;
+	self->NoOfAnimFrames = anim_settings->num_frames;
+	self->r.frame = anim_settings->default_frame;
+	self->r.oldframe = self->r.frame; //mxd
 
 	if (anim & 0x80)
 	{
-		// Animate (special animate for cocoon).
-		self->Update = ((atype == FX_ANIM_COCOON) ? AnimateRandomGo : AnimateGo);
-		self->nextThinkTime = fxi.cl->time + irand(40, 1600); // So they don't all start on frame 0 at the same time.
+		self->Update = AnimationUpdate;
+		self->nextThinkTime = fxi.cl->time + irand(40, 500); // So they don't all start on frame 0 at the same time.
 	}
 	else
 	{
