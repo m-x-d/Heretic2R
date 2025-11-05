46d314216218729ace15b4f4579a7ddc40cc4cd7
m-x-d <j.maxed@gmail.com>
2025-11-05 13:29:37 +0300

    Cosmetic tweaks in Message.c.

--- src/qcommon/Message.c
+++ src/qcommon/Message.c
@@ -6,57 +6,56 @@
 
 #include "Message.h"
 
-void MSG_Queue(MsgQueue_t* this_ptr, void* msg) //mxd. Named 'QueueMessage' in original logic.
+void MSG_Queue(MsgQueue_t* self, void* msg) //mxd. Named 'QueueMessage' in original logic.
 {
-	GenericUnion4_t temp;
-
-	temp.t_void_p = msg;
-	SLList_Push(&this_ptr->msgs, temp);
+	const GenericUnion4_t temp = { .t_void_p = msg };
+	SLList_Push(&self->msgs, temp);
 }
 
-size_t MSG_SetParms(SinglyLinkedList_t* this_ptr, const char* format, va_list marker) //mxd. Named 'SetParms' in original logic.
+size_t MSG_SetParms(SinglyLinkedList_t* parms, const char* format, va_list marker) //mxd. Named 'SetParms' in original logic.
 {
 	qboolean append = false;
-	GenericUnion4_t parm;
 	int count = 0;
 	size_t bytesParsed = 0;
 
-	SLList_Front(this_ptr);
+	SLList_Front(parms);
 
 	while (format[count] != 0)
 	{
+		GenericUnion4_t parm;
+
 		switch (format[count])
 		{
 			case 'b':
 				parm.t_byte = va_arg(marker, byte);
-				bytesParsed += sizeof(byte);
+				bytesParsed += sizeof(parm.t_byte);
 				break;
 
 			case 's':
 				parm.t_short = va_arg(marker, short);
-				bytesParsed += sizeof(short);
+				bytesParsed += sizeof(parm.t_short);
 				break;
 
 			case 'i':
 				parm.t_int = va_arg(marker, int);
-				bytesParsed += sizeof(int);
+				bytesParsed += sizeof(parm.t_int);
 				break;
 
 			case 'f':
 				parm.t_float = (float)va_arg(marker, double);
-				bytesParsed += sizeof(double);
+				bytesParsed += sizeof(parm.t_float); //mxd. sizeof(double) in original logic.
 				break;
 
 			case 'e': // A pointer is a pointer is a pointer.
 			case 'v': // This better be not be a local variable or this will be bunk when the message is received and parsed.
 			case 'g': // g for generic.
 				parm.t_void_p = va_arg(marker, void*);
-				bytesParsed += sizeof(void*);
+				bytesParsed += sizeof(parm.t_void_p);
 				break;
 
 			case 'c':
 				parm.t_RGBA = va_arg(marker, paletteRGBA_t);
-				bytesParsed += sizeof(paletteRGBA_t);
+				bytesParsed += sizeof(parm.t_RGBA);
 				break;
 
 			default:
@@ -66,17 +65,17 @@ size_t MSG_SetParms(SinglyLinkedList_t* this_ptr, const char* format, va_list ma
 
 		if (append)
 		{
-			SLList_InsertAfter(this_ptr, parm);
-			SLList_Increment(this_ptr);
+			SLList_InsertAfter(parms, parm);
+			SLList_Increment(parms);
 		}
 		else
 		{
-			SLList_ReplaceCurrent(this_ptr, parm);
+			SLList_ReplaceCurrent(parms, parm);
 
-			if (SLList_AtLast(this_ptr))
+			if (SLList_AtLast(parms))
 				append = true;
 			else
-				SLList_Increment(this_ptr);
+				SLList_Increment(parms);
 		}
 
 		count++;
@@ -85,7 +84,7 @@ size_t MSG_SetParms(SinglyLinkedList_t* this_ptr, const char* format, va_list ma
 	return bytesParsed;
 }
 
-int MSG_GetParms(SinglyLinkedList_t* this_ptr, const char* format, va_list marker) //mxd. Named 'GetParms' in original logic.
+int MSG_GetParms(SinglyLinkedList_t* parms, const char* format, va_list marker) //mxd. Named 'GetParms' in original logic.
 {
 	int count = 0;
 
@@ -94,11 +93,11 @@ int MSG_GetParms(SinglyLinkedList_t* this_ptr, const char* format, va_list marke
 	if (format == NULL)
 		Sys_Error("MSG_GetParms: null format string");
 
-	SLList_Front(this_ptr);
+	SLList_Front(parms);
 
-	assert(!SLList_AtEnd(this_ptr));
+	assert(!SLList_AtEnd(parms));
 
-	if (SLList_AtEnd(this_ptr))
+	if (SLList_AtEnd(parms))
 		Sys_Error("MSG_GetParms: empty parameter list");
 
 	while (format[count] != 0)
@@ -108,44 +107,44 @@ int MSG_GetParms(SinglyLinkedList_t* this_ptr, const char* format, va_list marke
 			case 'b':
 			{
 				byte* b = va_arg(marker, byte*);
-				*b = SLList_PostIncrement(this_ptr).t_byte;
+				*b = SLList_PostIncrement(parms).t_byte;
 			} break;
 
 			case 's':
 			{
 				short* s = va_arg(marker, short*);
-				*s = SLList_PostIncrement(this_ptr).t_short;
+				*s = SLList_PostIncrement(parms).t_short;
 			} break;
 
 			case 'i':
 			{
 				int* i = va_arg(marker, int*);
-				*i = SLList_PostIncrement(this_ptr).t_int;
+				*i = SLList_PostIncrement(parms).t_int;
 			} break;
 
 			case 'f':
 			{
 				float* f = va_arg(marker, float*);
-				*f = SLList_PostIncrement(this_ptr).t_float;
+				*f = SLList_PostIncrement(parms).t_float;
 			} break;
 
 			case 'v':
 			{
 				float* v = va_arg(marker, float*);
-				v = SLList_PostIncrement(this_ptr).t_float_p;
+				v = SLList_PostIncrement(parms).t_float_p;
 			} break;
 
 			case 'e': // A pointer is a pointer is a pointer.
 			case 'g':
 			{
 				void** g = va_arg(marker, void**);
-				*g = SLList_PostIncrement(this_ptr).t_void_p;
+				*g = SLList_PostIncrement(parms).t_void_p;
 			} break;
 
 			case 'c':
 			{
 				paletteRGBA_t* c = va_arg(marker, paletteRGBA_t*);
-				*c = SLList_PostIncrement(this_ptr).t_RGBA;
+				*c = SLList_PostIncrement(parms).t_RGBA;
 			} break;
 
 			default:
