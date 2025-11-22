m-x-d/Heretic2R

commit 3f5e0c3cca51e66952911afbea5ba77d82286427

PM_TryStepUp(): don't step up if we can move forwards without it (fixes successfully stepping up (and breaking ASEQ_SLIDE_FORWARD/ASEQ_SLIDE_BACKWARD animations as a result) while sliding down a slope) (H2 BUG).

src/qcommon/pmove.c
@@ -120,12 +120,20 @@ static qboolean PM_TryStepUp(const float step_height, const float frametime, tra
 	if (vhlen(vel, vec3_origin) < 0.01f)
 		return false;
 
-	// Check headroom.
+	//mxd. Check horizontal move from initial position.
 	vec3_t start = VEC3_INIT(pml.origin);
-	vec3_t end = VEC3_INIT(pml.origin);
-	end[2] += step_height + max(0.0f, vel[2]);
+	vec3_t end = VEC3_INITA(pml.origin, vel[0], vel[1], 0.0f);
 
 	trace_t trace;
+	pm->trace(start, pm->mins, pm->maxs, end, &trace);
+
+	//mxd. If we can move forwards, we don't need to step up (fixes successfully stepping up (and breaking ASEQ_SLIDE_FORWARD/ASEQ_SLIDE_BACKWARD animations as a result) while sliding down a slope...).
+	if (trace.fraction == 1.0f)
+		return false;
+
+	// Check headroom.
+	end[2] = pml.origin[2] + step_height + max(0.0f, vel[2]);
+
 	pm->trace(start, pm->mins, pm->maxs, end, &trace);
 
 	if (trace.startsolid || trace.allsolid || trace.endpos[2] < pml.origin[2] + 1.0f)

commit bff7b1a1329791fdaefac84db65c5b97f4c27853

Fix logic error in PM_TryStepUp() (introduced in 3f5e0c3).

src/qcommon/pmove.c
@@ -132,7 +132,8 @@ static qboolean PM_TryStepUp(const float step_height, const float frametime, tra
 		return false;
 
 	// Check headroom.
-	end[2] = pml.origin[2] + step_height + max(0.0f, vel[2]);
+	VectorCopy(pml.origin, end);
+	end[2] += step_height + max(0.0f, vel[2]);
 
 	pm->trace(start, pm->mins, pm->maxs, end, &trace);
 