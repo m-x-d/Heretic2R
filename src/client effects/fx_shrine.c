m-x-d/Heretic2R

commit c9abf32724e01e4c8251640de746b2868c28dea5

ClientEffects: update timing logic to properly handle paused/freezeworld state (add UpdateEffectsTime(), replace fxi.cl->time with new fx_time global var). Remove fx_FreezeWorld and associated logic (PostRenderUpdate() is not called when cl_freezeworld is set). Replace ParticleUpdateTime with fx_time (ParticleUpdateTime was always == fx_time).

src/client effects/fx_shrine.c
@@ -450,7 +450,7 @@ static void CreateLightning(struct client_entity_s* self, const centity_t* owner
 		fxi.Activate_Screen_Flash((int)0x80ffd0c0);
 
 		// Make our screen shake a bit.
-		fxi.Activate_Screen_Shake(4.0f, 800.0f, (float)fxi.cl->time, SHAKE_ALL_DIR);
+		fxi.Activate_Screen_Shake(4.0f, 800.0f, (float)fx_time, SHAKE_ALL_DIR);
 	}
 }
 

src/client effects/Ambient Effects.c
@@ -70,22 +70,22 @@ qboolean CreateFlyParticles(client_entity_t *this, centity_t *owner)
 
 /*	fly_stoptime = this->startTime + this->duration;
 
-	if (fly_stoptime < fxi.cl->time)
+	if (fly_stoptime < fx_time)
 	{
-		starttime = fxi.cl->time;
-		fly_stoptime = fxi.cl->time + 60000;
+		starttime = fx_time;
+		fly_stoptime = fx_time + 60000;
 	}
 	else
 	{
 		starttime = fly_stoptime - 60000;
 	}
 
-	n = fxi.cl->time - starttime;
+	n = fx_time - starttime;
 	if (n < 20000)
 		count = n * 162 / 20000.0;
 	else
 	{
-		n = fly_stoptime - fxi.cl->time;
+		n = fly_stoptime - fx_time;
 		if (n < 20000)
 			count = n * 162 / 20000.0;
 		else
@@ -103,7 +103,7 @@ qboolean CreateFlyParticles(client_entity_t *this, centity_t *owner)
 			avelocities[0][i] = flrand(0.0, 2.55);
 	}
 
-	ltime = (float)fxi.cl->time / 1000.0;
+	ltime = (float)fx_time / 1000.0;
 	for (i=0 ; i<count ; i+=2)
 	{
 		p = ResMngr_AllocateResource(&ParticleMngr, sizeof(*this->p_root));
@@ -124,7 +124,7 @@ qboolean CreateFlyParticles(client_entity_t *this, centity_t *owner)
 		forward[1] = cp*sy;
 		forward[2] = -sp;
 
-		p->startTime = fxi.cl->time;
+		p->startTime = fx_time;
 		p->duration = 50;
 
 		dist = sin(ltime + i)*64;

src/client effects/Client Effects.h
@@ -27,8 +27,6 @@ extern qboolean ref_soft;
 extern int numprocessedparticles;
 extern int numrenderedparticles;
 
-extern qboolean fx_FreezeWorld;
-
 typedef struct ClientEffect_s
 {
 	void (*SpawnCFX)(centity_t* owner, int type, int flags, vec3_t origin);

src/client effects/Client Entities.c
@@ -16,6 +16,7 @@
 #include "Vector.h"
 #include "Utilities.h"
 
+int fx_time; //mxd
 client_entity_t* clientEnts = NULL;
 
 static ResourceManager_t entity_manager;
@@ -93,8 +94,8 @@ client_entity_t* ClientEntity_new(const int type, const int flags, const vec3_t 
 
 	new_ent->updateTime = next_think_time;
 	new_ent->framelerp_scale = 1.0f / (float)next_think_time; //mxd. Setup frame interpolation.
-	new_ent->nextThinkTime = fxi.cl->time + next_think_time;
-	new_ent->startTime = fxi.cl->time;
+	new_ent->nextThinkTime = fx_time + next_think_time;
+	new_ent->startTime = fx_time;
 	new_ent->Update = RemoveSelfAI;
 
 	return new_ent;
@@ -274,7 +275,7 @@ int AddEffectsToView(client_entity_t** root, centity_t* owner)
 			{
 				if (current->flags & CEF_FRAME_LERP) //mxd
 				{
-					const float backlerp = 1.0f - (float)(fxi.cl->time - current->framelerp_time) * current->framelerp_scale;
+					const float backlerp = 1.0f - (float)(fx_time - current->framelerp_time) * current->framelerp_scale;
 					current->r.backlerp = max(0.0f, backlerp); // Otherwise it will advance towards negative infinity when the game is paused.
 				}
 
@@ -318,10 +319,6 @@ int UpdateEffects(client_entity_t** root, centity_t* owner)
 	assert(root);
 	assert(*root);
 
-	// If the world is frozen then add the particles, just don't update the world time. Always update the particle timer.
-	if (!fx_FreezeWorld)
-		ParticleUpdateTime = fxi.cl->time;
-
 	for (prev = root, current = *root; current != NULL; current = current->next)
 	{
 		num_fx++;
@@ -329,7 +326,7 @@ int UpdateEffects(client_entity_t** root, centity_t* owner)
 		if (current->msgHandler != NULL)
 			CE_ProcessMessages(current);
 
-		if (current->Update != NULL && current->nextThinkTime <= fxi.cl->time)
+		if (current->Update != NULL && current->nextThinkTime <= fx_time)
 		{
 			// Only think when not culled and not think culled.
 			const qboolean is_culled = (current->flags & CEF_VIEWSTATUSCHANGED) && (current->flags & CEF_CULLED); //mxd
@@ -343,7 +340,7 @@ int UpdateEffects(client_entity_t** root, centity_t* owner)
 			}
 
 			//mxd. Original logic asserts here when current->updateTime < 17. Removed to allow updating client_ents every renderframe.
-			current->nextThinkTime = fxi.cl->time + current->updateTime;
+			current->nextThinkTime = fx_time + current->updateTime;
 		}
 
 		entity_t* r = &current->r;

src/client effects/Client Entities.h
@@ -170,6 +170,7 @@ typedef struct client_entity_s
 	void* extra; // Extra whatever.
 } client_entity_t;
 
+extern int fx_time; //mxd
 extern client_entity_t* clientEnts;
 extern CE_ClassStatics_t ce_class_statics[NUM_CLASSIDS];
 

src/client effects/Generic Character Effects.c
@@ -62,7 +62,7 @@ void GenericGibTrail(centity_t* owner, const int type, const int flags, vec3_t o
 
 static qboolean PebbleUpdate(struct client_entity_s* self, centity_t* owner)
 {
-	const int cur_time = fxi.cl->time;
+	const int cur_time = fx_time;
 	const float d_time = (float)(cur_time - self->lastThinkTime) / 1000.0f;
 
 	self->acceleration[2] -= 75.0f;
@@ -137,7 +137,7 @@ void FXOgleHitPuff(centity_t* owner, const int type, const int flags, vec3_t ori
 		rock->d_scale = 0.0f;
 		rock->d_alpha = 0.0f;
 		rock->color = color_white; //mxd
-		rock->LifeTime = fxi.cl->time + 5000;
+		rock->LifeTime = fx_time + 5000;
 
 		AddEffect(NULL, rock); // Add the effect as independent world effect.
 	}
@@ -249,11 +249,11 @@ static void UpdateWaterParticles(const client_entity_t* self)
 			continue;
 		}
 
-		float add_val = SINEAMT / 128.0f * turbsin[(int)(((float)fxi.cl->time * 0.001f + (self->origin[0] * 2.3f + p->origin[1]) * 0.0015f) * SINESCALE) & 255];
-		add_val +=		SINEAMT / 256.0f * turbsin[(int)(((float)fxi.cl->time * 0.002f + (self->origin[1] * 2.3f + p->origin[0]) * 0.0015f) * SINESCALE) & 255];
+		float add_val = SINEAMT / 128.0f * turbsin[(int)(((float)fx_time * 0.001f + (self->origin[0] * 2.3f + p->origin[1]) * 0.0015f) * SINESCALE) & 255];
+		add_val +=		SINEAMT / 256.0f * turbsin[(int)(((float)fx_time * 0.002f + (self->origin[1] * 2.3f + p->origin[0]) * 0.0015f) * SINESCALE) & 255];
 
 		p->origin[2] += add_val;
-		p->duration = fxi.cl->time + 10000000;
+		p->duration = fx_time + 10000000;
 	}
 }
 

src/client effects/LightStyles.c
@@ -42,7 +42,7 @@ void CL_ClearLightStyles(void)
 void CL_RunLightStyles(void)
 {
 	float value; //mxd
-	const int ofs = fxi.cl->time / 100;
+	const int ofs = fx_time / 100;
 
 	if (ofs == lastofs)
 		return;

src/client effects/Main.c
@@ -38,8 +38,6 @@ qboolean ref_soft; //mxd. int in original logic.
 int numprocessedparticles;
 int numrenderedparticles;
 
-qboolean fx_FreezeWorld = false;
-
 static int num_owned_inview;
 
 static void Clear(void)
@@ -113,9 +111,6 @@ static void AddEffects(const qboolean freeze)
 {
 	int	num_free_inview = 0;
 
-	// If the world is frozen then the client effects, particularly the particles shouldn't update.
-	fx_FreezeWorld = freeze;
-
 	if (clientEnts != NULL)
 		num_free_inview += AddEffectsToView(&clientEnts, NULL);
 
@@ -131,6 +126,27 @@ static void AddEffects(const qboolean freeze)
 		Com_DPrintf("Active CE : free %d, owned %d. Particles : processed %d, rendered %d\n", num_free_inview, num_owned_inview, numprocessedparticles, numrenderedparticles);
 }
 
+//mxd. Original logic uses fxi.cl->time for effects/particles timing.
+static void UpdateEffectsTime(void)
+{
+	static int prev_leveltime;
+	static int rframetime;
+
+	// Use leveltime as a base, because it's not updated when the game is paused.
+	const int leveltime = (int)(*fxi.leveltime * 1000.0f);
+
+	if (leveltime != prev_leveltime)
+	{
+		prev_leveltime = leveltime;
+		rframetime = 0;
+	}
+
+	// leveltime updates at 100 ms. rate, so we need in-between interpolation...
+	rframetime += (int)(fxi.cls->rframetime * 1000.0f); 
+
+	fx_time = leveltime + rframetime;
+}
+
 static void PostRenderUpdate(void)
 {
 	int	num_free_active = 0;
@@ -138,6 +154,9 @@ static void PostRenderUpdate(void)
 
 	numprocessedparticles = 0;
 	numrenderedparticles = 0;
+
+	//mxd. Update timing vars (was done in UpdateEffects() in original logic).
+	UpdateEffectsTime();
 
 	if (clientEnts != NULL)
 		num_free_active += UpdateEffects(&clientEnts, NULL);
@@ -423,10 +442,10 @@ static void AddServerEntities(const frame_t* frame)
 	num_owned_inview = 0;
 
 	// Mace balls rotate at a fixed rate.
-	const float macerotate = anglemod((float)fxi.cl->time / 700.0f);
+	const float macerotate = anglemod((float)fx_time / 700.0f);
 
 	// Brush models can auto animate their frames.
-	const int autoanim = fxi.cl->time / 500;
+	const int autoanim = fx_time / 500;
 
 	memset(sv_ents, 0, sizeof(sv_ents));
 
@@ -474,7 +493,7 @@ static void AddServerEntities(const frame_t* frame)
 		if (effects & EF_ANIM_ALL)
 			ent->frame = autoanim;
 		else if (effects & EF_ANIM_ALLFAST)
-			ent->frame = fxi.cl->time / 100;
+			ent->frame = fx_time / 100;
 		else
 			ent->frame = s1->frame;
 

commit 5619b01987fb2c5107493aa0381cd0a281e1b8fd

Fix logic error in CreateLightning() (introduced in c9abf32).

src/client effects/fx_shrine.c
@@ -450,7 +450,7 @@ static void CreateLightning(struct client_entity_s* self, const centity_t* owner
 		fxi.Activate_Screen_Flash((int)0x80ffd0c0);
 
 		// Make our screen shake a bit.
-		fxi.Activate_Screen_Shake(4.0f, 800.0f, (float)fx_time, SHAKE_ALL_DIR);
+		fxi.Activate_Screen_Shake(4.0f, 800.0f, (float)fxi.cl->time, SHAKE_ALL_DIR); // 'current_time' MUST be cl.time, because that's what used by Perform_Screen_Shake() to calculate effect intensity/timing... --mxd.
 	}
 }
 