0e1e4497aeae2545d9160c08444d9f9f05d7b141
m-x-d <j.maxed@gmail.com>
2025-12-15 15:29:45 +0300

    Update SDL3 to 3.2.28.

--- include/SDL3/SDL_revision.h
+++ include/SDL3/SDL_revision.h
@@ -48,9 +48,9 @@
  */
 #define SDL_REVISION "Some arbitrary string decided at SDL build time"
 #elif defined(SDL_VENDOR_INFO)
-#define SDL_REVISION "release-3.2.26-0-gbadbf8da4 (" SDL_VENDOR_INFO ")"
+#define SDL_REVISION "release-3.2.28-0-g7f3ae3d57 (" SDL_VENDOR_INFO ")"
 #else
-#define SDL_REVISION "release-3.2.26-0-gbadbf8da4"
+#define SDL_REVISION "release-3.2.28-0-g7f3ae3d57"
 #endif
 
 #endif /* SDL_revision_h_ */
