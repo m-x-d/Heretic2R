40f30a61d993123f0655b3bcbf91e3e42bd46eb1
m-x-d <j.maxed@gmail.com>
2025-12-18 14:08:31 +0300

    fx_WaterSplash.h: remove FXWaterSplash declaration (now declared in Client Effects.h).

--- src/client effects/fx_WaterSplash.h
+++ src/client effects/fx_WaterSplash.h
@@ -9,4 +9,3 @@
 #include "Client Entities.h"
 
 extern void DoWaterSplash(client_entity_t* effect, paletteRGBA_t color, int count);
-extern void FXWaterSplash(centity_t* owner, int type, int flags, vec3_t origin);
