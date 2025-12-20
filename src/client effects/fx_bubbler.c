3cb463570e61f1f2cdd1d6bdc6fce8b0e6510a53
m-x-d <j.maxed@gmail.com>
2025-12-20 05:55:46 +0300

    BubblerParticleSpawner(): don't randomize origin[2] (because we already pre-calculated expire time), add subtle fade-in effect. FXBubbler(): use correct type for bubbles_per_min. FXBubble(): add subtle fade-in effect.

--- src/client effects/fx_bubbler.c
+++ src/client effects/fx_bubbler.c
@@ -57,7 +57,7 @@ static qboolean BubblerParticleSpawner(client_entity_t* spawner, centity_t* owne
 {
 	spawner->updateTime = irand(spawner->SpawnDelay / 2, spawner->SpawnDelay * 2);
 
-	const vec3_t origin = VEC3_INITA(spawner->r.origin, flrand(-5.0f, 5.0f), flrand(-5.0f, 5.0f), flrand(0.0f, 5.0f)); //mxd. XY: flrand(0.0f, 5.0f) in original logic.
+	const vec3_t origin = VEC3_INITA(spawner->r.origin, flrand(-5.0f, 5.0f), flrand(-5.0f, 5.0f), 0.0f); //mxd. XYZ: flrand(0.0f, 5.0f) in original logic.
 	client_entity_t* bubble = ClientEntity_new(-1, 0, origin, NULL, (int)spawner->SpawnData);
 
 	const int r_flags = (R_DETAIL > DETAIL_HIGH ? RF_LM_COLOR : 0); //mxd
@@ -65,6 +65,8 @@ static qboolean BubblerParticleSpawner(client_entity_t* spawner, centity_t* owne
 	bubble->r.model = &bubble_model;
 	bubble->r.scale = flrand(0.1f, 0.2f);
 	bubble->r.flags = (RF_TRANSLUCENT | r_flags);
+	bubble->alpha = 0.01f; //mxd. Added subtle fade-in effect.
+	bubble->d_alpha = 1.5f; //mxd
 	VectorCopy(spawner->acceleration, bubble->acceleration);
 	bubble->Update = BubbleThink;
 
@@ -81,7 +83,7 @@ void FXBubbler(centity_t* owner, const int type, int flags, vec3_t origin)
 	const float down = GetSolidDist(dest, BUBBLE_RADIUS * 0.5f, -BUBBLE_CHECK_DISTANCE, true);
 	const float time = GetTimeToReachDistance(0.0f, BUBBLE_ACCELERATION, fabsf(up + down));
 
-	char bubbles_per_min;
+	byte bubbles_per_min; //mxd. char in original logic (but sent as byte).
 	fxi.GetEffect(owner, flags, clientEffectSpawners[FX_BUBBLER].formatString, &bubbles_per_min);
 
 	flags |= (CEF_NO_DRAW | CEF_NOMOVE | CEF_CULLED | CEF_VIEWSTATUSCHANGED | CEF_CHECK_OWNER);
@@ -107,10 +109,12 @@ void FXBubble(centity_t* owner, int type, const int flags, vec3_t origin)
 	client_entity_t* bubble = ClientEntity_new(FX_BUBBLE, flags, origin, NULL, time);
 
 	const int r_flags = (R_DETAIL > DETAIL_HIGH ? RF_LM_COLOR : 0); //mxd
-	bubble->radius = BUBBLE_RADIUS * 2;
+	bubble->radius = BUBBLE_RADIUS * 2.0f;
 	bubble->r.model = &bubble_model;
 	bubble->r.scale = flrand(0.025f, 0.1f);
-	bubble->r.flags = RF_TRANSLUCENT | r_flags;
+	bubble->r.flags = (RF_TRANSLUCENT | r_flags);
+	bubble->alpha = 0.01f; //mxd. Added subtle fade-in effect.
+	bubble->d_alpha = 1.5f; //mxd
 	bubble->acceleration[2] = BUBBLE_ACCELERATION;
 	bubble->Update = BubbleThink;
 
