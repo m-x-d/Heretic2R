8b0e81717230775ad60703c5222f48f7c8fdeb57
m-x-d <j.maxed@gmail.com>
2026-01-14 16:57:57 +0300

    Rename DrawCrosshair() to CrosshairUpdate(). CrosshairUpdate(): rename 'crosshair_ent' arg to 'self', change type to client_entity_t*.

--- src/client effects/fx_crosshair.c
+++ src/client effects/fx_crosshair.c
@@ -13,24 +13,24 @@ void PreCacheCrosshair(void)
 	crosshair_model = fxi.RegisterModel("sprites/fx/crosshair.sp2");
 }
 
-static qboolean DrawCrosshair(struct client_entity_s* crosshair_ent, centity_t* owner)
+static qboolean CrosshairUpdate(client_entity_t* self, centity_t* owner) //mxd. Named 'FXDrawCrosshair' in original logic.
 {
-	crosshair_ent->flags |= CEF_CULLED | CEF_DISAPPEARED;
+	self->flags |= (CEF_CULLED | CEF_DISAPPEARED);
 
 	// Get new destination.
 	byte type;
-	if (fxi.Get_Crosshair(crosshair_ent->r.origin, &type))
+	if (fxi.Get_Crosshair(self->r.origin, &type))
 	{
-		crosshair_ent->r.frame = (type > 2 ? 0 : type);
-		crosshair_ent->r.flags = (RF_TRANSLUCENT | RF_TRANS_ADD | RF_TRANS_ADD_ALPHA | RF_NODEPTHTEST);
+		self->r.frame = (type > 2 ? 0 : type);
+		self->r.flags = (RF_TRANSLUCENT | RF_TRANS_ADD | RF_TRANS_ADD_ALPHA | RF_NODEPTHTEST);
 
 		float alpha = 0.5f + fabsf(sinf((float)fx_time / 800.0f)) * 0.5f;
 		alpha = Clamp(alpha, 0.0f, 1.0f) * 0.5f;
 
-		crosshair_ent->alpha = alpha + 0.25f;
-		crosshair_ent->r.scale = alpha;
+		self->alpha = alpha + 0.25f;
+		self->r.scale = alpha;
 
-		crosshair_ent->flags &= ~(CEF_CULLED | CEF_DISAPPEARED | CEF_NO_DRAW);
+		self->flags &= ~(CEF_CULLED | CEF_DISAPPEARED | CEF_NO_DRAW);
 	}
 
 	return true;
@@ -42,7 +42,7 @@ void FXCrosshair(centity_t* owner, const int type, int flags, vec3_t origin)
 	client_entity_t* xh = ClientEntity_new(type, flags, origin, NULL, 0); //mxd. next_think_time:20 in original logic. Update on every renderframe instead.
 
 	xh->r.model = &crosshair_model;
-	xh->Update = DrawCrosshair;
+	xh->Update = CrosshairUpdate;
 
 	AddEffect(owner, xh);
 }
