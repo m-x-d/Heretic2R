4cb65d03d380a1dd645168d66f03a0979064c569
m-x-d <j.maxed@gmail.com>
2026-01-15 17:17:04 +0300

    Rename LightningShieldSparkThink() to LightningShieldSparkAddToView(). Cosmetic tweaks in LightningShieldSparkAddToView(). Rename LightningShieldTerminate() to LightningShieldSparkUpdate(). Cosmetic tweaks in LightningShieldSparkUpdate(). Cosmetic tweaks in FXLightningShield().

--- src/client effects/fx_Shield.c
+++ src/client effects/fx_Shield.c
@@ -22,26 +22,26 @@ void PreCacheShield(void)
 	shield_model = fxi.RegisterModel("sprites/spells/spark_blue.sp2");
 }
 
-static qboolean LightningShieldSparkThink(struct client_entity_s* shield, centity_t* owner)
+static qboolean LightningShieldSparkAddToView(client_entity_t* self, centity_t* owner) //mxd. Named 'FXShieldSparkThink' in original logic.
 {
 	// Update the angle of the spark.
-	VectorMA(shield->direction, (float)(fx_time - shield->lastThinkTime) / 1000.0f, shield->velocity2, shield->direction);
+	VectorMA(self->direction, (float)(fx_time - self->lastThinkTime) / 1000.0f, self->velocity2, self->direction);
 
 	// Update the position of the spark.
-	vec3_t direction;
-	AngleVectors(shield->direction, direction, NULL, NULL);
-	VectorMA(owner->origin, shield->radius, direction, shield->r.origin);
+	vec3_t forward;
+	AngleVectors(self->direction, forward, NULL, NULL);
+	VectorMA(owner->origin, self->radius, forward, self->r.origin);
 
-	shield->lastThinkTime = fx_time;
+	self->lastThinkTime = fx_time;
 
 	// Leave a trail sometimes.
-	if (shield->SpawnDelay < fx_time)
+	if (self->SpawnDelay < fx_time)
 	{
 		int particle_type = PART_16x16_SPARK_B;
-		paletteRGBA_t color = { .c = 0xffffffff };
+		paletteRGBA_t color = { .c = color_white.c };
 
 		// If we are in software, make the blue bits single point particles half the time.
-		if (ref_soft && ++shield->SpawnInfo & 1)
+		if (ref_soft && ++self->SpawnInfo & 1)
 		{
 			particle_type |= PFL_SOFT_MASK;
 			color.c = 0xffff0000;
@@ -49,27 +49,27 @@ static qboolean LightningShieldSparkThink(struct client_entity_s* shield, centit
 
 		client_particle_t* spark = ClientParticle_new(particle_type, color, 500);
 
-		VectorCopy(shield->r.origin, spark->origin);
+		VectorCopy(self->r.origin, spark->origin);
 		spark->scale = 6.0f;
 		spark->d_scale = -10.0f;
 		spark->acceleration[2] = 0.0f; // Don't fall due to gravity...
 
-		AddParticleToList(shield, spark);
+		AddParticleToList(self, spark);
 
 		// Do it again in 1/10 sec.
-		shield->SpawnDelay = fx_time + SHIELD_TRAIL_DELAY;
+		self->SpawnDelay = fx_time + SHIELD_TRAIL_DELAY;
 	}
 
 	return true;
 }
 
-static qboolean LightningShieldTerminate(struct client_entity_s* shield, centity_t* owner)
+static qboolean LightningShieldSparkUpdate(client_entity_t* self, centity_t* owner) //mxd. Named 'FXShieldTerminate' in original logic.
 {
 	// Don't instantly delete yourself. Don't accept any more updates and die out within a second.
-	shield->d_alpha = -1.2f; // Fade out.
-	shield->SpawnDelay = fx_time + 2000; // No more particles.
-	shield->updateTime = 1000; // Die in one second.
-	shield->Update = RemoveSelfAI;
+	self->d_alpha = -1.2f; // Fade out.
+	self->SpawnDelay = fx_time + 2000; // No more particles.
+	self->updateTime = 1000; // Die in one second.
+	self->Update = RemoveSelfAI;
 
 	return true;
 }
@@ -81,36 +81,36 @@ void FXLightningShield(centity_t* owner, const int type, const int flags, vec3_t
 	// Add spinning electrical sparks.
 	for (int i = 0; i < count; i++)
 	{
-		client_entity_t* shield = ClientEntity_new(type, flags & (~CEF_OWNERS_ORIGIN), origin, NULL, SHIELD_DURATION * 1000);
+		client_entity_t* spark = ClientEntity_new(type, flags & (~CEF_OWNERS_ORIGIN), origin, NULL, SHIELD_DURATION * 1000);
 
-		shield->radius = SHIELD_RADIUS;
-		shield->flags |= CEF_ADDITIVE_PARTS | CEF_ABSOLUTE_PARTS;
-		shield->r.flags = RF_TRANS_ADD | RF_TRANS_ADD_ALPHA;
-		shield->r.model = &shield_model;
-		shield->color = color_white;
-		shield->alpha = 0.1f;
-		shield->d_alpha = 0.5f;
+		spark->radius = SHIELD_RADIUS;
+		spark->flags |= (CEF_ADDITIVE_PARTS | CEF_ABSOLUTE_PARTS);
+		spark->r.flags = (RF_TRANS_ADD | RF_TRANS_ADD_ALPHA);
+		spark->r.model = &shield_model;
+		spark->color = color_white;
+		spark->alpha = 0.1f;
+		spark->d_alpha = 0.5f;
 
-		VectorClear(shield->direction);
+		VectorClear(spark->direction);
 
 		// This is a velocity around the sphere.
 		for (int c = 0; c < 2; c++)
 		{
-			shield->direction[c] = flrand(0.0f, 360.0f); // This angle is kept at a constant distance from org.
-			shield->velocity2[c] = flrand(-180.0f, 180.0f);
-			shield->velocity2[c] += 180.0f * Q_signf(shield->velocity2[c]); // Assure that the sparks are moving around at a pretty good clip.
+			spark->direction[c] = flrand(0.0f, 360.0f); // This angle is kept at a constant distance from org.
+			spark->velocity2[c] = flrand(-180.0f, 180.0f);
+			spark->velocity2[c] += 180.0f * Q_signf(spark->velocity2[c]); // Assure that the sparks are moving around at a pretty good clip.
 		}
 
-		shield->lastThinkTime = fx_time;
-		shield->SpawnDelay = fx_time + SHIELD_TRAIL_DELAY;
+		spark->lastThinkTime = fx_time;
+		spark->SpawnDelay = fx_time + SHIELD_TRAIL_DELAY;
 
-		vec3_t direction;
-		AngleVectors(shield->direction, direction, NULL, NULL);
-		VectorMA(owner->origin, shield->radius, direction, shield->r.origin);
+		vec3_t forward;
+		AngleVectors(spark->direction, forward, NULL, NULL);
+		VectorMA(owner->origin, spark->radius, forward, spark->r.origin);
 
-		shield->AddToView = LightningShieldSparkThink;
-		shield->Update = LightningShieldTerminate;
+		spark->AddToView = LightningShieldSparkAddToView;
+		spark->Update = LightningShieldSparkUpdate;
 
-		AddEffect(owner, shield);
+		AddEffect(owner, spark);
 	}
 }
