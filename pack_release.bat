768ff8d20f41c22dd2d2307f51db81e1522ad88c
m-x-d <j.maxed@gmail.com>
2026-03-23 17:14:03 +0300

    Add pack_release.bat.

---
+++ pack_release.bat
@@ -0,0 +1,9 @@
+@echo off
+
+set SEVENZIP=c:\Program Files\7-Zip\7z.exe
+set ZIP_NAME=.\release\Heretic2R_%DATE%.zip
+
+if exist %ZIP_NAME% del /q %ZIP_NAME%
+
+:: Default_H2R.cfg is added with incorrect file path to keep relative file path...
+"%SEVENZIP%" a -tzip -mx=7 %ZIP_NAME% .\LICENSE .\README.md .\build\Heretic2R.exe .\build\Default_H2R.cfg .\build\*.dll -r
