m-x-d/Heretic2R

commit e56bd5159cb103e9d41c1ed4031a7a6593e44d4b

CL_SetupClipMoveToEntities(): fix logic error in CTF_CLIP_TO_BMODELS implementation. CL_SetupClipMoveToEntities(): fix logic error in CTF_CLIP_TO_ENTITIES implementation. Rename 'trace_ignore_bmodels' global var to 'trace_check_bmodels'. Rename 'trace_ignore_entities' global var to 'trace_check_entities'.

src/client/cl_prediction.c
@@ -17,8 +17,8 @@ int pred_pm_flags;
 int pred_pm_w_flags;
 qboolean trace_ignore_player;
 qboolean trace_ignore_camera;
-qboolean trace_ignore_bmodels; //mxd
-qboolean trace_ignore_entities; //mxd (except bmodels).
+qboolean trace_check_bmodels; //mxd
+qboolean trace_check_entities; //mxd (except bmodels).
 
 static int pred_effects = 0;
 static int pred_clientnum = 0;
@@ -172,10 +172,10 @@ void CL_ClipMoveToEntities(const vec3_t start, const vec3_t mins, const vec3_t m
 
 		const qboolean ent_is_bmodel = (ent->solid == 31); //mxd
 
-		if (trace_ignore_bmodels && ent_is_bmodel) //mxd
+		if (!trace_check_bmodels && ent_is_bmodel) //mxd
 			continue;
 
-		if (trace_ignore_entities && !ent_is_bmodel) //mxd
+		if (!trace_check_entities && !ent_is_bmodel) //mxd
 			continue;
 
 		// Check if ent collides with movebox.
@@ -202,7 +202,7 @@ void CL_ClipMoveToEntities(const vec3_t start, const vec3_t mins, const vec3_t m
 		trace_t trace;
 		CM_TransformedBoxTrace(start, end, mins, maxs, headnode, brushmask, ent->origin, angles, &trace);
 
-		if (trace.fraction <= tr->fraction)
+		if (trace.fraction <= tr->fraction) //TODO: Q2 version seems more logical: 'trace.allsolid || trace.startsolid || trace.fraction < tr->fraction'.
 		{
 			*tr = trace;
 			tr->ent = (struct edict_s*)ent;

src/client/cl_entities.c
@@ -1128,11 +1128,11 @@ static void CL_SetupClipMoveToEntities(const int brushmask, const int flags, con
 	if (brushmask & CONTENTS_WATER)
 		trace_check_water = set;
 
-	if (!(flags & CTF_CLIP_TO_BMODELS)) //mxd
-		trace_ignore_bmodels = set;
+	if (flags & CTF_CLIP_TO_BMODELS) //mxd
+		trace_check_bmodels = set;
 
-	if (!(flags & CTF_CLIP_TO_ENTITIES)) //mxd
-		trace_ignore_entities = set;
+	if (flags & CTF_CLIP_TO_ENTITIES) //mxd
+		trace_check_entities = set;
 
 	if (flags & CTF_IGNORE_PLAYER) //mxd
 		trace_ignore_player = set;

src/client/client.h
@@ -714,8 +714,8 @@ extern int pred_pm_flags; //mxd
 extern int pred_pm_w_flags; //mxd
 extern qboolean trace_ignore_player; //mxd
 extern qboolean trace_ignore_camera; //mxd
-extern qboolean trace_ignore_bmodels; //mxd
-extern qboolean trace_ignore_entities; //mxd
+extern qboolean trace_check_bmodels; //mxd
+extern qboolean trace_check_entities; //mxd
 
 extern void CL_CheckPredictionError(void);
 extern int CL_PMpointcontents(const vec3_t point); //mxd

commit 82b8d52fb87f4015f0b05499152291025fb7c43b

Fix regression introduced in e56bd51. Closes #12.

src/client/cl_prediction.c
@@ -172,10 +172,10 @@ void CL_ClipMoveToEntities(const vec3_t start, const vec3_t mins, const vec3_t m
 
 		const qboolean ent_is_bmodel = (ent->solid == 31); //mxd
 
-		if (!trace_check_bmodels && ent_is_bmodel) //mxd
+		if (trace_check_bmodels && !ent_is_bmodel) //mxd
 			continue;
 
-		if (!trace_check_entities && !ent_is_bmodel) //mxd
+		if (trace_check_entities && ent_is_bmodel) //mxd
 			continue;
 
 		// Check if ent collides with movebox.