2f7b88d5b0f01f5818b827c0445c317bd7e2b6da
m-x-d <j.maxed@gmail.com>
2026-04-08 18:23:05 +0300

    pack_release.bat: use VERSIONDISP in archive filename instead of current date.

--- pack_release.bat
+++ pack_release.bat
@@ -1,7 +1,27 @@
 @echo off
 
+:: Get revision number. Can't use Heretic2R.rc for this because of UTF-16 encoding...
+set QC_PATH=.\src\qcommon\qcommon.h
+
+:: Look for '#define VERSIONDISP "R667"' line...
+for /f "usebackq tokens=2,3" %%a in ("%QC_PATH%") do (
+	if "%%a"=="VERSIONDISP" (
+		set H2R_REV=%%~b
+		goto :break_loop
+	)
+)
+
+:break_loop
+
+if "%H2R_REV%"=="" (
+	echo Failed to find revision!
+	pause
+	exit
+)
+
+:: Now pack it.
 set SEVENZIP=c:\Program Files\7-Zip\7z.exe
-set ZIP_NAME=.\release\Heretic2R_%DATE%.zip
+set ZIP_NAME=.\release\Heretic2R_%H2R_REV%.zip
 
 if exist %ZIP_NAME% del /q %ZIP_NAME%
 
