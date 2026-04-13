884d66beee90e1a34debc4b455075b05a173809d
m-x-d <j.maxed@gmail.com>
2026-04-13 15:36:11 +0300

    Change version to R5.

--- src/qcommon/Version.h
+++ src/qcommon/Version.h
@@ -11,7 +11,7 @@
 
 #define VERSION_MAJOR				1
 #define VERSION_MINOR				0
-#define VERSION_REVISION			4
+#define VERSION_REVISION			5
 #define VERSION_BUILD				0
 
 #define VER_FILE_DESCRIPTION_STR	"Heretic2R reverse-engineered source port"
